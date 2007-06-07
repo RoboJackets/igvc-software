9376e5a1e1c7c5e340fe9dcaa9da1f0f0285f1cd
davidf <davidf@b8439097-6f30-0410-869d-b57f63bf1bb9>
2007-06-07 02:03:48 +0000

    added 'robot.cfg' file that runs ALL of the robot's sensors; added SetMotorOutput function to 'play.cpp'; can now startup without the camera or motors being present
    git-svn-id: https://localhost/igvc@16 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2007/candi/transform/play.cpp
+++ 2007/candi/transform/play.cpp
@@ -1,64 +1,89 @@
+#include "play.h"
+
 #include <GL/glut.h>
 #include <libplayerc++/playerc++.h>
 #include <stdlib.h>
+
 #include "ptypes.h"
 #include "pglobals.h"
 #include "vision/PCamera.h"
 #include "vision/vision.h"
 #include "main.h"
 
-Image *im1 = NULL;
-
 // ---------------------------------------------------------------------------------
 // Player code
 
-PlayerCc::PlayerClient *robot;
-PlayerCc::CameraProxy *camera;
-void resizeImage(int width, int height);
+static Image curFrame;
+
+static PlayerCc::PlayerClient *robot;
+static PlayerCc::CameraProxy *camera;		// NULL if unavailable
+static PlayerCc::Position2dProxy *motors;	// NULL if unavailable
+
+static void resizeImage(uint width, uint height);
 
 // Connects to the robot
 extern "C" void ConnectToRobot(char *hostname, int port) {
+	// Connect to the robot and its devices
 	robot = new PlayerCc::PlayerClient(hostname, port);
-	camera = new PlayerCc::CameraProxy(robot,0); // use camera:0
+	try {
+		camera = new PlayerCc::CameraProxy(robot,0); 		// use camera:0
+	} catch (PlayerCc::PlayerError e) {
+		printf("ERROR: Camera not found.\n");
+		camera = NULL;
+	}
+	try {
+		motors = new PlayerCc::Position2dProxy(robot,0);	// use position2d:0
+	} catch (PlayerCc::PlayerError e) {
+		printf("ERROR: Motors not found.\n");
+		motors = NULL;
+	}
 	
 	// Set data to PULL mode
 	// (i.e., always get the *latest* camera images,
 	//  even if some frames are dropped)
 	robot->SetDataMode(PLAYER_DATAMODE_PULL);
 	robot->SetReplaceRule((bool) 1, -1, PLAYER_MSGTYPE_DATA, PLAYER_CAMERA_CODE);
-
-	im1 = (Image*) malloc(sizeof(Image));
-	im1->width=0;
-	im1->height=0;
-    im1->data=NULL;
-	robot->Read();
-	resizeImage(camera->GetWidth(), camera->GetHeight());
-	camera->GetImage(im1->data);
 	
+	// Read the first frame from the camera
+	curFrame.width=0;
+	curFrame.height=0;
+	curFrame.data=NULL;
+	GetNextFrame();
+	
+	// Tell 'visFrame' to use this camera as input
 	Camera::current = &PCamera::INSTANCE;
 }
 
 extern "C" Image* GetNextFrame() {
 	robot->Read();
-	resizeImage(camera->GetWidth(), camera->GetHeight());
-	camera->GetImage(im1->data);
-	usleep(10);
-	return im1;
 	
+	if (camera != NULL) {
+		resizeImage(camera->GetWidth(), camera->GetHeight());
+		camera->GetImage(curFrame.data);
+	}
+	
+	usleep(10);
+	return &curFrame;
 }
 
 // Resizes the image buffer that received images are stored into
-void resizeImage(int width, int height) {
-	if ((width == im1->width) && (height==im1->height)) return;
-	im1->width=width;
-	im1->height=height;
+void resizeImage(uint width, uint height) {
+	if ((width == curFrame.width) && (height==curFrame.height)) return;
+	curFrame.width=width;
+	curFrame.height=height;
 	
-	if (im1->data) free(im1->data);
-	im1->data = (unsigned char *) malloc(width*height*3);
+	if (curFrame.data) free(curFrame.data);
+	curFrame.data = (unsigned char *) malloc(width*height*3);
 }
 
 extern "C" void ProcessTransformedFrame(void) {
 	PCamera::INSTANCE.update();
 	visFrame();
 	mainWindow->updateVideoView();
+}
+
+void SetMotorOutput(MotorOutput motorOutput) {
+	if (motors != NULL) {
+		motors->SetSpeed((double) motorOutput.leftSpeed, (double) motorOutput.rightSpeed);
+	}
 }
