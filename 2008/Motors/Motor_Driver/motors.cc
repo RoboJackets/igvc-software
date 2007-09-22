1cde9e6c2fb20fa0857f8cbd3f4c3ad7f10b3200
davidf <davidf@b8439097-6f30-0410-869d-b57f63bf1bb9>
2007-09-22 21:08:43 +0000

    extracted non-Player specific code from the motors Player driver to a generic driver class
    git-svn-id: https://localhost/igvc@30 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2008/Motors/Motor_Driver/motors.cc
+++ 2008/Motors/Motor_Driver/motors.cc
@@ -1,49 +1,4 @@
 #include "motors.h"
-
-/*
- *	Refer to the header for more information about the purpose of each function
- *
- */
-
-#define min(a, b) (((a) < (b)) ? (a) : (b))
-
-/* Various values for formatting data to be communicated with the motor controller */
-#define ERROR_MASK			0x3c
-#define ERROR_OFFSET		2
-#define CONTROL_MODE_MASK	0xc0
-#define CONTROL_MODE_OFFSET	6
-#define LEFT_DIR_MASK		0x01
-#define LEFT_DIR_OFFSET		0
-#define RIGHT_DIR_MASK		0x02
-#define RIGHT_DIR_OFFSET	1
-#define FORWARD				0
-#define REVERSE				1
-
-/* Posible errors recieved from the motor controller*/
-enum {
-	FRAME_ERROR = 1,
-	DATA_OVERRUN_ERROR = 2,
-	PARITY_ERROR = 4,
-	TIMEOUT_ERROR = 8,
-};
-
-/* Defines way the robot is controlled */
-typedef enum{
-	AUTONOMOUS_MODE = 0,
-	NAVIGATION_MODE = 1,
-	JOYSTICK_MODE = 2,
-	STOP_MODE = 3,
-} CONTROLMODE_T;
-
-//TODO:
-//	make sure the two different types of motor commands (MOVE and VEL) don't mess each other up
-//	maybe do multi-threading
-//	add something to keep the message if the motor set fails
-//	think about condensing the error reporting -maybe change warnings back to errors and add try catch blocks
-//	add stop stuff - maybe use interface for other stop thing
-//	fix PID
-//	change names
-
 Driver* Motors_Player::Motors_Player_Init(ConfigFile* cf, int section)
 {
 	return( (Driver*)(new Motors_Player(cf, section)) );
@@ -58,16 +13,16 @@ void Motors_Player::Motors_Player_Register(DriverTable* table)
 Motors_Player::Motors_Player(ConfigFile* cf, int section) 
  : Driver(cf, section, TRUE, PLAYER_MSGQUEUE_DEFAULT_MAXLEN, PLAYER_POSITION2D_CODE)
 {
-	/* Reset the file descriptor for the serial port */	fdMotor = -1;
-
 	/* Read parameters from the configuration file */
-	cpMotorPort = cf->ReadString(section, "port", DEFAULT_PORT);
-	pGain = cf->ReadFloat(section, "PGain", DEFAULT_PGAIN);
-	iGain = cf->ReadFloat(section, "IGain", DEFAULT_IGAIN);
-	dGain = cf->ReadFloat(section, "DGain", DEFAULT_DGAIN);
-	dVelocityScale = cf->ReadFloat(section, "Velocity Scale", DEFAULT_VELOCITY_SCALE);
+	const char* cpMotorPort = cf->ReadString(section, "port", DEFAULT_PORT);
+	double pGain = cf->ReadFloat(section, "PGain", DEFAULT_PGAIN);
+	double iGain = cf->ReadFloat(section, "IGain", DEFAULT_IGAIN);
+	double dGain = cf->ReadFloat(section, "DGain", DEFAULT_DGAIN);
+	this->dVelocityScale = cf->ReadFloat(section, "Velocity Scale", DEFAULT_VELOCITY_SCALE);
 	//dMaxSpeed
 	//dMaxAccel
+	
+	this->driver = new Motors(cpMotorPort, pGain, iGain, dGain);
 
 	/* Only recieve messages related to controlling the Motors_Player */
 	//SetFilter();
@@ -77,13 +32,11 @@ Motors_Player::Motors_Player(ConfigFile* cf, int section)
 	 * messages are replaced by newer ones (could be set otherwise if needed).
 	 */
 	InQueue->AddReplaceRule(device_addr, PLAYER_MSGTYPE_CMD, PLAYER_POSITION2D_CMD_POS, FALSE);
-
-	return;
 }
 
 int Motors_Player::Setup()
 {
-	if(SetupSerial()){ return(-1); }
+	if (!driver->Connect()) { return -1; }
 	StartThread();
 	return(0);
 }
@@ -91,7 +44,7 @@ int Motors_Player::Setup()
 int Motors_Player::Shutdown()
 {
 	StopThread();
-	if(ShutdownSerial()){ return(-1); }
+	if (!driver->Disconnect()) { return -1; }
 	return(0);
 }
 
@@ -107,7 +60,8 @@ int Motors_Player::ProcessMessage(MessageQueue* resp_queue, player_msghdr* hdr, 
 					PLAYER_POSITION2D_REQ_MOTOR_POWER,
 					device_addr) )
 		{
-			return( set_motors(*(player_position2d_power_config_t *)data) );
+			// TODO: implement
+			return 0;
 		}
 		/* Change the way velcoity commands are sent */
 		else if( Message::MatchMessage(hdr,
@@ -115,7 +69,8 @@ int Motors_Player::ProcessMessage(MessageQueue* resp_queue, player_msghdr* hdr, 
 					PLAYER_POSITION2D_REQ_VELOCITY_MODE,
 					device_addr) )
 		{
-			return( change_velocity_mode(*(player_position2d_velocity_mode_config_t *)data) );
+			// TODO: implement
+			return 0;
 		}
 		/* Choose between sending velocity commands or position commands */
 		else if( Message::MatchMessage(hdr,
@@ -123,7 +78,8 @@ int Motors_Player::ProcessMessage(MessageQueue* resp_queue, player_msghdr* hdr, 
 					PLAYER_POSITION2D_REQ_POSITION_MODE,
 					device_addr) )
 		{
-			return( set_position_mode(*(player_position2d_position_mode_req_t *)data) );
+			// TODO: implement
+			return 0;
 		}
 		/* Set velocity PID parameters */
 		else if( Message::MatchMessage(hdr,
@@ -131,7 +87,7 @@ int Motors_Player::ProcessMessage(MessageQueue* resp_queue, player_msghdr* hdr, 
 					PLAYER_POSITION2D_REQ_SPEED_PID,
 					device_addr) )
 		{
-			return( set_pid((player_position2d_speed_pid_req_t *)data) );
+			return SetMotorPID((player_position2d_speed_pid_req_t *)data);
 		}
 
 		/* Set maximum speed and accelleration */
@@ -140,7 +96,8 @@ int Motors_Player::ProcessMessage(MessageQueue* resp_queue, player_msghdr* hdr, 
 					PLAYER_POSITION2D_REQ_SPEED_PROF,
 					device_addr))
 		{
-			return( set_max_move_param(*(player_position2d_speed_prof_req_t *)data) );
+			// TODO: implement
+			return 0;
 		}
 		else
 		{
@@ -155,7 +112,7 @@ int Motors_Player::ProcessMessage(MessageQueue* resp_queue, player_msghdr* hdr, 
 					PLAYER_POSITION2D_CMD_VEL,
 					device_addr) )
 		{
-			return( set_motors(*(player_position2d_cmd_vel_t *)data) );
+			return( SetMotorSpeeds((player_position2d_cmd_vel_t *)data) );
 		}
 		/* This is a position to which the robot should move */
 		else if( Message::MatchMessage(hdr,
@@ -163,7 +120,8 @@ int Motors_Player::ProcessMessage(MessageQueue* resp_queue, player_msghdr* hdr, 
 					PLAYER_POSITION2D_CMD_POS,
 					device_addr) )
 		{
-			return( queqe_position(*(player_position2d_cmd_pos_t *)data) );
+			// TODO: implement
+			return 0;
 		}
 		else
 		{
@@ -171,7 +129,9 @@ int Motors_Player::ProcessMessage(MessageQueue* resp_queue, player_msghdr* hdr, 
 		}
 	}
 	else
+	{
 		return(-1);
+	}
 }
 
 void Motors_Player::Main()
@@ -186,265 +146,25 @@ void Motors_Player::Main()
 	}
 }
 
-int Motors_Player::SetupSerial()
+int Motors_Player::SetMotorPID(player_position2d_speed_pid_req_t* data)
 {
-	/* Open serial port with the specified flags
-	 * O_RDWR - for reading and writing
-	 * O_NOCTTY - don't act as the controlling terminal for the port
-	 * O_DSYNC | O_RSYNC - complete i/o operiations as definded by synchronized i/o data
-	 *	integrity completion (i.e. waits for the data but not meta-data to be written
-	 *	to the file before continuing execution) - this doesn't seem to work
-	 * O_NDELAY - don't wait for the DCD line to go to the space voltage
-	 */	if ((fdMotor = open(cpMotorPort, O_RDWR | O_NOCTTY | O_DSYNC | O_RSYNC | O_NDELAY)) < 0)
-	{
-		PLAYER_ERROR2("SetupSerial(): could not open port %s, %s\n", cpMotorPort, strerror(errno));
-		return(-1);
-	}
-
-	/* Set the port back to blocking (waiting) behavior for calls to read() */
-	fcntl(fdMotor, F_SETFL, 0);
-
-	/* Get the current options for the port */
-	struct termios options;
-	if(tcgetattr(fdMotor, &options) < 0)
-	{
-		PLAYER_ERROR1("SetupSerial(): could not get the serial port options, %s\n", strerror(errno));
-		ShutdownSerial();
-		return(-1);
-	}
-
-	/* Configure the port options */	 
-	options.c_cflag |= (CREAD | CLOCAL); /* Enable the receiver and set local mode */
-	cfsetispeed(&options, B115200); /* Set 115200 baud rate */
-	cfsetospeed(&options, B115200); 
-	options.c_cflag &= ~PARENB; /* Set no parity */
-	options.c_cflag &= ~CSTOPB; /* Set 1 stop bit */
-	options.c_cflag &= ~CSIZE; /* Set 8 data bits */
-	options.c_cflag |= CS8;
-	options.c_cc[VMIN]  = 0; /* Set .2 sec timeout */
-	options.c_cc[VTIME] = 2;
-	options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); /* Set raw input and output */
-	options.c_oflag &= ~OPOST;
-
-	/* Flush input and output buffers and then set the new options for the port */
-	if(tcsetattr(fdMotor, TCSAFLUSH, &options) < 0 )
-	{
-		PLAYER_ERROR1("SetupSerial(): could not set the serial port options, %s\n", strerror(errno));
-		ShutdownSerial();
-		return(-1);
-	}
-	return(0);
+	driver->SetPID(data->kp, data->ki, data->kd);
+	return 0;
 }
 
-int Motors_Player::ShutdownSerial()
+int Motors_Player::SetMotorSpeeds(player_position2d_cmd_vel_t* stVelocity)
 {
-	if(close(fdMotor) < 0)
-	{
-		PLAYER_ERROR1("ShutdownSerial(): could not close the serial port, %s\n", strerror(errno));
-		fdMotor = -1;
-		return(-1);
-	}
-	fdMotor = -1;
-	return(0);
-}
-
-int Motors_Player::set_pid(player_position2d_speed_pid_req_t *data)
-{
-	pGain = data->kp;
-	iGain = data->ki;
-	dGain = data->kd;
-	return(0);
-}
-
-int Motors_Player::move(void)
-{
-#ifdef _WOOT__MOVE_IS_GOOD_
-	POSTION_T dIState, dDState, output; // intialize these
-
-	while() // TODO: when should this end?
-	{
-		if(bStop)
-		{
-			return(-1);
-		}
-
-		/* Get the current posistion and error */
-		POSTION_T position = sRobotPosition;
-		POSTION_T error = position - output;
-
-		/* calculate the proportional term and assign it to the output */
-		output = dPGain * error;
-
-		/* calculate the integral state with appropriate limiting and add it to the output */
-		dIState += error;
-		if(dIState > dIMax)
-			dIState = dIMax;
-		else if(dIState < dIMin)
-			dIState = dIMin;
-		output += dIGain * dIState;
-
-		/* calculate the differential term and subtract it from the output */
-		output -= dDGain * (postion - dDState);
-		dDState = position;
-
-		/* Calculate the motor velocities based on the output */
-		MOTOR_STATES_T stNewMotorStates;
-		r = sqrt()
-		stNewMotorStates.iLeftVelocity = output.x * output.theta; //fix**********
-		stNewMotorStates.iRightVelocity = output.x * output.theta;
-
-		/* Set the motors to the calculated velocities */
-		setmotors(/*?*/);
-	}
-#endif
-	return(0);
-}
-
-/* NOTE:
- *	When you raise the DTR line, if the motor controller is in AUTONOMOUS_MODE,
- *	it reads in any data in its uart buffer (3 bytes in size). Then, in all modes,
- *	it sends back its current motor states, control mode, and any errors it had
- *	with the transmission.
- */
-
-int Motors_Player::set_motors(player_position2d_cmd_vel_t stVelocity)
-{
-	//printf("LeftVel command: %f\n", stVelocity.vel.px);
-	//printf("RightVel command: %f\n\n", stVelocity.vel.pa);
-
+	//printf("LeftVel command: %f\n", stVelocity->vel.px);
+	//printf("RightVel command: %f\n\n", stVelocity->vel.pa);
+	
 	/* Scale the motor velocities, limit them to vaild values, and store them in the
 	 * correct form to transmit to the motor controller.
 	 */
-	int iScaledLeftVel = (int)(dVelocityScale * stVelocity.vel.px);
-	int iScaledRightVel = (int)(dVelocityScale * stVelocity.vel.pa);
-	unsigned char data[3] ={
-		(unsigned char)( (((iScaledLeftVel < 0) ? REVERSE : FORWARD) << LEFT_DIR_OFFSET) | (((iScaledRightVel < 0) ? REVERSE : FORWARD) << RIGHT_DIR_OFFSET) ),
-		(unsigned char)( min(abs(iScaledLeftVel), 255) ),
-		(unsigned char)( min(abs(iScaledRightVel), 255) )  };
-
-	/* Send the motor states to the motor controller */
-	if( write(fdMotor, data, 3) != 3 )
-	{
-		PLAYER_WARN1("set_motors(): could not write to serial port, %s\n", strerror(errno));
-		return(-1);
-	}
-
-	/* Make sure the data has finished being written - this can be removed if the option can be made work in open() */
-	tcdrain(fdMotor);
-
-	/* Get the current motor states from the motor controller */
-	return( get_motor_states() );
-}
-
-int Motors_Player::set_motors(player_position2d_power_config_t stPower)
-{
-	#if 0 // To be used once position commands are accepted
-	if(stPower.state == false)
-	{
-		set_motors( (player_position2d_cmd_vel_t){{0, 0, 0}, FALSE} );
-		stopped = true;
-	}
-	else
-	{
-		stopped = false;
-	}
-	#endif
-	return(0);
-}
-
-int Motors_Player::get_motor_states(void)
-{
-	/* Flush any old data in the read buffer */
-	tcflush(fdMotor, TCIFLUSH);
-
-	/* Set the DTR line high to intterupt the motor controller */
-	int status;
-	if(ioctl(fdMotor, TIOCMGET, &status))
-	{
-		PLAYER_WARN1("get_motor_states(): could not get serial port status, %s\n", strerror(errno));
-		return(-1);
-	}
-	status |= TIOCM_DTR;
-	if(ioctl(fdMotor, TIOCMSET, &status))
-	{
-		PLAYER_WARN1("get_motor_states(): could not set RTS high, %s\n", strerror(errno));
-		return(-1);
-	}
+	int iScaledLeftVel = (int)(dVelocityScale * stVelocity->vel.px);
+	int iScaledRightVel = (int)(dVelocityScale * stVelocity->vel.pa);
 	
-	/* 
-	 * If the DTR line can be raised and lowered in less than a full clock cycle
-	 * of the controller, there should be a delay here to make sure the motor
-	 * controller interrupts.
-	 */
-
-	/* Set the DTR line back to low
-	 *	If there are problems, you might need to get the motor status again with
-	 *	ioctl(fdMotor, TIOCMGET, &status) before doing this.
-	 */
-	status &= ~TIOCM_DTR;
-	if(ioctl(fdMotor, TIOCMSET, &status))
-	{
-		PLAYER_WARN1("get_motor_states(): could not set RTS low, %s\n", strerror(errno));
-		return(-1);
-	}
-
-	/* Read each byte sent from the controller */
-	unsigned char u8Buff[3];
-	int bytesRead;
-	if((bytesRead = read(fdMotor, u8Buff, 3)) != 3)
-	{
-		PLAYER_WARN1("get_motor_states(): the motor controller did not respond or only partial responded, %s\n", strerror(errno));
-		return(-1);
-	}
-
-	//Test
-	/*printf("%d bytes read\n", bytesRead);
-	for(int i = 0; i < bytesRead; i++)
-	{
-		printf("%d ", u8Buff[i]);
-	}
-	printf("\n\n");*/
-
-	/* Report any errors the motor controller had with the transmission */
-	int iError = (u8Buff[0] & ERROR_MASK) >> ERROR_OFFSET;
-	if( iError & (FRAME_ERROR|DATA_OVERRUN_ERROR|PARITY_ERROR|TIMEOUT_ERROR) )
-	{
-		PLAYER_WARN1("get_motor_states(): the motor controller could not process comand, Error %d\n", iError);
-		return(-1);
-	}
-	
-
-	/* Store the current control mode */
-	controlMode = ( u8Buff[0] & CONTROL_MODE_MASK ) >> CONTROL_MODE_OFFSET;
-
-	/* Store the current motor velocities */
-	stMotorStates.iLeftVelocity = ((((u8Buff[0] & LEFT_DIR_MASK) >> LEFT_DIR_OFFSET)==FORWARD) ? 1 : -1) * (int)u8Buff[1];
-	stMotorStates.iRightVelocity = ((((u8Buff[0] & RIGHT_DIR_MASK) >> RIGHT_DIR_OFFSET)==FORWARD)? 1 : -1) * (int)u8Buff[2];
-
-	return(0);
+	return driver->SetSpeeds(iScaledLeftVel, iScaledRightVel) ? 0 : -1;
 }
-
-int change_velocity_mode(player_position2d_velocity_mode_config_t data)
-{
-	return(0);
-}
-
-int Motors_Player::set_position_mode(player_position2d_position_mode_req_t data)
-{
-	return(0);
-}
-
-int Motors_Player::set_max_move_param(player_position2d_speed_prof_req_t data)
-{
-	return(0);
-}
-
-
-int Motors_Player::queqe_position(player_position2d_cmd_pos_t stPosition)
-{
-	return(0);
-}
-
 
 /* Extra stuff for building a shared object.
  * need the extern to avoid C++ name-mangling
--- 2008/Motors/Motor_Driver/motors.h
+++ 2008/Motors/Motor_Driver/motors.h
@@ -12,18 +12,14 @@
 
 #include <libplayercore/playercore.h>
 
+#include "motordriver.h"
+
 /* Values that will be used if they are not specified in the configuration file */
 #define DEFAULT_PORT		"/dev/ttyS0"
 #define DEFAULT_PGAIN		1
 #define DEFAULT_IGAIN		.001
 #define DEFAULT_DGAIN		1
 #define DEFAULT_VELOCITY_SCALE	1
-
-/* Used to internally represent the motor velocities and possibly other motor information */
-typedef struct _motor_states_
-{
-	int iLeftVelocity, iRightVelocity;
-} MOTOR_STATES_T;
 
 /* The driver class */
 class Motors_Player: public Driver
@@ -48,64 +44,18 @@ class Motors_Player: public Driver
 	protected:
 		/* This is run as a thread */
 		virtual void Main();
-
-		/* String name of serial port to use */
-		const char* cpMotorPort;
-
-		/* File descriptor of the serial port */
-		int fdMotor;
-
-		/* Called in Setup() and Shutdown() to open and close the serial port */
-		int SetupSerial();
-		int ShutdownSerial();
-
-		/* Current motor velocities */
-		MOTOR_STATES_T stMotorStates;
-
-		/* PID controller parameters */
-		double pGain, iGain, dGain;
-
-		/* Set the PID controller parameters */
-		int set_pid(player_position2d_speed_pid_req_t *data);
-
+		
+	private:
+		int SetMotorPID(player_position2d_speed_pid_req_t* data);
+		/* Set the motor velocities to the specified value */
+		int SetMotorSpeeds(player_position2d_cmd_vel_t* stVelocity);
+		
+	private:
+		// The actual motor driver
+		Motors* driver;
+		
 		/* Scales external velocity representations to interal ones */
 		double dVelocityScale;
-
-		/* Current postion, recieved from the ___ driver */
-		player_pose_t stRobotPosition;
-
-		/* Current mode the motor controller is in - either JOYSTICK_MODE or AUTONOMOUS_MODE */
-		int controlMode;
-
-		/* Use PID control to set the motor velocities to reach the specified position */
-		int move(void);
-
-		/* Set the motor velocities to the specified value */
-		int set_motors(player_position2d_cmd_vel_t stVelocity);
-
-		/* Same a previous, but uses PID to do motor breaking until the robot is stopped */
-		int set_motors(player_position2d_power_config_t stPower);
-
-		/* Retrieves the current motor states (velocities and other control data)
-		 * - is always called when the motor velocities are set
-		 */
-		int get_motor_states(void);
-
-		//
-		//not implemented yet
-		//
-
-		/* change between the different velocity modes */
-		int change_velocity_mode(player_position2d_velocity_mode_config_t data);
-
-		/* Change between position mode and velocity mode */
-		int set_position_mode(player_position2d_position_mode_req_t data);
-
-		/**/
-		int set_max_move_param(player_position2d_speed_prof_req_t data);
-
-		/**/
-		int queqe_position(player_position2d_cmd_pos_t stPosition);
 };
 
 #endif /* _MOTORS_H_ */
