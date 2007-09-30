1645adf7ddbaf7dbe2ced64010e4ad6ab97b1940
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2007-09-30 04:10:44 +0000

    went through and tested all functions, changes to nearly all.  the code i used to test is commented out but left in
    git-svn-id: https://localhost/igvc@41 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2008/compassdriver/compass.cc
+++ 2008/compassdriver/compass.cc
@@ -14,49 +14,41 @@ typedef signed long int sint_32;//defining this like this becuase i'm not sure i
 int main(void){
 //testing correct endianndess
 
-uint_8 array[6] = {0xC2, 0xed, 0x40, 0x00, 0x07, 0x00};
+/*uint_8 array[6] = {0xC2, 0xed, 0x40, 0x00, 0x07, 0x00};
 uint_8 outarray[10];
-uint_8 * temp;
-/*//bytes2sint32LE(&array[0]);
-printf("\nLE 0x B4C4, human C4B4 bytes to sint32: %X\n",bytes2sint32LE(&array[0]));
+uint_8 * temp;*/
 
-outarray = sint32_bytesLE(500000);
-printf("this should be {32,161,7,0): {%u,%u,%u,%u}\n", outarray[0], outarray[1], outarray[2], outarray[3]);
+/*CalDataResp testcaldata;
+testcaldata.ByteCount = 24;
+testcaldata.XOffset = 0xFF;
+testcaldata.YOffset = 0xFF;
+testcaldata.XGain = 0xFF;
+testcaldata.YGain = 0xFF;
+testcaldata.phi = 0xFF;
+testcaldata.CalibrationMagnitude = 0xFF;*/
 
+//CompassDriver testdriver(7);
+//testdriver.GetModInfo();//works
+//testdriver.SetDataComponents(3);//works
 
-printf("bytes to float:-118.625 %f\n",bytes2floatLE(&array[0]));
+/*compassData testgetdata;
+testgetdata = testdriver.GetData();//seems to work
+printf("\n\nXRaw: %li",testgetdata.XRaw);
+printf("\n\nYRaw: %li",testgetdata.YRaw);*/
 
+/*testdriver.SetConfig(declination, float2bytesLE(25));//seems to work
+uint_8 test[1]= {false};
+testdriver.SetConfig(true_north, test);*/
 
-outarray = float2bytesLE(-118.625);
-printf("0xC2, 0xed, 0x40, 0x00: {%X,%X,%X,%X}\n", outarray[0], outarray[1], outarray[2], outarray[3]);
-//compassData foo = CompassDriver.GetData();*/
+//testdriver.GetConfig(true_north);//works
 
+//testdriver.SaveConfig();//works
+//testdriver.StartCal();
+//testdriver.StopCal();
 
-/*temp = float2bytesLE(-118.625);
+//testcaldata = testdriver.GetCalData();
+//printf("%i",test.YOffset);
 
-outarray[3] = temp[0];
-outarray[4] = temp[1];
-outarray[5] = temp[2];
-outarray[6] = temp[3];
-
-printf("0xC2, 0xed, 0x40, 0x00: {%X,%X,%X,%X}\n", outarray[3], outarray[4], outarray[5], outarray[6]);*/
-CalDataResp test;
-test.ByteCount = 24;
-test.XOffset = 0xFF;
-test.YOffset = 0xFF;
-test.XGain = 0xFF;
-test.YGain = 0xFF;
-test.phi = 0xFF;
-test.CalibrationMagnitude = 0xFF;
-
-CompassDriver testdriver(7);
-//testdriver.SetCalData(test);
-
-compassData testgetdata;
-testdriver.SetDataComponents(63);
-testgetdata = testdriver.GetData(outarray);
-
-printf("%li",testgetdata.XRaw);
-
+//testdriver.SetCalData(testcaldata);//works
 }
 
