db738cb5bcf82d0a1d445693ffb8e336445a365f
paulv <paulv@b8439097-6f30-0410-869d-b57f63bf1bb9>
2007-10-08 00:31:32 +0000

    Moved the stuff in message.h to JMessage.h.  Corrected the "header" struct.
    git-svn-id: https://localhost/igvc@43 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2008/jaus/JMessage.h
+++ 2008/jaus/JMessage.h
@@ -1,8 +1,13 @@
 // -------------------------------------------------------------------------------------
 
+// TODO: make a macro that will change these bitpacket structs if on a Big Endian system
 class JMessage {
+public:
+	struct header;
+	void *data;
+
 private:
-	JMessageType type;
+	JMessageType type; // this is in the header
 
 protected:
 	JMessage(JMessageType type) {
@@ -37,5 +42,32 @@ public:
 		this->missionID = missionID;
 	}
 }; 
+
+// -------------------------------------------------------------------------------------
+
+struct header {
+	union {
+		struct {
+			Jushort priority:4; 
+			Jushort ackNak:2;
+			Jushort scFlag:1; 
+			Jushort expFlag:1;
+			Jushort version:6; 
+			Jushort reserved:2;
+		}
+		Jushort messageProperties;
+	}
+	Jushort commandCode;
+	Juint destAddress
+	Juint sourceAddress
+	union {
+		struct {
+			Jushort dataSize:12;
+			Jushort dataFlag:4;
+		}
+		Jushort dataControl;
+	}
+	Jushort sequenceNumber;//number of message in serial connection, 0-65535
+}
 
 // -------------------------------------------------------------------------------------
