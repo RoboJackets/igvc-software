49f3864e27aef2c1414308e581b2a57095ebedff
davidf <davidf@b8439097-6f30-0410-869d-b57f63bf1bb9>
2007-10-11 14:36:57 +0000

    1. Removed unnecessary structural elements from JMessage. 2. Documented various JAUS classes. 3. Moved JAUS typedefs from JMessage.h to jtypes.h. 4. Added all of the JAUS message types to JMessageType.h.
    git-svn-id: https://localhost/igvc@46 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2008/jaus/JMessage.h
+++ 2008/jaus/JMessage.h
@@ -1,11 +1,9 @@
 // -------------------------------------------------------------------------------------
 
-// TODO: make a macro that will change these bitpacket structs if on a Big Endian system
+/**
+ * Base class for all types of JAUS messages.
+ */
 class JMessage {
-public:
-	struct header;
-	void *data;
-
 private:
 	JMessageType type; // this is in the header
 
@@ -18,12 +16,17 @@ protected:
 		// nothing
 	}
 	
+public:
 	// TODO: implement
 	//virtual void write(OutputStream& out);
 };
 
 // -------------------------------------------------------------------------------------
 
+/**
+ * Class for all JAUS messages that have no extra arguments
+ * (i.e., the message only consists of a type code).
+ */
 class JSimpleMessage : public JMessage {
 public:
 	JSimpleMessage(JMessageType type) : JMessage(type) {
@@ -45,7 +48,11 @@ public:
 
 // -------------------------------------------------------------------------------------
 
-struct header {
+/**
+ * Header of a JAUS message that can be transmitted.
+ */
+// TODO: make a macro that will change these bitpacket structs if on a Big Endian system
+typedef struct {
 	union {
 		struct {
 			Jushort priority:4; 
@@ -68,6 +75,6 @@ struct header {
 		Jushort dataControl;
 	}
 	Jushort sequenceNumber;//number of message in serial connection, 0-65535
-}
+} jmessage_header;
 
 // -------------------------------------------------------------------------------------
---
+++ 2008/jaus/jtypes.h
@@ -0,0 +1,14 @@
+#ifndef JTYPES_H
+#define JTYPES_H
+
+typedef Jbyte unsigned char;	// Byte
+typedef Jshort signed short;	// Short Integer
+typedef Jint signed int;		// Integer
+typedef Jlong long;				// Long Int
+typedef Jushort unsigned short; // Unsigned Short Int
+typedef Juint unsigned int;		// Unsigned Int
+typedef Julong unsigned long;	// Unsigned Long
+typedef Jfloat float;			// Float
+typedef Jdouble double;			// Long Float
+
+#endif
