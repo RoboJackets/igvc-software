4fcc97a050ccdfbbe9e8182da01a11a36e0e42f6
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-01-20 22:52:43 +0000

    dio code
    git-svn-id: https://localhost/igvc@89 b8439097-6f30-0410-869d-b57f63bf1bb9

---
+++ 2008/compassdriver/dio/testdio2.c
@@ -0,0 +1,32 @@
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/mman.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<string.h>
+#include<unistd.h>
+
+int main(int argc, char **argv)
+{
+   volatile unsigned int *dio, *dio_in;
+   int i;
+   unsigned char rev;
+   unsigned char *start;
+   int fd = open("/dev/mem", O_RDWR|O_SYNC);
+
+   start = mmap(0, getpagesize(), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0xe8000000);
+	dio =  (unsigned int *)(start + 0x8);  // dio 1
+	dio_in = (unsigned int *)(start + 0x4);
+
+	*dio_in = 0;
+
+while(1){
+	*dio |= 0xFFFFFFFF;//write crap
+	usleep(1000);
+	*dio = 0; // &  ~(1 << 16);
+	usleep(1000);
+}
+   
+   close(fd);
+   return 0;
+}
