135be652b4a55c6f34f206769b1ae9febd010088
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-01-31 06:09:11 +0000

    new gpio code
    git-svn-id: https://localhost/igvc@102 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2008/compassdriver/pc104_gpio/gpio.c
+++ 2008/compassdriver/pc104_gpio/gpio.c
@@ -6,35 +6,39 @@
 #include<string.h>
 #include<unistd.h>
 
-#define PC104A_IN_OUT 0x10
-#define PC104A_DIRECTION 0x20
-#define PC104A_MUX 0x30
 
-int main(int argc, char **argv)
-{
-   volatile unsigned int *gpio_a_data, *gpio_a_direction, *gpio_a_mux;
-   unsigned char *start;
-   int fd = open("/dev/mem", O_RDWR|O_SYNC);
+//set A is on the 64-pin header, closest to the 40 pin header.  pin 1 is closest to serial port, pin 32 other side down the row (linearly)
+#define PC104_A_DATA 0x10 //data register
+#define PC104_A_DIRECTION 0x20 //direction register
+#define PC104_A_MUX 0x30 //function register; 0 = gpio, 1 = ISA, 2,3 = reserved.  pins are configured in sets of two (bits 0&1 configure pins 1&2)
 
-   start = mmap(0, getpagesize(), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0xe8000000);
-        gpio_a_data =  (unsigned int *)(start + PC104A_IN_OUT);  // dio 1
-        gpio_a_direction = (unsigned int *)(start + PC104A_DIRECTION);
-	gpio_a_mux = (unsigned int *)(start + PC104A_MUX);
+#define SYSCONN_BASE 0xe8000000 //system controller base addr
+
+int main(void){
+	volatile unsigned int *gpio_a_data, *gpio_a_direction, *gpio_a_mux;
+	unsigned char *start;
+	int fd = open("/dev/mem", O_RDWR|O_SYNC);
+
+	start = mmap(0, getpagesize(), PROT_READ|PROT_WRITE, MAP_SHARED, fd, SYSCONN_BASE);
+
+        gpio_a_data =  (unsigned int *)(start + PC104_A_DATA);  // dio 1
+        gpio_a_direction = (unsigned int *)(start + PC104_A_DIRECTION);
+	gpio_a_mux = (unsigned int *)(start + PC104_A_MUX);
 
         *gpio_a_data = 0;
-	*gpio_a_direction = 1;
-	*gpio_a_mux = 0;//set isa A pins 1-31 to gpio
-	
+	*gpio_a_direction = 1;//which way is output?
+	*gpio_a_mux = 0;//set pc104_A pins 1-32 to gpio
+
 
 while(1){
         //*dio |= 0xFFFFFFFF;
         *gpio_a_data |= 1 << 0;
         usleep(1000);
-        *gpio_a_data = 0; // &  ~(1 << 16);
+        *gpio_a_data &= ~(1 << 0);
         usleep(1000);
 }
 
-   close(fd);
-   return 0;
+	close(fd);
+	return(0);
 }
 
