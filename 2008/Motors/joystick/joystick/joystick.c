20d877f412f2c3287b6be59847a0d4c76f2b644d
davidf <davidf@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-02-17 21:56:11 +0000

    abstracted the joystick driver
    git-svn-id: https://localhost/igvc@132 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2008/Motors/joystick/joystick/joystick.c
+++ 2008/Motors/joystick/joystick/joystick.c
@@ -1,124 +1,167 @@
 #include <SDL.h>
-#include <stdio.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdio.h>		// for printf in main()
 
-int main(int argc, char *argv[]) {
-	int x_move;
-	int y_move;
-	int garbage;
+#include "joystick.h"
+
+volatile int leftAnalogX;
+volatile int leftAnalogY;
+volatile int rightAnalogX;
+volatile int rightAnalogY;
+volatile int dPadX;
+volatile int dPadY;
+volatile int joystickButtons;
+
+static SDL_Joystick *joystick;
+
+static void* joystick_run(void* noArg) {
 	int i;
 	int done;
-	int buttons = 0;
-	int dPadX = 0;
-	int dPadY = 0;
-	int leftAnalogX = 0;
-	int leftAnalogY = 0;
-	int rightAnalogX = 0;
-	int rightAnalogY = 0;
 	SDL_Event event;
-	SDL_Joystick *joystick;
-	//setup starts:
-	/* Initialize SDL */
-	if ( SDL_Init(SDL_INIT_VIDEO|SDL_INIT_JOYSTICK) < 0 ) {
-		fprintf(stderr, "Couldn't initialize SDL: %s\n",SDL_GetError());
-		exit(1);
-	}
-	//garbage = SDL_JoystickEventState(SDL_ENABLE);
-	joystick = SDL_JoystickOpen(0);
-	if (SDL_JoystickOpened(0)) {
-		printf("Joystick Opened!\n");
-	}
-	else {
-		printf("Failed to open joystick!\n");
-	}
-	done=0;
-	for (i=1;i<20;i++) {
-		SDL_PollEvent(&event);//clear out any bad data
-		/*leftAnalogX = 0;
-		leftAnalogY = 0;
-		rightAnalogX = 0;
-		rightAnalogY = 0;*/
+	
+	// Ignore initial events during startup
+	for (i=0;i<20;i++) {
+		SDL_PollEvent(&event);
 	}
 	
-	//setup ends
-	//main loop starts:
-	while ( ! done ) {
+	// Poll the joystick for events, and update globals
+	done = 0;
+	while ( !done ) {
 		while ( SDL_PollEvent(&event) ) {
 			switch (event.type) {
-			    case SDL_JOYAXISMOTION:
-				//printf("Joystick %d axis %d value: %d\n",
-				       //event.jaxis.which,
-				       //event.jaxis.axis,
-				       //event.jaxis.value);
-				switch (event.jaxis.axis) {
-					case 4:
-						dPadX=event.jaxis.value;
-						printf("dPadX: %d\n", dPadX);
+				case SDL_JOYAXISMOTION:
+					switch (event.jaxis.axis) {
+						case 0:
+							leftAnalogX = event.jaxis.value;
+							break;
+						case 1:
+							leftAnalogY = event.jaxis.value;
+							break;
+						case 2:
+							rightAnalogX = event.jaxis.value;
+							break;
+						case 3:
+							rightAnalogY = event.jaxis.value;
+							break;
+						case 4:
+							dPadX = event.jaxis.value;
+							break;
+						case 5:
+							dPadY = event.jaxis.value;
+							break;
+						default:
+							break;
+					}
 					break;
-					case 5:
-						dPadY=event.jaxis.value;
-						printf("dPadY: %d\n", dPadY);
+				case SDL_JOYBUTTONDOWN:
+					joystickButtons |= (1 << event.jbutton.button);
 					break;
-					case 0:
-						leftAnalogX=event.jaxis.value;
-						printf("leftAnalogX: %d\n", leftAnalogX);
+				case SDL_JOYBUTTONUP:
+					joystickButtons &= ~(1 << event.jbutton.button);
 					break;
-					case 1:
-						leftAnalogY=event.jaxis.value;
-						printf("leftAnalogY: %d\n", leftAnalogY);
+				/*
+				case SDL_KEYDOWN:
+					if ( event.key.keysym.sym != SDLK_ESCAPE ) {//this line doesnt work
+						break;
+					}
+					// Fall through to signal quit
+				*/
+				case SDL_QUIT:
+					done = 1;
 					break;
-					case 2:
-						rightAnalogX=event.jaxis.value;
-						printf("rightAnalogX: %d\n", rightAnalogX);
+				default:
 					break;
-					case 3:
-						rightAnalogY=event.jaxis.value;
-						printf("rightAnalogY: %d\n", rightAnalogY);
-					break;
-					default:
-					break;
-				}
-				break;
-			    case SDL_JOYBUTTONDOWN:
-				//printf("Joystick %d button %d down\n",
-				       //event.jbutton.which,
-				       //event.jbutton.button);
-				       buttons = buttons | 1<<event.jbutton.button;
-						printf("buttons: %d\n", buttons);
-				break;
-			    case SDL_JOYBUTTONUP:
-				//printf("Joystick %d button %d up\n",
-				       //event.jbutton.which,
-				       //event.jbutton.button);
-				       buttons = buttons & ~1<<event.jbutton.button;
-						printf("buttons: %d\n", buttons);
-				break;
-			    case SDL_KEYDOWN:
-				if ( event.key.keysym.sym != SDLK_ESCAPE ) {//this line doesnt work
-					break;
-				}
-				/* Fall through to signal quit */
-			    case SDL_QUIT:
-				done = 1;
-				break;
-			    default:
-				break;
 			}
 		}
 	}
-	//main loop ends
 	
-	//shutdown starts:
-	  // Close if opened
-	if(SDL_JoystickOpened(0)) {
+	// Close joystick if it is opened
+	if (SDL_JoystickOpened(0)) {
 		SDL_JoystickClose(joystick);
 	}
+	
+	// Kill SDL
 	SDL_QuitSubSystem(SDL_INIT_JOYSTICK);
-	//shutdown ends
-	return 0;
+	
+	return NULL;
 }
 
+static pthread_t joystick_thread;
 
+/**
+ * Opens the joystick.
+ * 
+ * @return		NULL if success;
+ * 				or a message if error.
+ */
+const char* joystick_open(void) {
+	int err;
+	
+	// Initialize SDL
+	if ( SDL_Init(SDL_INIT_VIDEO|SDL_INIT_JOYSTICK) < 0 ) {
+		return SDL_GetError();
+	}
+	
+	joystick = SDL_JoystickOpen(0);
+	if (!SDL_JoystickOpened(0)) {
+		// TODO: would it work to return SDL_GetError() here?
+		return "SDL_JoystickOpen failed";
+	}
+	
+	// Call joystick_run() in new thread
+	err = pthread_create(
+		&joystick_thread,
+		NULL,
+		&joystick_run,
+		NULL);
+	switch (err) {
+		case EAGAIN:
+			return "insufficient thread resources";
+		case EINVAL:
+			return "invalid thread attributes";
+		default:
+			break;
+	}
+	
+	return NULL;	// no error
+}
 
+/**
+ * Closes the joystick.
+ */
+void joystick_close(void) {
+	// Send an SDL_QUIT event to the joystick event loop
+	SDL_QuitEvent quitEvent = { SDL_QUIT };
+	SDL_PushEvent((SDL_Event*) &quitEvent);
+}
 
+// ---
 
-
+int main(int argc, char *argv[]) {
+	// Open joystick
+	const char* err = joystick_open();
+	if (err != NULL) {
+		printf("Failed to initialize joystick: %s\n", err);
+		return 1;
+	}
+	
+	// Print joystick status every second
+	while (1) {
+		printf("left=(%d,%d), right=(%d,%d), dpad=(%d,%d), btns=%d\n",
+			leftAnalogX,
+			leftAnalogY,
+			rightAnalogX,
+			rightAnalogY,
+			dPadX,
+			dPadY,
+			joystickButtons);
+		
+		usleep(1000000);	// 1 sec
+	}
+	
+	// Close joystick
+	joystick_close();
+	
+	return 0;
+}
