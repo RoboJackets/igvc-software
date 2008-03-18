e2c940bbbccc937a90558a9f2e90ac5ae79d2a90
paulf <paulf@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-03-18 03:49:58 +0000

    Adding files I forgot in previous revision.
    git-svn-id: https://localhost/igvc@184 b8439097-6f30-0410-869d-b57f63bf1bb9

---
+++ 2008/candi/transform/c_code/getwhim.h
@@ -0,0 +1,16 @@
+#ifndef getwhim_h
+#define getwhim_h
+
+
+
+/* --- Public C Functions --- */
+#if __cplusplus
+extern "C" {
+#endif
+char * getwhim(Image* im);
+
+#if __cplusplus
+}
+#endif
+
+#endif
