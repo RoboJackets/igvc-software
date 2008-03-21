884e995fc043ff9c22d90b4afcb06ad49b0aa2da
paulf <paulf@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-03-21 15:24:19 +0000

    upgraded to c++
    git-svn-id: https://localhost/igvc@190 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2008/candi/transform/c_code/imageutils.h
+++ 2008/candi/transform/c_code/imageutils.h
@@ -1,10 +1,14 @@
 #ifndef _IMAGEUTILS_H_
 #define _IMAGEUTILS_H_
 
+<<<<<<< .mine
+Buffer2D<PixelRGB>* imageAsBuffer2D(Image* img);
+=======
 #include "Buffer2D.h"
 #include "PixelRGB.h"
 #include "image.h"
 
+>>>>>>> .r189
 //member variable
 Buffer2D<PixelRGB>* buf;
 
