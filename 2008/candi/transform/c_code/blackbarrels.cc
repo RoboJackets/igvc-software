e517621f78fd25d0e15ccca71923ca8f29ce40a7
chrism <chrism@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-03-22 19:47:03 +0000

    cleaned up blackbarrels.cc
    git-svn-id: https://localhost/igvc@201 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2008/candi/transform/c_code/blackbarrels.cc
+++ 2008/candi/transform/c_code/blackbarrels.cc
@@ -6,14 +6,11 @@
 #include "screenio.h"
 #include "blackmain.h"
 
-
 #define ESCAPE 27
-
 #define OWIDTH 720
 #define OHEIGHT 480
 
 void keyPressed(unsigned char key, int x, int y);
-
 int mainwindow;
 int i=0;
 Image out;
@@ -23,14 +20,13 @@ unsigned char tvarindex=0;
 void display ()
 {
 	long long tvar2;
-	//glLoadMatrixd(getPjMat());
+	/* clear screen */
+	glClear (GL_COLOR_BUFFER_BIT);
 	
-	//tvar=nanotime();
-	//printf("hi");
-	glClear (GL_COLOR_BUFFER_BIT);  
-	blackmain();                                           
-
-	//glColor(.5,.5,.5);
+	/* get image ready */  
+	blackmain();      
+	
+	/* create 3D flat plane to display image on */                                     
 	glBegin(GL_QUADS);
 		glTexCoord2f(0.f,0.f); glVertex3f(0.f		,0.f	,0);
 		glTexCoord2f(1.f,0.f); glVertex3f(OWIDTH	,0.f	,0);
@@ -40,17 +36,14 @@ void display ()
   	glBegin(GL_POINTS);
 		glVertex3f(0.5f		,0.5f	,0.5f);
 	glEnd();
-  	//printf("hi");
-  	
+	
+	/* prepare image buffer */
   	glFlush (); // makes sure commands are executed immediately
   	
-	/*
-	drawRose();
-  	glFlush ();
-  	*/
-  	
+  	/* double buffering */
 	glutSwapBuffers();
 	   
+	/* finish image buffer */
 	glFinish();
 	
 }
@@ -64,91 +57,52 @@ void keyPressed(unsigned char key, int x, int y)
 	double avg=0;
 	FILE *outp;
 	
-    /* avoid thrashing this procedure */
-   //debug
-   /*
-   for(xx=0;xx<256;xx++){
-   		for(yy=0;yy<7;yy++,xx++)
-   			printf("%lf : ",((double)tvararr[xx])/2000000000);
-   		printf("\n");
-   }
-   for(xx=0;xx<256;xx++)
-		avg+=((double)tvararr[xx])/2000000000;
-	printf("\naverage fps of processing:%lf\n",1/(avg/256));
-	*/
-   //debug/
-	
-    		
-    /* If escape is pressed, kill everything. */
-    if (key == 'd') 
-	    { 
-			/* shut down our window */
-			free(out.data);
-			glutDestroyWindow(mainwindow); 
-		      /* exit the program...normal termination. */
-			exit(0);                   
-	    }
+	/* If 's' is pressed, save everything, then kill everyting. */  
 	if (key == 's') //save raw data
-	    { 
-		    out.data=(char*)malloc(OWIDTH*OHEIGHT*sizeof(char)*3);
-		    glReadPixels(	0				,		//GLint x,
-			     			0				,		//GLint y,
-			     			OWIDTH			,		//GLsizei width,
-			     			OHEIGHT			,		//GLsizei height,
-			     			GL_BGRA			,		//GLenum format,
-			     			GL_UNSIGNED_BYTE,		//GLenum type,
-			     			out.data	);			//GLvoid *pixels
-		     					
-		    outp=fopen("outraw", "wb");
-
-		    for(xx=0;xx<OHEIGHT*OWIDTH*3;xx++){
-		      fprintf(outp,"%c",*(out.data+xx));
-		    }
-		    /* for(xx=0;xx<OWIDTH;xx++){ */
-/* 			    for(yy=0;yy<OHEIGHT;yy++){ */
-/* 				    for(c=0;c<3;c++){ */
-/* 					    fprintf(outp,"%c",*(out.data+xx*OWIDTH*3+yy*3+c)); */
-/* 				    } */
-/* 			    } */
-/* 		    } */
-		    fclose(outp);
-			/* shut down our window */
-			free(out.data);
-			glutDestroyWindow(mainwindow); 
-		      /* exit the program...normal termination. */
-			exit(0);                   
+	{ 
+	    out.data=(char*)malloc(OWIDTH*OHEIGHT*sizeof(char)*3);
+	    glReadPixels(	0				,		//GLint x,
+		     			0				,		//GLint y,
+		     			OWIDTH			,		//GLsizei width,
+		     			OHEIGHT			,		//GLsizei height,
+		     			GL_BGRA			,		//GLenum format,
+		     			GL_UNSIGNED_BYTE,		//GLenum type,
+		     			out.data	);			//GLvoid *pixels
+	    /* write data */ 					
+	    outp=fopen("outraw", "wb");
+	    for(xx=0;xx<OHEIGHT*OWIDTH*3;xx++){
+	      fprintf(outp,"%c",*(out.data+xx));
 	    }
-	glutDestroyWindow(mainwindow); 
-      /* exit the program...normal termination. */
-	exit(0); 
+	    fclose(outp);
+		/* shut down our window */
+		free(out.data);
+		glutDestroyWindow(mainwindow); 
+		/* exit the program...normal termination. */
+		exit(0);                   
+	 }
+	    
+	/* if any key is pressed, kill everything */
+		/* shut down our window */
+		glutDestroyWindow(mainwindow); 
+	    /* exit the program...normal termination. */
+		exit(0); 
 }
 
 void init ()
 {
-	texinit();
-	//LoadGLTextures();					// Load The Texture(s) 
+	texinit();							// Init textures & image buffer
+	//LoadGLTextures();					// Load The Texture(s) (secodnary failsafe)
     glEnable(GL_TEXTURE_2D);			// Enable Texture Mapping
-   	
-   	//glClearDepth(1.0);				// Enables Clearing Of The Depth Buffer
-   	
+  	//glClearDepth(1.0);				// Enables Clearing Of The Depth Buffer
     glShadeModel(GL_FLAT);				// Enables Smooth Color Shading
-
 	glClearColor (0.0, 0.0, 0.0, 0.0); 	// sets background color to black                      
-	
-	
-	 
-	//glMatrixMode(GL_MODELVIEW);
-	//glLoadIdentity ();
-	// sets up basic scale for input for you to draw on
-	//glOrtho (-1.0, 1, -1, 1, -10.0, 10.0);
-	//glPushMatrix();					//push down stack to avoid overriding when we load next matrix
-	glMatrixMode(GL_PROJECTION);
-	//glLoadMatrixd(getPjMat());		//apply planar perspective transform
-	//glLoadIdentity ();					//make sure OpenGL doesn't f*ck with my results
-	glOrtho (.0, 720., 0, 480., -10.0, 10.0);
-	out.width = OWIDTH;
-	out.height = OHEIGHT;
-	out.data = (char*)malloc(OWIDTH * OHEIGHT * 4);
+	glMatrixMode(GL_PROJECTION);		// Specify matrix used
+	//glLoadMatrixd(getPjMat());		// apply planar perspective transform
+	//glLoadIdentity ();				// make sure OpenGL doesn't f*ck with my results
+	glOrtho (.0, 720., 0, 480., -10.0, 10.0);	// creating othogonal mapping matrix
+	out.width = OWIDTH;							// sets up display canvas
+	out.height = OHEIGHT;						// sets up display canvas
+	out.data = (char*)malloc(OWIDTH * OHEIGHT * 4);	// sets up display canvas
 }
 
 int main(int argc, char** argv)
@@ -160,9 +114,9 @@ int main(int argc, char** argv)
 	mainwindow=glutCreateWindow ("Transform"); // sets the window title        
 	init (); // calls the init() function above
 	glutDisplayFunc (display); // uses the function called 'display' for displaying 
-	glutIdleFunc(display);
-	glutKeyboardFunc(&keyPressed);
-	glutMainLoop (); // will execute OpenGL functions continuously if window left up
+	glutIdleFunc(display); // also display function when idle
+	glutKeyboardFunc(&keyPressed); // setup keyboard interrupt
+	glutMainLoop (); // run program = will execute OpenGL functions continuously if window left up
 	return 0;
 }
 
