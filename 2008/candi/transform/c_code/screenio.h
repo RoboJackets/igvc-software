2000157d1acea71dbd3d60cad87bb9717cf06256
paulf <paulf@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-03-22 17:40:30 +0000

    Working Copy, updated to c++ except external accessors.
    git-svn-id: https://localhost/igvc@198 b8439097-6f30-0410-869d-b57f63bf1bb9

---
+++ 2008/candi/transform/c_code/screenio.h
@@ -0,0 +1,26 @@
+#ifndef _SCREENIO_H_
+#define _SCREENIO_H_
+#include "image.h"
+extern Image* screen0;
+
+#if __cplusplus
+	#include "Buffer2D.h"
+	#include "PixelRGB.h"
+
+	extern Buffer2D<PixelRGB>* screen;
+
+	extern "C" {
+
+#endif
+
+void screeninit(void);
+
+
+
+
+
+
+#if __cplusplus
+	}
+#endif
+#endif
