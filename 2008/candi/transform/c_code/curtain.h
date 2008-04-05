f106b487e1b287b1cf403415b9758118cdba77e7
paulf <paulf@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-04-05 20:06:40 +0000

    cleaning...
    git-svn-id: https://localhost/igvc@217 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2008/candi/transform/c_code/curtain.h
+++ 2008/candi/transform/c_code/curtain.h
@@ -12,7 +12,8 @@ typedef  Buffer2D<PixelRGB> b2drgb;
 typedef  Buffer2D<bool> b2dbool;
 
 b2drgb& curtain (Buffer2D<PixelRGB> whim, Buffer2D<PixelRGB> orim);
-
-
+void RGBtoBool (Buffer2D<PixelRGB> img,Buffer2D<PixelRGB>& dst);
+void dialate1D (Buffer2D<bool> arr);
+Buffer2D<bool>* cutout(int x,int y,Buffer2D<bool>& img);
 
 #endif
