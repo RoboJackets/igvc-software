1d00451ed6be52c792228a210bb7786a3d493180
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-04-05 19:57:47 +0000

    initial pd control for left and right motor seperatly
    git-svn-id: https://localhost/igvc@216 b8439097-6f30-0410-869d-b57f63bf1bb9

---
+++ 2008/pd_ctrl/pd.hh
@@ -0,0 +1,75 @@
+#ifndef PD_HH
+#define PD_HH
+
+#include "misc.hh"
+
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct {
+	public:
+		float Pgain;
+		float Dgain;
+	
+		struct timeval t1,t2;
+
+		float preverr;
+		
+		float current_vel;
+		float target_vel;
+
+		BOOL isRight;
+} PDstatus;
+
+float getError(PDstatus * pd_state){
+	return(pd_state->target_vel - pd_state->current_vel);
+}
+
+float newPD(PDstatus * pd_state, float err){
+	getTime(&pd_state->t2);
+
+	double dt = calcdt(pd_state->t1, pd_state->t2);
+
+	float P = pd_state->Pgain * err;
+
+	float D = pd_state->Dgain * (err - pd_state->preverr) / (dt);
+	pd_state->preverr = err;
+
+
+	pd_state->current_vel += (P + D);//update signal
+	return(pd_state->current_vel);
+}
+
+float getLinVel_L(PDstatus * pd_state){
+	return(pd_state->current_vel);//replace with code to find real vel
+}
+
+float getLinVel_R(PDstatus * pd_state){
+	return(pd_state->current_vel);//replace with code to find real vel
+}
+
+
+void PDupdateLin(PDstatus * pd_state){
+	float error, newsignal;
+
+	if(pd_state->isRight){
+		getLinVel_R(pd_state);
+	}
+	else{
+		getLinVel_L(pd_state);
+	}
+
+	error = getError(pd_state);
+	newsignal = newPD(pd_state, error);
+
+	getTime(&pd_state->t1);
+}
+
+int updateMotors(PDstatus * pd_left, PDstatus * pd_right){
+	return(1);
+}
+
+
+#endif
---
+++ 2008/pd_ctrl/reckon.hh
@@ -0,0 +1,86 @@
+#ifndef RECKON_HH
+#define RECKON_HH
+
+//#include <cstdio>
+#include <cmath>
+#include "misc.hh"
+
+
+#define b 1//width of wheels
+
+typedef struct{
+	double x_0;
+	double y_0;
+	double x_t;
+	double y_t;
+
+	double th_0;
+
+	struct timeval t1, t2;
+
+	double t;
+	double lastdt;	
+	
+	double x,y,th;
+} ddrn;
+
+void deadReckon(ddrn * reckon, float v_left, float v_right){
+	getTime(&(reckon->t2));
+
+	reckon->lastdt = calcdt(reckon->t1, reckon->t2);
+
+	reckon->t += reckon->lastdt;
+		
+	float r = ( b*(v_right + v_left) ) / ( 2*(v_left - v_right) );
+
+	reckon->th = reckon->th_0 + (v_left - v_right)*reckon->t / b;
+
+	reckon->y = reckon->y_0 - ( r ) * ( cos( (v_left - v_right)*(reckon->t)/b + reckon->th_0 ) - cos(reckon->th_0) );
+	reckon->x = reckon->x_0 + ( r ) * ( sin( (v_left - v_right)*(reckon->t)/b + reckon->th_0 ) - sin(reckon->th_0) );
+
+//	reckon->y = reckon->y_0 - ( r ) * ( cos( (v_right - v_left)*(reckon->t)/b + reckon->th_0 ) - cos(reckon->th_0) );
+//	reckon->x = reckon->x_0 + ( r ) * ( sin( (v_right - v_left)*(reckon->t)/b + reckon->th_0 ) - sin(reckon->th_0) );
+
+
+	getTime(&(reckon->t1));
+}
+
+float getTargetHeading(ddrn * reckon){
+	return(atan( (reckon->y_t - reckon->y) / (reckon->x_t - reckon->x) ));
+}
+
+/*
+float findSpeedRatio_90(ddrn * reckon){
+	//get l in terms of h (law od sines, assuiming right triangle)
+	//use distance form to get h
+	//get l, set radius/vel eqn equal to l
+
+	//float h = hypotf((xt-x0), (yt-y0));
+
+	float v_left_over_v_right = fabs( ( b - ( hypotf((reckon->x_t - reckon->x_0), (reckon->y_t - reckon->y_0)) ) * M_SQRT2 ) / ( b + ( hypotf((reckon->x_t - reckon->x_0), (reckon->y_t - reckon->y_0)) ) * M_SQRT2 ) ) ;
+	return(v_left_over_v_right);
+}
+*/
+
+float findSpeedRatio_Rot(ddrn * reckon, float face_angle){
+	
+	//return(v_left_over_v_right);
+}
+
+double radiusofcircle(double r_t, double th_t){
+	double radius = (r_t) / (2 * sin(th_t));
+	return(radius);
+}
+
+double findSpeedRatio(ddrn * reckon){
+	
+	double len = hypotf( (reckon->x_t - reckon->x) , (reckon->y_t - reckon->y) );
+	double deg = atan( (reckon->x_t - reckon->x) / (reckon->y_t - reckon->y) );
+
+	double r = radiusofcircle( len, deg );
+	double vl_vr = (2*r - b) / (2*r + b);
+	
+	return(vl_vr);
+}
+
+#endif //RECKON_HH
