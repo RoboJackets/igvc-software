808f022310c81c351c3fa1dbd24d5f67a14f68e4
paulf <paulf@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-04-05 19:57:03 +0000

    merged+ added cutout to curtain
    git-svn-id: https://localhost/igvc@215 b8439097-6f30-0410-869d-b57f63bf1bb9

---
+++ 2008/candi/transform/c_code/blackglobals.h
@@ -0,0 +1,6 @@
+#ifndef blackglobals_h
+#define blackglobals_h
+#define OWIDTH 720
+#define OHEIGHT 480
+
+#endif
--- 2008/candi/transform/c_code/curtain.cc
+++ 2008/candi/transform/c_code/curtain.cc
@@ -2,6 +2,7 @@
 #include "blackglobals.h"
 #include "Point2D.h"
 
+
 static Buffer2D<PixelRGB> barim;
 static Buffer2D<bool> boolimg;
 static Buffer2D<bool> boolarr;
@@ -77,8 +78,92 @@ b2drgb& curtain (Buffer2D<PixelRGB> whim, Buffer2D<PixelRGB> orim) {
 	return barim;
 }
 
-Buffer2D<bool>* cutout(int x,int y,Buffer2D<bool>& img){
+Buffer2D<bool>* cutout(int x,int y,Buffer2D<bool>& img) {
+	static Point2D<int>* plst=0;		// pixel list
+	static int plsz=0;					// pixel list size
+	static Buffer2D<bool>  cr;			//current region of interest
+	Point2D<int> xmax(x, y);
+	Point2D<int> xmin(x, y);
+	Point2D<int> ymax(x, y);
+	Point2D<int> ymin(x, y);
+	const int w=img.width;
+	//const int h=img.height;
+	
+	if (img.numElements()!=cr.numElements()) {
+		plst = new Point2D<int>[img.numElements()];
+		plsz = img.numElements();
+		cr.resizeToMatch (img) ;
+	}
+	
+	//fill cr with 0's
+		// Free old data buffer (if one was allocated)
+		if (cr.data != NULL) {
+			delete[] cr.data;
+		}
 
-
+		// Allocate new data buffer (if width & height are non-zero)
+		if ( (img.width != 0) && (img.height != 0)) {
+			cr.data = (bool*)malloc(sizeof(bool)*width * height);
+		} else {
+			cr.data = NULL;
+		}
+		
+	// lets cut!
+	int cp=0;		//current point index
+	int np=1;		//num points so far found, starts with 1
+	
+	plst[cp].x=x;	//read in initial point
+	plst[cp].y=y;
+	
+	img.data[plst[cp].x+plst[cp].y*w]=0;		//clear initial point
+	for(;cp<np;cp++){						//run till out of points
+		//clear and record 4 neighbors
+		{
+			//cache base x and y (hopefully in registers)			 
+				int cx=plst[cp].x;
+				int cy=plst[cp].y;
+			
+			//white top
+			{
+				if(img.data[cx+(cy+1)*w]){
+					img.data[cx+(cy+1)*w]=0;	//clear it
+					plst[np].y=cy+1;			//add to found list
+					plst[np].x=cx;
+					np++;
+				}
+			}
+			//white right
+			{
+				if(img.data[cx+1+(cy)*w]){		
+					img.data[cx+1+(cy)*w]=0;	//clear it
+					plst[np].y=cy;			//add to found list
+					plst[np].x=cx+1;
+					np++;
+				}
+			}
+			//white bottom
+			{
+				if(img.data[cx+(cy-1)*w]){
+					img.data[cx+(cy-1)*w]=0;	//clear it
+					plst[np].y=cy-1;			//add to found list
+					plst[np].x=cx;
+					np++;
+				}
+			}
+			//white left
+			{
+				if(img.data[cx-1+(cy)*w]){		
+					img.data[cx-1+(cy)*w]=0;	//clear it
+					plst[np].y=cy;			//add to found list
+					plst[np].x=cx-1;
+					np++;
+				}
+			}
+		}
+	}
+	//pump out region to cr
+	for(;cp<np;cp++){
+		cr.data[plst[cp].x+plst[cp].y*w]=0;
+	}
+	return &cr;
 }
-
