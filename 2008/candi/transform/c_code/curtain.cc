16dfa50b6d17bf451fe29dfd2fdfe06efb2097d0
chrism <chrism@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-04-05 19:40:30 +0000

    added some misc funcs
    git-svn-id: https://localhost/igvc@212 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2008/candi/transform/c_code/curtain.cc
+++ 2008/candi/transform/c_code/curtain.cc
@@ -1,6 +1,41 @@
 #include "curtain.h"
+#include "blackglobals.h"
+#include "Point2D.h"
 
 static Buffer2D<PixelRGB> barim;
+static Buffer2D<bool> boolimg;
+static Buffer2D<bool> boolarr;
+
+b2dbool& RGBtoBool (Buffer2D<PixelRGB> img) {
+	int ii;
+	int buffLength = img->width * img->height;
+
+	/*standard boolean conversion*/
+	for(ii = 0; ii < buffLength; ii++){
+		if(img[ii].r > 0 || img[ii].g > 0 || img[ii].b > 0)
+			boolim[ii] == 1;
+		else
+			boolim[ii] == 0;
+	}
+	
+	return boolim;
+}
+
+b2dbool& dialate1D (Buffer2D<bool> arr) {
+	int ii;
+	int buffLength = arr->width * arr->height;
+
+	/*if current pos or neighbor pos is 1, then current pos is 1*/
+	for(ii = 1; ii < buffLength-1; ii++){
+		if(arr[ii-1] || arr[ii] || arr[ii+1){
+			boolarr[ii] = 1;
+		}
+		else
+			boolarr[ii] = 0;
+	}	
+	
+	return boolarr;
+}
 
 b2drgb& curtain (Buffer2D<PixelRGB> whim, Buffer2D<PixelRGB> orim) {
 	
@@ -9,13 +44,16 @@ b2drgb& curtain (Buffer2D<PixelRGB> whim, Buffer2D<PixelRGB> orim) {
 	int buffLength = orim->width * orim->height;
 	bool isOrange = true;
 	
+	/*get boolean orange image*/
+	boolim = RGBtoBool(orim);
+	
 	/*loop untill no more orange pixels*/
 	while(isOrange){
 	
 		/*locate first index of first orange pixel*/
 		orangeIndex = -1;
 		for(ii = 0; ii < buffLength; ii++){
-			if(orim[ii].r == 255){
+			if(boolim){
 				orangeIndex = ii;
 				break;
 			}
@@ -28,7 +66,7 @@ b2drgb& curtain (Buffer2D<PixelRGB> whim, Buffer2D<PixelRGB> orim) {
 		}
 		
 		
-
+	
 
 		
 		
@@ -38,3 +76,9 @@ b2drgb& curtain (Buffer2D<PixelRGB> whim, Buffer2D<PixelRGB> orim) {
 
 	return barim;
 }
+
+Buffer2D<bool>* cutout(int x,int y,Buffer2D<bool>& img){
+
+
+}
+
