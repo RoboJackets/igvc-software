a3d96e8538a1c0d6f90897d106fb8a4821ba460f
paulf <paulf@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-04-05 22:24:15 +0000

    curtain almost works
    git-svn-id: https://localhost/igvc@228 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2008/candi/transform/c_code/curtain.h
+++ 2008/candi/transform/c_code/curtain.h
@@ -13,8 +13,8 @@ typedef  Buffer2D<bool> b2dbool;
 
 b2drgb& curtain (Buffer2D<PixelRGB>& whim, Buffer2D<PixelRGB>& orim);
 void RGBtoBool (Buffer2D<PixelRGB>& img,Buffer2D<bool>& dst);
-void dialate1D (Buffer2D<bool>& arr);
-Buffer2D<bool>* cutout(int x,int y,Buffer2D<bool>& img);
+void dilate1D (Buffer2D<bool>& arr);
+Buffer2D<bool>& cutout(int idx,Buffer2D<bool>& img);
 void booltoRGB (Buffer2D<bool>& img, Buffer2D<PixelRGB>& dst);
 
 
