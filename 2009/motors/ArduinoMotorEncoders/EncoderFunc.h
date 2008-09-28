90a5cc5c2b66cac0205d65a0155fadb49b2878cd
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-09-28 04:58:28 +0000

    add proto header and makefile
    git-svn-id: https://localhost/igvc@336 b8439097-6f30-0410-869d-b57f63bf1bb9

---
+++ 2009/motors/ArduinoMotorEncoders/EncoderFunc.h
@@ -0,0 +1,25 @@
+#ifndef ENCODERFUNC_H_
+#define ENCODERFUNC_H_
+
+void resetTime();
+
+unsigned int getTime();
+
+void readMotorEncoders(struct motorEncoderData *data);
+
+void serialPrintBytes(void *data, int numBytes);
+
+void sendStatus();
+
+void calcDelta(void);
+
+int delta(int p2, int p1, int maxDiff, int maxVal, int dir);
+
+void readSerial(void);
+
+int SPIReadInt(int inputPin, int slaveSelectPin, int clockPin);
+
+int convertMotorEncoderFormat(unsigned int data);
+
+void setVariable(int num, int val);
+#endif // ENCODERFUNC_H_
