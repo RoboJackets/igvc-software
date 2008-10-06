28adb3090d598b787eeb641056e7abbe9c1f6e92
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-10-06 05:25:36 +0000

    git-svn-id: https://localhost/igvc@359 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2009/motors/MotorEncodersTest.cc
+++ 2009/motors/MotorEncodersTest.cc
@@ -10,18 +10,28 @@ int main(void) {
 
 	cout << "Size of short = " << sizeof(short) << endl;
 	cout << "size of struct = " << sizeof(reply_t) << endl;
+
+	int packetnum = 1;
+
 	while(true) {
 		//cout << "Heading = " << encoders.getHeading() << endl;
 		//encoders.setArduinoClock();
 		reply_t packet = encoders.getInfo();
 		printf("timestamp (s): %d\n", (packet.timestamp) / 1000);
 		printf("packetnum: %d\n", packet.packetnum);
+
+		if(packetnum != packet.packetnum){
+			cout << "dropped packet" << endl;
+			exit(0);
+		}
+		packetnum++;
+
 		printf("dl: %X\n", (unsigned short)packet.dl);
 		printf("dr: %X\n", (unsigned short)packet.dr);
 		printf("dt: %X\n", (unsigned short)packet.dt);
 		cout << "\n\n";
 		//status = encoders.getInfo();
 		//cout << "Heading = " << status.heading << endl;
-		usleep(.5*1e6);
+		usleep(.01*1e6);
 	}
 }
