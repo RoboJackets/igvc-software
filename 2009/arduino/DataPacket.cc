640b6c278601d688e8af596af82a432ce11c6432
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2008-11-08 06:08:48 +0000

    print in hex format for debug
    git-svn-id: https://localhost/igvc@407 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2009/arduino/DataPacket.cc
+++ 2009/arduino/DataPacket.cc
@@ -17,17 +17,17 @@ std::ostream& operator<<(std::ostream& output, DataPacket pk){
 }
 
 std::ostream& operator<<(std::ostream& output, DataPacket::encoder_reply_t data){
-	output.setf(std::ios::hex);
-	output << "dr: " << data.dr << std::endl;
-	output << "dl: " << data.dl << std::endl;
-	output << "dt: " << data.dt << std::endl;
+	//output.setf(std::ios::hex);
+	output << std::hex << "dr: " << data.dr << std::endl;
+	output << std::hex << "dl: " << data.dl << std::endl;
+	output << std::hex << "dt: " << data.dt << std::endl;
 	return(output);
 }
 
 std::ostream& operator<<(std::ostream& output, DataPacket::encoder_reply_t* data){
-	output.setf(std::ios::hex);
-	output << "dr: " << data->dr << std::endl;
-	output << "dl: " << data->dl << std::endl;
-	output << "dt: " << data->dt << std::endl;
+	//output.setf(std::ios::hex);
+	output << std::hex << "dr: " << data->dr << std::endl;
+	output << std::hex << "dl: " << data->dl << std::endl;
+	output << std::hex << "dt: " << data->dt << std::endl;
 	return(output);
 }
