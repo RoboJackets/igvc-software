5938415bdb436e0fffbfadf33bb8850f6f66249e
chrism <chrism@b8439097-6f30-0410-869d-b57f63bf1bb9>
2009-01-31 20:10:04 +0000

    NEW Feature Tracking & Correlation files & functions added to MapGen !!!!!!!!!!!!; also formatted other files;
    git-svn-id: https://localhost/igvc@475 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2009/candiii/logging/bottime.cc
+++ 2009/candiii/logging/bottime.cc
@@ -15,29 +15,29 @@ char* RUN_TIME_STR=(char *)"00.00.00.00.00.00";	//time code for this run
 
 char * getTimeStr()
 {
-	time_t tsec;
-	struct tm t;
-	char *c;
-	c=(char *)malloc(19*sizeof(char));
-	tsec=time((time_t *)0);
-	localtime_r(&tsec,&t);
-	sprintf(c,"%.2d.%.2d.%.2d.%.2d.%.2d.%.2d",t.tm_year-100,t.tm_mon+1,t.tm_mday,t.tm_hour,t.tm_min,t.tm_sec);
-	return c;
+    time_t tsec;
+    struct tm t;
+    char *c;
+    c=(char *)malloc(19*sizeof(char));
+    tsec=time((time_t *)0);
+    localtime_r(&tsec,&t);
+    sprintf(c,"%.2d.%.2d.%.2d.%.2d.%.2d.%.2d",t.tm_year-100,t.tm_mon+1,t.tm_mday,t.tm_hour,t.tm_min,t.tm_sec);
+    return c;
 }
 
 long long CORE_TIME()
 {
-	return currentTimeMicros()-ROBOT_CORE_START_TIME;
+    return currentTimeMicros()-ROBOT_CORE_START_TIME;
 }
 
 void START_CORE_TIME()
 {
-	ROBOT_CORE_START_TIME=currentTimeMicros();
-	RUN_TIME_STR=getTimeStr();
+    ROBOT_CORE_START_TIME=currentTimeMicros();
+    RUN_TIME_STR=getTimeStr();
 }
 void SET_CORE_TIME(long long usec)
 {
-	ROBOT_CORE_START_TIME=currentTimeMicros()-usec;
+    ROBOT_CORE_START_TIME=currentTimeMicros()-usec;
 }
 
 
@@ -47,40 +47,40 @@ void SET_CORE_TIME(long long usec)
 inline unsigned long long nanotime(void)
 {
 #ifdef __i386__
-	unsigned long long val;
-	//asm( "statements" : output_registers : input_registers : clobbered_registers);
-	//or _asm_ )"statements" : output_registers : input_registers : clobbered_registers);
+    unsigned long long val;
+    //asm( "statements" : output_registers : input_registers : clobbered_registers);
+    //or _asm_ )"statements" : output_registers : input_registers : clobbered_registers);
 __asm__ __volatile__("rdtsc" : "=A" (val) : );
-	return(val);
+    return(val);
 #else
-	// This is architecture-independent but provides low granularity
-	return 1000*currentTimeMicros();
+    // This is architecture-independent but provides low granularity
+    return 1000*currentTimeMicros();
 #endif
 }
 
 
 long long currentTimeMillis()
 {
-	long long t;
-	struct timeval tv;
+    long long t;
+    struct timeval tv;
 
-	gettimeofday(&tv, (struct timezone*)NULL);
+    gettimeofday(&tv, (struct timezone*)NULL);
 
-	t = tv.tv_sec;
-	t = (t * 1000) + (tv.tv_usec/1000);
+    t = tv.tv_sec;
+    t = (t * 1000) + (tv.tv_usec/1000);
 
-	return t;
+    return t;
 }
 
 long long currentTimeMicros()
 {
-	long long t;
-	struct timeval tv;
+    long long t;
+    struct timeval tv;
 
-	gettimeofday(&tv, (struct timezone*)NULL);
+    gettimeofday(&tv, (struct timezone*)NULL);
 
-	t = tv.tv_sec;
-	t = (t * 1000 * 1000) + (tv.tv_usec);
+    t = tv.tv_sec;
+    t = (t * 1000 * 1000) + (tv.tv_usec);
 
-	return t;
+    return t;
 }
--- 2009/candiii/pathplan/PathPlan.h
+++ 2009/candiii/pathplan/PathPlan.h
@@ -28,49 +28,49 @@ enum {NEW=0, ACTIVE=1, DONE=2};
 class PathPlan
 {
 public:
-	PathPlan();
-	virtual ~PathPlan();
+    PathPlan();
+    virtual ~PathPlan();
 
-	void Init(int x_size, int y_size,  int widthRobo, int heightRobo);
-	/* internal variables to speed things up (no re-creations in each function) */
-	int* costMap;
-	int gridsize;
-	int xsize;
-	int ysize;
-	rectangle myrect;
-	shape *myshape;  //Base pointer to one of the above 3 derived shape objects
-	float *cost;
-	int *coord;
-	int *vect;
-	int *ptracker;
-	int *status;    //Status (NEW/ACTIVE/DONE) of grid points
-	int *bckptr;    //Backpointers to grid points within heap
-	int initialized;
+    void Init(int x_size, int y_size,  int widthRobo, int heightRobo);
+    /* internal variables to speed things up (no re-creations in each function) */
+    int* costMap;
+    int gridsize;
+    int xsize;
+    int ysize;
+    rectangle myrect;
+    shape *myshape;  //Base pointer to one of the above 3 derived shape objects
+    float *cost;
+    int *coord;
+    int *vect;
+    int *ptracker;
+    int *status;    //Status (NEW/ACTIVE/DONE) of grid points
+    int *bckptr;    //Backpointers to grid points within heap
+    int initialized;
 
 
-	/* copied from pathplan.h */
+    /* copied from pathplan.h */
 
-	inline float rint(float x)
-	{
-		return float((int)(x+.5));
-	}
+    inline float rint(float x)
+    {
+        return float((int)(x+.5));
+    }
 
-	void computeDistances(unsigned char *obst, shape *s,int dest,float *dist,int xsize,int ysize, int *costMap);
+    void computeDistances(unsigned char *obst, shape *s,int dest,float *dist,int xsize,int ysize, int *costMap);
 
-	//void CreatePotentialfield(int *costMap, int xsize, int ysize, int gridsize, int maxd);
-	void CreatePotentialfield(int *costMap, int xsize, int gridsize, int maxd);
+    //void CreatePotentialfield(int *costMap, int xsize, int ysize, int gridsize, int maxd);
+    void CreatePotentialfield(int *costMap, int xsize, int gridsize, int maxd);
 
-	int navigate(unsigned char *image,int xsize, int ysize, int xstart,int ystart,int &xend, int &yend, int scaredD, int widthRobo, int heightRobo);
-	//int navigate(unsigned char *image, int xstart,int ystart, int &xend, int &yend,  int scaredD);
+    int navigate(unsigned char *image,int xsize, int ysize, int xstart,int ystart,int &xend, int &yend, int scaredD, int widthRobo, int heightRobo);
+    //int navigate(unsigned char *image, int xstart,int ystart, int &xend, int &yend,  int scaredD);
 
-	/* copied from distance2D.h */
+    /* copied from distance2D.h */
 
-	inline float min(float a,float b)
-	{
-		return a<=b ? a : b;
-	}
+    inline float min(float a,float b)
+    {
+        return a<=b ? a : b;
+    }
 
-	float solve(int i,int j,int xsize,int ysize,int *status,float *dist, int *costMap);
+    float solve(int i,int j,int xsize,int ysize,int *status,float *dist, int *costMap);
 
 };
 
@@ -82,113 +82,113 @@ class staticheap
 {
 
 protected:
-	int *pixel;  //Pointer to array of pixels stored in heap (1-offset)
-	int size;    //Current size of heap
-	int maxsize; //Amount of space allocated to pixel and size arrays
-	float *dist; //Signed distance array
-	int *bckptr; //Array of backpointers
+    int *pixel;  //Pointer to array of pixels stored in heap (1-offset)
+    int size;    //Current size of heap
+    int maxsize; //Amount of space allocated to pixel and size arrays
+    float *dist; //Signed distance array
+    int *bckptr; //Array of backpointers
 
 public:
-	staticheap(float *dist,int *bckptr,int maxsize)
-	{
-		if (maxsize<0)
-			maxsize=0;
-		this->dist=dist;
-		this->bckptr=bckptr;
-		this->maxsize=maxsize;
-		pixel=(new int[maxsize])-1;
-		size=0;
-	}
+    staticheap(float *dist,int *bckptr,int maxsize)
+    {
+        if (maxsize<0)
+            maxsize=0;
+        this->dist=dist;
+        this->bckptr=bckptr;
+        this->maxsize=maxsize;
+        pixel=(new int[maxsize])-1;
+        size=0;
+    }
 
-	~staticheap()
-	{
-		delete[](pixel+1);
-	}
+    ~staticheap()
+    {
+        delete[](pixel+1);
+    }
 
-	virtual int order(const int &pix1,const int &pix2) = 0;
+    virtual int order(const int &pix1,const int &pix2) = 0;
 
-	void push(int pix)
-	{
-		size++;
-		pixel[size]=pix;
-		bckptr[pix]=size;
-		upheap(size);
-	}
+    void push(int pix)
+    {
+        size++;
+        pixel[size]=pix;
+        bckptr[pix]=size;
+        upheap(size);
+    }
 
-	int pop(int &pix)
-	{
-		if (size)
-		{
-			pix=pixel[1];
-			pixel[1]=pixel[size];
-			bckptr[pixel[1]]=1;
-			size--;
-			downheap(1);
-			return 1;
-		}
-		else
-			return 0;
-	}
+    int pop(int &pix)
+    {
+        if (size)
+        {
+            pix=pixel[1];
+            pixel[1]=pixel[size];
+            bckptr[pixel[1]]=1;
+            size--;
+            downheap(1);
+            return 1;
+        }
+        else
+            return 0;
+    }
 
-	void upheap(int node)
-	{
-		int parent, pix=pixel[node];
-		while ((parent=(node>>1)) && !order(pixel[parent],pix))
-		{
-			pixel[node]=pixel[parent];
-			bckptr[pixel[node]]=node;
-			node=parent;
-		}
-		pixel[node]=pix;
-		bckptr[pix]=node;
-	}
+    void upheap(int node)
+    {
+        int parent, pix=pixel[node];
+        while ((parent=(node>>1)) && !order(pixel[parent],pix))
+        {
+            pixel[node]=pixel[parent];
+            bckptr[pixel[node]]=node;
+            node=parent;
+        }
+        pixel[node]=pix;
+        bckptr[pix]=node;
+    }
 
-	void downheap(int node)
-	{
-		int child, pix=pixel[node];
-		while ((child=(node<<1))<=size)
-		{
-			if (child<size && order(pixel[child+1],pixel[child]))
-				child++;
-			if (order(pix,pixel[child]))
-				break;
-			pixel[node]=pixel[child];
-			bckptr[pixel[node]]=node;
-			node=child;
-		}
-		pixel[node]=pix;
-		bckptr[pix]=node;
-	}
+    void downheap(int node)
+    {
+        int child, pix=pixel[node];
+        while ((child=(node<<1))<=size)
+        {
+            if (child<size && order(pixel[child+1],pixel[child]))
+                child++;
+            if (order(pix,pixel[child]))
+                break;
+            pixel[node]=pixel[child];
+            bckptr[pixel[node]]=node;
+            node=child;
+        }
+        pixel[node]=pix;
+        bckptr[pix]=node;
+    }
 };
 
 
 class minheap : public staticheap
 {
 public:
-	minheap(float *dist,int *bckptr,int maxsize) :
-			staticheap(dist,bckptr,maxsize)
-	{
-		;
-	}
-	int order(const int &pix1,const int &pix2)
-	{
-		return dist[pix1]<=dist[pix2];
-	}
+    minheap(float *dist,int *bckptr,int maxsize) :
+            staticheap(dist,bckptr,maxsize)
+    {
+        ;
+    }
+    int order(const int &pix1,const int &pix2)
+    {
+        return dist[pix1]<=dist[pix2];
+    }
 };
 
 
 class maxheap : public staticheap
 {
 public:
-	maxheap(float *dist,int *bckptr,int maxsize) :
-			staticheap(dist,bckptr,maxsize)
-	{
-		;
-	}
-	int order(const int &pix1,const int &pix2)
-	{
-		return dist[pix1]>=dist[pix2];
-	}
+    maxheap(float *dist,int *bckptr,int maxsize) :
+            staticheap(dist,bckptr,maxsize)
+    {
+        ;
+    }
+    int order(const int &pix1,const int &pix2)
+    {
+        return dist[pix1]>=dist[pix2];
+    }
 };
 
 
