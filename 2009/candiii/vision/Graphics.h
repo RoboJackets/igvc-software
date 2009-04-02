2dc9ed6bd5d16c099bd43ddacb4e5506db8a35cb
chrism <chrism@b8439097-6f30-0410-869d-b57f63bf1bb9>
2009-04-02 03:32:19 +0000

    added processmap() to mapgen, leaving it commented out for now; updates to camera initialization; dependency install script update also; probably other misc stuff too;
    git-svn-id: https://localhost/igvc@562 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2009/candiii/vision/Graphics.h
+++ 2009/candiii/vision/Graphics.h
@@ -14,14 +14,14 @@
 #include <QVector>
 
 
-int Math_min(int n1, int n2)
-{
-	return (n1 < n2) ? n1 : n2;
-}
-int Math_max(int n1, int n2)
-{
-	return (n1 > n2) ? n1 : n2;
-}
+//int Math_min(int n1, int n2)
+//{
+//	return (n1 < n2) ? n1 : n2;
+//}
+//int Math_max(int n1, int n2)
+//{
+//	return (n1 > n2) ? n1 : n2;
+//}
 
 
 
@@ -37,6 +37,15 @@ public:
 		this->buffer = buffer;
 		color = CV_RGB(0,0,0); //default to black
 	}
+
+static int Math_min(int n1, int n2)
+{
+	return (n1 < n2) ? n1 : n2;
+}
+static int Math_max(int n1, int n2)
+{
+	return (n1 > n2) ? n1 : n2;
+}
 
 	void setColor(const CvScalar color)
 	{
