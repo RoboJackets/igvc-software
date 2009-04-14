825a747cd59e57a34f5d9b4f2a284b8b27d11ac0
chrism <chrism@b8439097-6f30-0410-869d-b57f63bf1bb9>
2009-04-14 01:24:12 +0000

    now using GLUT mask for worldmap processing; added lots of comments and code cleanup;
    git-svn-id: https://localhost/igvc@588 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2009/candiii/Robot.cpp
+++ 2009/candiii/Robot.cpp
@@ -58,7 +58,7 @@ void keyboardFunc(unsigned char key, int x, int y)   // handles keyboard button 
 /* for selecting images to display in the opencv window */
 void trackbarHandler(int pos)
 {
-	printf("pos = %d \n", pos);
+	printf("view = %d \n", pos);
 }
 /*****************************************************/
 
@@ -124,6 +124,8 @@ void Robot::releaseAllImages()
 	if ( visCvGreyBig !=NULL) cvReleaseImage(&visCvGreyBig );
 
 	if ( visCvRawTransform !=NULL) cvReleaseImage(&visCvRawTransform );
+
+	if ( visCvGlutMask !=NULL) cvReleaseImage(&visCvGlutMask );
 
 }
 
@@ -314,9 +316,9 @@ void Robot::Go()
 	initGlut();
 
 	/* get transform edge mask */
-    for (int i=0;i<6;i++)
+    for (int i=0;i<10;i++)
     {
-        getGlutMask(i);
+        getGlutMask(i); // loop == hack!
     }
 
 
@@ -660,7 +662,8 @@ void Robot::getGlutMask(int first)
 {
     /* this function gets a mask image of the transformed space
     *   to help when projecting the visCvThresh image into world space.
-    *    this function must be run in a loop so the updateGlutDisplay fully initializes the mask */
+    *    this function must be run in a loop so the updateGlutDisplay fully initializes the mask,
+    *     and even still, the mask still isn't fully correct sometimes... */
     if(first==0) visCvGlutMask = cvCreateImage(cvSize(visCvRaw->width/2,visCvRaw->height/2), IPL_DEPTH_8U, 3);
     cvSet( visCvRaw, CV_RGB(255,255,255) );
     updateGlutDisplay();
