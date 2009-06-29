8177fb84daf8e51251627baed3e72691761f9e2f
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2009-06-29 02:29:40 +0000

    fix more test code
    git-svn-id: https://localhost/igvc@670 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2009/motors/ArduinoMotorEncoders/libinctest.cc
+++ 2009/motors/ArduinoMotorEncoders/libinctest.cc
@@ -1,4 +1,28 @@
+#include "WProgram.h"
+#define ATMEGA168
+
+
+//http://zedcode.blogspot.com/2007/02/gcc-c-link-problems-on-small-embedded.html
+//to keep from needing to link to c++ std lib, maybe? not totally sure
+// solves this error:
+//../../libarduino/libarduinocore.a(Print.o):(.data+0x6): undefined reference to `__cxa_pure_virtual'
+extern "C" void __cxa_pure_virtual(void)
+{
+// call to a pure virtual function happened ... wow, should never happen ... stop
+while(1)
+;
+}
+
 int main()
 {
+	init();
+
+	Serial.begin(9600);
+
+	for(;;)
+	{
+		Serial.println("Hello");
+		delay(1000);
+	}
 	return 0;
 }
