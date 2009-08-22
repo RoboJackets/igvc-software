ec55cf5ab909a03b5b672779ce586f1971c81df2
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2009-08-22 21:09:36 +0000

    pull more code from 2009
    git-svn-id: https://localhost/igvc@678 b8439097-6f30-0410-869d-b57f63bf1bb9

---
+++ 2010/software/trunk/arduino/ArduinoCmds.hpp
@@ -0,0 +1,35 @@
+#ifndef ARDUINOCMDS_HPP_
+#define ARDUINOCMDS_HPP_
+
+#define PACKET_ERROR_CMD 0xFF
+
+//1 byte commands, laptop -> arduino
+#define ARDUINO_GETSTATUS_CMD 'r'
+#define ARDUINO_SETVAR_CMD 'w'
+#define ARDUINO_ID_CMD 'i'
+#define ARDUINO_RSND_PK_CMD 'p'
+
+//1 byte commands/response arduino -> laptop
+#define ARDUINO_ERROR_RESP 0xFF
+#define ARDUINO_UNKOWN_CMD_RESP 0xFE
+
+#define ARDUINO_GETSTATUS_RESP 'r'
+#define ARDUINO_SETVAR_RESP 'w'
+#define ARDUINO_ID_RESP 'i'
+#define ARDUINO_RSND_PK_RESP 'p'
+
+
+// MC ENUMS
+// Enums for data settings
+enum mc_opttype_t { MC_PUSHPULL = 0, MC_RET_T, MC_INTEROG_DL, MC_SETCLK, MC_RESENDPKT};//interogdl - miliseconds
+enum mc_opt_t {MC_PUSH = 0, MC_PULL, MC_SEND_DTICK, MC_SEND_CURRENT};
+
+//enum for general errors
+enum error_t {DROPPED_PACKET, REQUESTED_PACKET_OUT_OF_RANGE};
+
+
+//enum for sonar options
+enum sonar_opttype_t { SN_SET_RNG = 0, SN_SET_FREQ, SN_SET_GAIN, SN_SET_WARN,  };
+
+
+#endif //ARDUINO_COMMANDS_HPP_
---
+++ 2010/software/trunk/arduino/DataPacket.cc
@@ -0,0 +1,100 @@
+#include "DataPacket.hpp"
+
+DataPacket::DataPacket()
+{
+	data = NULL;
+	header.size = 0;
+}
+
+DataPacket::~DataPacket()
+{
+	if (data != NULL)
+	{
+		delete[] data;
+		data = NULL;
+		header.size = 0;
+	}
+}
+
+DataPacket::DataPacket(const DataPacket& pk)
+{
+	this->header = pk.header;
+	this->data = new byte[pk.header.size];
+	memcpy(this->data, pk.data, pk.header.size);
+
+	//std::cout << "copy constructor called" << std::endl;
+}
+
+void DataPacket::clear()
+{
+	header.timestamp_sec = -1;
+	header.timestamp_usec = -1;
+	header.packetnum = 0;
+	header.cmd = 0;
+	header.size = 0;
+
+	if (data != NULL)
+	{
+		delete[] data;
+		data = NULL;
+		header.size = 0;
+	}
+}
+
+std::ostream& operator<<(std::ostream& output, DataPacket& pk)
+{
+//	output << std::hex;
+	output << "timestamp sec: " << pk.header.timestamp_sec << std::endl;
+	output << "timestamp usec: " << pk.header.timestamp_usec << std::endl;
+//	output << std::dec;
+	output << "packetnum: " << pk.header.packetnum << std::endl;
+	output << "cmd (char): " << (char) pk.header.cmd << "\tcmd (ascii): " << (unsigned int) pk.header.cmd << std::endl;
+	output << "size: " << (int) pk.header.size << std::endl;
+
+	return(output);
+}
+
+std::ostream& operator<<(std::ostream& output, header_t header)
+{
+//	output << std::hex;
+	output << "timestamp sec: " << header.timestamp_sec << std::endl;
+	output << "timestamp usec: " << header.timestamp_usec << std::endl;
+//	output << std::dec;
+	output << "packetnum: " << header.packetnum << std::endl;
+	output << "cmd (char): " << (char) header.cmd << "\tcmd (ascii): " << (unsigned int) header.cmd << std::endl;
+	output << "size: " << (int) header.size << std::endl;
+
+	return(output);
+}
+
+std::ostream& operator<<(std::ostream& output, encoder_reply_t data)
+{
+	//output.setf(std::ios::hex);
+
+	output << std::hex;
+
+	output << "dr: " << data.dr << std::endl;
+	output << "dl: " << data.dl << std::endl;
+	output << "dt: " << data.dt << std::endl;
+
+	output << std::dec;
+
+	return(output);
+}
+/*
+std::ostream& operator<<(std::ostream& output, DataPacket::encoder_reply_t* data){
+	//output.setf(std::ios::hex);
+	output << std::hex << "timestamp: " << header->timestamp << std::endl;
+	output << std::hex << "packetnum: " << header->packetnum << std::endl;
+	output << std::hex << "cmd: " << header.cmd << std::endl;
+	output << std::hex << "size: " << header.size << std::endl;
+
+
+	output << std::hex << "dr: " << data->dr << std::endl;
+	output << std::hex << "dl: " << data->dl << std::endl;
+	output << std::hex << "dt: " << data->dt << std::endl;
+
+	output << std::dec;
+	return(output);
+}
+*/
