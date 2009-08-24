bbd07c6276769fbf9252c2ed341729cf442d9bc6
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2009-08-24 15:23:42 +0000

    misc osmc code
    git-svn-id: https://localhost/igvc@699 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2010/software/trunk/motors/osmc/arduino/wheel_encoders.cc
+++ 2010/software/trunk/motors/osmc/arduino/wheel_encoders.cc
@@ -1,5 +1,5 @@
 #include "wheel_encoders.hpp"
-
+/*
 unsigned int getTime()
 {
 	unsigned int time;
@@ -8,8 +8,19 @@ unsigned int getTime()
 	time |= TCNT1H << 8;
 	return(time);
 }
+*/
 
-void calcDelta(struct motorEncoderData current, struct motorEncoderData previous, int *dl, int *dr, long *dt)
+uint32 getTime()
+{
+	uint32 time = millis();
+	return time;
+	//TCNT1 is a 16 bit timer/counter ~ pg 121
+	//time = TCNT1L;//how often does this tick? tests say clkio = F_CPU.
+	//time |= TCNT1H << 8;
+	//return(time);
+}
+
+void calcDelta(struct motorEncoderData current, struct motorEncoderData previous, int16 *dl, int16 *dr, uint32 *dt)
 {
 	*dl = delta(current.leftMotorTick, previous.leftMotorTick, 100, TOTAL_ENCODER_TICKS - 1,  LEFT_MOTOR_ENCODER_DIRECTION); 
 	*dr = delta(current.rightMotorTick, previous.rightMotorTick, 100, TOTAL_ENCODER_TICKS - 1, RIGHT_MOTOR_ENCODER_DIRECTION);
@@ -18,29 +29,30 @@ void calcDelta(struct motorEncoderData current, struct motorEncoderData previous
 		*dt = (current.time - previous.time);
 	}
 	else{
-		*dt =  ( ((long)current.time) + 65536) - previous.time;
+		*dt =  (((uint32)current.time) + 65536) - ((uint32)previous.time);
 	}
 }
 
-void readMotorEncoders(struct motorEncoderData *data)
+motorEncoderData readMotorEncoders()
 {
+	motorEncoderData data;
 
-	unsigned int rawData;
+	data.time = getTime(); // WARNING: the left and right values aren't measured at exactly the same time
 
-	data->time = getTime(); // WARNING: the left and right values aren't measured at exactly the same time
-
-	rawData = SPIReadInt(SPI_MISO, SPI_SS_LEFT_MOTOR_ENCODER, SPI_CLK);
-	data->leftMotorTick = convertMotorEncoderFormat(rawData);
+	unsigned int rawDataLeft = SPIReadInt(SPI_MISO, SPI_SS_LEFT_MOTOR_ENCODER, SPI_CLK);
+	data.leftMotorTick = convertMotorEncoderFormat(rawDataLeft);
 
 	/*Serial.print("Left Raw: ");
 	Serial.print(rawData, BIN);
 	Serial.print("\t");*/
 
-	rawData = SPIReadInt(SPI_MISO, SPI_SS_RIGHT_MOTOR_ENCODER, SPI_CLK);
-	data->rightMotorTick = convertMotorEncoderFormat(rawData);
+	unsigned int rawDataRight = SPIReadInt(SPI_MISO, SPI_SS_RIGHT_MOTOR_ENCODER, SPI_CLK);
+	data.rightMotorTick = convertMotorEncoderFormat(rawDataRight);
 	/*Serial.print("Right Raw: ");
 	Serial.print(rawData, BIN);
 	Serial.print("\t\n");*/
+
+	return data;
 }
 
 // must hold: maxDiff <= maxVal
@@ -83,4 +95,19 @@ int convertMotorEncoderFormat(unsigned int data)
 	} else {
 		return( (data & 0x0007) | ((data & 0x3F00) >> 5) );
 	}
+}
+
+reply_dtick_t getEncoderStatus()
+{
+	motorEncoderData first = readMotorEncoders();
+	delay(10);
+	motorEncoderData second = readMotorEncoders();
+
+	reply_dtick_t packet;
+
+	calcDelta(second, first, &packet.dl, &packet.dr, &packet.dt);
+
+	//packet.dt = 0xFACEBEEF;
+
+	return(packet);
 }
