3bb1fdb89a3faa7a442e3b064d34b09a2889340f
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2009-09-19 21:04:23 +0000

    analog joystick
    git-svn-id: https://localhost/igvc@709 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2010/software/trunk/motors/osmc/PC_test/OSMC_driver.hpp
+++ 2010/software/trunk/motors/osmc/PC_test/OSMC_driver.hpp
@@ -9,8 +9,14 @@ class OSMC_driver
 
 	reply_dtick_t getEncoderData();
 	current_reply_t getCurrentData();
+	joystick_reply_t getJoystickData();
 
-	void setmotorPWM(char rightDutyCycle, char leftDutyCycle);
+	//make 0 vel a nonrolling stop
+	bool setmotorPWM(byte rightDir, byte rightDutyCycle, byte leftDir, byte leftDutyCycle);
+
+	//add these commands
+	//bool enableMotor();
+	//bool disableMotor();
 
 	private:
 	ArduinoInterface ai;
