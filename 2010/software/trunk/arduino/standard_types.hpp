307f62dceeae73425d6e414e24b5b9128c068851
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2010-02-06 01:02:06 +0000

    git-svn-id: https://localhost/igvc@716 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2010/software/trunk/arduino/standard_types.hpp
+++ 2010/software/trunk/arduino/standard_types.hpp
@@ -1,24 +1,36 @@
 #ifndef STANDARD_TYPES_HPP
 #define STANDARD_TYPES_HPP
 
-#ifndef __AVR_LIBC_VERSION_STRING__
-	typedef char int8;
-	typedef unsigned char uint8;
+#include <stdint.h>
 
-	typedef short int16;
-	typedef unsigned short uint16;
+#if 0
+	#ifndef __AVR_LIBC_VERSION_STRING__
+		typedef char int8;
+		typedef unsigned char uint8;
 
-	typedef int int32;
-	typedef unsigned int uint32;
+		typedef short int16;
+		typedef unsigned short uint16;
+
+		typedef int int32;
+		typedef unsigned int uint32;
+	#else
+		typedef char int8;
+		typedef unsigned char uint8;
+
+		typedef int int16;
+		typedef unsigned int uint16;
+
+		typedef long int32;
+		typedef unsigned long uint32;
+	#endif
 #else
-	typedef char int8;
-	typedef unsigned char uint8;
+	typedef int8_t int8;
+	typedef uint8_t uint8;
 
-	typedef int int16;
-	typedef unsigned int uint16;
+	typedef int16_t int16;
+	typedef uint16_t uint16;
 
-	typedef long int32;
-	typedef unsigned long uint32;
+	typedef int32_t int32;
+	typedef uint32_t uint32;
 #endif
-
 #endif
