605cb7e361cfee9cd63655f08a3718d8f666b4fe
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2010-02-06 01:06:02 +0000

    git-svn-id: https://localhost/igvc@717 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2010/software/trunk/arduino_diecimila_crosscompile.cmake
+++ 2010/software/trunk/arduino_diecimila_crosscompile.cmake
@@ -4,9 +4,10 @@ SET(CMAKE_C_COMPILER avr-gcc)
 SET(CMAKE_CXX_COMPILER avr-g++)
 
 SET(CSTANDARD "-std=gnu99")
+SET(CXXSTANDARD "-std=gnu++0x")
 SET(CDEBUG "-gstabs")
 SET(CWARN "-Wall -Wstrict-prototypes -Wextra")
-SET(CPPWARN "-Wall -Wextra")
+SET(CXXWARN "-Wall -Wextra")
 SET(CTUNING "-funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums")
 SET(COPT "-Os")
 SET(CMCU "-mmcu=atmega168")
@@ -15,7 +16,7 @@ SET(CDEFS "-DF_CPU=16000000")
 
 
 SET(CFLAGS "${CMCU} ${CDEBUG} ${CDEFS} ${CINCS} ${COPT} ${CWARN} ${CSTANDARD} ${CEXTRA}")
-SET(CXXFLAGS "${CMCU} ${CDEFS} ${CINCS} ${COPT} ${CPPWARN}")
+SET(CXXFLAGS "${CMCU} ${CDEFS} ${CINCS} ${COPT} ${CXXWARN} ${CXXSTANDARD}")
 
 SET(CMAKE_C_FLAGS  ${CFLAGS})
 SET(CMAKE_CXX_FLAGS ${CXXFLAGS})
