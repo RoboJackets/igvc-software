64996507e71edec49ac5a20bec9ee0bca7f96f18
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2010-02-06 01:51:50 +0000

    move candiii with modifications
    git-svn-id: https://localhost/igvc@720 b8439097-6f30-0410-869d-b57f63bf1bb9

---
+++ 2010/software/trunk/candiii/vision/image_buffers.cpp
@@ -0,0 +1,38 @@
+#include "image_buffers.h"
+
+
+/* buffers ***********************************************/
+
+
+
+
+IplImage* visCvRaw;
+IplImage* visCvDebug;
+IplImage* visCvRedChannel;
+IplImage* visCvGreenChannel;
+IplImage* visCvBlueChannel;
+IplImage* visCvHSV;
+IplImage* visCvHue;
+IplImage* visCvSaturation;
+IplImage* visCvGrey;
+IplImage* visCvThresh;
+IplImage* visCvPath;
+IplImage* visCvHSVSmall;
+
+IplImage* visCvAdapt;
+IplImage* visCvAdaptSmall;
+
+IplImage* visCvGreyBig;
+IplImage* visCvRawTransform;
+IplImage* visCvGlutMask;
+IplImage* visCvRawTransformSmall;
+
+IplImage* visCvRamp;
+IplImage* visCvRampLines;
+
+
+/* conversions ***********************************************/
+
+
+
+
