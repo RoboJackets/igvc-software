a0e598cb31ae0fb0a1de38943d73c1018880db28
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2010-03-12 21:44:12 +0000

    saner defs
    git-svn-id: https://localhost/igvc@748 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2010/software/trunk/arduino/standard_types.hpp
+++ 2010/software/trunk/arduino/standard_types.hpp
@@ -3,34 +3,13 @@
 
 #include <stdint.h>
 
-#if 0
-	#ifndef __AVR_LIBC_VERSION_STRING__
-		typedef char int8;
-		typedef unsigned char uint8;
+typedef int8_t int8;
+typedef uint8_t uint8;
 
-		typedef short int16;
-		typedef unsigned short uint16;
+typedef int16_t int16;
+typedef uint16_t uint16;
 
-		typedef int int32;
-		typedef unsigned int uint32;
-	#else
-		typedef char int8;
-		typedef unsigned char uint8;
+typedef int32_t int32;
+typedef uint32_t uint32;
 
-		typedef int int16;
-		typedef unsigned int uint16;
-
-		typedef long int32;
-		typedef unsigned long uint32;
-	#endif
-#else
-	typedef int8_t int8;
-	typedef uint8_t uint8;
-
-	typedef int16_t int16;
-	typedef uint16_t uint16;
-
-	typedef int32_t int32;
-	typedef uint32_t uint32;
-#endif
 #endif
