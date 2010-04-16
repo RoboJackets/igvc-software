a857788a1a84bebf0245a9127edb135fcd660505
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2010-04-16 23:41:50 +0000

    init support for 2 arduino
    git-svn-id: https://localhost/igvc@758 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2010/software/trunk/motors/osmc/PC_4wd/OSMC_driver.hpp
+++ 2010/software/trunk/motors/osmc/PC_4wd/OSMC_driver.hpp
@@ -12,19 +12,14 @@ class OSMC_driver
 	bool is_connected();
 	#endif
 
-	reply_dtick_t getEncoderData();
-	current_reply_t getCurrentData();
-	joystick_reply_t getJoystickData();
+	bool getCurrentData(current_reply_t& fwd, current_reply_t& aft);
+	bool getJoystickData(joystick_reply_t& f, joystick_reply_t& b);
 
 	//make 0 vel a nonrolling stop
-	bool setmotorPWM(byte rightDir, byte rightDutyCycle, byte leftDir, byte leftDutyCycle);
-
-	//shims from old motor
-	bool set_motors(int leftVelocity, int rightVelocity);
-	int set_heading(int iFwdVelocity, int iRotation);
+	bool setmotorPWM(const byte rightDirFWD, const byte rightDutyCycleFWD, const byte leftDirFWD, const byte leftDutyCycleFWD, const byte rightDirAFT, const byte rightDutyCycleAFT, const byte leftDirAFT, const byte leftDutyCycleAFT);
 
 	void getNewVel_dumb(const double rtarget, const double ltarget, const double rvel, const double lvel, const int rmset, const int lmset,  int& out_rmset, int& out_lmset);
-	void getNewVel_pd(const double ltarget, const double rtarget, const double lvel, const double rvel,const double lastlvel, const double lastrvel const int rmset, const int lmset, double dt, int& out_rmset, int& out_lmset);
+	void getNewVel_pd(const double ltarget, const double rtarget, const double lvel, const double rvel,const double lastlvel, const double lastrvel, const int rmset, const int lmset, double dt, int& out_rmset, int& out_lmset);
 
 	//follow path
 	void followCirc(double radius, double vmag);
@@ -34,7 +29,13 @@ class OSMC_driver
 	//bool disableMotor();
 
 	private:
-	ArduinoInterface ai;
+
+	bool setmotorPWM(ArduinoInterface& ai, const byte rightDir, const byte rightDutyCycle, const byte leftDir, const byte leftDutyCycle);
+	current_reply_t getCurrentData(ArduinoInterface& ai);
+	joystick_reply_t getJoystickData(ArduinoInterface& ai);
+
+	ArduinoInterface fwr;
+	ArduinoInterface aft;
 
 	const static int _max_speed_;
 	const static int MINREQSPEED;
