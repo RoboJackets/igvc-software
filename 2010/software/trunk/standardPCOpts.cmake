7349049f6fb01f4be2fe11b855a8b4e252a351d4
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2010-05-01 19:43:34 +0000

    none
    git-svn-id: https://localhost/igvc@765 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2010/software/trunk/standardPCOpts.cmake
+++ 2010/software/trunk/standardPCOpts.cmake
@@ -1,12 +1,9 @@
-SET(CDEBUG "-g3")
-SET(CWARN "-Wall -Wstrict-prototypes -Wextra -Wuninitialized -Werror=uninitialized")
-SET(CXXWARN "-Wall -Wextra -Wuninitialized -Werror=uninitialized")
-SET(CXXSTANDARD "-std=gnu++0x")
-#SET(CTUNING "-funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums")
-SET(COPT "-O2")
 
-SET(CFLAGS "${CDEBUG} ${COPT} ${CWARN}")
-SET(CXXFLAGS "${CDEBUG} ${COPT} ${CXXWARN} ${CXXSTANDARD}")
+#SET(CFLAGS "-Wall -Wstrict-prototypes -Wextra -Wuninitialized -Werror=uninitialized -g3 -O2 -pthread")
+#SET(CXXFLAGS "-std=gnu++0x -Wall -Wextra -Wuninitialized -Werror=uninitialized -g3 -O2 -pthread")
+
+SET(CFLAGS "-Wall -Wstrict-prototypes -Wextra -g3 -O0 -pthread")
+SET(CXXFLAGS "-std=gnu++0x -Wall -Wextra -g3 -O0 -pthread")
 
 SET(CMAKE_C_FLAGS  ${CFLAGS})
 SET(CMAKE_CXX_FLAGS ${CXXFLAGS})
