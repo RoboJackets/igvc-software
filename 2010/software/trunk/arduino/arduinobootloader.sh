d040e19887c41652f82a6934fc8c86099e35c680
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2010-05-01 19:42:53 +0000

    none
    git-svn-id: https://localhost/igvc@763 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2010/software/trunk/arduino/arduinobootloader.sh
+++ 2010/software/trunk/arduino/arduinobootloader.sh
@@ -1,7 +1,8 @@
 input=$1
+port=$2
 ext=.hex
 hex=$input$ext
 
 avr-objcopy -O ihex -R .eeprom $input $hex
-avrdude -V -c stk500v1 -p m168 -F -b 19200 -P /dev/ttyUSB0 -U flash:w:$hex
+avrdude -V -c stk500v1 -p m168 -F -b 19200 -P $port -U flash:w:$hex
 rm $hex
