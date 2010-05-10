3fe8529e96712b7bc492612c311802dd247db6a6
jmadd <jmadd@b8439097-6f30-0410-869d-b57f63bf1bb9>
2010-05-10 17:49:28 +0000

    John's joystick code
    git-svn-id: https://localhost/igvc@780 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2010/software/trunk/motors/osmc/arduino/analogjoystick.cc
+++ 2010/software/trunk/motors/osmc/arduino/analogjoystick.cc
@@ -64,10 +64,54 @@ void joystickSetMotors()
 	/* Calculate drive outputs -- magic*/
 	//int leftVelocity = .5*float(yAxis) + 2 * sign(xAxis) * sqrt(abs(xAxis));
 	//int rightVelocity = .5*float(yAxis) - 2 * sign(xAxis) * sqrt(abs(xAxis));
-	int leftVelocity = yAxis + 3 * sign(xAxis) * sqrt(abs(xAxis));
-	int rightVelocity = yAxis - 3 * sign(xAxis) * sqrt(abs(xAxis));
+	//int leftVelocity = yAxis + 3 * sign(xAxis) * sqrt(abs(xAxis));
+	//int rightVelocity = yAxis - 3 * sign(xAxis) * sqrt(abs(xAxis));
+	int leftVelocity;
+	int rightVelocity;
+	
+	if ((yAxis > xAxis)&&(xAxis >= 0)) {
+	  leftVelocity = 512;
+	  rightVelocity = 512 - xAxis/2;
+	}
+	else if ((yAxis <= xAxis)&&(yAxis > 0)) {
+	  leftVelocity = 256 + yAxis/2;
+	  rightVelocity = yAxis - 256;
+	}
+	else if ((-yAxis < xAxis)&&(yAxis <= 0)) {
+	  leftVelocity = 256 - yAxis;
+	  rightVelocity = -256 - yAxis/2;
+	}
+	else if ((-yAxis >= xAxis)&&(xAxis > 0)) {
+	  leftVelocity = -512 + xAxis/2;
+	  rightVelocity = -512;
+	}
+	else if ((yAxis < xAxis)&&(xAxis <= 0)) {
+	  leftVelocity = -512;
+	  rightVelocity = -512 - xAxis/2;
+	}
+	else if ((yAxis >= xAxis)&&(yAxis < 0)) {
+	  leftVelocity = -256 + yAxis/2;
+	  rightVelocity = -256 + yAxis;
+	}
+	else if ((-yAxis < xAxis)&&(yAxis >= 0)) {
+	  leftVelocity = -256 + yAxis;
+	  rightVelocity = 256 + yAxis/2;
+	}
+	else if ((-yAxis >= xAxis)&&(xAxis < 0)) {
+	  leftVelocity = 512 + xAxis/2;
+	  rightVelocity = 512;
+	}
+
+	leftVelocity = float(leftVelocity/2)*abs(float(xAxis + yAxis)/float(1023 - abs(xAxis - yAxis)));
+	rightVelocity = float(rightVelocity/2)*abs(float(xAxis + yAxis)/float(1023 - abs(xAxis - yAxis)));
 
 	/* Limit drive velcocities to vaild values and convert them to drive speeds and directions */
+
+	Serial.print("L/Rev: ");
+	Serial.println(leftVelocity, DEC);
+
+	Serial.print("R/Rev: ");
+	Serial.println(rightVelocity, DEC);
 
 	byte leftSpeed = min(abs(leftVelocity), 255);
 	byte leftDir = (leftVelocity < 0) ? MC_MOTOR_REVERSE : MC_MOTOR_FORWARD;
