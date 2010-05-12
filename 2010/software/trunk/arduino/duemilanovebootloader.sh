d9270bd017316dcf7e41921024cfe1934844dc47
jmadd <jmadd@b8439097-6f30-0410-869d-b57f63bf1bb9>
2010-05-12 01:59:40 +0000

    working script for duemilanovebootloader.sh
    git-svn-id: https://localhost/igvc@787 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2010/software/trunk/arduino/duemilanovebootloader.sh
+++ 2010/software/trunk/arduino/duemilanovebootloader.sh
@@ -4,5 +4,5 @@ ext=.hex
 hex=$input$ext
 
 avr-objcopy -O ihex -R .eeprom $input $hex
-avrdude -V -c stk500v1 -p m328 -F -b 19200 -P $port -U flash:w:$hex
+avrdude -V -c arduino -p m328p -F -b 57600 -P $port -U flash:w:$hex
 rm $hex
