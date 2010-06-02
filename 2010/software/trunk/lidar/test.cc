af6359ba35ee1944b6f86543672a2f3a9be38924
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2010-06-02 05:50:48 +0000

    fully integrate lidar
    git-svn-id: https://localhost/igvc@829 b8439097-6f30-0410-869d-b57f63bf1bb9

--- 2010/software/trunk/lidar/test.cc
+++ 2010/software/trunk/lidar/test.cc
@@ -2,20 +2,56 @@
 #include "NAV200.hpp"
 
 #include <iostream>
+
 #include "boost/tuple/tuple.hpp"
+#include "boost/tuple/tuple_io.hpp"
 #include "boost/foreach.hpp"
 
-typedef boost::tuple<int,int> range;
+typedef boost::tuple<float,float> range;
+
+template<typename T>
+void printarray(T* v, size_t len)
+{
+	std::cout << "<" << v[0] << ", ";
+	for(size_t i = 1; i < (len-1); i++)
+	{
+		std::cout << v[i] << ", ";
+	}
+	std::cout << v[len-1] << ">\n";
+}
+
+template<typename T>
+void printarray(T* v, size_t start, size_t stop)
+{
+	std::cout << "<" << v[start] << ", ";
+	for(size_t i = start+1; i < stop; i++)
+	{
+		std::cout << v[i] << ", ";
+	}
+	std::cout << v[stop] << ">\n";
+}
 
 int main()
 {
 
 	//NAV200 a;
-
+	float domains[NAV200::Num_Points] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28};
 	float ranges[NAV200::Num_Points] = {1,2,3,4,5,0,2,4,6,8,0,3,6,9,0,4,9,16,25, -2,-4,-6, .1, .3, .5, 5, 15, 25};
+	boost::tuple<float,float> testcoord[NAV200::Num_Points];
 
-	std::list< boost::tuple<int,int> > lines;
-	NAV200::findLinearRuns(ranges, lines);
+	for(int i = 0; i < NAV200::Num_Points; i++)
+	{
+		testcoord[i].get<0>() = domains[i];
+		testcoord[i].get<1>() = ranges[i];
+	}
+
+	std::deque< boost::tuple<float,float> > lines;
+	NAV200::findLinearRuns(testcoord, lines);
+
+	boost::tuple<float,float> longest;
+	NAV200::getLongestRun(lines, longest);
+
+	std::cout << "longest run: " << longest << std::endl;
 
 	int rampmap[NAV200::Num_Points];//differ between linear runs with different slopes
 	std::fill(rampmap, rampmap+NAV200::Num_Points, -1);
@@ -25,10 +61,11 @@ int main()
 		std::cout << "start: " << e.get<0>() << ", stop: " << e.get<1>() << std::endl;
 		std::cout << "\t";
 		//printarray(ranges, e.get<0>(), e.get<1>());
-		std::fill(rampmap+e.get<0>(), rampmap+e.get<1>()+1, i);
+		//std::fill(rampmap+e.get<0>(), rampmap+e.get<1>()+1, i);
 		i++;
 	}
-	std::cout << "rampmap: ";//printarray(rampmap, NAV200::Num_Points);
+	std::cout << "ranges: ";printarray(ranges, NAV200::Num_Points);
+	std::cout << "rampmap: ";printarray(rampmap, NAV200::Num_Points);
 
 	return 0;
 
