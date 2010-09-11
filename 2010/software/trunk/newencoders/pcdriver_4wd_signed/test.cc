e0765a68e299587919797c7c07d0fccd5ad413a3
jacobs <jacobs@b8439097-6f30-0410-869d-b57f63bf1bb9>
2010-09-11 19:32:56 +0000

    code from competition
    git-svn-id: https://localhost/igvc@835 b8439097-6f30-0410-869d-b57f63bf1bb9

---
+++ 2010/software/trunk/newencoders/pcdriver_4wd_signed/test.cc
@@ -0,0 +1,43 @@
+#include "quadCoderDriver_4wd_signed.hpp"
+
+volatile bool reset = false;
+
+void handler(int signum)
+{
+	reset = true;
+}
+
+
+int main()
+{
+
+	signal(SIGINT, handler);
+
+	quadCoderDriver_4wd_signed qD;
+	qD.resetCount();
+	size_t t0 = time(NULL);
+	new_encoder_pk_t pk;
+	for(;;)
+	//while((time(NULL) - t0) < 10)
+	{
+		double fr,fl, br,bl;
+		if(!qD.getEncoderVel(fr,fl,br,bl))
+		{
+			std::cout << "fr: " << fr << " fl: " << fl << " br: " << br << " bl: " << bl << std::endl;
+			std::cout << std::endl;
+		}
+
+		if(reset)
+		{
+			reset = false;
+			qD.resetCount();
+		}
+
+		usleep(1e5);
+	}
+	//if(!qD.getEncoderState(pk))
+	//{
+	//	std::cout << pk << std::endl;
+	//}
+
+}
