298f1c890a7c4f9d6deb631e2985815fb990b628
Jacob Schloss <jacobschloss@gmail.com>
2011-05-16 18:56:58 -0400

    add new gps library and test function

---
+++ 2011/software/gps/gps_test.cc
@@ -0,0 +1,18 @@
+#include "gps.hpp"
+
+#include <iostream>
+
+int main()
+{
+	gps gpsA;
+
+	gpsA.open("/dev/ttyUSB0", 38400);
+	gpsA.start();
+
+	while(true)
+	{
+		gps::GPSState state = gpsA.get_last_pos();
+		std::cout << "lat: " << state.lat << "\tlon: " << state.lon << std::endl;
+	}
+
+}
