28ad11f3c0957834d8b13036931eac88bb11c961
RoboJackets <robojackets@robojackets-igvc.(none)>
2011-05-17 02:16:44 -0400

    async io

--- 2011/software/gps/gps.cc
+++ 2011/software/gps/gps.cc
@@ -52,31 +52,96 @@ bool gps::get_last_pos(GPSState& state)
 	return true;
 }
 
-bool gps::open(char* port, size_t baud)
+bool gps::open(const char* port, size_t baud)
 {
-	gps_port.open(port);
+	m_port = port;
+	m_baud = baud;
+
+	try
+	{
+		gps_port.open(port);
+	}
+	catch(...)
+	{
+		std::cerr << "Failed to open serial port" << std::endl;
+		return false;
+	}
 
 	if(!gps_port.is_open())
 	{
 		return false;
 	}
 	
-	gps_port.set_option(boost::asio::serial_port_base::baud_rate(baud));
-	//gps_port.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
-	gps_port.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
-	gps_port.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
+	try
+	{
+		gps_port.set_option(boost::asio::serial_port_base::baud_rate(baud));
+		//gps_port.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
+		gps_port.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
+		gps_port.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
+	}
+	catch(...)
+	{
+		return false;
+	}
+	
+	m_connected = true;
 
 	return true;
+}
+
+void gps::close()
+{
+	gps_port.close();
+	m_connected = false;
+}
+
+void gps::handle_serial_read(const boost::system::error_code& ec, size_t len)
+{
+	readPending = false;
 }
 
 void gps::gps_comm()
 {
 	while(running)
 	{
+		std::string line;
+		try
+		{
+			time_t t1 = time(NULL);
+			readPending = true;
+			boost::asio::async_read_until(gps_port, comm_buffer, '\n', boost::bind(&gps::handle_serial_read, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+			do
+			{
+				time_t t2 = time(NULL);
+
+				if((t2 - t1) > SERIAL_TIMOUT_SEC)
+				{
+					std::cout << "serial timeout" << std::endl;
+					gps_port.cancel();
+					do
+					{
+					reconnect();
+					} while(!m_connected);					
+					continue;
+				}
+
+				io_service.poll();
+				usleep(100);
+			} while(readPending);
+			io_service.reset();
+
 		boost::asio::read_until(gps_port, comm_buffer, '\n');
 		std::istream is(&comm_buffer);
-		std::string line;
+		
 		std::getline(is, line);
+		}
+		catch(...)
+		{
+			do
+			{
+				reconnect();
+			} while(!m_connected);
+		}
 
 		try
 		{
@@ -93,8 +158,20 @@ void gps::gps_comm()
 		catch(...)
 		{
 			std::cerr << "Error parsing GPS packet!" << std::endl;
+			std::cerr << "Reconnecting" << std::endl;
+			do
+			{
+				reconnect();
+			} while(!m_connected);
 		}
 	}
+}
+
+void gps::reconnect()
+{
+	close();
+	usleep(1e6);
+	open(m_port.c_str(), m_baud);
 }
 
 bool gps::parse_message(const std::string& line, GPSState& state)
--- 2011/software/gps/gps.hpp
+++ 2011/software/gps/gps.hpp
@@ -15,27 +15,37 @@ class gps
 	gps();
 	~gps();
 
-	bool open(char* port, size_t baud);
+	bool open(const char* port, size_t baud);
+	void close();
 	void start();
 	void stop();
 
 	bool get_last_pos(GPSState& state);
 
 	private:
-	bool running;
+	volatile bool running;
 
 	boost::asio::streambuf comm_buffer;
 	void gps_comm();
+	void handle_serial_read(const boost::system::error_code& ec, size_t len);
+
+	void reconnect();
 
 	bool parse_message(const std::string& line, GPSState& state);
 
 	boost::thread iothread;
 	boost::asio::io_service io_service;
 	boost::asio::serial_port gps_port;
+	volatile bool m_connected;
+	volatile bool readPending;
+	std::string m_port;
+	size_t m_baud;
 
 	boost::mutex state_queue_mutex;
 	std::list<GPSState> state_queue;
 	size_t queue_len;
+
+	const static size_t SERIAL_TIMOUT_SEC = 2;
 };
 
 #endif
