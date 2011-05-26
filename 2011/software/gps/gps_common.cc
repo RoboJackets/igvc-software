d0e0b3505d990e928ab7af7873a7adb78b09e41d
Kenny <kenny@KennyUbuntu.(none)>
2011-05-26 01:14:11 -0400

    Added method

--- 2011/software/gps/gps_common.cc
+++ 2011/software/gps/gps_common.cc
@@ -5,7 +5,7 @@
 //distance in meters
 double lambert_distance(const GPSState& a, const GPSState& b)
 {
-	static const double eq_rad = 6378.1e3;
+	static const double eq_rad = 6378.137e3;
 	static const double r = 298.257223563;
 
 	const double psia = atan2( (r - 1.0) * tan(a.lat) , r );
@@ -31,5 +31,16 @@ double lambert_distance(const GPSState& a, const GPSState& b)
 
 	const double d = eq_rad * (sigma - (X+Y) / (2.0*r));
 
+	return d;
+}
+
+double haversine_distance(const GPSState& a, const GPSState& b)
+{
+	static const double r = 6378.137e3;	
+	double dlat = b.lat - a.lat;
+	double dlon = b.lon - a.lon;
+	double a1 = sin(dlat/2.0)*sin(dlat/2.0)+cos(a.lat)*cos(b.lat)*sin(dlon/2.0)*sin(dlon/2.0);
+	double c = 2*atan2(sqrt(a1),sqrt(1-a1));
+	double d = r*c;
 	return d;
 }
