616c6e3c9d8049fa6c975c81f7930c37c77c9aac
Jacob Schloss <nebousuru@gpsdev.(none)>
2011-06-01 21:31:22 -0700

    reassign pin for light

--- 2011/software/motors/osmc/arduino/pinDefs.hpp
+++ 2011/software/motors/osmc/arduino/pinDefs.hpp
@@ -8,7 +8,8 @@ const int rightCurrentADCPin	= 4;
 /** SPI **/
 //#define SPI_MOSI 				/*13*/
 const int SPI_MISO			= 7;/*12*/
-const int SPI_CLK			= 6;/*11*/
+//const int SPI_CLK			= 6;/*11*/
+const int SPI_CLK			= 13;/*11*/
 const int SPI_SS_LEFT_MOTOR_ENCODER	= 5;/*10*/
 const int SPI_SS_RIGHT_MOTOR_ENCODER	= 4;/*9*/
 
@@ -24,7 +25,7 @@ const int joystickXADC = 0;
 const int joystickYADC = 1;
 const int joystickEnable = 8;
 
-const int LIGHT_PIN_LB = 13;
+const int LIGHT_PIN_LB = 6;
 
 /*
 const int rightDirectionPin = 4;
