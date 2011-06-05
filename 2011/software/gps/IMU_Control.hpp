629bea68d5fb668279aa5c53b82d64f1859b24ee
Jacob Schloss <nebousuru@aether.(none)>
2011-06-05 13:34:26 -0400

    incr

--- 2011/software/gps/IMU_Control.hpp
+++ 2011/software/gps/IMU_Control.hpp
@@ -19,7 +19,7 @@ Call heading() to get the heading
 class IMU_Control
 {
 
-	IMU_Control(gps& g, gyro& i) : gpsref(g), imuref(i), imu_off(0), update_interval(0)
+	IMU_Control(gps& g, gyro& i) : gpsref(g), gyroref(i), imu_off(0), update_interval(0)
 	{
 
 	}
@@ -31,16 +31,15 @@ class IMU_Control
 
 	private:
 
-	double simpletime()
+	inline double simpletime()
 	{
 		timeval now;
 		gettimeofday(&now, NULL);
-
-		return now.tv_sec + now.tv_usec
+		return double(now.tv_sec) + double(now.tv_usec) / 1e6;
 	}
 
 	gps& gpsref;
-	gyro& imuref;
+	gyro& gyroref;
 
 	double last_time;// last time we updated [s]
 	static const double min_update_wait;//shortest IMU correct interval [s]
