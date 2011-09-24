7319b9a9109644b7f4461b29f0293aed07de0982
Andrey Kurenkov <andrey@andrey-VirtualBox.(none)>
2011-09-24 15:09:52 -0400

    added another encoder testing program to check some stuff

---
+++ 2012/software/encoders/encodertester/encoderTesting2.cc
@@ -0,0 +1,36 @@
+
+#define encoderPinA 4
+#define encoderPinB 6
+#define encoderOutputI 5
+
+int counter=0;
+int stateA;
+int stateB;
+int lastStateA=LOW;
+int lastStateB=LOW;
+
+int main()
+{
+init();
+pinMode(encoderPinA,INPUT);
+pinMode(encoderPinB,INPUT);
+loopTest();
+return 0;
+}
+
+void loopTest()
+{
+while(true)
+{
+stateA=digitalRead(encoderPinA);
+stateB=digitalRead(encoderPinB);
+if(stateA=!lastStateA && encoderPinB!=lastStateB)
+{
+counter++;
+lastStateA=stateA;
+lastStateB=stateB;
+}
+
+printf("%d\n",counter);
+}
+}
