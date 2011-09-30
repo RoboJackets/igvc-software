8ac1bdb572f6a1e152304356b703f418a73f3ad5
Kenny <kenny@KennyUbuntu.(none)>
2011-09-30 08:16:20 -0400

    Added more functions, created seperate file for vector operations, and commented potential fields code

---
+++ 2012/software/roxi/potentialfields/commonVecOps.cc
@@ -0,0 +1,65 @@
+#include "commonVecOps.hpp"
+
+/* Converts the x and y components to a vector with magnitude and angle. Angle given by 0-360 with 0 at North */
+void xyToVec(double x, double y, double& mag, double& ang)
+{
+	mag = sqrt(x*x + y*y);
+	ang = rad2deg(atan2(y,x));
+	ang = vec2bear(ang);
+	return;
+}
+
+/* Converts a vector with magnitude and angle to x and y components. Angle given by 0-360 with 0 at North */
+void VecToxy(double mag, double ang, double& x, double& y)
+{
+	x = mag * cos(deg2rad(ang));
+	y = mag * cos(rad2deg(ang));
+	return;
+}
+
+/* Adds an array of vectors together */
+void AddVecs(double* xvals, double* yvals, int numVecs, double& xnet, double& ynet)
+{
+	xnet = ynet = 0;
+	for (int i = 0; i < numVecs; i++)
+	{
+		xnet += xvals[i];
+		ynet += yvals[i];
+	}
+	return;
+}
+
+/* Converts radians to degrees */
+double rad2deg(double radians)
+{
+	return (radians*180)/M_PI;
+}
+
+/* Converts degrees to radians */
+double deg2rad(double degrees)
+{
+	return (degrees*M_PI)/180;
+}
+
+/* Converts angle ccw from due East to angle cw from due North */
+double vec2bear(double ang)
+{
+	double bearing = (-ang+90);
+	while(ang < 0)
+	{
+		ang += 360;
+	}
+	while(ang > 360)
+	{
+		ang -= 360;
+	}
+	return ang;
+}
+
+/* Returns distance between two coordinates */
+double Distance2D(double x1, double y1, double x2, double y2)
+{
+	double dx = x2 - x1;
+	double dy = y2 - y1;
+	return sqrt(dx*dx + dy*dy);
+}
