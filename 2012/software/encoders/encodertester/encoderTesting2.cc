47c8dcffdaa942e10c99d15f8c8b4570ca80083b
Andrey Kurenkov <andrey@andrey-VirtualBox.(none)>
2011-09-30 03:45:36 -0400

    Changed encodertesting2 to make it build and produce valuable info. Also added basic encoder odometry code.

--- 2012/software/encoders/encodertester/encoderTesting2.cc
+++ 2012/software/encoders/encodertester/encoderTesting2.cc
@@ -4,28 +4,44 @@
 #define encoderPinB 6
 #define encoderOutputI 5
 
-int counter=0;
+int countA;
+int countB;
 int stateA;
 int stateB;
 int lastStateA=LOW;
 int lastStateB=LOW;
 
+int main()
+{
+	setup();
+	while(1)
+		loop();
+	return 0;
+}
 void setup()
 {
-	serial.Begin(57600);
+	Serial.begin(57600);
 	pinMode(encoderPinA,INPUT);
 	pinMode(encoderPinB,INPUT);
+	stateA=lastStateA=digitalRead(encoderPinA);
+	stateB=lastStateB=digitalRead(encoderPinB);
 }
 
 void loop()
 {
 	stateA=digitalRead(encoderPinA);
 	stateB=digitalRead(encoderPinB);
-	if(stateA=!lastStateA && encoderPinB!=lastStateB)
+	if(stateA==HIGH && lastStateA!=stateA)
 	{
-		counter++;
-		lastStateA=stateA;
-		lastStateB=stateB;
+		countA++;
 	}
-	Serial.println(analogValue,counter);
+	if(stateB==HIGH && lastStateB!=stateB)
+	{
+		countB++;
+	}
+	lastStateA=stateA;
+	lastStateB=stateB;
+	Serial.println(countA);
+	Serial.println(countB);
+	Serial.println(0);//lame way to seperate pairs of value due to uncertainty as to how method works
 }
