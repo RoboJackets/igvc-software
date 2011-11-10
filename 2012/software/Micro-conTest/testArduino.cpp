33dc36fb2c65d3145d4b5d5a418500f38872312c
RoboJackets <robojackets@rj-elect-01>
2011-11-09 19:37:59 -0500

    Altered test.cc and encoder main file to interface correctly with encoders

--- 2012/software/Micro-conTest/testArduino.cpp
+++ 2012/software/Micro-conTest/testArduino.cpp
@@ -10,12 +10,14 @@ int analogPins[]={3,5,6,9,10,11};
 while(pin<14)
 {
 testDigital(pin);
+pin++;
 }
 pin=0;
-while(pin<6)
+/*while(pin<6)
 {
 testAnalog(analogPins[pin]);
-}
+pin++;
+}*/
 
 return 0;
 }
@@ -24,16 +26,16 @@ void testDigital(int pin)
 {
 pinMode(pin,OUTPUT);
 digitalWrite(pin,HIGH);
-delay(5000);
-digitalWrite(pin,LOW);
-pin++;
+//delay(5);
+//digitalWrite(pin,LOW);
+
 }
 
-void testAnalog(int pin)
+/*void testAnalog(int pin)
 {
 analogWrite(pin,200);
 delay(5000);
 analogWrite(pin,0);
-pin++;
 }
+*/
 
