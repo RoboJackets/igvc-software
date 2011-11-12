12628e2f0326280edfc6300f112382fac02b3485
Andrey Kurenkov <andrey@andrey-VirtualBox.(none)>
2011-11-12 08:53:58 -0500

    adding robot position stuff with relatively few updates - does not build so cmakelists are commented out

--- 2012/software/roxi/RobotPosition/RobotPosition.cpp
+++ 2012/software/roxi/RobotPosition/RobotPosition.cpp
@@ -1,7 +1,8 @@
 #include "RobotPosition.hpp"
-
+#define updatesUntilReset 100
 RobotPosition::RobotPosition(OSMC_4wd_driver* driver, gps& gpsObject, IMU_Control& imuObject)
 {
+	updatesSinceReset=0;
 	x=0;	
 	y=0;
 	z=0;
@@ -10,13 +11,41 @@ RobotPosition::RobotPosition(OSMC_4wd_driver* driver, gps& gpsObject, IMU_Contro
 	IMU=&imuObject;
 	MagnetometerTracking magnetometer(driver);
 	gpsA=&gpsObject;
+	gpsA.get_last_state(&gpsFirstState);
 }
 
 void RobotPosition::update()
 {
-	//GPS.update();
-	//encoder.update();
-	//IMU.update();
-	//magnetometer.update();
+	GPSState lastState;
+	gpsA.get_last_state(&lastState);
+	double xGPS;
+	double yGPS;
+	lambert_distance_xy(&gpsFirstState,&lastState,&xGPS,&yGPS);
 
+
+	encoder.update();
+	IMU.update();
+	double xDeltaEncoder=encoder.getX();
+	double yDeltaEncoder=encoder.getY();
+	double angleDeltaEncoder=encoder.getAngle();
+	magnetometer.update();
+	double magBearing=magnetometer.getBearing();
+	x+=(xDeltaEncoder*encoderWeight+xGPS*GPSWeights)/2;
+	y+=(yDeltaEncoder*encoderWeight+yGPS*GPSWeight)/2;
+	
+	encoder.reset();
+	bearing=magBearing;
+}
+
+double RobotPosition::getX()
+{
+return x;
+}
+double RobotPosition::getY()
+{
+return y;
+}
+double getBearing()
+{
+return bearing;
 }
