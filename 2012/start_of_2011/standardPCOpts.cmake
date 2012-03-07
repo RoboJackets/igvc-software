9adf283786e7ef154bcd78c82aff37587ee248d2
Kenneth Marino <kmarino3@gatech.edu>
2012-03-06 19:08:13 -0500

    Deleted start_of_2011 folder

--- 2012/start_of_2011/standardPCOpts.cmake
+++
@@ -1,8 +0,0 @@
-
-IF((${ROBOT_REV} MATCHES "candiiidbg") OR (${ROBOT_REV} MATCHES "jeannidbg"))
-	SET(CMAKE_C_FLAGS  "-march=native -pipe -Wall -Wstrict-prototypes -Wextra -g3 -O0 -pthread")
-	SET(CMAKE_CXX_FLAGS "-march=native -pipe -std=gnu++0x -Wall -Wextra -g3 -O0 -pthread")
-ELSEIF( (${ROBOT_REV} MATCHES "candiii") OR (${ROBOT_REV} MATCHES "jeanni"))
-	SET(CMAKE_C_FLAGS  "-march=native -pipe -Wall -Wstrict-prototypes -Wextra -Wuninitialized -Werror=uninitialized -g3 -O2 -pthread")
-	SET(CMAKE_CXX_FLAGS "-march=native -pipe -std=gnu++0x -Wall -Wextra -Wuninitialized -Werror=uninitialized -g3 -O2 -pthread")
-ENDIF((${ROBOT_REV} MATCHES "candiiidbg") OR (${ROBOT_REV} MATCHES "jeannidbg"))
