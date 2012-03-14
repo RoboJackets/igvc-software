17cb64fe16a37fc3e69061fc706f7427ef2f279b
Andrey Kurenkov <andrey@andrey-VirtualBox.(none)>
2012-03-13 22:10:20 -0400

    Progress on Lidar stuffs

--- 2012/software/lidar/lidarWithVision.cc
+++ 2012/software/lidar/lidarWithVision.cc
@@ -2,11 +2,6 @@
 
 const int RADIUS=3;
 
-struct point{
-	int x;
-	int y;
-};
-
 int height;
 int width;
 
@@ -18,8 +13,8 @@ IplImage correctVisionWithLidar(float lidarX[], float lidarY[], IplImage* vision
 	width=visionImage->width;
 	int channels=visionImage->nChannels;
 	int imageArraySize=width*height;
-	bool lidarBitMap[height*weight];
-	std:fill_n(lidarBitMap,sizeof(lidarBitMap)/sizeof(bool),false);
+	bool lidarBitMap[height*width];
+	std::fill_n(lidarBitMap,sizeof(lidarBitMap)/sizeof(bool),false);
 //Construct blotted pixel representation of lidar places
 	for(int i=0;0<sizeof(lidarX)/sizeof(float);i++){
 		for(int y=(int)lidarY[i]-RADIUS/2;y<(int)lidarY[i]+RADIUS;y++){
@@ -27,36 +22,81 @@ IplImage correctVisionWithLidar(float lidarX[], float lidarY[], IplImage* vision
 					if((y*width+x)<imageArraySize && (y*width+x)>0)
 						lidarBitMap[y*width+x]=true;
 				}
-			}	
-		}
-	}
+			}
+        }
+		
+
+	
 	//Construct checked array with all starting of as unchecked
-	
-	for(int i=0;i<sizeof(lidarBitMap)/sizeof(bool),i++){
-	if(lidarBitMap[i]){
-		bool checked[imageArraySize];
-		std:fill_n(checked,sizeof(checked)/sizeof(bool),false);
-		std::vector <Point> shape;
-		buildShape(i%width,i/width,checked,&shape);
-		eraseShadow(shape, &visionImage);
-
+	for(int i=0;i<sizeof(lidarBitMap)/sizeof(bool);i++){
+                if(lidarBitMap[i]){
+                	bool checked[imageArraySize];
+                	std::fill_n(checked,sizeof(checked)/sizeof(bool),false);
+                        Shape presentShape;
+                        buildShape(i%width,i/width,checked,lidarBitMap,&presentShape);
+                        eraseShapeOnBitmap(presentShape,lidarBitMap);
+                        eraseShadow(presentShape, &visionImage);
 	}
+	}	
+}
+
+
+void buildShape(int currX,int currY, bool* checked, bool* bitMap,std::vector<Point>* currShape){
+        Point currPoint={currX,currY};
+	checked[currX+currY*width]=true;
+        
+	if(bitMap[currX+currY*width]){
+            addPointToShape(currPoint,currShape);
+		if(!checked[(currX+1)+currY*width]){
+                    buildShape(currX+1,currY, checked, bitMap,currShape);
+		}
+		if(!checked[currX+(currY+1)*width]){
+                    buildShape(currX,currY+1, checked, bitMap,currShape);
+		}
+		if(!checked[(currX+1)+(currY+1)*width]){
+                    buildShape(currX+1,currY+1, checked, bitMap,currShape);
+		}
+		if(!checked[(currX-1)+(currY)*width]){
+                    buildShape(currX-1,currY, checked, bitMap,currShape);
+		}
+                 if(!checked[currX+(currY-1)*width]){
+                     buildShape(currX,currY-1, checked, bitMap,currShape);
+		}
+		if(!checked[(currX-1)+(currY-1)*width]){
+                    buildShape(currX-1,currY-1, checked, bitMap,currShape);
+		}
+		if(!checked[(currX+1)+(currY-1)*width]){
+                     buildShape(currX+1,currY-1, checked, bitMap,currShape);
+		}
+		if(!checked[(currX-1)+(currY+1)*width]){
+                     buildShape(currX-1,currY+1, checked, bitMap,currShape);
+		}	
 	}
-	
 }
 
+void addPointToShape(Point toAdd, Shape* addTo){
+	addTo->points.push_back(toAdd);
+	addTo->totalX+=toAdd.x;
+	addTo->totalY+=toAdd.y;
+	if(toAdd.x>addTo->maxX)
+		addTo->maxX=toAdd.x;
+	if(toAdd.y>addTo->maxY)
+		addTo->maxY=toAdd.y;
+	if(toAdd.x<addTo->minX)
+		addTo->minX=toAdd.x;
+	if(toAdd.y<addTo->minY)
+		addTo->minY=toAdd.y;
+}
 
-void buildShape(int currX,int currY, bool* checked, vector<Point>* currShape){
+void eraseShapeOnBitmap(Shape shape,bool** eraseFrom){
+    for(int i=0;i<shape.numPoints;i++){
+        (*eraseFrom)[shape.points.at(i).x+shape.points.at(i).y*width]=false;
+    }
+}
+
+void eraseShadow(Shape shape, IplImage* img){
 
 
 }
 
-void eraseShapeOnBitmap(vector<Point> shape,bool** eraseFrom){
-
-}
-
-void eraseShadow(vector<Point> shape, IplImage* eraseFrom){
-
-
-}
 
--- 2012/software/lidar/lidarWithVision.hpp
+++ 2012/software/lidar/lidarWithVision.hpp
@@ -7,7 +7,29 @@
 #include <opencv/cvinternal.h>
 #include <vector>
 
+typedef struct shape{
+	std::vector <Point> points;
+	int numPoints;
+	float avgX;
+	float avgY;
+	int totalX;
+	int totalY;
+	int maxX;
+	int minX;
+	int maxY;
+	int minY;
+}Shape;
+
+typedef struct point{
+	int x;
+	int y;
+}Point;
+
 /*
 take in cartesian points from the lidar, create ipl image of lidar points, erase shadows from camera image.
 */
 IplImage correctVisionWithLidar(float lidarX[], float lidarY[], IplImage* visionImage);
+void buildShape(int currX,int currY, bool* checked, bool* bitMap,std::vector<Point>* currShape);
+void addPointToShape(Point point, Shape* addTo);
+void eraseShapeOnBitmap(std::vector<Point> shape,bool** eraseFrom);
+void eraseShadow(Shape shape, IplImage img);
