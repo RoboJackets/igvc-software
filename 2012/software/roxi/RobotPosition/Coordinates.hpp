d23b836be668b383ffdf0d59795074c3f7dcb052
Andrey Kurenkov <andrey@andrey-laptop.(none)>
2012-04-10 20:24:28 -0400

    Added coordinate changing code, will make RobotPosition a singleton soon

---
+++ 2012/software/roxi/RobotPosition/Coordinates.hpp
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <sys/time.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include "RobotPosition.hpp"
+CvMat* getRobotToWorldMat();
+CvMat* getWorldToRobotMat();
+
+
+
+
