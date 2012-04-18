5538ae3485eaac2bfb90d9cfcba2e17f5006bfb4
Andrey Kurenkov <andrey@andrey-laptop.(none)>
2012-04-17 20:30:43 -0400

    Some additions for supporting more than just matrix convertion

--- 2012/software/roxi/RobotPosition/Coordinates.hpp
+++ 2012/software/roxi/RobotPosition/Coordinates.hpp
@@ -5,7 +5,12 @@
 #include "RobotPosition.hpp"
 CvMat* getRobotToWorldMat();
 CvMat* getWorldToRobotMat();
-
+CvMat* getRobotToWorldMat(RobotPosition robotAt);
+CvMat* getWorldToRobotMat(RobotPosition robotAt);
+void convertRobotToWorld(double *x, double *y, RobotPosition pos);
+void convertWorldToRobot(double *x, double *y, RobotPosition pos);
+void convertRobotToWorld(double *x, double *y);
+void convertWorldToRobot(double *x, double *y);
 
 
 
