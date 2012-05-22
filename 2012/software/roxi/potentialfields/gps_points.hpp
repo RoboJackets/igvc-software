ab430973a965c6ba038d93f669d13c499cd74165
= <kmarino3@gatech.edu>
2012-05-22 15:42:21 -0400

    minor changes to get RobotPosition compiling

--- 2012/software/roxi/potentialfields/gps_points.hpp
+++ 2012/software/roxi/potentialfields/gps_points.hpp
@@ -1,8 +1,17 @@
 #ifndef _GPS_POINTS_H_
 #define _GPS_POINTS_H_
+
+#define GPS_POINTS FourteenthStreet
 static const int num_GPS_Goals = 4;
-//static const double goalWaypointLat[] = {42.6781012505,42.67847883,42.6789,42.6793};
-//static const double goalWaypointLon[] = {-83.1948615905,-83.19472683,-83.1945,-83.1943};
+#if GPS_POINTS == FourteenthStreet
+static const double goalWaypointLat[] = {33.787433};
+static const double goalWaypointLon[] = {-84.406306};
+#elif GPS_POINTS == Competition2011
+static const double goalWaypointLat[] = {42.6781012505,42.67847883,42.6789,42.6793};
+static const double goalWaypointLon[] = {-83.1948615905,-83.19472683,-83.1945,-83.1943};
+#else
 static const double goalWaypointLat[] = {42.70};
 static const double goalWaypointLon[] = {-83.194};
+#endif
+
 #endif
