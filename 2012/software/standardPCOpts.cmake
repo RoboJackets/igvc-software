6e02d91c85e26cd2de5588aa48430e800cad4af9
= <kmarino3@gatech.edu>
2012-05-31 19:18:15 -0400

    allow profiling

--- 2012/software/standardPCOpts.cmake
+++ 2012/software/standardPCOpts.cmake
@@ -1,7 +1,7 @@
 
 IF((${ROBOT_REV} MATCHES "candiiidbg") OR (${ROBOT_REV} MATCHES "jeannidbg"))
-	SET(CMAKE_C_FLAGS  "-march=native -pipe -Wall -Wstrict-prototypes -Wextra -ggdb3 -O0 -pthread -DOSMC_4WD -fopenmp")
-	SET(CMAKE_CXX_FLAGS "-march=native -pipe -Wall -Wextra -ggdb3 -O0 -pthread -DOSMC_4WD  -fopenmp -rdynamic -g")
+	SET(CMAKE_C_FLAGS  "-march=native -pipe -Wall -Wstrict-prototypes -Wextra -ggdb3 -O0 -pthread -DOSMC_4WD -fopenmp -p")
+	SET(CMAKE_CXX_FLAGS "-march=native -pipe -Wall -Wextra -ggdb3 -O0 -pthread -DOSMC_4WD  -fopenmp -rdynamic -g -p")
 ELSEIF( (${ROBOT_REV} MATCHES "candiii") OR (${ROBOT_REV} MATCHES "jeanni"))
 	SET(CMAKE_C_FLAGS  "-march=native -pipe -Wall -Wstrict-prototypes -Wextra -Wuninitialized -Werror=uninitialized -ggdb3 -O2 -pthread -DOSMC_4WD  -fopenmp")
 #	SET(CMAKE_CXX_FLAGS "-march=native -pipe -std=gnu++0x -Wall -Wextra -Wuninitialized -Werror=uninitialized -g3 -O2 -pthread")
