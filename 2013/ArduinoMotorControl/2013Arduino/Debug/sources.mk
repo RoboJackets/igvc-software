caeb80e43fa8adbff13adde0291258599111cf5c
alex <alexander.trimm@gmail.com>
2012-11-03 13:31:38 -0400

    Added Intial Test Code for Arduino Motor Control

---
+++ 2013/ArduinoMotorControl/2013Arduino/Debug/sources.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+O_SRCS := 
+CPP_SRCS := 
+C_UPPER_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+S_SRCS := 
+OBJ_SRCS := 
+ASM_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+CC_SRCS := 
+OBJS := 
+C_DEPS := 
+ELFS := 
+LSS := 
+S_DEPS := 
+CXX_DEPS := 
+S_UPPER_DEPS := 
+C++_DEPS := 
+ASM_DEPS := 
+CC_DEPS := 
+CPP_DEPS := 
+C_UPPER_DEPS := 
+SIZEDUMMY := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+
