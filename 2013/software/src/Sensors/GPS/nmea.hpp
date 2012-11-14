a0f0710025f66cfe637d41c514778f8606361b37
Matthew Barulic <matthew.barulic@gmail.com>
2012-11-13 22:12:24 -0500

    Bug fixes in the GPS code

--- 2013/software/src/Sensors/GPS/nmea.hpp
+++ 2013/software/src/Sensors/GPS/nmea.hpp
@@ -15,6 +15,9 @@ namespace nmea
 	bool decodeGPGGA(const std::string& line, GPSState& state);
 	bool decodeGPGSA(const std::string& line);
 	bool decodeGPGSV(const std::string& line);
+	void decodeUTCTime(const std::string& val);
+	double decodeLatitude(const std::string& val, const char hemi);
+	double decodeLongitude(const std::string& val, const char hemi);
 }
 
 #endif
