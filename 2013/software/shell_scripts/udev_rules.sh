5a12ba317122f1dff9021417e2fe0fa6772437de
Matthew Barulic <matthew.barulic@gmail.com>
2012-11-20 20:08:38 -0500

    Fixed permissions bug in udev install script

--- 2013/software/shell_scripts/udev_rules.sh
+++ 2013/software/shell_scripts/udev_rules.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Moves file containing udev rules for addressing sensors to appropriate folder
 
-cp igvc.rules /etc/udev/rules.d/
+sudo cp igvc.rules /etc/udev/rules.d/
 sudo service udev restart
 exit
 $SHELL
