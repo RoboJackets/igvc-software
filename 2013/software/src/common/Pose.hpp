6768c5ee75727b5de0dc09417ed9e619195d2789
Matthew Barulic <matthew.barulic@gmail.com>
2012-11-30 21:42:32 -0500

    Beginning to work on mapping datatypes.

---
+++ 2013/software/src/common/Pose.hpp
@@ -0,0 +1,21 @@
+/*
+ * Pose.hpp
+ *
+ *  Created on: Nov 30, 2012
+ *      Author: Matthew Barulic
+ */
+
+#ifndef POSE_HPP_
+#define POSE_HPP_
+
+namespace IGVC {
+
+class Pose {
+public:
+	float x, y, theta;
+};
+
+}
+
+
+#endif /* POSE_HPP_ */
