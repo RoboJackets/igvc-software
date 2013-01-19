40894db8de8e63f87fffd067213fc595f16e6eab
Matthew Barulic <matthew.barulic@gmail.com>
2013-01-19 16:42:16 -0500

    Added udev rules for 2012 robot.

--- 2013/software/src/shell_scripts/install_udev_rules.sh
+++ 2013/software/src/shell_scripts/install_udev_rules.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Moves file containing udev rules for addressing sensors to appropriate folder
 
-sudo cp igvc.rules /etc/udev/rules.d/
+sudo cp ../udev/igvc_2012.rules /etc/udev/rules.d/
 sudo service udev restart
 exit
 $SHELL
