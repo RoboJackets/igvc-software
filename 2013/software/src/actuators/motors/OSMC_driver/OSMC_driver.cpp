18cbce60c281a7140a9b6e5482ccd82cf6755f79
Al Chaussee <alchaussee@ubuntu.(none)>
2013-01-19 17:52:50 -0500

    added functions for old robot

--- 2013/software/src/actuators/motors/OSMC_driver/OSMC_driver.cpp
+++ 2013/software/src/actuators/motors/OSMC_driver/OSMC_driver.cpp
@@ -1,24 +1,25 @@
 #include <string.h>
 
-#include "ASIOSerialPort.h"
+#include "serial/ASIOSerialPort.h"
 #include <iostream>
-#include "OSMC_driver.hpp"
+#include "actuators/motors/OSMC_driver/OSMC_driver.hpp"
 
 using namespace std;
 
-ASIOSerialPort arduinoLeft("/dev/arduinoLeft", 9600);
-ASIOSerialPort arduinoRight("/dev/arduinoRight", 9600);
-ASIOSerialPort arduinoEncoder("/dev/arduinoEncoder", 9600);
+ASIOSerialPort arduinoLeft("/dev/igvc_2012_left_motor_shield", 9600);
+ASIOSerialPort arduinoRight("/dev/igvc_2012_right_motor_shield", 9600);
+ASIOSerialPort arduinoEncoder("/dev/igvc_2012_right_encoder_shield", 9600);
 
 //TODO: Need to be checked
-const byte OSMC_driver::maxPwm = 130;
-const byte OSMC_driver::minPwm = 30;
+const char OSMC_driver::maxPwm = 130;
+const char OSMC_driver::minPwm = 30;
 
 OSMC_driver::~OSMC_driver()
 {
 	stopMotors();
 	arduinoRight.close();
 	arduinoLeft.close();
+	arduinoEncoder.close();
 }
 
 bool OSMC_driver::arduinoCheck()	//Checks we are connected and able to send and receive data from the arduinos
@@ -41,44 +42,42 @@ bool OSMC_driver::arduinoCheck()	//Checks we are connected and able to send and 
 	}
 }
 
-void OSMC_driver::setPwm(byte pwm, byte dir)	//writes pwm to arduinos setting both motors to the same pwm and direction (0 = forwards, 1 = backwards)
+void OSMC_driver::setPwm(char pwm, char dir)	//writes pwm to arduinos setting both motors to the same pwm and direction (0 = forwards, 1 = backwards)
 {
 	checkPwm(pwm, dir);
-	arduinoLeft.write("W");
-	arduinoRight.write("W");
-	arduinoLeft.write(dir);
-	arduinoRight.write(dir);
-	arduinoLeft.write(pwm);
-	arduinoRight.write(pwm);
+	arduinoLeft.write("WS" + dir + pwm);
+	arduinoRight.write("WS"+dir+pwm);
 }
 
-void OSMC_driver::setMotorsPwm(byte pwmLeft, byte dirLeft, byte pwmRight, byte dirRight)		//writes pwmLeft and pwmRight to respective arduinos
+void OSMC_driver::setMotorsPwm(char pwmLeft, char dirLeft, char pwmRight, char dirRight)		//writes pwmLeft and pwmRight to respective arduinos
 {
-	checkPwm(pwmLeft, pwmRight);
-	arduinoLeft.write("W");
-	arduinoRight.write("W");
-	arduinoLeft.write(dirLeft);
-	arduinoRight.write(dirRight);
-	arduinoLeft.write(pwmLeft);
-	arduinoRight.write(pwmRight);
+	checkPwm2(pwmLeft, pwmRight);
+	arduinoLeft.write("W" + dirLeft + pwmLeft);
+	arduinoRight.write("W"+dirRight+pwmRight);
 }
 
-void OSMC_driver::goTurn(int degree, byte dir)
+void OSMC_driver::goTurn(int degree, char dir)
 {
 
 }
 
-void OSMC_driver::goForward(double dist, byte pwm, byte dir)
+void OSMC_driver::goForward(double dist, char pwm, char dir)
 {
     checkPwm(pwm, dir);
+
+    std::ostringstream left;
+    left << dist << dir << pwm;
+//    std::cout<<s.str().c_str()<<endl;
+
     arduinoLeft.write("WD");
-    arduinoLeft.write(dist);
-    arduinoLeft.write(dir);
-    arduinoLeft.write(pwm);
+    arduinoLeft.write(left.str().c_str());
+
+    std::ostringstream right;
+    right << dist << dir << pwm;
+
     arduinoRight.write("WD");
-    arduinoRight.write(dist);
-    arduinoRight.write(dir);
-    arduinoRight.write(pwm);
+    arduinoRight.write(right.str().c_str());
+
     while(arduinoRight.readln() == "!" && arduinoLeft.readln() == "!")
     {
         break;
@@ -88,13 +87,14 @@ void OSMC_driver::goForward(double dist, byte pwm, byte dir)
 float OSMC_driver::readEncoder()
 {
     arduinoEncoder.write("R");
-    float dist = arduinoEncoder.readln();
+    std::string dist1 = arduinoEncoder.readln();
+    double dist = ::atof(dist1.c_str());
     return dist;
 }
 
 void OSMC_driver::encoderLoop(float totalDist)
 {
-    dist = readEncoder();
+    double dist = readEncoder();
     while (dist<totalDist)
     {
         dist = readEncoder();
@@ -102,15 +102,13 @@ void OSMC_driver::encoderLoop(float totalDist)
     stopMotors();
 }
 
-void OSMC_driver::goForwardOld(float totalDist, byte pwm, byte dir)
+void OSMC_driver::goForwardOld(float totalDist, char pwm, char dir)
 {
     checkPwm(pwm, dir);
-    arduinoLeft.write("WS");
-    arduinoLeft.write(dir);
-    arduinoLeft.write(pwm);
-    arduinoRight.write("WS");
-    arduinoRight.write(dir);
-    arduinoRight.write(pwm);
+
+    arduinoLeft.write("WS"+dir+pwm);
+    arduinoRight.write("WS"+dir+pwm);
+
     encoderLoop(totalDist);
 
 }
@@ -121,7 +119,7 @@ void OSMC_driver::stopMotors()		//Stops the motors
 	arduinoRight.write("S");
 }
 
-void OSMC_driver::checkPwm(byte pwm, byte dir)	//checks that the pwm is within the minPwm and maxPwm
+void OSMC_driver::checkPwm(char pwm, char dir)	//checks that the pwm is within the minPwm and maxPwm
 {
 	if (pwm < minPwm)
 	{
@@ -131,13 +129,9 @@ void OSMC_driver::checkPwm(byte pwm, byte dir)	//checks that the pwm is within t
 	{
 		setPwm(maxPwm, dir);
 	}
-	else
-	{
-		return;
-	}
 }
 
-void OSMC_driver::checkPwm(byte pwmLeft, byte pwmRight)	//checks that the pwm for the left and right are within the minPwm and maxPwm
+void OSMC_driver::checkPwm2(char pwmLeft, char pwmRight)	//checks that the pwm for the left and right are within the minPwm and maxPwm
 {
 	if (pwmLeft < minPwm or pwmRight< minPwm)
 	{
@@ -147,9 +141,6 @@ void OSMC_driver::checkPwm(byte pwmLeft, byte pwmRight)	//checks that the pwm fo
 	{
 		setPwm(maxPwm, 1);
 	}
-	else
-	{
-		return;
-	}
+
 }
 
