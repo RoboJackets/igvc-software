890da449ce2fbe1ceb22a076e6592925bfa7c408
alex <alex@WisdomCubeJr.(none)>
2013-01-21 12:53:24 -0500

    Added first version of abstract class for sensordata, template for past sensor data, and sensor interface

---
+++ 2013/software/src/sensors/DataArray.cpp
@@ -0,0 +1,27 @@
+/*
+#include "DataArray.h"
+//#include <boost/circular_buffer.hpp>
+
+template<class DataType>
+DataArray<DataType>::DataArray(int NumEl=100) : buff(boost::circular_buffer<DataType>(NumEl)), length(NumEl)
+{
+}
+
+template<class DataType>
+void DataArray<DataType>::push(DataType newData)
+{
+    buff.push_front(newData);
+}
+
+template<class DataType>
+DataType DataArray<DataType>::operator[] (int subscript)
+{
+    return buff[subscript];
+}
+
+template<class DataType>
+DataArray<DataType>::~DataArray()
+{
+    ~buff;
+}
+*/
---
+++ 2013/software/src/sensors/DataArray.h
@@ -0,0 +1,44 @@
+#ifndef DATAARRAY_H
+#define DATAARRAY_H
+#include <boost/circular_buffer.hpp>
+
+template <class DataType>
+class DataArray
+{
+    public:
+        DataArray(int);
+        void push(DataType);
+        DataType operator[](int);
+        virtual ~DataArray();
+
+    private:
+        boost::circular_buffer<DataType> buff;
+        int length;
+};
+
+
+template<class DataType>
+DataArray<DataType>::DataArray(int NumEl=100) : buff(boost::circular_buffer<DataType>(NumEl)), length(NumEl)
+{
+}
+
+template<class DataType>
+void DataArray<DataType>::push(DataType newData)
+{
+    buff.push_front(newData);
+}
+
+template<class DataType>
+DataType DataArray<DataType>::operator[] (int subscript)
+{
+    return buff[subscript];
+}
+
+template<class DataType>
+DataArray<DataType>::~DataArray()
+{
+    //TODO - find out if you need to destruct buff
+}
+
+
+#endif // DATAARRAY_H
