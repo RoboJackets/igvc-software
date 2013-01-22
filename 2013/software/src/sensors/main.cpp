c11081563c76b857a3c53af15bdca091ca7b3df5
atrimm <alexander.trimm@gmail.com>
2013-01-22 15:36:00 -0500

    Added new internal functionality to be built on in DataArray

--- 2013/software/src/sensors/main.cpp
+++ 2013/software/src/sensors/main.cpp
@@ -32,9 +32,10 @@ int main()
     //std::cout << "Longitude is "<< dat.Long() << " Latitude is "<< dat.Lat() << " Time is " << dat.time() << std::endl;
 
     DataArray<GPSData> GPSArray(100);
-    GPSData dat(49.12, 0.2122);
-    GPSArray.push(dat);
+    //GPSData dat(49.12, 0.2122);
+    //GPSArray.push(dat);
     GPSArray.push(GPSData(49.12, 0.2122));
-    std::cout << GPSArray[0].Lat() << "  but what is" <<  " Should be 49.12"<< std::endl;
+    std::cout << GPSArray[0].Lat() <<  " Should be 49.12"<< std::endl;
+    std::cout << GPSArray.size() << std::endl;
 
 }
