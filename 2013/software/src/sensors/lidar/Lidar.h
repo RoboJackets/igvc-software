fe5c9a1b3bc8857a4d1a837584625ac309c828e5
Alexander Huynh <a.huynh@gatech.edu>
2013-01-22 00:27:28 -0500

    Mimicked GPS class structure.

--- 2013/software/src/sensors/lidar/Lidar.h
+++ 2013/software/src/sensors/lidar/Lidar.h
@@ -1,14 +1,78 @@
 #ifndef LIDAR_H
 #define LIDAR_H
 
+#include <time.h>
+#include "events/EventGenerator.cpp"
 
-class Lidar
-{
+namespace IGVC {
+namespace Sensors {
+
+//class Lidar
+//{
+    //public:
+    class Point
+    {
+        public:
+            Point()
+            {
+                valid = false;
+                distance = 0;
+                intensity = 0;
+            }
+
+            // If false, the other members are not meaningful
+            bool valid;
+
+            // Angle in radians counterclockwise from right, with the LEDs pointing forward.
+            float angle;
+
+            // Raw distance
+            int raw;
+
+            // Distance in meters
+            float distance;
+
+            // Intensity of return, unknown units
+            int intensity;
+    };
+    static const int Num_Points = 1024;
+
+    //Point pnt[Num_Points];
+
+    class LidarListener
+    {
+        public:
+            virtual void onNewStateAvailable(void* state) = 0;
+    };
+
+    /*
+     * Interface for GPS devices.
+     */
+    class Lidar : public EventGenerator<LidarListener>
+    {
     public:
-        Lidar();
-        virtual ~Lidar();
-    protected:
-    private:
+        virtual ~Lidar() { }
+
+        /*
+         * Returns the most recent state acquired from the GPS device.
+         */
+        //virtual LidarState GetState() = 0;
+
+        /*
+         * Returns the LidarState with the given timestamp.
+         * Throws an error if no such LidarState exists in the buffer.
+         */
+        //virtual LidarState GetStateAtTime(timeval time) = 0;
+
+        /*
+         * Return true if there is at least one state in the buffer.
+         */
+        virtual bool StateIsAvailable() = 0;
+
 };
 
+//};
+
+} /* namespace Sensors */
+} /* namespace IGVC */
 #endif // LIDAR_H
---
+++ 2013/software/src/sensors/lidar/NAV200.h
@@ -0,0 +1,27 @@
+#ifndef NAV200_H
+#define NAV200_H
+
+#include "Lidar.h"
+
+#include <boost/thread.hpp>
+#include "../../serial/ASIOSerialPort.h"
+
+namespace IGVC {
+namespace Sensors {
+
+class NAV200
+{
+    public:
+        NAV200();
+        virtual ~NAV200();
+    private:
+        //ASIOSerialPort serialPort;
+        //boost::thread iothread;
+        //boost::mutex queueLocker;
+
+        void threadRun(); // the method that runs on iothread
+};
+
+} /* namespace Sensors */
+} /* namespace IGVC */
+#endif // NAV200_H
