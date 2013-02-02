21aeb27a3616ab252eed4292dadd7427fa40c969
atrimm <alexander.trimm@gmail.com>
2013-02-02 01:25:41 -0500

    Made a bunch of modifications to accomodate and work towards sensor fusion

--- 2013/software/src/sensors/RobotPosition.cpp
+++ 2013/software/src/sensors/RobotPosition.cpp
@@ -1,6 +1,7 @@
 #include "RobotPosition.h"
+#include <math.h>
 
-RobotPosition::RobotPosition()
+RobotPosition::RobotPosition() : Lat(100), Long(100), Speed(100), Heading(100)
 {
     //ctor
 }
@@ -8,4 +9,71 @@ RobotPosition::RobotPosition()
 RobotPosition::~RobotPosition()
 {
     //dtor
+}
+
+void RobotPosition::push(GPSData newData)
+{
+    Lat.push(newData.Lat());
+    Long.push(newData.Long());
+    Speed.push(newData.Speed());
+    Heading.push(newData.Heading());
+}
+
+double RobotPosition::accel(int Index)
+{
+    int millisPerSecond = 1000;
+    if (Index==0)
+    {
+        return 0;
+    }
+    else
+    {
+        return millisPerSecond*(Speed[Index].value()-Speed[Index-1].value())/(Speed[Index].time()-Speed[Index-1].time());
+    }
+}
+
+
+double RobotPosition::angVel(int Index)
+{
+
+    if (Index==0)
+    {
+        return 0;
+    }
+    else
+    {
+        return millisPerSecond*(Heading[Index].value()-Heading[Index-1].value())/(Heading[Index].time()-Heading[Index-1].time());
+    }
+}
+
+double RobotPosition::ptIntSpeed(int endingInd)
+{
+    //TODO Account for when an indice lower than 2 is pased
+    int dti = Speed[endingInd].time() - Speed[endingInd-1].time();
+    int dtiMinus = Speed[endingInd-1].time() - Speed[endingInd-2].time();
+    int dt = dti + dtiMinus;
+    double A = (millisPerSecond*(accel(endingInd)-accel(endingInd-1))/dt)/2;
+    double B = accel(endingInd-1);
+    double C = Speed[endingInd-1].value();
+    int integral = A/3*pow((dti/millisPerSecond),3) + (B/2) * pow((dti/millisPerSecond),2) + C*dti/millisPerSecond;
+    return integral;
+}
+
+double RobotPosition::avgSpeed(long long end, long long start)
+{
+    int startInd = Speed.firstIndBefore(start);
+    int endInd = Speed.firstIndBefore(end);
+    double integral=0;
+    int i;
+    long long dt;
+    for (i=startInd+1;i<=endInd;i++)
+    {
+        integral+=ptIntSpeed(i);
+    }
+    dt = Speed[endInd].time() - Speed[startInd].time();
+    return millisPerSecond*integral/dt;
+
+
+
+
 }
--- 2013/software/src/sensors/RobotPosition.h
+++ 2013/software/src/sensors/RobotPosition.h
@@ -2,6 +2,10 @@
 #define ROBOTPOSITION_H
 
 #include "DataStructures/GPSData.h"
+#include "DataStructures/DataArray.h"
+#include "DataStructures/DataPoint.hpp"
+
+const int millisPerSecond = 1000;
 
 class RobotPosition
 {
@@ -11,14 +15,29 @@ class RobotPosition
         //fuse(IMUData);
         //fuse(EncoderData);
         virtual ~RobotPosition();
+        double accel(int Index);
+        double angVel(int Index);
+        void push(GPSData newData);
 
 
     private:
-        DataArray<double> Lat(100);
-        DataArray<double> Long(100);
-        DataArray<double> Speed(100);
-        DataArray<double> Heading(100);
+        double avgLat(long long end, long long start);
+        double avgLong(long long end, long long start);
+        double avgSpeed(long long end, long long start);
+        double avgHeading(long long end, long long start);
+        double ptIntLat(int endingInd); //Integral of value over the period between endingInd-1 and endingInd
+        double ptIntLong(int endingInd);
+        double ptIntSpeed(int endingInd);
+        double ptIntHeading(int endingInd);
 
+        DataArray<DataPoint <double> > Lat;
+        DataArray<DataPoint <double> >  Long;
+        DataArray<DataPoint <double> > Speed;
+        DataArray<DataPoint <double> >  Heading;
+        double LatVar;
+        double longVar;
+        double SpeedVar;
+        double HeadingVar;
 };
 
 #endif // ROBOTPOSITION_H
