7d261fdd039b675526489a5cb404d1ae23952544
Al Chaussee <alchaussee@ubuntu.(none)>
2013-02-02 15:26:44 -0500

    motor control changes

--- 2013/software/src/actuators/motors/OSMC_driver/OSMC_driverTest.cpp
+++ 2013/software/src/actuators/motors/OSMC_driver/OSMC_driverTest.cpp
@@ -5,15 +5,11 @@
 #include <iostream>
 #include "OSMC_driver.hpp"
 
-int main() {
+int main()
+{
+    OSMC_driver driver;
+
 	std::cout << "Going Forward" << std::endl;
-	OSMC_driver driver;
-
-    for (int i=0; i<200; i++)
-    {
-        driver.setPwm(i,1);
-    }
-    sleep(2);
+    driver.setPwm(100,1);
     driver.stopMotors();
-
 }
