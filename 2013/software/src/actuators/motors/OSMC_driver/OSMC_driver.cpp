e3f2c24249740ea5c480801218e8318eb551d630
Al Chaussee <alchaussee@ubuntu.(none)>
2013-02-04 13:48:26 -0500

    Cleaned up motor and arduino code

--- 2013/software/src/actuators/motors/OSMC_driver/OSMC_driver.cpp
+++ 2013/software/src/actuators/motors/OSMC_driver/OSMC_driver.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 ASIOSerialPort arduinoLeft("/dev/igvc_2012_left_motor_shield", 9600);
 ASIOSerialPort arduinoRight("/dev/igvc_2012_right_motor_shield", 9600);
-//ASIOSerialPort arduinoEncoder("/dev/igvc_2012_right_encoder_shield", 9600);
+ASIOSerialPort arduinoEncoder("/dev/igvc_2012_right_encoder_shield", 9600);
 
 //TODO: Need to be checked
 const char OSMC_driver::maxPwm = 130;
@@ -18,18 +18,18 @@ const char OSMC_driver::minPwm = 30;
 OSMC_driver::~OSMC_driver()
 {
 	stopMotors();
-//	arduinoRight.close();
+	arduinoRight.close();
 	arduinoLeft.close();
-//	arduinoEncoder.close();
+	arduinoEncoder.close();
 }
 
 bool OSMC_driver::arduinoCheck()	//Checks we are connected and able to send and receive data from the arduinos
 {
 	std::cout << "Testing serial API..." << std::endl;
 	arduinoLeft.write("T");
-//	arduinoRight.write("T");
+	arduinoRight.write("T");
 	sleep(0.01);
-	if (/*arduinoRight.readln() == "T" and */arduinoLeft.readln() == "T")
+	if (arduinoRight.readln() == "T" and arduinoLeft.readln() == "T")
 	{
 		std::cout<<"Successfully read from Arduinos" << std::endl;
 		return true;
@@ -38,7 +38,7 @@ bool OSMC_driver::arduinoCheck()	//Checks we are connected and able to send and 
 	{
 		std::cout<<"Failed to read from Arduinos"<<std::endl;
 		arduinoLeft.close();
-	//	arduinoRight.close();
+		arduinoRight.close();
 		return false;
 	}
 }
@@ -53,20 +53,19 @@ void OSMC_driver::setRightLeftPwm(char pwmRight, char dirRight, char pwmLeft, ch
     pwmRight = adjustSpeedRight(pwmRight, dirRight);
     dirLeft = adjustDirLeft(dirLeft);
     pwmLeft = adjustSpeedLeft(pwmLeft, dirLeft);
-    string wt = "WS";
-    wt.append(1,dirRight);
-    wt.append(1,pwmRight);
-    wt.append(1,dirLeft);
-    wt.append(1,pwmLeft);
-    cout<<wt.c_str()<<endl;
-    arduinoRight.write(wt);
-    string wt1 = "WS";
-    wt1.append(1,dirLeft);
-    wt1.append(1,pwmLeft);
-    wt1.append(1,dirRight);
-    wt1.append(1,pwmRight);
-    arduinoLeft.write(wt1);
-    //usleep(1100000);
+    string w = "W";
+    w.append(1,dirRight);
+    w.append(1,pwmRight);
+    w.append(1,dirLeft);
+    w.append(1,pwmLeft);
+    cout<<w.c_str()<<endl;
+    arduinoRight.write(w);
+    string w1 = "W";
+    w1.append(1,dirLeft);
+    w1.append(1,pwmLeft);
+    w1.append(1,dirRight);
+    w1.append(1,pwmRight);
+    arduinoLeft.write(w1);
 }
 
 char OSMC_driver::adjustSpeedRight(char pwm, char dir)
@@ -122,37 +121,34 @@ void OSMC_driver::turn(int degree, char dir)
     }
 }
 */
+
+// For use with new robot
+/*
 void OSMC_driver::goForward(double dist, char pwm, char dir)
 {
-    checkPwm(pwm, dir);
+    string d = "D";
+    wd.append(1,dist);
+    wd.append(1,dir);
+    wd.append(1,pwm);
 
-    std::ostringstream left;
-    left << dist << dir << pwm;
-//    std::cout<<s.str().c_str()<<endl;
+    arduino.write(wd);
 
-    arduinoLeft.write("WD");
-    arduinoLeft.write(left.str().c_str());
-
-    std::ostringstream right;
-    right << dist << dir << pwm;
-
- //   arduinoRight.write("WD");
- //   arduinoRight.write(right.str().c_str());
-
-    while(/*arduinoRight.readln() == "!" && */arduinoLeft.readln() == "!")
+    while(arduino.readln() == "!")
     {
         break;
     }
 }
 
+*/
+
 double OSMC_driver::readEncoder()
 {
     string r = "R";
-   // arduinoEncoder.write("R");
+    arduinoEncoder.write("R");
     sleep(0.1);
-   // std::string dist1 = arduinoEncoder.readln();
-    //double dist = ::atof(dist1.c_str());
-    //return dist;
+    std::string dist1 = arduinoEncoder.readln();
+    double dist = ::atof(dist1.c_str());
+    return dist;
 }
 
 void OSMC_driver::encoderLoop(double totalDist)
@@ -171,11 +167,11 @@ void OSMC_driver::encoderLoop(double totalDist)
 void OSMC_driver::goForwardOld(double totalDist, char pwm, char dir)
 {
     string f = "F";
- //   arduinoEncoder.write(f);
+    arduinoEncoder.write(f);
     sleep(1);
-  //  string var;
- //   var = "";
- //  var = arduinoEncoder.readln();
+    string var;
+    var = "";
+   var = arduinoEncoder.readln();
  //   cout<<var<<endl;
  //   sleep(1);
     setRightLeftPwm(pwm, dir, pwm, dir);
@@ -187,30 +183,6 @@ void OSMC_driver::stopMotors()		//Stops the motors
     setRightLeftPwm(0,1,0,1);
 }
 
-void OSMC_driver::checkPwm(char pwm, char dir)	//checks that the pwm is within the minPwm and maxPwm
-{
-	if (pwm < minPwm)
-	{
-		setPwm(minPwm, dir);
-	}
-	else if (pwm > maxPwm)
-	{
-		setPwm(maxPwm, dir);
-	}
-}
-
-void OSMC_driver::checkPwm2(char pwmLeft, char pwmRight)	//checks that the pwm for the left and right are within the minPwm and maxPwm
-{
-	if (pwmLeft < minPwm or pwmRight< minPwm)
-	{
-		setPwm(minPwm, 1);
-	}
-	else if (pwmLeft > maxPwm or pwmRight > maxPwm)
-	{
-		setPwm(maxPwm, 1);
-	}
 
 
-
-}
 
