4b3fabd8b7dd5545210a5feedcf2bbbc13e8b534
Matthew Barulic <matthew.barulic@gmail.com>
2013-02-15 23:14:01 -0500

    Add SimulatedLidar

---
+++ 2013/software/src/sensors/lidar/SimulatedLidar.h
@@ -0,0 +1,31 @@
+#ifndef SIMULATEDLIDAR_H
+#define SIMULATEDLIDAR_H
+
+#include <boost/thread.hpp>
+#include "sensors/lidar/Lidar.h"
+
+namespace IGVC
+{
+    namespace Sensors
+    {
+        class SimulatedLidar : public Lidar
+        {
+            public:
+                SimulatedLidar();
+                virtual ~SimulatedLidar();
+                LidarState GetState();
+                LidarState GetStateAtTime(timeval time);
+                bool StateIsAvailable();
+            protected:
+            private:
+
+            boost::thread _thread;
+
+            bool _running;
+
+            void thread_run();
+        };
+    }
+}
+
+#endif // SIMULATEDLIDAR_H
