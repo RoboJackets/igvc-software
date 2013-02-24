87d625fa327fccd654890d7bdaa0f87fc8537523
atrimm <alexander.trimm@gmail.com>
2013-02-23 19:25:12 -0500

    Sensor Fusion implemented

---
+++ 2013/software/src/sensors/DataStructures/GPSAccuracy.hpp
@@ -0,0 +1,68 @@
+#ifndef GPSACCURACY_H
+#define GPSACCURACY_H
+
+class GPSAccuracy
+{
+    public:
+        inline GPSAccuracy(double latVar, double longVar, double headingVar, double speedVar) : _LatVariance(latVar), _LongVariance(longVar),
+         _HeadingVariance(headingVar), _SpeedVariance(speedVar)
+         {
+         }
+
+         inline double LatVar(void)
+         {
+            return _LatVariance;
+         }
+
+
+         inline double LongVar(void)
+         {
+             return _LongVariance;
+         }
+
+         inline double HeadingVar()
+         {
+             return _HeadingVariance;
+         }
+
+        inline double SpeedVar(void)
+         {
+            return _SpeedVariance;
+         }
+
+         inline void ScaleUncertainty(double scale)
+         {
+             _LatVariance*=scale;
+             _LongVariance*=scale;
+            _HeadingVariance*=scale;
+            _SpeedVariance*=scale;
+         }
+
+        inline void LatVar(double newVar)
+        {
+            _LatVariance = newVar;
+        }
+
+        inline void LongVar(double newVar)
+        {
+            _LongVariance = newVar;
+        }
+
+        inline void HeadingVar(double newVar)
+        {
+            _HeadingVariance = newVar;
+        }
+
+        inline void SpeedVar(double newVar)
+        {
+            _SpeedVariance = newVar;
+        }
+
+    private:
+        double _LatVariance;
+        double _LongVariance;
+        double _HeadingVariance;
+        double _SpeedVariance;
+};
+
+#endif // GPSACCURACY_H
--- 2013/software/src/sensors/RobotPosition.cpp
+++ 2013/software/src/sensors/RobotPosition.cpp
@@ -1,8 +1,13 @@
 #include "RobotPosition.h"
 #include <math.h>
+#include <cmath>
 #include "timing.h"
+#include <iostream>
 
-RobotPosition::RobotPosition() : _Lat(100), _Long(100), _Speed(100), _Heading(100)
+#define deg2rad(a) a/180*M_PI
+#define rad2deg(a) a*M_PI/180
+
+RobotPosition::RobotPosition() : _Lat(100), _Long(100), _Speed(100), _Heading(100), _Accuracy(100, 100,100, 100)
 {
     //ctor
 }
@@ -13,11 +18,6 @@ RobotPosition::~RobotPosition()
 }
 
 
-DataArray<DataPoint <double> > RobotPosition::Speed(void)
-{
-    return _Speed;
-}
-
 
 void RobotPosition::push(GPSData newData)
 {
@@ -26,6 +26,7 @@ void RobotPosition::push(GPSData newData)
     _Long.push(DataPoint<double>(newData.Long(), time));
     _Speed.push(DataPoint<double>(newData.Speed(), time));
     _Heading.push(DataPoint<double>(newData.Heading(), time));
+    //combine accuracies
 }
 
 
@@ -36,14 +37,14 @@ double RobotPosition::accel(int Index)
 {
     double dT;
     double dV;
-    if (Index==0)
+    if (Index==(_Speed.size()-1))
     {
         return 0;
     }
     else
     {
-        dV = _Speed[Index].value()-_Speed[Index-1].value();
-        dT = _Speed[Index].time()-_Speed[Index-1].time();
+        dV = _Speed[Index].value()-_Speed[Index+1].value();
+        dT = _Speed[Index].time()-_Speed[Index+1].time();
         return dV/dT;
     }
 }
@@ -54,46 +55,259 @@ double RobotPosition::angVel(int Index)
     double dTheta;
     double dT;
 
-    if (Index==0)
+    if (Index==(_Heading.size()-1))
     {
         return 0;
     }
     else
     {
-        dTheta = (_Heading[Index].value()-_Heading[Index-1].value());
-        dT = (_Heading[Index].time()-_Heading[Index-1].time());
+        dTheta = (_Heading[Index].value()-_Heading[Index+1].value());
+        dT = (_Heading[Index].time()-_Heading[Index+1].time());
         return dTheta/dT;
     }
 }
 
+/***
+Returns the Integral of the speed(i.e. distance) over the period between endingInd and endingInd+1
+***/
 
-
-
-//TODO MAKE SURE THIS GUY WAS CHANGED CORRECTLY FOR
 double RobotPosition::ptIntSpeed(int endingInd)
 {
     //TODO Account for when an indice lower than 2 is pased
-    int dti = _Speed[endingInd].time() - _Speed[endingInd-1].time();
-    int dtiMinus = _Speed[endingInd-1].time() - _Speed[endingInd-2].time();
+    int dti = _Speed[endingInd].time() - _Speed[endingInd+1].time();
+    int dtiMinus = _Speed[endingInd+1].time() - _Speed[endingInd+2].time();
     int dt = dti + dtiMinus;
-    double A = ((accel(endingInd)-accel(endingInd-1))/dt)/2;
-    double B = accel(endingInd-1);
-    double C = _Speed[endingInd-1].value();
-    int integral = A/3*pow((dti),3) + (B/2) * pow((dti),2) + C*dti;
+    double A = ((accel(endingInd)-accel(endingInd+1))/dt)/2;
+    double B = accel(endingInd+1);
+    double C = _Speed[endingInd+1].value();
+    double integral = A/3*pow((dti),3) + (B/2) * pow((dti),2) + C*dti;
     return integral;
 }
 
-double RobotPosition::avgSpeed(long long end, long long start)
+
+/**
+
+**/
+
+double RobotPosition::ptIntHeading(int endingInd)
+{
+    //TODO Account for when an indice lower than 2 is pased
+    int dti = _Heading[endingInd].time() - _Heading[endingInd+1].time();
+    int dtiMinus = _Heading[endingInd+1].time() - _Heading[endingInd+2].time();
+    int dt = dti + dtiMinus;
+    double A = ((angVel(endingInd)-angVel(endingInd+1))/dt)/2;
+    double B = angVel(endingInd+1);
+    double C = _Heading[endingInd+1].value();
+    double integral = A/3*pow((dti),3) + (B/2) * pow((dti),2) + C*dti;
+    return integral;
+}
+
+double RobotPosition::ptAvgHeading(int endingInd)
+{
+    double integral = ptIntHeading(endingInd);
+    int dT = _Heading[endingInd].time() - _Heading[endingInd+1].time();
+    return integral/dT;
+}
+
+void RobotPosition::CatersianStep(int endingInd, double& x, double& y)
+{
+    x = ptIntSpeed(endingInd)*cos(ptAvgHeading(endingInd));
+    y = ptIntSpeed(endingInd)*cos(ptAvgHeading(endingInd));
+}
+
+void RobotPosition::Cartesian2Polar(double x, double y, double& theta, double& r)
+{
+    //inputs reversed from normal usage because theta is complement of angle
+    theta = atan2(x,y);
+    r = sqrt(pow(x,2)+pow(y,2));
+}
+
+double RobotPosition::avgSpeed(double end, double start)
 {
     int startInd = _Speed.firstIndBefore(start);
-    int endInd = _Speed.firstIndBefore(end);
+    int endInd = _Speed.firstIndAfter(end);
     double integral=0;
     int i;
-    long long dt;
-    for (i=startInd+1;i<=endInd;i++)
+    double dT;
+    for (i=(startInd-1); i>=endInd; i--)
     {
         integral+=ptIntSpeed(i);
     }
-    dt = _Speed[endInd].time() - _Speed[startInd].time();
-    return millisPerSecond*integral/dt;
+    dT = _Speed[endInd].time() - _Speed[startInd].time();
+    return integral/dT;
+}
+
+//TODO simplify this by writing a method to do probabibalistic combination
+int RobotPosition::update(GPSData newData)
+{
+    double newTime = newData.time();
+
+
+    if (_Lat.size()<3) //if there aren't enough points for a full data model, just use it as new data
+    {
+        push(newData);
+    }
+    else if (newData.time() < _Lat[0].time()) // If data is behind current knowledge, disregard it
+    {
+        return -1;
+    }
+    else //Otherwise, using Kalmann filtering
+    {
+        double predLat = latAtTime(newTime);
+        double predLong = longAtTime(newTime);
+        double predSpeed = speedAtTime(newTime);
+        double predHeading = headingAtTime(newTime);
+
+         double newLatVar = _Accuracy.LatVar() * (1+abs(newData.Lat()-_Lat[0].value()));
+         double newLongVar = _Accuracy.LongVar()  * (1+abs(newData.Long()-_Long[0].value()));
+         double newHeadingVar = _Accuracy.HeadingVar() * (1+abs(newData.Heading()-_Heading[0].value()));
+         double newSpeedVar = _Accuracy.SpeedVar() * (1+abs(newData.Speed()-_Speed[0].value()));
+
+
+         double newLat = (predLat*newData.LatVar() + newData.Lat()*newLatVar)/(newData.LatVar()+newLatVar);
+         double newLong = (predLong*newData.LongVar() + newData.Long()*newLongVar)/(newData.LongVar()+newLongVar);
+         double newHeading = (predHeading*newData.HeadingVar() + newData.Heading()*newHeadingVar)/(newData.HeadingVar()+newHeadingVar);
+         double newSpeed = (predSpeed*newData.SpeedVar() + newData.Speed()*newSpeedVar)/(newData.SpeedVar()+newSpeedVar);
+
+         _Lat.push(DataPoint<double>(newLat, newData.time()));
+         _Long.push(DataPoint<double>(newLong, newData.time()));
+         _Heading.push(DataPoint<double>(newHeading, newData.time()));
+         _Speed.push(DataPoint<double>(newSpeed, newData.time()));
+
+        _Accuracy.LatVar(_Accuracy.LatVar()*newData.Accuracy().LatVar()/(newData.Accuracy().LatVar()+newLatVar));
+        _Accuracy.LongVar(_Accuracy.LongVar()*newData.Accuracy().LongVar()/(newData.Accuracy().LongVar()+newLongVar));
+        _Accuracy.HeadingVar(_Accuracy.HeadingVar()*newData.Accuracy().HeadingVar()/(newData.Accuracy().HeadingVar()+newHeadingVar));
+        _Accuracy.SpeedVar(_Accuracy.SpeedVar()*newData.Accuracy().SpeedVar()/(newData.Accuracy().SpeedVar()+newSpeedVar));
+
+        std::cout << "Prediction was " << predSpeed << ". Measurement was " << newSpeed<< "." << std::endl;
+    }
+
+    return 0;
+}
+
+
+int RobotPosition::update(IMUData newData)
+{
+    GPSData newForm = IMU2GPS(newData);
+    return update(newForm);
+}
+
+GPSData RobotPosition::IMU2GPS(IMUData in)
+{
+    double time = in.time();
+    double lastMeasurementTime = in.time()-in.deltaTime();
+    double offsetHeading;
+    double offsetSpeed;
+    Cartesian2Polar(in.deltaX(), in.deltaY(), offsetHeading, offsetSpeed);
+    double oldLat = latAtTime(lastMeasurementTime);
+    double oldLong = longAtTime(lastMeasurementTime);
+    double newLat;
+    double newLong;
+    //latLongUpdate(oldLat, oldLong, offsetSpeed, offsetHeading, newLat, newLong);
+    latLongCartesianUpdate(oldLat, oldLong, in.deltaX(), in.deltaY(), newLat, newLong);
+    GPSData newForm((double)newLat, (double)newLong, in.Heading(), in.Speed(), in.time());
+    return newForm;
+
+}
+
+double RobotPosition::latAtTime(double time)
+{
+    return linInterp(time, _Lat);
+}
+
+double RobotPosition::longAtTime(double time)
+{
+    return linInterp(time, _Long);
+}
+
+double RobotPosition::headingAtTime(double time)
+{
+    return linInterp(time, _Heading);
+}
+
+double RobotPosition::speedAtTime(double time)
+{
+    return linInterp(time, _Speed);
+}
+
+double RobotPosition::linInterp(double time, DataArray<DataPoint <double> >& attr)
+{
+    int previous = attr.firstIndBefore(time);
+    if (previous == 0) // If the time is after our latest information
+    {
+        return attr[0].value();
+    }
+
+    else //Do linear interpolation
+    {
+        double last = attr[previous].value();
+        double slope = rateOfChange((previous-1), attr);
+        double dT =  time-attr[previous].time();
+        return last + (slope*dT);
+
+
+        /*
+        double before = attr[previous].value();
+        double after = attr[previous-1].value();
+        double percentStep = (time-attr[previous].time())/(attr[previous-1].time()-attr[previous].time());
+        return before + (after-before)*percentStep;
+        */
+    }
+
+
+
+    //TODO add code to ensure that there are at least two values available or state as assumption
+    //There may need to be code written for the case in which
+}
+
+//Gives the linear rate of change between two indices of one of the data structures
+double RobotPosition::rateOfChange(int ind, DataArray<DataPoint <double> >& attr)
+{
+    double newVal = attr[ind].value();
+    double oldVal = attr[ind+1].value();
+    double dT = attr[ind].time()-attr[ind+1].time();
+    return (newVal-oldVal)/dT;
+}
+
+
+
+//Taken from http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+void RobotPosition::latLongCartesianUpdate(double oldLat, double oldLong, double deltaX, double deltaY, double& newLat, double& newLong)
+{
+    newLat = oldLat + deltaY/111,111;
+    newLong = oldLong + deltaX/(111,111*cos(oldLat));
+}
+
+void RobotPosition::latLongUpdate(double oldLat, double oldLong, double distance, double angle, double& newLat, double& newLong)
+{
+    oldLat = deg2rad(oldLat);
+    double lat=asin(sin(oldLat)*cos(distance)+cos(oldLat)*sin(distance)*cos(angle));
+    double lon;
+    if (cos(lat)==0)
+    {
+        lon=oldLong;      // endpoint a pole
+    }
+
+    else
+    {
+        lon=fmod(oldLat-asin(sin(angle)*sin(distance)/cos(lat))+M_PI,2*M_PI)-M_PI;
+    }
+
+    newLat = rad2deg(newLat);
+    newLong = rad2deg(newLong);
+}
+
+double RobotPosition::LatRateOfChange(double endInd)
+{
+    rateOfChange(endInd, _Lat);
+}
+
+double RobotPosition::LongRateOfChange(double endInd)
+{
+    rateOfChange(endInd, _Long);
+}
+
+void RobotPosition::print(void) {
+std::cout << "Lat: " << _Lat[0].value() << ".Long: " << _Long[0].value() << ".Heading: " << _Heading[0].value() << "Speed: " << _Speed[0].value() << ".";
+//TODO add print method to acccuracy and call it here
 }
