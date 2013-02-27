6d3febac38d494d3acbdeb63bf1b49957207c5f3
atrimm <alexander.trimm@gmail.com>
2013-02-26 20:03:27 -0500

    Simplified RobotPosition

--- 2013/software/src/sensors/RobotPosition.h
+++ 2013/software/src/sensors/RobotPosition.h
@@ -5,6 +5,9 @@
 #include "DataStructures/DataArray.h"
 #include "DataStructures/DataPoint.hpp"
 #include "DataStructures/IMUData.hpp"
+#include "IMU/IMU.h"
+#include "GPS/HemisphereA100GPS.h"
+
 
 const int millisPerSecond = 1000;
 
@@ -12,6 +15,11 @@ class RobotPosition
 {
     public:
         RobotPosition();
+        //RobotPosition(IGVC::Sensors::GPS* gps, IMU*);
+        //LISTENER(RobotPosition, onNewGPSData, GPSData);
+        //LISTENER(RobotPosition, onNewIMUData, IMUData);
+        int onNewGPSData(GPSData);
+        int onNewIMUData(IMUData);
         virtual ~RobotPosition();
         double accel(int Index);
         double angVel(int Index);
@@ -19,11 +27,10 @@ class RobotPosition
         void push (IMUData newData);
         int update(GPSData);
         int update (IMUData);
+
         //Private functions being tested
         double avgSpeed(double end, double start);
         double avgHeading(double end, double start);
-        void CatersianStep(int endingInd, double& x, double& y);
-        void Cartesian2Polar(double x, double y, double& theta, double& r);
         double linInterp(double time, DataArray<DataPoint <double> >& attr);
         double rateOfChange(int ind, DataArray<DataPoint <double> >& attr);
         double latAtTime(double time);
@@ -31,23 +38,18 @@ class RobotPosition
         double speedAtTime(double time);
         double headingAtTime(double time);
         void latLongCartesianUpdate(double oldLat, double oldLong, double deltaX, double deltaY, double& newLat, double& newLong);
+        GPSData IMU2GPS(IMUData in);
+        void print(void);
         DataArray<DataPoint <double> >& Lat(void);
         DataArray<DataPoint <double> >& Long(void);
         DataArray<DataPoint <double> >& Heading(void);
         DataArray<DataPoint <double> >& Speed(void);
-        GPSData IMU2GPS(IMUData in);
-        void print(void);
+
 
     private:
-        //double avgSpeed(double end, double start);
-        //double avgHeading(double end, double start);
         double ptIntSpeed(int endingInd);
         double ptIntHeading(int endingInd);
         double ptAvgHeading(int endingInd);
-        double predictSpeed(double time);
-        double predictHeading(double time);
-        double predictLat(double time);
-        double predictLong(double time);
         DataArray<DataPoint <double> > _Lat;
         DataArray<DataPoint <double> >  _Long;
         DataArray<DataPoint <double> > _Speed;
