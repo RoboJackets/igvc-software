9f7ba7108c8916dadbd94dd33a08c7d9de4bd8b1
atrimm <alexander.trimm@gmail.com>
2013-03-10 01:34:48 -0500

    Added code to interface bumblebee with opencv

--- 2013/software/src/sensors/camera3D/imgUtils.h
+++ 2013/software/src/sensors/camera3D/imgUtils.h
@@ -4,13 +4,21 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <Magick++.h>
+//#include <Magick++.h>
+#include <flycapture/FlyCapture2.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdio.h>
+#include <stdarg.h>
+
 
 using namespace std;
-
+/*
 void greyscale(Magick::Image& image);
 void zeroAllButRed(Magick::Image& image);
 void ZeroAllButGreen(Magick::Image& image);
 void copyGreen(Magick::Image& into, Magick::Image& outof);
 void getDispInputImg(string lPath,string rPath, Magick::Image& dispInput);
+*/
+//void ptgrey2opencv(FlyCapture2::Image, cv::Mat&);
 #endif // IMGUTILS_H
