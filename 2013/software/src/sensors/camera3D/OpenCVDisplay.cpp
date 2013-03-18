9b67d951009a13a918dc7f28732722e8208e8902
atrimm <alexander.trimm@gmail.com>
2013-03-18 12:06:02 -0400

    added early display code

---
+++ 2013/software/src/sensors/camera3D/OpenCVDisplay.cpp
@@ -0,0 +1,19 @@
+#include "OpenCVDisplay.h"
+
+OpenCVDisplay::OpenCVDisplay(Bumblebee2& cam) : LonNewFrame(this), _LeftWindowName("left"), _RightWindowName("right")
+{
+    cam.onNewData += &LonNewFrame;
+    namedWindow(_RightWindowName,1);
+    namedWindow(_LeftWindowName,1);
+}
+
+void OpenCVDisplay::onNewFrame(StereoPair newFrame)
+{
+    imshow(_RightWindowName, newFrame.RightImage());
+    imshow(_LeftWindowName, newFrame.LeftImage());
+}
+
+OpenCVDisplay::~OpenCVDisplay()
+{
+    //dtor
+}
