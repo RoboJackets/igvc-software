d52e692cf8d696e5a6fe5d9c14306535eea0c587
atrimm <alexander.trimm@gmail.com>
2013-03-18 12:06:02 -0400

    added early display code

---
+++ 2013/software/src/sensors/camera3D/OpenCVDisplay.h
@@ -0,0 +1,21 @@
+#ifndef OPENCVDISPLAY_H
+#define OPENCVDISPLAY_H
+
+#include "sensors/camera3D/StereoPair.hpp"
+#include "events/Delegate.hpp"
+#include "sensors/camera3D/Bumblebee2.h"
+
+class OpenCVDisplay
+{
+    public:
+        OpenCVDisplay(Bumblebee2&);
+        virtual ~OpenCVDisplay();
+        void onNewFrame(StereoPair newFrame);
+        LISTENER(OpenCVDisplay, onNewFrame, StereoPair);
+
+    private:
+        string _LeftWindowName;
+        string _RightWindowName;
+};
+
+#endif // OPENCVDISPLAY_H
