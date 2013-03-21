b73d2382b9c5b12d2229569d22e6fb4d8fe756c5
Matthew Barulic <matthew.barulic@gmail.com>
2013-03-21 08:52:39 -0400

    dropping development on LidarObstacleExtractor

--- 2013/software/src/executables/LidarTestGUI-build-desktop-Desktop_Qt_4_8_1_for_GCC__Qt_SDK__Release/moc_mainwindow.cpp
+++ 2013/software/src/executables/LidarTestGUI-build-desktop-Desktop_Qt_4_8_1_for_GCC__Qt_SDK__Release/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Mon Mar 18 14:00:55 2013
+** Created: Tue Mar 19 22:57:11 2013
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
