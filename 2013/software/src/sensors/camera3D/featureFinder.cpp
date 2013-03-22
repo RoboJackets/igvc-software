0818e02f6ff23044e5d903c97eafa2c2753c8799
atrimm <alexander.trimm@gmail.com>
2013-03-22 16:42:07 -0400

    Initial commit of featureFinder class, which will manage shared data structures and messaging among processes

---
+++ 2013/software/src/sensors/camera3D/featureFinder.cpp
@@ -0,0 +1,33 @@
+#include "featureFinder.h"
+
+
+
+featureFinder::featureFinder(StereoSource& source) : LonNewFrame(this), _stereo(), _disparityMap()
+{
+    source.onNewData += &LonNewFrame;
+}
+
+void featureFinder::StereoBM(StereoPair& newPair, CvArr* disparity)
+{
+    _stereo(newPair.LeftImage(), newPair.RightImage(), _disparityMap);
+}
+
+/*
+void featureFinder::setDefaultBMState(CvStereoBMState& def)
+{
+    def.preFilterSize=5;
+    def.preFilterCap=6;
+    def.SADWindowSize = 45;
+    def.minDisparity = 0;
+    def.numberOfDisparities = 80;
+    def.textureThreshold = 0;
+    def.uniquenessRatio =2;
+    def.speckleWindowSize = 0;
+    def.speckleRange = 60;
+}
+*/
+
+featureFinder::~featureFinder()
+{
+    //dtor
+}
