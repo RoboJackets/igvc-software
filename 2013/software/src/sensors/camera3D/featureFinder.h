012c7e61001f031d7e731414a3c4488cbbb3b860
atrimm <alexander.trimm@gmail.com>
2013-03-22 16:42:07 -0400

    Initial commit of featureFinder class, which will manage shared data structures and messaging among processes

---
+++ 2013/software/src/sensors/camera3D/featureFinder.h
@@ -0,0 +1,26 @@
+#ifndef FEATUREFINDER_H
+#define FEATUREFINDER_H
+
+#include "sensors/camera3D/StereoPair.hpp"
+#include "sensors/camera3D/StereoSource.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+
+
+
+class featureFinder
+{
+    public:
+        featureFinder(StereoSource& source);
+        void StereoBM(StereoPair& newPair, CvArr* disparity);
+        virtual ~featureFinder();
+        LISTENER(featureFinder, onNewFrame, StereoPair);
+    private:
+        void setDefaultBMState(CvStereoBMState& def);
+        Event<StereoPair> onNewFrame;
+        Event<Mat> onNewDepthMap;
+        cv::StereoBM _stereo;
+        Mat _disparityMap;
+};
+
+#endif // FEATUREFINDER_H
--- 2013/software/src/xbee.cpp
+++ 2013/software/src/xbee.cpp
@@ -2,11 +2,28 @@
 #include <iostream>
 #include <unistd.h>
 #include <string>
+#include <iostream>
 
 #include "serial/ASIOSerialPort.h"
 
 
 using namespace std;
+/*
+int main()
+{
+    ASIOSerialPort xbee("/dev/ttyUSB0", 57600);
+    //ASIOSerialPort xbee("/dev/ttyUSB0", 9600);
+    string testString = "0123456789";
+    while (1)
+    {
+        usleep(5000);
+        xbee.write(testString);
+    }
+
+}
+
+*/
+
 int main(int argc, char *argv[]){
     string newLine;
 
@@ -20,7 +37,7 @@ int main(int argc, char *argv[]){
 
 
 
-    ASIOSerialPort xbee("/dev/XBEE", 57600);
+    ASIOSerialPort xbee("/dev/ttyUSB1", 57600);
 
     while(1)
     {
@@ -41,3 +58,4 @@ int main(int argc, char *argv[]){
     return 0;
 
 }
+
