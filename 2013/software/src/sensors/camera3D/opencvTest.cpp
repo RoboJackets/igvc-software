eedda03defcf3f96394ac75e6fd4a27eedaddf79
atrimm <alexander.trimm@gmail.com>
2013-03-28 20:31:19 -0400

    added functionality to feature finder and added experimental changes to StereoVidMaker to be tested

--- 2013/software/src/sensors/camera3D/opencvTest.cpp
+++ 2013/software/src/sensors/camera3D/opencvTest.cpp
@@ -7,27 +7,68 @@
 #include "sensors/camera3D/StereoVidMaker.h"
 #include "sensors/camera3D/StereoSource.hpp"
 #include "sensors/camera3D/StereoPlayback.h"
+#include "sensors/camera3D/featureFinder.h"
+
 
 using namespace cv;
 
+
 int main()
 {
-    /*
-    std::string fName = "/home/robojackets/Desktop/this";
+    std::string  left = "/home/alex/Desktop/IGVC/2013/software/trainingSets/cone/set2/11123906-0-Left.bmp";
+    std::string  right = "/home/alex/Desktop/IGVC/2013/software/trainingSets/cone/set2/11123906-0-Right.bmp";
+    Mat L, R, D, LG, RG;
+    namedWindow("this",0);
+    L = imread(left);
+    R = imread(right);
+    cvtColor(L, LG, CV_BGR2GRAY );
+    cvtColor(R, RG, CV_BGR2GRAY );
+    StereoBM S;
+    //S.init(0);
+
+    Ptr<CvStereoBMState> def = S.state;
+    def->preFilterSize=5;
+    def->preFilterCap=6;
+    def->SADWindowSize = 45;
+    def->minDisparity = 0;
+    def->numberOfDisparities = 80;
+    def->textureThreshold = 0;
+    def->uniquenessRatio =2;
+    def->speckleWindowSize = 0;
+    def->speckleRange = 60;
+
+
+    S(LG,RG,D,CV_16S);
+    Mat dispD;
+    convertScaleAbs(D, dispD, 2);
+    imshow("this", dispD);
+    if(waitKey(20000) >= 0);
+}
+
+
+
+/*
+int main()
+{
+
+    //std::string fName = "/home/robojackets/Desktop/this";
+    std::string fName = "/home/alex/Desktop/IGVC/2013/software/trainingSets/this";
     std::string lName, rName;
     lName = fName + "_left" + ".mpeg";
     rName = fName + "_right" + ".mpeg";
-    StereoPlayback thisguy(lName, rName, 20);
-    */
-    Bumblebee2 thisguy;
+    StereoPlayback thisguy(lName, rName, 2);
 
-    OpenCVDisplay Disp(thisguy);
+    //Bumblebee2 thisguy;
 
+    //OpenCVDisplay Disp(thisguy);
+    featureFinder he(thisguy);
 
     //initialize video maker
 
     std::string saveName = "/home/robojackets/Desktop/camTesting/highRes1024packet20fps";
-    int nFrame = 100;
+
+
+    //int nFrame = 100;
     //StereoVidMaker maker(thisguy, nFrame, saveName);
 
 
@@ -35,3 +76,4 @@ int main()
     {
     }
 }
+*/
