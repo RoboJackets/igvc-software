448bcd75ba404689a4698d9ce234400773ad8782
atrimm <alexander.trimm@gmail.com>
2013-03-28 21:44:18 -0400

    Added functionality to vid maker to record a series of still images instead of video

--- 2013/software/src/sensors/camera3D/StereoVidMaker.cpp
+++ 2013/software/src/sensors/camera3D/StereoVidMaker.cpp
@@ -1,7 +1,7 @@
 #include "sensors/camera3D/StereoVidMaker.h"
 
-StereoVidMaker::StereoVidMaker(StereoSource& source, string videoName,int frameCount, int frameRate=10): LonNewFrame(this), _frameCount(frameCount), _source(source),
-_leftWriter(), _rightWriter()
+StereoVidMaker::StereoVidMaker(StereoSource& source, string videoName,int frameCount, int frameRate, bool video): LonNewFrame(this), _totalFrames(frameCount), _source(source),
+_leftWriter(), _rightWriter(), _nFrames(0), _video(video), _name(videoName)
 {
     _source.onNewData += &LonNewFrame;
     const std::string leftVidName = videoName + "_left" +".mpeg";
@@ -13,19 +13,40 @@ _leftWriter(), _rightWriter()
 
 void StereoVidMaker::onNewFrame(StereoPair& newPair)
 {
-    if (_frameCount <= 0)
+    if (_nFrames < totalFrames)
     {
-        _source.onNewData -= &LonNewFrame;
+        if (_video)
+        {
+            addFrame(newPair);
+        }
+        else
+        {
+            takeStereoImage(newPair);
+        }
     }
     else
     {
-        std::cout << "printing frame" << std::endl;
-        //_rightWriter.write((const cv::Mat&) newPair.RightImage());
-        //_leftWriter.write((const cv::Mat&) newPair.LeftImage());
-        _rightWriter << newPair.RightImage();
-        _leftWriter << newPair.LeftImage();
-        _frameCount--;
+        _source.onNewData -= &LonNewFrame;
     }
+}
+
+void StereoVidMaker::addFrame(StereoPair& newPair)
+{
+    std::cout << "printing frame" << std::endl;
+    //_rightWriter.write((const cv::Mat&) newPair.RightImage());
+    //_leftWriter.write((const cv::Mat&) newPair.LeftImage());
+    _rightWriter << newPair.RightImage();
+    _leftWriter << newPair.LeftImage();
+    _nFrames++;
+}
+
+
+void StereoVidMaker::takeStereoImage(StereoPair& newPair)
+{
+    const std::string leftImageName = videoName + _nFrames + "_left" +".jpg";
+    const std::string rightImageName = videoName + _nFrames + "_right" + ".jpg";
+    imwrite(leftImageName, newPair.LeftImage());
+    imwrite(rightImageName, newPair.RightImage());
 }
 
 StereoVidMaker::~StereoVidMaker()
--- 2013/software/src/sensors/camera3D/StereoVidMaker.h
+++ 2013/software/src/sensors/camera3D/StereoVidMaker.h
@@ -13,15 +13,18 @@ using namespace cv;
 class StereoVidMaker
 {
     public:
-        StereoVidMaker(StereoSource& source, int frameCount, std::string videoName); //videoName must NOT have an extension, it will be saved as MPEG
+        StereoVidMaker(StereoSource& source, std::string videoName, int frameCount, int frameRate=10, bool video=1); //videoName must NOT have an extension, it will be saved as MPEG
         virtual ~StereoVidMaker();
         void onNewFrame(StereoPair&);
         LISTENER(StereoVidMaker,onNewFrame, StereoPair&);
     private:
-        int _frameCount;
         VideoWriter _leftWriter;
         VideoWriter _rightWriter;
         StereoSource& _source;
+        int _nFrames;
+        int _totalFrames;
+        bool _video=true;
+        string _name;
 
 
 };
