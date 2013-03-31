b3ad38d05306c0c009e7e426cc184361692d8959
Matthew Barulic <matthew.barulic@gmail.com>
2013-03-31 14:50:13 -0400

    More experimenting with lane marker detection.

--- 2013/software/src/sensors/camera2D/laneDetection/testLaneDetection.cpp
+++ 2013/software/src/sensors/camera2D/laneDetection/testLaneDetection.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <cmath>
+#include <set>
+#include <vector>
 
 using namespace cv;
 
@@ -18,105 +20,94 @@ void on_mouse(int evt, int c, int r, int flags, void* param)
     }
 }
 
+void normalizeRGB(Mat mat)
+{
+    uchar* p;
+    for(int i = 0; i < mat.rows; i++)
+    {
+        p = mat.ptr<uchar>(i);
+        for(int j = 0; j < mat.cols*mat.channels(); j += 3)
+        {
+            uchar blue = p[j];
+            uchar green = p[j+1];
+            uchar red = p[j+2];
+
+            double sum = red+ green + blue;
+
+            if(sum > 0)
+            {
+                p[j] = blue / sum * 255;
+                p[j+1] = green / sum * 255;
+                p[j+2] = red / sum * 255;
+            }
+        }
+    }
+}
+
 int main()
 {
-    VideoCapture cam(1);
+    int trackVal = 215;
+    cvNamedWindow("Binary");
+    cvCreateTrackbar("trackbar", "Binary", &trackVal, 255);
+    while(true) {
+    frame = imread("/home/matt/Pictures/2005IGVCcoursePics 154.jpg", 1);
+    resize(frame, frame, Size(640,480));
+    imshow("Raw", frame);
 
-    namedWindow("Filtered");
-    setMouseCallback("Filtered", on_mouse, 0);
+    frame.copyTo(filtered);
 
-    while(true)
+    boxFilter(filtered, filtered, 0, Size(9,9));
+
+    { // Increase contrast of color image
+        // http://stackoverflow.com/questions/15007304/histogram-equalization-not-working-on-color-image-opencv
+        Mat ycrcb;
+        cvtColor(filtered, ycrcb, CV_BGR2YCrCb);
+
+        vector<Mat> channels;
+        split(ycrcb, channels);
+
+        equalizeHist(channels[0], channels[0]);
+
+        merge(channels, ycrcb);
+
+        cvtColor(ycrcb, filtered, CV_YCrCb2BGR);
+    }
+
+    imshow("Filtered", filtered);
+
+    binary = Mat(filtered.rows, filtered.cols, CV_8UC3);
+    filtered.copyTo(binary);
+
+    uchar* p;
+    for(int r = 0; r < binary.rows; r++)
     {
-        cam >> frame;
-
-//        frame = imread("/home/matt/Pictures/DSC09490.JPG", 1);
-
-        resize(frame, frame, Size(640,480));
-
-        Rect roi(0, 200, frame.cols, frame.rows -200 );
-
-//        frame = frame(roi);
-
-        frame *= 2;
-
-        imshow("Orig", frame);
-
-        boxFilter(frame, filtered,0,Size(9,9));
-
-        uchar* p;
-        for(int i = 0; i < filtered.rows; i++)
+        p = binary.ptr<uchar>(r);
+        for(int c = 0; c < binary.cols*binary.channels(); c+=3)
         {
-            p = filtered.ptr<uchar>(i);
-            for(int j = 0; j < filtered.cols*filtered.channels(); j += 3)
+            uchar blue = p[c];
+            uchar green = p[c+1];
+            uchar red = p[c+2];
+
+            p[c] = 0;
+            p[c+1] = 0;
+            p[c+2] = 0;
+
+//            if(abs(blue - 85) < 10 && abs(green - 85) < 10 && abs(red - 85) < 10) //White
+            if(blue > trackVal && green > trackVal && red > trackVal)
             {
-                uchar blue = p[j];
-                uchar green = p[j+1];
-                uchar red = p[j+2];
-
-                double sum = red+ green + blue;
-
-                if(sum > 0)
-                {
-                    p[j] = blue / sum * 255;
-                    p[j+1] = green / sum * 255;
-                    p[j+2] = red / sum * 255;
-                }
+                p[c] = 255;
+                p[c+1] = 255;
+                p[c+2] = 255;
             }
         }
+    }
 
-        imshow("Filtered", filtered);
+    cvtColor(binary, binary, CV_BGR2GRAY);
 
-        binary = filtered.clone();
-        binary = Mat(filtered.rows, filtered.cols, CV_8UC3);
-        filtered.copyTo(binary);
+    imshow("Binary", binary);
 
-        uchar* binary_p;
-        for(int i = 0; i < binary.rows; i++)
-        {
-
-            binary_p = binary.ptr<uchar>(i);
-            for(int j = 0; j < binary.cols*binary.channels(); j += 3)
-            {
-                uchar blue = binary_p[j];
-                uchar green = binary_p[j+1];
-                uchar red = binary_p[j+2];
-                if(abs(blue-green) < 7 && abs(red-green) < 7 && abs(red-blue) < 7)
-                {
-                    binary_p[j] = 255;
-                    binary_p[j+1] = 255;
-                    binary_p[j+2] = 255;
-                } else {
-                    binary_p[j] = 0;
-                    binary_p[j+1] = 0;
-                    binary_p[j+2] = 0;
-                }
-            }
-        }
-
-        cvtColor(binary, binary, CV_BGR2GRAY);
-
-        imshow("binary", binary);
-
-        vector<Vec4i> lines;
-        HoughLinesP(binary, lines, 1, CV_PI/180, 50, 50, 10 );
-        for( size_t i = 0; i < lines.size(); i++ )
-        {
-            Vec4i l = lines[i];
-            Point p0(l[0], l[1]);
-            Point p1(l[2], l[3]);
-            double dx = (double)p1.x - (double)p0.x;
-            double m = dx != 0 ? ((double)p1.y - (double)p0.y) / dx : 1000000000.0;
-            m = m < 0 ? m * -1 : m;
-            if(m > 0.2)
-            {
-                line( frame, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255,0,0), 1, CV_AA);
-            }
-        }
-
-        imshow("Lines", frame);
-
-        if(waitKey(10) >= 0)
-            break;
+    if(waitKey(10) > 0)
+        break;
     }
 
     return 0;
