709248eeb1d8db73b4d7cac9bac190ea89501058
Matthew Barulic <matthew.barulic@gmail.com>
2013-04-09 17:40:27 -0400

    Added necessary infrastructure for Ardupilot to use ASIOSerialPort's events system rather than a polling architecture.

--- 2013/software/src/sensors/ardupilot/Ardupilot.cpp
+++ 2013/software/src/sensors/ardupilot/Ardupilot.cpp
@@ -8,14 +8,53 @@
 using namespace std;
 
 Ardupilot::Ardupilot():
-ardupilotPort("/dev/ttyIMU", 115200)
+ardupilotPort("/dev/ttyIMU", 115200),
+LonNewSerialLine(this)
 {
     timeLast = seconds_since_IGVCpoch();
+    ardupilotPort.onNewLine += &LonNewSerialLine;
+    ardupilotPort.startEvents();
 }
 
 Ardupilot::~Ardupilot()
 {
     ardupilotPort.close();
+}
+
+void Ardupilot::onNewSerialLine(string line)
+{
+    istringstream iss(line);
+
+    string check;
+    iss>>check;
+
+    if(check.compare("A") == 0)
+    {
+        timeCurrent = seconds_since_IGVCpoch();
+        string headingS;
+        iss>>headingS;
+        heading = atof(headingS.c_str());
+
+        string speedS;
+        iss>>speedS;
+        speed = atof(speedS.c_str());
+
+        string positionXS;
+        iss>>positionXS;
+        positionX = atof(positionXS.c_str());
+
+        string positionYS;
+        iss>>positionYS;
+        positionY = atof(positionYS.c_str());
+
+        deltat = timeCurrent-timeLast;
+
+        cout<<"H:" << heading<<"\tS:"<<speed<<"\tPx:"<<positionX<<"\tPy:"<<positionY<<"\tDt:"<<deltat<<endl;
+
+        onNewIMUData(IMUData(positionX, positionY, heading, speed, deltat));
+
+        timeLast = timeCurrent;
+    }
 }
 
 void Ardupilot::update()
@@ -57,7 +96,8 @@ void Ardupilot::update()
 
 void Ardupilot::write(char a)
 {
-    std::ostringstream s;
-    s<<a;
-    ardupilotPort.write(s.str());
+    ardupilotPort.write(&a, 1);
+//    std::ostringstream s;
+//    s<<a;
+//    ardupilotPort.write(s.str());
 }
