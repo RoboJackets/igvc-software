dab927dd5e6930eeb8a36471bbd579151d68abc9
Matthew Barulic <matthew.barulic@gmail.com>
2013-04-28 11:13:15 -0400

    Successful isolation of lane markers from orange/white striped obstacles in TestLaneDetection

--- 2013/software/src/sensors/camera2D/laneDetection/testLaneDetection.cpp
+++ 2013/software/src/sensors/camera2D/laneDetection/testLaneDetection.cpp
@@ -48,35 +48,106 @@ void normalizeRGB(Mat mat)
 int main()
 {
     int trackVal = 215;
-    cvNamedWindow("Binary");
-    cvCreateTrackbar("trackbar", "Binary", &trackVal, 255);
+    //cvNamedWindow("Binary");
+    //cvCreateTrackbar("trackbar", "Binary", &trackVal, 255);
     while(true) {
     frame = imread("/home/matt/Pictures/2005IGVCcoursePics 154.jpg", 1);
     resize(frame, frame, Size(640,480));
+
+    { // ROI
+        int startrow = frame.rows / 4;
+        frame = frame(Rect(0,startrow,frame.cols,frame.rows - startrow));
+    }
+
     imshow("Raw", frame);
 
     frame.copyTo(filtered);
 
-//    boxFilter(filtered, filtered, 0, Size(9,9));
+    boxFilter(filtered, filtered, 0, Size(9,9));
 
-    cvtColor(filtered, filtered, CV_BGR2HSV);
-
-    {
+    { // Filter Orange hues to Black
+        Mat HSV;
+        cvtColor(filtered, HSV, CV_BGR2HSV);
         vector<Mat> channels;
-        split(filtered, channels);
+        split(HSV, channels);
         Mat H = channels[0];
-        equalizeHist(H, H);
-        imshow("Hue", H);
-        Mat S = channels[1];
-        S.at
+        Mat V = channels[2];
+        uchar* pH;
+        uchar* pV;
+        for(int r = 0; r < H.rows; r++)
+        {
+            pH = H.ptr<uchar>(r);
+            pV = V.ptr<uchar>(r);
+            for(int c = 0; c < H.cols; c++)
+            {
+                uchar hVal = pH[c];
+                if(abs(hVal - 170) < 10 || abs(hVal - 0) < 10)
+                {
+                    pV[c] = 0;
+                }
+            }
+        }
+        //imshow("H", channels[0]);
+        merge(channels, filtered);
+        cvtColor(filtered, filtered, CV_HSV2BGR);
+    }
+
+    { // Remove White segments of columns between Orange segments
+        Mat transposed;
+        transpose(filtered, transposed);
+        uchar* p;
+        bool seenBlack, seenWhite;
+        int startInd = 0;
+        for(int c = 0; c < transposed.rows; c++)
+        {
+            p = transposed.ptr<uchar>(c);
+            seenBlack = false;
+            seenWhite = false;
+            for(int r = 0; r < transposed.cols*transposed.channels(); r+=3)
+            {
+                uchar blue = p[r], green = p[r+1], red = p[r+2];
+                if( red == 0 && green == 0 && blue == 0)
+                {
+                    if(seenBlack && seenWhite)
+                    {
+                        for(int i = startInd; i <= r; i++)
+                        {
+                            p[i] = 0;
+                            p[i+1] = 0;
+                            p[i+2] = 0;
+                        }
+                    }
+                    else if(!seenBlack)
+                    {
+                        seenBlack = true;
+                        startInd = r;
+                    }
+                } else if(abs(red-green) < 5 && abs(green-blue) < 30)//if(red > 150 && green > 150 && blue > 150)
+                {
+                    if(seenBlack && !seenWhite)
+                    {
+                        seenWhite = true;
+                    }
+                }
+            }
+        }
+        //imshow("Transposed", transposed);
+        transpose(transposed, filtered);
     }
 
     imshow("Filtered", filtered);
+    //cvSetMouseCallback("Raw", on_mouse, 0);
 
-/*    { // Increase contrast of color image
+    cvtColor(filtered, filtered, CV_BGR2GRAY);
+    threshold(filtered, filtered, 0, 1, THRESH_BINARY);
+    cvtColor(filtered, filtered, CV_GRAY2BGR);
+    Mat masked;
+    multiply(filtered, frame, masked);
+
+    { // Increase contrast of color image
         // http://stackoverflow.com/questions/15007304/histogram-equalization-not-working-on-color-image-opencv
         Mat ycrcb;
-        cvtColor(filtered, ycrcb, CV_BGR2YCrCb);
+        cvtColor(masked, ycrcb, CV_BGR2YCrCb);
 
         vector<Mat> channels;
         split(ycrcb, channels);
@@ -85,41 +156,51 @@ int main()
 
         merge(channels, ycrcb);
 
-        cvtColor(ycrcb, filtered, CV_YCrCb2BGR);
+        cvtColor(ycrcb, masked, CV_YCrCb2BGR);
     }
+    imshow("Masked", masked);
 
-    imshow("Filtered", filtered);
+    binary = Mat(masked.rows, masked.cols, CV_8UC3);
+    masked.copyTo(binary);
 
-    binary = Mat(filtered.rows, filtered.cols, CV_8UC3);
-    filtered.copyTo(binary);
-
-    uchar* p;
-    for(int r = 0; r < binary.rows; r++)
-    {
-        p = binary.ptr<uchar>(r);
-        for(int c = 0; c < binary.cols*binary.channels(); c+=3)
+    { // Threshold for White
+        uchar threshold = 240;
+        uchar* p;
+        for(int r = 0; r < binary.rows; r++)
         {
-            uchar blue = p[c];
-            uchar green = p[c+1];
-            uchar red = p[c+2];
-
-            p[c] = 0;
-            p[c+1] = 0;
-            p[c+2] = 0;
-
-//            if(abs(blue - 85) < 10 && abs(green - 85) < 10 && abs(red - 85) < 10) //White
-            if(blue > trackVal && green > trackVal && red > trackVal)
+            p = binary.ptr<uchar>(r);
+            for(int c = 0; c < binary.cols*binary.channels(); c+=3)
             {
-                p[c] = 255;
-                p[c+1] = 255;
-                p[c+2] = 255;
+                uchar blue = p[c];
+                uchar green = p[c+1];
+                uchar red = p[c+2];
+
+                p[c] = 0;
+                p[c+1] = 0;
+                p[c+2] = 0;
+
+                if(blue > threshold && green > threshold && red > threshold)
+                {
+                    p[c] = 255;
+                    p[c+1] = 255;
+                    p[c+2] = 255;
+                }
             }
         }
+
+        cvtColor(binary, binary, CV_BGR2GRAY);
     }
 
-    cvtColor(binary, binary, CV_BGR2GRAY);
 
-    imshow("Binary", binary);*/
+    int erosion_size =2;
+    cv::Mat element = cv::getStructuringElement(cv::MORPH_CROSS,
+                          cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+                          cv::Point(erosion_size, erosion_size) );
+
+    erode(binary, binary, element);
+    dilate(binary, binary, element);
+
+    imshow("Binary", binary);
 
     if(waitKey(10) > 0)
         break;
