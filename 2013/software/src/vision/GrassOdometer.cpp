dd7056d41a8e5a2d0ab177b02eb9ed49846263c1
atrimm <alexander.trimm@gmail.com>
2013-05-24 17:50:53 -0400

    second commit of grass Odometry, still in rough shape but somewhat functional

--- 2013/software/src/vision/GrassOdometer.cpp
+++ 2013/software/src/vision/GrassOdometer.cpp
@@ -8,6 +8,8 @@ void GrassOdometer::processImage(Mat src, int numPoints)
 {
 }
 
+void GrassOdometer::
+
 
 
 void GrassOdometer::ProcesImageSURF(Mat& frame)
@@ -50,12 +52,13 @@ void GrassOdometer::ProcesImageSURF(Mat& frame)
   Eigen::MatrixXd currentPos(4,keypoints.size());
   Vector3d cameraPos, cameraOffset;
   double mastHeight, d2c;
+  double phi, theta;
   d2c = 0.508; //meters
   mastHeight = 1.676; //meters
-  cameraPos << -d2c, 0, mastHeight;
+  cameraPos << -d2c, 0, -mastHeight; //both d2c and mastHeight are negative because of NED direction conventions
 
   cameraOffset = rotDynMat.topLeftCorner(3,3)*cameraPos; //describes position relative to
-  cameraHeight = cameraOffset(2);
+  cameraHeight = -cameraOffset(2);
 
   rotDynMat = HomogRotMat3d(roll, pitch, yaw);
   //Need to get the positions for all points, not just those matched, because non-matched may appear in next frame
@@ -66,27 +69,30 @@ void GrassOdometer::ProcesImageSURF(Mat& frame)
     pos << r,c,1; //BE CAREFUL, REMEMBER R CORRESPONDS TO THE Y VALUE
 
     pos = centerImageCoords(nRows, nCols) * pos; //Changes coordinates in picture such that center of image is 0,0
-    pos = ImgRollCorrectMat(roll)*pos; //Correct for roll of image by rotating it back the opposite way
+    pos = HomogImgRotMat(roll)*pos; //Correct for roll of image by rotating it back the opposite way
 
+    phi = cameraAngle - pitch + dPhi * pos(0);
+    theta = dTheta * pos(1);
 
-    //xRobot = cameraPos()
-    //yRobot =
+    xCam = cameraHeight/tan(phi);
+    yCam = cameraHeight/tan(theta);
+
+    xRobot = xCam - cameraOffset(0);
+    yRobot = yCam - cameraOffset(1);
+    zRobot = 0;  //Since we are defining the robot position as on the ground and the points are as welll
 
     //Get positions of objects relative to camera
 
-    //currentPos(0,i) = //xRobot;
-    //currentPos(1,i) = //yRobot;
-    //currentPos(2,i) = //zRobot;
-    //currentPos(3,i) = 1;
-
+    currentPos(0,i) = xRobot;
+    currentPos(1,i) = yRobot;
+    currentPos(2,i) = zRobot;
+    currentPos(3,i) = 1;
   }
 
+  currentPos = rotDynMat.inverse()*currentPos; //makes positions extrinsic. i.e. x is now north, etc
 
-
-
-  currentPos = rotDynMat.inverse()*currentPos; //makes positions extrinsic. i.e. y is now north, etc
-  //Match points
   std::vector< DMatch > matches;
+
   MatchPointsFLANN(matches, descriptors, _lastFrameDescriptors);
 
   //Create matrices of corresponding points
@@ -107,10 +113,13 @@ void GrassOdometer::ProcesImageSURF(Mat& frame)
   double deltax = deltaMat.row(0).mean();
   double deltay = deltaMat.row(1).mean();
 
+  _lastFramePositions = currentPos;
+  _lastFrameDescriptors = descriptors;
+  _lastFrame = frame.clone();
 
 }
 
-Matrix3d GrassOdometer::RollRotMatrix(int roll)
+Matrix3d GrassOdometer::RollRotMatrix(double roll)
 {
   Matrix3d rMat = MatrixXd::Zero(3,3);
   rMat(0,0) = 1;
@@ -121,7 +130,7 @@ Matrix3d GrassOdometer::RollRotMatrix(int roll)
   return rMat;
 }
 
-Matrix4d GrassOdometer::HomogRollRotMatrix(int roll)
+Matrix4d GrassOdometer::HomogRollRotMatrix(double roll)
 {
   Matrix4d rMat = MatrixXd::Zero(4,4);
   rMat.topLeftCorner(3,3) = RollRotMatrix(roll);
@@ -129,7 +138,7 @@ Matrix4d GrassOdometer::HomogRollRotMatrix(int roll)
   return rMat;
 }
 
-Matrix3d GrassOdometer::PitchRotMatrix(int pitch)
+Matrix3d GrassOdometer::PitchRotMatrix(double pitch)
 {
    Matrix3d rMat = MatrixXd::Zero(3,3);
    rMat(0,0) = cos(pitch);
@@ -139,14 +148,14 @@ Matrix3d GrassOdometer::PitchRotMatrix(int pitch)
    rMat(2,2) = cos(pitch);
 }
 
-Matrix4d GrassOdometer::HomogPitchRotMatrix(int pitch)
+Matrix4d GrassOdometer::HomogPitchRotMatrix(double pitch)
 {
   Matrix3d rMat = MatrixXd::Zero(4,4);
   rMat.topLeftCorner(3,3) = PitchRotMatrix(pitch);
   rMat(3,3) = 1;
 }
 
-Matrix3d GrassOdometer::YawRotMatrix(int yaw)
+Matrix3d GrassOdometer::YawRotMatrix(double yaw)
 {
   MatrixXd rMat = MatrixXd::Zero(3,3);
   rMat(0,0) = cos(yaw);
@@ -156,19 +165,19 @@ Matrix3d GrassOdometer::YawRotMatrix(int yaw)
   rMat(2,2) = 1;
 }
 
-Matrix4d GrassOdometer::HomogYawRotMatrix(int yaw)
+Matrix4d GrassOdometer::HomogYawRotMatrix(double yaw)
 {
   MatrixXd rMat = MatrixXd::Zero(4,4);
   rMat.topLeftCorner(3,3) = YawRotMatrix(yaw);
   rMat(3,3) = 1;
 }
 
-Matrix3d GrassOdometer::RotMat3d(int roll, int pitch, int yaw)
+Matrix3d GrassOdometer::RotMat3d(double roll, double pitch, double yaw)
 {
   return YawRotMatrix(yaw)*PitchRotMatrix(pitch)*RollRotMatrix(roll);
 }
 
-Matrix4d GrassOdometer::HomogRotMat3d(int roll, int pitch, int yaw)
+Matrix4d GrassOdometer::HomogRotMat3d(double roll, double pitch, double yaw)
 {
   return HomogYawRotMatrix(yaw)*HomogPitchRotMatrix(pitch)*HomogRollRotMatrix(roll);
 }
@@ -177,8 +186,11 @@ Matrix4d GrassOdometer::HomogRotMat3d(int roll, int pitch, int yaw)
 Matrix3d GrassOdometer::centerImageCoords(int nRows, int nCols)
 {
   Matrix3d transMat = MatrixXd::Identity(3,3);
-  transMat(0,3) = -(nRows-1)/2;
-  transMat(1,3) = -(nCols-1)/2;
+  transMat(0,0) = 1;
+  transMat(1,1) = 1;
+  transMat(0,2) = -(nRows-1)/2;
+  transMat(1,2) = -(nCols-1)/2;
+
   return transMat;
 }
 
@@ -190,6 +202,15 @@ Matrix2d GrassOdometer::ImgRotMat(double angle)
   rotMat(0,1) = sin(angle);
   rotMat(1,0) = -sin(angle);
   rotMat(1,1) = cos(angle);
+  return rotMat;
+}
+
+Matrix3d GrassOdometer::HomogImgRotMat(double angle)
+{
+  Matrix3d rotMat = MatrixXd::Zero(3,3);
+  rotMat.topLeftCorner(2,2) = ImgRotMat(angle);
+  rotMat(2,2) = 1;
+  return rotMat;
 }
 
 
