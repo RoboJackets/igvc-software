b03ace25cab20fe128e4e35e9d3f2f1e7badf03c
Matthew Barulic <matthew.barulic@gmail.com>
2013-05-24 17:45:51 -0400

    Matt's big commit. It's got like stabilization and a new ardupilot and such.

---
+++ 2013/software/src/common/utils/StringUtils.hpp
@@ -0,0 +1,23 @@
+#ifndef STRINGUTILS_HPP_INCLUDED
+#define STRINGUTILS_HPP_INCLUDED
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
+#endif // STRINGUTILS_HPP_INCLUDED
