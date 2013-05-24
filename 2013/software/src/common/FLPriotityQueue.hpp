5aa20e02e8d28222755e4761df34fbba3286cdd7
atrimm <alexander.trimm@gmail.com>
2013-05-24 02:20:35 -0400

    Initial commit of Visual Odometry code, currently broken, commiting in case my compy gets stolen walking home

--- 2013/software/src/common/FLPriotityQueue.hpp
+++ 2013/software/src/common/FLPriotityQueue.hpp
@@ -2,57 +2,92 @@
 #define FLPRIOTITYQUEUE_H
 
 
+#include "opencv2/features2d/features2d.hpp"
 #include <queue>
 
 
 using namespace std;
+using namespace cv;
 
 class HarrisScore
 {
   public:
-    inline HarrisScore(double score, int row, int col) : _score(score), _row(row), _col(col)
+    HarrisScore() : _score(0), _row(0), _col(0)
     {
     }
 
-    inline double score()
+    HarrisScore(double score, int row, int col) : _score(score), _row(row), _col(col)
+    {
+    }
+
+    double score()
     {
       return _score;
     }
 
-    inline int row()
+    int row()
     {
       return _row;
     }
-    inline int col()
+    int col()
     {
       return _col;
     }
 
-    inline friend bool operator>(HarrisScore score1, HarrisScore score2)
+    friend bool operator>(HarrisScore score1, HarrisScore score2)
     {
       return score1.score() > score2.score();
     }
 
-    inline bool operator>(HarrisScore otherScore)
+    bool operator>(HarrisScore otherScore)
     {
       return score() > otherScore.score();
     }
 
-    inline unsigned int operator-(HarrisScore& otherScore)
+    unsigned int operator-(HarrisScore& otherScore)
     {
       return score()-otherScore.score();
     }
 
 
-    inline friend bool operator<(HarrisScore& score1, HarrisScore& score2)
+    friend bool operator<(HarrisScore& score1, HarrisScore& score2)
     {
       return score1.score() < score2.score();
     }
 
-    inline friend unsigned int operator+(HarrisScore& score1, HarrisScore& score2)
+    friend unsigned int operator+(HarrisScore& score1, HarrisScore& score2)
     {
       return score1.score() + score2.score();
     }
+
+    friend ostream& operator<< (ostream &out, HarrisScore& score)
+    {
+      out << "Score: " << score.score() << ". Row: " << score.row() << ". Col: " << score.col() << "." << std::endl;
+      return out;
+    }
+
+    static void toKeyPointArray(HarrisScore* scores, int numEl, KeyPoint* points, int diameter=8)
+    {
+      KeyPoint nextPoint;
+      for (int i =0;i<numEl;i++)
+      {
+        nextPoint = KeyPoint(scores[i].col(), scores[i].row(), 8);
+        points[i] = nextPoint;
+      }
+    }
+
+    static void toKeyPointVector(HarrisScore* scores, int numEl, Vector<KeyPoint> points, int diameter=8)
+    {
+      KeyPoint nextPoint;
+      for (int i =0;i<numEl;i++)
+      {
+        nextPoint = KeyPoint(scores[i].col(), scores[i].row(), 8);
+        points.push_back(nextPoint);
+      }
+    }
+
+
+
 
   private:
     double _score;
@@ -71,7 +106,28 @@ class FLPriotityQueue
     dataType top();
     bool push(dataType);
     bool pop(dataType&);
+    void unload(dataType* array);
     virtual ~FLPriotityQueue();
+
+    friend ostream& operator<< (ostream &out, FLPriotityQueue<dataType, comparison>& queue)
+    {
+      int length=queue.size();
+      dataType array[length];
+      int i = 0;
+      for(i=0;i<length;i++)
+      {
+        queue.pop(array[i]);
+        cout << array[i];
+      }
+
+      for(i=0;i<length;i++)
+      {
+        queue.push(array[i]);
+      }
+    cout.flush();
+    }
+
+
   protected:
   private:
       //priority_queue< int, vector<int>, greater<int> > data;
@@ -138,10 +194,25 @@ bool FLPriotityQueue<dataType, comparison>::push( const dataType x)
   }
 }
 
+
+template <typename dataType, typename comparison>
+void FLPriotityQueue<dataType, comparison>::unload(dataType* array)
+{
+  for(int i =0;i<size();i++)
+  {
+    array[i] = pop();
+  }
+}
+
+
 template <typename dataType, typename comparison>
 FLPriotityQueue<dataType, comparison>::~FLPriotityQueue()
 {
   //dtor
 }
+
+
+
+
 
 #endif // FLPRIOTITYQUEUE_H
