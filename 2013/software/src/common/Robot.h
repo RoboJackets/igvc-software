e059ae4521f510e72caacf04d4c0d7e50e1c21f4
atrimm <alexander.trimm@gmail.com>
2013-05-25 19:08:09 -0400

    Addition of Robot class encapsulating all relavent physical constant, additions to GrassOdometer

---
+++ 2013/software/src/common/Robot.h
@@ -0,0 +1,43 @@
+
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+
+class Robot
+{
+  public:
+    Robot();
+    static Robot Misti();
+    static Robot Roxii();
+    double Baseline();
+    double HeightOfMast();
+    double Mast2Center();
+    double HorizontalCamOffset();
+    double TireDiameter();
+    double TireRadius();
+    double TireCircumference();
+    double Lidar2Center();
+    double CameraAngle();
+    void Baseline(double);
+    void HeightOfMast(double);
+    void Mast2Center(double);
+    void HorizontalCamOffset(double);
+    void TireDiameter(double);
+    void CameraAngle(double);
+    void Lidar2Center(double);
+    virtual ~Robot();
+  private:
+    double _Baseline;
+    double _HeightOfMast;
+    double _Mast2Center;
+    double _HorizontalCamOffset;
+    double _TireDiameter;
+    double _CameraAngle;
+    double _Lidar2Center;
+};
+
+#endif // ROBOT_H
