51a52e7c258a9bbb4a1f0b9bea1636e40b6779b8
Matthew Barulic <matthew.barulic@gmail.com>
2013-05-26 15:59:26 -0400

    Added pulling encoder position data in laptop-side code.

--- 2013/software/src/actuators/motors/MotorDriver/TestMotorDriver.cpp
+++ 2013/software/src/actuators/motors/MotorDriver/TestMotorDriver.cpp
@@ -3,11 +3,46 @@
 
 using namespace std;
 
+class EncoderListener
+{
+public:
+    MotorEncoderDriver2013 *_enc;
+
+    EncoderListener(MotorEncoderDriver2013 *enc)
+        : LonNewPose(this)
+    {
+        _enc = enc;
+        enc->onNewPosition += &LonNewPose;
+    }
+
+    void onNewPose(EncPose pose)
+    {
+        cout << pose.x << "\t" << pose.y << "\t" << pose.theta << endl;
+    }
+    LISTENER(EncoderListener, onNewPose, EncPose);
+
+    ~EncoderListener()
+    {
+        _enc->onNewPosition -= &LonNewPose;
+        _enc = 0;
+    }
+};
+
 int main()
 {
     MotorEncoderDriver2013 driver;
 
-    cout << "Forward" << endl;
+    EncoderListener list(&driver);
+
+    cout << "initialization done" << endl;
+
+    driver.setVelocities(0.5, 1.0);
+
+    cout << "main sleeping..." << endl;
+
+    sleep(10);
+
+    /*cout << "Forward" << endl;
 
     for(double speed = 0; speed < 1.0; speed += 0.05)
     {
@@ -22,7 +57,7 @@ int main()
         driver.setVelocities(speed, speed);
         cout << "\t" << speed << endl;
         sleep(1);
-    }
+    }*/
 
     cout << "Stop" << endl;
     driver.stop();
