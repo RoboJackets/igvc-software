cfa16231cb15f5210e20a5865bf85d651ae5c131
atrimm <alexander.trimm@gmail.com>
2013-05-29 22:00:01 -0400

    Image Data Stuff

---
+++ 2013/software/src/sensors/DataStructures/StereoImageData.hpp
@@ -0,0 +1,44 @@
+#ifndef STEREOIMAGEDATA_H
+#define STEREOIMAGEDATA_H
+
+#include "sensors/DataStructures/ImageData.hpp"
+
+class StereoImageData : public SensorData
+{
+  public:
+    StereoImageData() {}
+    StereoImageData(Mat& left, Mat& right)
+    {
+      _leftImage = left;
+      _rightImage = right;
+    }
+
+    ImageData left()
+    {
+      return ImageData(_leftImage, time());
+    }
+
+    ImageData right()
+    {
+      return ImageData(_leftImage, time());
+    }
+
+    Mat& leftMat()
+    {
+      return _leftImage;
+    }
+
+    Mat& rightMat()
+    {
+      return _rightImage;
+    }
+
+    virtual ~StereoImageData() {}
+  protected:
+  private:
+    Mat _leftImage;
+    Mat _rightImage;
+
+};
+
+#endif // STEREOIMAGEDATA_H
