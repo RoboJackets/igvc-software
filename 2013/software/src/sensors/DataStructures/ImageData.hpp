31b3afd2b400b90a2e1a329ef05de23acd7e1fa3
atrimm <alexander.trimm@gmail.com>
2013-05-29 22:00:01 -0400

    Image Data Stuff

---
+++ 2013/software/src/sensors/DataStructures/ImageData.hpp
@@ -0,0 +1,26 @@
+#ifndef IMAGEDATA_H
+#define IMAGEDATA_H
+
+
+#include <opencv2/opencv.hpp>
+#include "sensors/DataStructures/SensorData.h"
+
+using namespace cv;
+
+class ImageData : public SensorData
+{
+  public:
+    ImageData() {}
+    ImageData(Mat image): SensorData(), _img(image) {}
+    ImageData(Mat image, double time): SensorData(time),_img(image) {}
+    ImageData deepCopy() { return ImageData(_img.clone(),time()); }
+    Mat& mat() {return _img;}
+    virtual ~ImageData() {}
+
+
+
+  private:
+    Mat _img;
+};
+
+#endif // IMAGEDATA_H
