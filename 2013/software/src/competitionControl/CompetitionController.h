992aadcc3cc627d5c5360086ac3b0e01b4ea7978
IGVC <robojackets@IGVC1.(none)>
2013-06-07 23:03:15 -0400

    Friday's big commit.

--- 2013/software/src/competitionControl/CompetitionController.h
+++ 2013/software/src/competitionControl/CompetitionController.h
@@ -1,26 +1,60 @@
 #ifndef COMPETITIONCONTROLLER_H
 #define COMPETITIONCONTROLLER_H
 
+#include <IGVC.hpp>
 #include <sensors/GPS/GPS.hpp>
 #include <competitionControl/WaypointReader.h>
 #include <sensors/camera3D/StereoSource.hpp>
 #include <sensors/lidar/Lidar.h>
+#include <sensors/ardupilot/Ardupilot.hpp>
 #include <vector>
+
+namespace IGVC
+{
+namespace Control
+{
+
+enum Mode
+{
+    ObstacleAvoidance,
+    WaypointNavigation
+};
 
 class CompetitionController
 {
     public:
         CompetitionController(IGVC::Sensors::GPS* gps,
+                              IGVC::Sensors::Lidar* lidar,
+                              StereoSource* stereoCam,
+                              Ardupilot* imu,
                               WaypointReader* waypointReader);
         virtual ~CompetitionController();
+        bool isRunning();
     protected:
     private:
         IGVC::Sensors::GPS* _gps;
+        IGVC::Sensors::Lidar* _lidar;
+        StereoSource* _stereoCam;
+        Ardupilot* _imu;
         WaypointReader* _waypointReader;
         std::vector<GPSData> _gpsBuffer;
+        const unsigned int GPS_BUFFER_SIZE;
+        GPSData _currentAvgGPS;
+        Mode _mode;
+        double _currentHeading;
 
         void OnNewGPSData(GPSData data);
         LISTENER(CompetitionController, OnNewGPSData, GPSData);
+
+        void OnNewIMUData(IMURawData data);
+        LISTENER(CompetitionController, OnNewIMUData, IMURawData);
+
+        double headingFromAToB(GPSData A, GPSData B);
 };
+
+}
+}
+
+
 
 #endif // COMPETITIONCONTROLLER_H
--- 2013/software/src/competitionControl/TestCompetitionController.cpp
+++ 2013/software/src/competitionControl/TestCompetitionController.cpp
@@ -1,8 +1,23 @@
 #include <competitionControl/CompetitionController.h>
+#include <sensors/GPS/HemisphereA100GPS.h>
+#include <sensors/lidar/NAV200.h>
+#include <sensors/camera3D/Bumblebee2.h>
+
+using namespace IGVC::Control;
+using namespace IGVC::Sensors;
 
 int main()
 {
-    CompetitionController controller(NULL, NULL);
+    WaypointReader waypointReader;
+    waypointReader.LoadWaypoints("");
 
+    CompetitionController controller(new HemisphereA100GPS(),
+                                     new NAV200(),
+                                     new Bumblebee2(),
+                                     new Ardupilot(),
+                                     &waypointReader);
 
+    while(controller.isRunning()) { }
+
+    return 0;
 }
