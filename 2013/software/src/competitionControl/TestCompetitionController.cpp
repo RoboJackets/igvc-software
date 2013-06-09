a8a5aa9b5311291facbed57cbf31fc9ca94a4469
IGVC <robojackets@IGVC1.(none)>
2013-06-08 20:14:05 -0400

    Outdoor testing 1

--- 2013/software/src/competitionControl/TestCompetitionController.cpp
+++ 2013/software/src/competitionControl/TestCompetitionController.cpp
@@ -4,32 +4,50 @@
 #include <sensors/camera3D/Bumblebee2.h>
 #include <sensors/camera2D/laneDetection/CameraListener.hpp>
 #include <actuators/motors/MotorDriver/MotorEncoderDriver2013.h>
+#include <iostream>
 
 using namespace IGVC::Control;
 using namespace IGVC::Sensors;
+using namespace std;
 
 int main()
 {
+    cout << "Loading waypoints..." << endl;
+
     WaypointReader waypointReader;
-    waypointReader.LoadWaypoints("");
+    waypointReader.LoadWaypoints("/home/robojackets/practiceCourse.txt");
+
+    cout << "Connecting to GPS..." << endl;
+    HemisphereA100GPS GPS;
+
+    cout << "Connecting to motor driver..." << endl;
 
     MotorEncoderDriver2013 driver;
 
+    cout << "Connecting to camera..." << endl;
+
     Bumblebee2 camera;
+
+    cout << "Connecting to lidar..." << endl;
 
     NAV200 lidar;
 
+    cout << "Initializing listener..." << endl;
+
     CameraListener camList(&camera, &lidar);
 
+    cout << "Initializing controller..." << endl;
 
-
-    CompetitionController controller(new HemisphereA100GPS(),
+    CompetitionController controller(&GPS,
                                      &camList.OnNewData,
-                                     new Ardupilot(),
                                      &waypointReader,
                                      &driver);
 
+    cout << "Running..." << endl;
+
     while(controller.isRunning()) { }
+
+    cout << "done" << endl;
 
     return 0;
 }
---
+++ 2013/software/src/sensors/camera2D/laneDetection/TestLaneFilter.cpp
@@ -0,0 +1,52 @@
+#include <sensors/lidar/NAV200.h>
+#include <sensors/camera3D/Bumblebee2.h>
+#include <sensors/camera2D/laneDetection/CameraListener.hpp>
+#include <iostream>
+
+using namespace IGVC::Sensors;
+using namespace std;
+
+class CameraListenerListener
+{
+public:
+    CameraListenerListener(CameraListener* camList)
+        : LOnNewFileteredFrame(this)
+    {
+        _camList = camList;
+        _camList->OnNewFilteredFrame += &LOnNewFileteredFrame;
+    }
+
+private:
+    CameraListener* _camList;
+
+    void OnNewFileteredFrame(Mat frame)
+    {
+        imshow("Frame", frame);
+    }
+    LISTENER(CameraListenerListener, OnNewFileteredFrame, Mat);
+};
+
+int main()
+{
+
+    Bumblebee2 camera;
+
+    cout << "Connecting to lidar..." << endl;
+
+    NAV200 lidar;
+
+    cout << "Initializing listener..." << endl;
+
+    CameraListener camList(&camera, &lidar);
+
+    CameraListenerListener camListList(&camList);
+
+
+    cout << "Running..." << endl;
+
+    while(true) { }
+
+    cout << "done" << endl;
+
+    return 0;
+}
