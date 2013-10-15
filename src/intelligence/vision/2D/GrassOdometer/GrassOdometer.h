ddcee66402aa3d648409e43dd718ecaacef77979
Matthew Barulic <matthew.barulic@gmail.com>
2013-10-15 15:46:08 -0400

    More hierarchy cleanup: Imports GrassOdometer from 2013 code base. Consolidates unnecessary .h/.cpp pairs in DataStructures to .hpp Relocates ColorRange to utils Temporarily imports Robot. This class will be replaced via the new configuration system.

---
+++ src/intelligence/vision/2D/GrassOdometer/GrassOdometer.h
@@ -0,0 +1,56 @@
+#ifndef GRASSODOMETER_H
+#define GRASSODOMETER_H
+
+#include <list>
+#include <eigen3/Eigen/Dense>
+
+#include <common/events/Event.hpp>
+#include <common/utils/ColorRange.h>
+#include <common/Robot.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <hardware/sensors/DataStructures/VisOdomData.hpp>
+#include <hardware/sensors/camera3D/CameraInfo.h>
+#include <hardware/sensors/DataStructures/ImageData.hpp>
+#include <common/utils/ImageUtils.h>
+
+
+using namespace cv;
+using namespace Eigen;
+using namespace IGVC;
+
+class RobotPosition;
+
+class GrassOdometer
+{
+  public:
+    GrassOdometer(ColorRange limits, int numKeyPoints=100);
+    void processImage(ImageData src);
+    void RemoveNonGrassPts(Mat& frame, std::vector<KeyPoint>& keypoints);
+    void ShowCorrespondence(Mat& frame1, std::vector<KeyPoint>& keypoints_1, Mat& frame2, std::vector<KeyPoint>& keypoints_2,
+                            std::vector<DMatch>&  good_matches);
+    void MatchPointsFLANN(std::vector<DMatch>& good_matches, Mat& descriptors_1, Mat& descriptors_2 );
+    Event<VisOdomData> onNewData;
+
+
+    ~GrassOdometer();
+  private:
+    Robot _robot;
+    IGVC::CameraInfo _cam;
+    ColorRange _colors;
+    int _numKeyPoints;
+    ImageData _lastFrame;
+    std::vector<KeyPoint> _previousKeyPoints;
+    Mat _lastFrameDescriptors;
+    MatrixXd _lastFramePositions;
+    bool _firstFrame;
+
+
+    void findKeypointsSURF(Mat& frame, vector<KeyPoint>& theKeyPoints, Mat& theDescriptors, MatrixXd& thePositions);
+    void findDeltas(Mat& newDescriptors, MatrixXd& newPos, double& deltax, double deltay);
+
+
+
+};
+
+#endif //GRASSODOMETER_H
