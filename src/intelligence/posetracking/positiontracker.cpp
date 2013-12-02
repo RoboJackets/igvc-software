37b99360a48b2eada0e6e2e5b2b8ee0b924e1d6d
Matthew Barulic <matthew.barulic@gmail.com>
2013-12-02 10:44:46 -0500

    Adds listeners to PositionTracker for GPSData, IMUData, and MotorCommand. Adds GetPosition() to PositionTracker. Adds MotorCommand paramter to DelaFromMotionCommand().
    Former-commit-id: 6ea8cd6395ff5cb3133ca6580304c1fdc683e81c

--- src/intelligence/posetracking/positiontracker.cpp
+++ src/intelligence/posetracking/positiontracker.cpp
@@ -2,45 +2,77 @@
 #include <cmath>
 
 PositionTracker::PositionTracker()
+    : LOnGPSData(this),
+      LOnMotionCommand(this),
+      LOnIMUData(this)
 {
+}
+
+Position PositionTracker::GetPosition()
+{
+    return _current_estimate;
 }
 
 Position PositionTracker::UpdateWithMeasurement(Position S, Position Measurement)
 {
     Position result;
-    result.X.SetValue( ( S.X.Value()*Measurement.X.Variance() + Measurement.X.Value()*S.X.Variance() ) / (S.X.Variance()+Measurement.X.Variance()) );
-    result.Y.SetValue( ( S.Y.Value()*Measurement.Y.Variance() + Measurement.Y.Value()*S.Y.Variance() ) / (S.Y.Variance()+Measurement.Y.Variance()) );
-    result.T.SetValue( ( S.T.Value()*Measurement.T.Variance() + Measurement.T.Value()*S.T.Variance() ) / (S.T.Variance()+Measurement.T.Variance()) );
+    result.Latitude.SetValue( ( S.Latitude.Value()*Measurement.Latitude.Variance() + Measurement.Latitude.Value()*S.Latitude.Variance() ) / (S.Latitude.Variance()+Measurement.Latitude.Variance()) );
+    result.Longitude.SetValue( ( S.Longitude.Value()*Measurement.Longitude.Variance() + Measurement.Longitude.Value()*S.Longitude.Variance() ) / (S.Longitude.Variance()+Measurement.Longitude.Variance()) );
+    result.Heading.SetValue( ( S.Heading.Value()*Measurement.Heading.Variance() + Measurement.Heading.Value()*S.Heading.Variance() ) / (S.Heading.Variance()+Measurement.Heading.Variance()) );
 
-    result.X.SetVariance( 1.0 / (1.0/S.X.Variance() + 1.0 / Measurement.X.Variance()) );
-    result.Y.SetVariance( 1.0 / (1.0/S.Y.Variance() + 1.0 / Measurement.Y.Variance()) );
-    result.T.SetVariance( 1.0 / (1.0/S.T.Variance() + 1.0 / Measurement.T.Variance()) );
+    result.Latitude.SetVariance( 1.0 / (1.0/S.Latitude.Variance() + 1.0 / Measurement.Latitude.Variance()) );
+    result.Longitude.SetVariance( 1.0 / (1.0/S.Longitude.Variance() + 1.0 / Measurement.Longitude.Variance()) );
+    result.Heading.SetVariance( 1.0 / (1.0/S.Heading.Variance() + 1.0 / Measurement.Heading.Variance()) );
     return result;
 }
 
 Position PositionTracker::UpdateWithMotion(Position S, Position Delta)
 {
     Position result;
-    result.X.SetValue(S.X.Value() + Delta.X.Value());
-    result.Y.SetValue(S.Y.Value() + Delta.Y.Value());
-    result.T.SetValue(S.T.Value() + Delta.T.Value());
+    result.Latitude.SetValue(S.Latitude.Value() + Delta.Latitude.Value());
+    result.Longitude.SetValue(S.Longitude.Value() + Delta.Longitude.Value());
+    result.Heading.SetValue(S.Heading.Value() + Delta.Heading.Value());
 
-    result.X.SetVariance(S.X.Variance() + Delta.X.Variance());
-    result.Y.SetVariance(S.Y.Variance() + Delta.Y.Variance());
-    result.T.SetVariance(S.T.Variance() + Delta.T.Variance());
+    result.Latitude.SetVariance(S.Latitude.Variance() + Delta.Latitude.Variance());
+    result.Longitude.SetVariance(S.Longitude.Variance() + Delta.Longitude.Variance());
+    result.Heading.SetVariance(S.Heading.Variance() + Delta.Heading.Variance());
     return result;
 }
 
-Position PositionTracker::DelaFromMotionCommand()
+Position PositionTracker::DelaFromMotionCommand(MotorCommand cmd)
 {
     using namespace std;
-    double V, W, t;
+    double V = ( cmd.leftVel + cmd.rightVel ) / 2.0;
+    double W;
+    double t = cmd.millis / 1000.0;
     double R = V/W;
     Position delta;
-    double theta = _current_estimate.T.Value() * M_PI/180.0;
-    double thetaPrime = (_current_estimate.T.Value() + W*t) * M_PI/180.0;
-    delta.T.SetValue(W*t);
-    delta.X.SetValue(R*(cos(theta) - cos(thetaPrime)));
-    delta.Y.SetValue(R*(sin(thetaPrime) - sin(theta)));
+    double theta = _current_estimate.Heading.Value() * M_PI/180.0;
+    double thetaPrime = (_current_estimate.Heading.Value() + W*t) * M_PI/180.0;
+    delta.Heading.SetValue(W*t);
+    delta.Latitude.SetValue(R*(cos(theta) - cos(thetaPrime)));
+    delta.Longitude.SetValue(R*(sin(thetaPrime) - sin(theta)));
+    // TODO - handle variances
     return delta;
+}
+
+void PositionTracker::OnGPSData(GPSData data)
+{
+    Position measurement;
+    measurement.Latitude.SetValue(data.Lat());
+    measurement.Longitude.SetValue(data.Long());
+    // TODO - make sure the GPS actually outputs heading data
+    measurement.Heading.SetValue(data.Heading());
+    // TODO - handle variances
+    _current_estimate = UpdateWithMeasurement(_current_estimate, measurement);
+}
+
+void PositionTracker::OnIMUData(IMUData data)
+{
+
+}
+
+void PositionTracker::OnMotionCommand(MotorCommand cmd)
+{
+    _current_estimate = UpdateWithMotion(_current_estimate, DelaFromMotionCommand(cmd));
 }
