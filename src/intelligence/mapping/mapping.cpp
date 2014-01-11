64651c3cbbefa18118d9262250ec920a722f14f3
Al Chaussee <alchaussee@ubuntu.(none)>
2014-01-11 16:10:33 -0500

    pseudocode for incorporation of robot posisition and heading to mapping with lidar
    Former-commit-id: feaae804811264e299815da51024082bb580a7da

--- src/intelligence/mapping/mapping.cpp
+++ src/intelligence/mapping/mapping.cpp
@@ -3,6 +3,7 @@
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
 #include <math.h>
+#include "intelligence/posetracking/RobotPosition.h"
 
 Mapping::Mapping(IGVC::Sensors::Lidar *lidar):LOnLidarData(this)
 {
@@ -17,6 +18,23 @@ Mapping::Mapping(IGVC::Sensors::Lidar *lidar):LOnLidarData(this)
         _lidar = lidar;
         _lidar->onNewData += &LOnLidarData;
     }
+    /*
+     if(gps)
+     {
+         startLat = RobotPosition::currentLat();
+         startLong = RobotPosition::currentLong();
+     }
+
+    float R = 6378.137; // Radius of earth in KM
+    var dLat = (lat2 - lat1) * Math.PI / 180;
+    var dLon = (lon2 - lon1) * Math.PI / 180;
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d * 1000; // meters
+*/
     cloud = _cloud;
 }
 
@@ -28,6 +46,43 @@ Mapping::~Mapping()
 
 void Mapping::OnLidarData(IGVC::Sensors::LidarState state)
 {
+    /*
+    float lat2 = RobotPosition::currentLat();
+    float lon2 = RobotPosition::currentLong();
+    float dLat = (lat2 - startLat) * Math.PI / 180;
+    float dLon = (lon2 - startLon) * Math.PI / 180;
+    float a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(startLat * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLon/2) * Math.sin(dLon/2);
+    float c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    float d = R * c * 1000; meters
+
+    float heading = RobotPosition::currentHeading();
+    float xDist;
+    float yDist;
+
+    if(heading>=0.0 && heading<=90)
+    {
+        xDist = Math.cos(90.0-heading)*d;
+        yDist  = Math.sin(90.0-heading)*d;
+    }
+    else if(heading>90 && heading<180)
+    {
+        xDist = Math.cos(heading-90.0)*d;
+        yDist = Math.sin(heading-90.0)*d;
+    }
+    else if(heading>=180 && heading<=270)
+    {
+        xDist = Math.cos(270.0-heading)*d;
+        yDist = Math.sin(270.0-heading)*d;
+    }
+    else
+    {
+        xDist = Math.cos(heading-270.0)*d;
+        yDist = Math.sin(heading-270.0)*d;
+    }
+
+    add xDist and yDist to x and y points
+    */
+
     for (int i=0;i<1024;i++)
     {
         if(state.points[i].valid)
@@ -35,6 +90,7 @@ void Mapping::OnLidarData(IGVC::Sensors::LidarState state)
             cloud->points[i].x = cos(state.points[i].angle)*state.points[i].distance;
             cloud->points[i].y = sin(state.points[i].angle)*state.points[i].distance;
             cloud->points[i].z = 0;
+
         }
     }
     onNewMap(cloud);
