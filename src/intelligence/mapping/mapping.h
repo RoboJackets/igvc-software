3fb62a944828977b5b61f012a6cb26d590e1c27c
Al Chaussee <alchaussee@ubuntu.(none)>
2014-01-15 21:15:10 -0500

    mapping now makes use of distance travelled and heading to create point cloud added methods to convert from gps points to meters distance travelled and current position to find starting gps coords
    Former-commit-id: 0e7564d4e49d73a2cd25bcdc2c17f2ffb3e134e8

--- src/intelligence/mapping/mapping.h
+++ src/intelligence/mapping/mapping.h
@@ -2,9 +2,11 @@
 #define MAPPING_H
 
 #include "hardware/sensors/lidar/Lidar.h"
+#include "hardware/sensors/gps/GPS.hpp"
 #include "common/events/Event.hpp"
 #include <pcl/point_types.h>
 #include <pcl/io/file_io.h>
+#include "intelligence/posetracking/RobotPosition.h"
 
 /*!
  * \brief Maps the course from sensor data.
@@ -13,7 +15,7 @@
 class Mapping
 {
 public:
-    Mapping(IGVC::Sensors::Lidar *lidar);
+    Mapping(IGVC::Sensors::Lidar *lidar, RobotPosition robot);
     ~Mapping();
     pcl::PointCloud<pcl::PointXYZ>::Ptr getCloud();
     //void saveCloud(std::string path);
@@ -24,6 +26,9 @@ private:
     void OnLidarData(IGVC::Sensors::LidarState state);
     LISTENER(Mapping, OnLidarData, IGVC::Sensors::LidarState)
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+    double startLat;
+    double startLon;
+    RobotPosition robot;
 };
 
 #endif // MAPPING_H
