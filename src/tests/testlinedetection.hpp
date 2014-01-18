e7bcde3d5e6247165b15f68e50e185a72d417999
Matthew Barulic <matthew.barulic@gmail.com>
2014-01-18 14:37:13 -0500

    Adding test case for line detection code.
    Former-commit-id: 6e72827568ca39d04afb4ee33128d941f969a056 [formerly 218bca446b4f4898dc8a7ec998e54a0181f578ea]
    Former-commit-id: 6e72827568ca39d04afb4ee33128d941f969a056

---
+++ src/tests/testlinedetection.hpp
@@ -0,0 +1,17 @@
+#ifndef TESTLINEDETECTION_HPP
+#define TESTLINEDETECTION_HPP
+
+#include <QtTest>
+
+class TestLineDetection: public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void testCase1()
+    {
+
+    }
+};
+
+#endif // TESTLINEDETECTION_HPP
