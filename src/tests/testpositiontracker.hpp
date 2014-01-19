b069a2c9af52c7e6946547b7a9a70b3e64a323b8
Matthew Barulic <matthew.barulic@gmail.com>
2014-01-19 05:21:27 -0500

    Adds test cases for GPS updates and stationary IMU updates. Adds variance copying in OnGPSData. Fixes bug in MeasurementFromIMUData. Adds large initial variances for position data.

--- src/tests/testpositiontracker.hpp
+++ src/tests/testpositiontracker.hpp
@@ -2,15 +2,45 @@
 #define TESTPOSITIONTRACKER_HPP
 
 #include <QTest>
+#include <math.h>
+#include <iostream>
 #include <intelligence/posetracking/positiontracker.h>
 
 class TestPositionTracker: public QObject
 {
     Q_OBJECT
 private Q_SLOTS:
-    void testCase1()
+    void testUpdateFromGPS()
     {
+        PositionTracker tracker;
+        Event<GPSData> testEvent;
+        testEvent += &tracker.LOnGPSData;
+        auto start = tracker.GetPosition();
+        auto data = GPSData(1,1,M_PI_2,0);
+        data.LatVar(8);
+        data.LongVar(8);
+        data.HeadingVar(8);
+        testEvent(data);
+        auto end = tracker.GetPosition();
+        QVERIFY(start != end);
+        QVERIFY(end.Latitude - 0.999201 < 0.000001);
+        QVERIFY(end.Longitude - 0.999201 < 0.000001);
+        QVERIFY(end.Heading - 1.56954 < 0.00001);
+        QVERIFY(end.Latitude.Variance - 7.99361 < 0.00001);
+        QVERIFY(end.Longitude.Variance - 7.99361 < 0.00001);
+        QVERIFY(end.Heading.Variance - 7.99361 < 0.00001);
+    }
 
+    void testUpdateFromIMU_Stationary()
+    {
+        PositionTracker tracker;
+        Event<IMUData> testEvent;
+        testEvent += &tracker.LOnIMUData;
+        auto start = tracker.GetPosition();
+        auto data = IMUData(0,0,0,0,0,0);
+        testEvent(data);
+        auto end = tracker.GetPosition();
+        QCOMPARE(start, end);
     }
 };
 
