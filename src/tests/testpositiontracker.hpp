ea1b49b06cc5e84d9d5c6545ea111441865460ed
Matthew Barulic <matthew.barulic@gmail.com>
2014-01-19 04:34:11 -0500

    Adds test case for PositionTracker.
    Former-commit-id: 87178e1ae2c0bc72312fb1e7ea6e2583f47f9100

---
+++ src/tests/testpositiontracker.hpp
@@ -0,0 +1,17 @@
+#ifndef TESTPOSITIONTRACKER_HPP
+#define TESTPOSITIONTRACKER_HPP
+
+#include <QTest>
+#include <intelligence/posetracking/positiontracker.h>
+
+class TestPositionTracker: public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testCase1()
+    {
+
+    }
+};
+
+#endif // TESTPOSITIONTRACKER_HPP
