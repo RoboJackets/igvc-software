4bb4f1e07e812ffebe088be2bc639701a74da80f
Claire Bergman <claire@claire-Latitude-E4310.(none)>
2014-01-22 21:11:23 -0500

    Documented code using Doxygen
    Former-commit-id: 073e827e49f311092d8d8a1240ac2c8e1090293a

--- src/intelligence/linedetection/linedetector.h
+++ src/intelligence/linedetection/linedetector.h
@@ -4,18 +4,20 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
+///\def ABS(a)
+///\brief a Macro that returns the absolute value of a number
 #define ABS(a) (((a) < 0)? -1*(a):(a))
+///\def ABSDIFF(a,b)
+///\brief a Macro that returns the positive difference between a and b
 #define ABSDIFF(a, b) (ABS((a)-(b)))
-#define AVGDIFF(a, b ,c) ((ABSDIFF(a,b)+ABSDIFF(b,c)+ABSDIFF(a,c))/3)
-#define LOWTHRESHOLDCANNY 40
-#define RATIOCANNY 1.05
+///\def DELAY
+///\brief the Delay to use when displaying images. 0 for infinity
 #define DELAY 1
-#define COLORRATIO 1.4
-#define COLORRATIOMAX 1.47
-#define GAUSSSIZE 19
-#define WHITETHRESHOLDBLOCK 5
+///\def GAUSSIZE
+///\brief The size of the Gaussian blur. The bigger the greater the blur
+///       Must be odd!
+#define GAUSSSIZE 7
 
-typedef void (*fpt) (int, void*);
 
 using namespace std;
 using namespace cv;
@@ -26,8 +28,6 @@ public:
     LineDetector(std::string imgFile);
     void applyAlgorithm();
     bool loadImage(std::string imgFile);
-    //Mat src;
-   // Mat dst;
 private:
     int getBlockAvg(int rowl, int rowu, int coll, int colu);
     int checkNbors(int rowStartNum, int colStartNum, int rowSize, int colSize, int threshold);
