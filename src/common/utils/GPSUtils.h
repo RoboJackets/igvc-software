c493b67ceee966e4cd2cbca8344a92655ab23015
Al Chaussee <alchaussee@ubuntu.(none)>
2014-01-25 16:26:06 -0500

    coordsAfterMotion works with marginal error
    Former-commit-id: 2f93544067a085c0e6350a12b2977495bd4e78fe

--- src/common/utils/GPSUtils.h
+++ src/common/utils/GPSUtils.h
@@ -9,21 +9,36 @@
 class GPSUtils
 {
     public:
-        static void latLongCartesianUpdate(double oldLat, double oldLong, double deltaX, double deltaY, double &newLat, double &newLong)
-        {
-            newLat = oldLat + deltaY/111111;
-            newLong = oldLong + deltaX/(111111*cos(oldLat));
-        }
+
+        static constexpr double EARTHRADIUS_KM = 6378.137;
 
         static double coordsToMeter(double lat1, double lon1, double lat2, double lon2)
         {
-            double dLat = (lat2 - lat1) * M_PI / 180;
-            double dLon = (lon2 - lon1) * M_PI / 180;
-            double a = sin(dLat/2) * sin(dLat/2) + cos(lat1 * M_PI / 180) * cos(lat2 * M_PI / 180) * sin(dLon/2) * sin(dLon/2);
-            double c = 2 * atan2(sqrt(a), sqrt(1-a));
+            double dLat = (lat2 - lat1) * (M_PI / 180.0);
+            double dLon = (lon2 - lon1) * (M_PI / 180.0);
+            lat1 = lat1 * (M_PI/180.0);
+            lat2 = lat2 * (M_PI/180.0);
+            double a = sin(dLat/2.0) * sin(dLat/2.0) +
+                        cos(lat1) * cos(lat2) * sin(dLon/2.0) * sin(dLon/2.0);
+            double c = 2.0 * atan2(sqrt(a), sqrt(1.0-a));
             double d = GPSUtils::EARTHRADIUS_KM * c;
-            return d * 1000; // meters
+            return d * 1000.0; // meters
         }
 
-        static constexpr double EARTHRADIUS_KM = 6378.137;
+        static void coordAfterMotion(double lat1, double lon1, double d, double a, double &newLat, double &newLon)
+        {
+            //angle in radians
+            //dist in meters
+            d = d/(EARTHRADIUS_KM*1000.0); //angle corresponding to arc length/dist on earth
+            //tc = tc * (M_PI/180.0); //angle from initial point to current location
+            lat1 = lat1 * (M_PI/180.0);
+            lon1 = lon1 * (M_PI/180.0);
+            newLat=asin(sin(lat1)*cos(d)+cos(lat1)*sin(d)*cos(a));
+            if (cos(newLat)==0)
+               newLon=lon1;      // endpoint a pole
+            else
+               newLon=fmod(lon1-asin(sin(a)*sin(d)/cos(newLat))+M_PI,2.0*M_PI)-M_PI;
+            newLat = newLat * (180.0/M_PI);
+            newLon = newLon * (180.0/M_PI);
+        }
 };
