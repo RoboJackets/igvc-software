feb9a068b4ca10aac9e167c411e0d6af6e82ba16
Al Chaussee <alchaussee@ubuntu.(none)>
2014-01-25 16:26:06 -0500

    coordsAfterMotion works with marginal error
    Conflicts:
    
    	src/common/utils/GPSUtils.h

--- src/tests/testgpsutils.h
+++ src/tests/testgpsutils.h
@@ -15,25 +15,38 @@ private Q_SLOTS:
     {
         using namespace std;
 
-        double lat1 = 33.778402;
-        double lon1 = -84.401301;
-        double lat2 = 33.778430;
-        double lon2 = -84.401215;
+        double lat1 =  33.787295;
+        double lon1 = -84.406323;
+        double lat2 =  33.787301;
+        double lon2 = -84.406185;
+
+        //double lat1 =     30.855795;
+        //double lon1 = -97.331163;
+        //double lat2 =      30.856040;
+        //double lon2 = -97.331447;
 
         double dist = GPSUtils::coordsToMeter(lat1,lon1,lat2,lon2);
-        std::cout<<"Distance: "<<dist<<"m"<<endl;
+        //std::cout<<"Distance(m): "<<dist<<endl;
 
-        double deltaY = 7.9238;
-        double deltaX = 3.2014;
         double newLat;
         double newLon;
 
-        GPSUtils::latLongCartesianUpdate(lat1,lon1,deltaX,deltaY,newLat,newLon);
+        double dy = lat2 - lat1;
+        double dx = cos(M_PI/180*lat1)*(lon2 - lon1);
+        double angle = atan2(dy, dx);
 
-        std::cout<<std::setprecision(9)<<"Expected\t Lat: "<<lat2<<"   Lon"<<lon2<<endl;
-        std::cout<<std::setprecision(9)<<"Was\t Lat: "<<newLat<<" Lon"<<newLon<<endl;
+        std::cout<<"Angle(deg): "<<angle * (180/M_PI)<<endl;
+
+        //GPSUtils::latLongCartesianUpdate(lat1,lon1,deltaX,deltaY,newLat,newLon);
+        GPSUtils::coordAfterMotion(lat1,lon1,dist,angle+(270*(M_PI/180.0)),newLat,newLon);
         double distBetween = GPSUtils::coordsToMeter(lat2,lon2,newLat,newLon);
-        std::cout<<"Distance between expected and calculated is: "<<distBetween<<"m"<<endl;
+
+        std::cout<<std::setprecision(9)<<"Expected\t Lat: "<<lat2<<"   Lon"<<lon2<<"   Dist: "<<dist<<endl;
+        std::cout<<std::setprecision(9)<<"Test\t Lat: "<<newLat<<"  Lon"<<newLon<<"  Dist: "<<distBetween<<endl;
+
+
+
+
     }
 };
 
