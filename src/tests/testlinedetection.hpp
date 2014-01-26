fd886cb81c2df83406d882e9d0f7a20e1cc74b71
Matthew Barulic <matthew.barulic@gmail.com>
2014-01-25 19:05:55 -0500

    More code cleanup to move display stuff out of LineDetector.
    Former-commit-id: 8d983416d5ad1acaf5d1d96ee731420440f98924 [formerly ebc3f8d74d4381806b6566342108ad7ca1470701]
    Former-commit-id: 8d983416d5ad1acaf5d1d96ee731420440f98924

--- src/tests/testlinedetection.hpp
+++ src/tests/testlinedetection.hpp
@@ -5,25 +5,38 @@
 #include <intelligence/linedetection/linedetector.h>
 #include <iostream>
 #include <common/events/Event.hpp>
-#ifdef _WIN32
-    #include <direct.h>
-    #define GetCurrentDir _getcwd
-#else
-    #include <unistd.h>
-    #define GetCurrentDir getcwd
-#endif
-//My event of type <
+
 class TestLineDetection: public QObject
 {
     Q_OBJECT
+public:
+    TestLineDetection() : LonResults(this) { }
+
+private:
+    cv::Mat src, lines;
+    bool responded;
+    void DisplayImages()
+    {
+        //Show transformed image
+        cv::imshow("Filter Practice", lines);
+        //Show original image in a different window
+        cv::imshow("Original", src);
+    }
+
+    void onResults(ImageData img)
+    {
+        responded = true;
+        lines = img.mat();
+        DisplayImages();
+    }
+    LISTENER(TestLineDetection, onResults, ImageData)
 
 private Q_SLOTS:
     void testCase1()
     {
         ///NOTE: The directory may not be the same on your computer!
         char videoFile[] = "../src/intelligence/igvc_cam_data/video/CompCourse_left0.mpeg";
-        VideoCapture cap(videoFile);
-        Mat src;
+        cv::VideoCapture cap(videoFile);
         Event<ImageData> newImageFrameEvent;
         bool success = cap.read(src);
 
@@ -33,10 +46,14 @@ private Q_SLOTS:
         }
 
         LineDetector ldl(newImageFrameEvent);
+        ldl.onNewLines += &LonResults;
 
         while (success){
+            responded = false;
             newImageFrameEvent(src);
-            waitKey(1);
+            // This checks that the LineDetector is actually responding to events
+            QTRY_VERIFY_WITH_TIMEOUT(responded, 10);
+            cv::waitKey(1);
             success = cap.read(src);
         }
     }
