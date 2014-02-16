36d312df6bc106f5e8f749a4b5f7e17892965f7f
Matthew Barulic <matthew.barulic@gmail.com>
2014-02-15 20:43:05 -0500

    Connects MapBuilder to BasicPositionTracker. Removes old GPS udev rule. A whole bunch of mapping stuff not quite finished.
    Former-commit-id: b1511fcb4070b2d94ce198651631d15535e10068

--- src/intelligence/mapping/mapbuilder.h
+++ src/intelligence/mapping/mapbuilder.h
@@ -2,33 +2,35 @@
 #define MAPPING_H
 
 #include "hardware/sensors/lidar/Lidar.h"
-#include "hardware/sensors/gps/GPS.hpp"
 #include "common/events/Event.hpp"
 #include <pcl/point_types.h>
 #include <pcl/io/file_io.h>
-#include "intelligence/posetracking/RobotPosition.h"
+#include <intelligence/posetracking/basicpositiontracker.h>
 
 /*!
  * \brief Maps the course from sensor data.
  * \author Al Chaussee
  */
-class Mapping
+class MapBuilder
 {
 public:
-    Mapping(Lidar *lidar);
-    ~Mapping();
+    MapBuilder(Lidar *lidar, BasicPositionTracker *poseTracker);
+    ~MapBuilder();
     pcl::PointCloud<pcl::PointXYZ>::Ptr getCloud();
     //void saveCloud(std::string path);
     //bool readCloud(std::string path);
     Event<pcl::PointCloud<pcl::PointXYZ>::Ptr> onNewMap;
+
+    void Clear();
+
 private:
-    Lidar *_lidar;
+
     void OnLidarData(LidarState state);
-    LISTENER(Mapping, OnLidarData, LidarState)
+    LISTENER(MapBuilder, OnLidarData, LidarState)
+
+    Lidar *_lidar;
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
-    double startLat;
-    double startLon;
-    RobotPosition robot;
+    BasicPositionTracker *poseTracker;
 };
 
 #endif // MAPPING_H
