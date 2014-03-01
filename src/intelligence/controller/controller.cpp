dc503bb8225fbe13ab9a908e60a971e96c5fdf85
Al Chaussee <alchaussee@ubuntu.(none)>
2014-03-01 15:13:13 -0500

    Adds basic competition controller to switch to the next waypoint once within 1 meter of the current one

---
+++ src/intelligence/controller/controller.cpp
@@ -0,0 +1,31 @@
+#include "controller.h"
+#include <common/utils/GPSUtils.h>
+
+
+Controller::Controller(GPSWaypointSource *source, GPS *gps) : _gps(gps)
+{
+    _source = source;
+    currentWaypoint = _source->getNext();
+    if(_gps != nullptr)
+            connect(_gps, SIGNAL(onNewData(GPSData)), this, SLOT(onNewGPS(GPSData)));
+}
+
+Controller::~Controller()
+{
+    if(_gps != nullptr)
+        disconnect(_gps, SIGNAL(onNewData(GPSData)), this, SLOT(onNewGPS(GPSData)));
+}
+
+void Controller::onNewGPS(GPSData data)
+{
+    if(GPSUtils::coordsToMeter(currentWaypoint.Lat(), currentWaypoint.Long(), data.Lat(), data.Long()) < 1.0) //need to be within 1 meter radius of the waypoint
+    {
+        currentWaypoint = _source->getNext();
+        onNewWaypoint(currentWaypoint);
+    }
+}
+
+GPSData Controller::getCurrentWaypoint()
+{
+    return currentWaypoint;
+}
