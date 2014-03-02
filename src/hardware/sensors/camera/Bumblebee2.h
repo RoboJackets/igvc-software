b1111641efc16d9e135459846380aa08d71220f3
Matthew Barulic <matthew.barulic@gmail.com>
2014-03-02 16:54:21 -0500

    Adds wrappers for AStar path planning to fit into events pipeline. Removes a lot of 'using' statements from headers that were causing conflicts between PCL and OpenCV.
    Former-commit-id: 8765d9bed513ec2181053b8bb6992bd9c243dc8b

--- src/hardware/sensors/camera/Bumblebee2.h
+++ src/hardware/sensors/camera/Bumblebee2.h
@@ -14,16 +14,13 @@
 #include "common/utils/ImageUtils.h"
 
 
-void ProcessFrame(Image* rawImage, void const* otherThings);
+void ProcessFrame(FlyCapture2::Image* rawImage, void const* otherThings);
 void PrintError(FlyCapture2::Error error);
-
-using namespace cv;
-using namespace FlyCapture2;
 
 class Bumblebee2 : public StereoSource
 {
     public:
-        Bumblebee2(string fileName ="/home/robojackets/igvc/2013/software/src/sensors/camera3D/calib/out_camera_data.xml");
+        Bumblebee2(std::string fileName ="/home/robojackets/igvc/2013/software/src/sensors/camera3D/calib/out_camera_data.xml");
         virtual ~Bumblebee2();
         int Run();
         //void LockImages();
@@ -31,19 +28,19 @@ class Bumblebee2 : public StereoSource
         static void ptgrey2opencv(FlyCapture2::Image& img, cv::Mat& mat);
         ImageData left();
         ImageData right();
-        Mat& LeftMat();
-        Mat& RightMat();
+        cv::Mat& LeftMat();
+        cv::Mat& RightMat();
         StereoImageData Images();
-        void Images(Mat& leftImage, Mat& rightImage);
+        void Images(cv::Mat& leftImage, cv::Mat& rightImage);
         int frameCount;
         boost::mutex frameLock;
         FlyCapture2::Camera& Cam();
-        Mat correctImage(Mat rawImg);
+        cv::Mat correctImage(cv::Mat rawImg);
     private:
         int StartCamera();
         int CloseCamera();
-        Mat _cameraMatrix;
-        Mat _distCoeffs;
+        cv::Mat _cameraMatrix;
+        cv::Mat _distCoeffs;
         StereoImageData _images;
         FlyCapture2::Camera _cam;
         //boost::mutex _imagesLock;
