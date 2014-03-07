a7c15f74f1720e801ce5ab03d56132746849d15e
Matthew Barulic <matthew.barulic@gmail.com>
2014-03-07 12:35:27 -0500

    Adds testing for AStar path planning.
    Former-commit-id: 598173b880f07a685b636f743c6d5972bfbffad2

--- src/intelligence/pathplanning/astarplanner.cpp
+++ src/intelligence/pathplanning/astarplanner.cpp
@@ -4,7 +4,23 @@
 
 AStarPlanner::AStarPlanner()
 {
+    /*
+     * These ConfigManager calls just make sure the config values get added
+     * to the file even if the algorithm doesn't run. This is simply for
+     * ease of use, ensuring that the config values appear in the editor at
+     * the start of the application.
+     */
     ConfigManager::Instance().getValue("AStarPlanner", "GoalThreshold", 2.0);
+    ConfigManager::Instance().getValue("AStarPlanner", "OnPathThreshold", 0.6);
+    ConfigManager::Instance().getValue("AStarPLanner", "CollisionThreshold", 1.0);
+    ConfigManager::Instance().getValue("AStarPlanner", "Velocity", 1.0);
+    ConfigManager::Instance().getValue("AStarPlanner", "DeltaT", 1.0);
+    ConfigManager::Instance().getValue("Robot", "Baseline", 0);
+    ConfigManager::Instance().getValue("AStarPlanner", "CollisionRadius", 1.0);
+
+    mapSet = false;
+    goalSet = false;
+    startSet = false;
 }
 
 path_t AStarPlanner::GetPath()
@@ -14,15 +30,25 @@ path_t AStarPlanner::GetPath()
 
 void AStarPlanner::run()
 {
-    auto newpath = GraphSearch::AStar(searchproblem);
-    path.clear();
-    for(int i = 0; i < newpath.getNumberOfSteps(); i++)
-        path.push_back(std::pair<SearchMove,SearchLocation>(newpath.getAction(i), newpath.getState(i)));
-    OnNewPath(path);
+    if(mapSet && startSet && goalSet)
+    {
+        searchproblem.Speed = ConfigManager::Instance().getValue("AStarPlanner", "Velocity", 1.0);
+        searchproblem.DeltaT = ConfigManager::Instance().getValue("AStarPlanner", "DeltaT", 1.0);
+        searchproblem.PointTurnsEnabled = false;
+        searchproblem.Baseline = ConfigManager::Instance().getValue("Robot", "Baseline", 0);
+        searchproblem.Threshold = ConfigManager::Instance().getValue("AStarPlanner", "CollisionRadius", 1.0);
+        auto newpath = GraphSearch::AStar(searchproblem);
+        path.clear();
+        for(int i = 0; i < newpath.getNumberOfSteps(); i++)
+            path.push_back(std::pair<SearchMove,SearchLocation>(newpath.getAction(i), newpath.getState(i)));
+        OnNewPath(path);
+    }
 }
 
 bool AStarPlanner::pathIsValid()
 {
+    if(path.size() == 0)
+        return false;
     if(path[path.size()-1].second.distTo(searchproblem.Goal) > ConfigManager::Instance().getValue("AStarPlanner", "GoalThreshold", 2.0))
         return false;
     if(distanceFromPath(searchproblem.Start) > ConfigManager::Instance().getValue("AStarPlanner", "OnPathThreshold", 0.6))
@@ -36,8 +62,8 @@ bool AStarPlanner::pathIsValid()
     for(auto pair : path)
     {
         pcl::PointXYZ point;
-        point.x = pair[1].X;
-        point.y = pair[1].Y;
+        point.x = pair.second.x;
+        point.y = pair.second.y;
         point.z = 0;
         if( kdtree.radiusSearch(point, radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0)
         {
@@ -62,6 +88,7 @@ float AStarPlanner::distanceFromPath(SearchLocation point)
 void AStarPlanner::OnNewMap(pcl::PointCloud<pcl::PointXYZ>::Ptr map)
 {
     searchproblem.Map = *map;
+    mapSet = true;
     if(!pathIsValid())
         run();
 }
@@ -69,6 +96,7 @@ void AStarPlanner::OnNewMap(pcl::PointCloud<pcl::PointXYZ>::Ptr map)
 void AStarPlanner::OnNewStartPos(RobotPosition pos)
 {
     searchproblem.Start = SearchLocation(pos.X, pos.Y, pos.Heading);
+    startSet = true;
     if(!pathIsValid())
         run();
 }
@@ -76,6 +104,7 @@ void AStarPlanner::OnNewStartPos(RobotPosition pos)
 void AStarPlanner::OnNewGoalPos(RobotPosition pos)
 {
     searchproblem.Goal = SearchLocation(pos.X, pos.Y, pos.Heading);
+    goalSet = true;
     if(!pathIsValid())
         run();
 }
