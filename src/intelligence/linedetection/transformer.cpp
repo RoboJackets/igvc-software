a57c8b969708cc6b51e713a7ffab6fd881c4f585
Claire Bergman <claire@claire-Latitude-E4310.(none)>
2014-03-08 17:28:03 -0500

    Completed the perspective transform and export points to a pointcloud
    Former-commit-id: 1c48daa86dbae5491fb07e3ec83cc56888479e5c

--- src/intelligence/linedetection/transformer.cpp
+++ src/intelligence/linedetection/transformer.cpp
@@ -10,44 +10,51 @@ transformer::transformer(Event<ImageData> &evtSrc)
     evtSrc += &LonImageEvent;
     //TODO here put in the coordinates of pc1-pc2
     //As measured in real life
-    /// (x1, x2, x3, y1, y2, y3, 1 1 1)
+    int offset = 0; //20
+    pcam = (cv::Mat_<float>(4,2) << offset-12,72, offset, 72, offset, 60,offset -12, 60);
+    //pcam = pcam*3+450;
+    //cout<< pcam << endl;
 
-    pcam = (cv::Mat_<float>(2,2) << 427, 511, 642, 642);
-    cout<< pcam << endl;
+    //    430, 642
+    //    513, 642
+    //    513, 588
+    //    430, 588
 
 //    427, 642
 //    515, 642
 //    512, 589
 //    432, 588
-    //TODO here put in the coordinates of p1-p2
-    //which you will get from gimp.
-    p = (cv::Mat_<float>(2,2) << 427, 515, 642, 642);
-    cv::Mat pcamInv = pcam.inv(cv::DECOMP_LU);
-    transformMat = p*pcamInv;
-    transformMat = abs(transformMat);
+
+    p = (cv::Mat_<float>(4,2) << 427, 642, 515, 642, 512, 589, 432, 588);
+\
+    transformMat = cv::getPerspectiveTransform(p, pcam);//cv::findHomography(cv::Mat(p), cv::Mat(pcam));
     cout<<transformMat<<endl;
 }
 
 void transformer::onImageEvent(ImageData imgd){
     src = imgd.mat();
 
-    cv::Mat dim = (cv::Mat_<float>(2,1)<< src.cols, src.rows);
+    cv::warpPerspective(src, dst, transformMat, dst.size());
 
-    dim = transformMat*dim;
+     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
+     // Fill in the cloud data
+     cloud->width  = 15;
+     cloud->height = 1;
+     cloud->points.resize (cloud->width * cloud->height);
 
-    dst = cv::Mat( (int) dim.at<float>(1,0)+1, (int) dim.at<float>(0,0)+1, CV_8UC3);
-    cv::Mat newLoc(2,1, CV_32F);
+     // Generate the data
+     for (size_t i = 0; i < cloud->points.size (); ++i)
+     {
+       cloud->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
+       cloud->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
+       cloud->points[i].z = 1.0;
+     }
 
-    for(int r=0; r<src.rows; r++){
-        for (int c=0; c<src.cols; c++){
-            newLoc.at<float>(0,0) = c;
-            newLoc.at<float>(1,0) = r;
-            newLoc = transformMat*newLoc;
-            dst.at<cv::Vec3b>((int)newLoc.at<float>(1,0),(int) newLoc.at<float>(0,0)) = src.at<cv::Vec3b>(r,c);
-
-        }
-    }
+     // Set a few outliers
+     cloud->points[0].z = 2.0;
+     cloud->points[3].z = -2.0;
+     cloud->points[6].z = 4.0;
     onNewLines(ImageData(dst));
 
 }
