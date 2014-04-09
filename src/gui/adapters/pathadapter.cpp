ecc1a62d1a1646d1660ed18df18345490b87c958
Matthew Barulic <matthew.barulic@gmail.com>
2014-04-09 13:08:43 -0400

    Math for lining arc up to starting point complete. Example in paintEvent().
    Former-commit-id: 43a54af5c8039c5b39de01b42c85fb3d3054fd52

--- src/gui/adapters/pathadapter.cpp
+++ src/gui/adapters/pathadapter.cpp
@@ -1,6 +1,8 @@
 #include "pathadapter.h"
 #include "ui_pathadapter.h"
 #include <QPainter>
+#include <math.h>
+#include <common/utils/AngleUtils.h>
 
 PathAdapter::PathAdapter(QWidget *parent) :
     QWidget(parent),
@@ -18,7 +20,30 @@ void PathAdapter::paintEvent(QPaintEvent *)
 {
     QPainter painter(this);
 
+    float pixPerM = 50;
 
+    float xp = 10;
+    float yp = 10;
+    float tp = 0;
+
+    float V = 1;
+    float w = 2;
+    float dt = 1;
+
+    float R = (V/w) * pixPerM;
+
+    int x = xp*pixPerM - R + R*cos(tp);
+
+    int y = yp*pixPerM + R*sin(tp) - R;
+
+    int a = AngleUtils::radsToDeg((M_PI - tp) - (w * dt)) * 16;
+
+    int alen = AngleUtils::radsToDeg(w*dt) * 16;
+
+    painter.setPen(Qt::black);
+    painter.drawArc(x, y, 2*R, 2*R, a, alen);
+    painter.setPen(Qt::red);
+    painter.drawPoint(xp*pixPerM,yp*pixPerM);
 
     painter.end();
 }
