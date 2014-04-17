f30080a00a2490c9a4d6eb3e4cef8e0b9f8a26a8
Matthew Barulic <matthew.barulic@gmail.com>
2014-04-16 20:52:33 -0400

    Curved path plotting now works!!!!! Yay Math!
    Former-commit-id: 066123baed69c9e041614ce7bcc34e073755dca1

--- src/gui/adapters/pathadapter.cpp
+++ src/gui/adapters/pathadapter.cpp
@@ -24,9 +24,9 @@ PathAdapter::PathAdapter(QWidget *parent) :
     setStart(RobotPosition(0,0,0));
     setEnd(RobotPosition(10,10,0));
     pcl::PointCloud<pcl::PointXYZ>::Ptr map(new pcl::PointCloud<pcl::PointXYZ>());
-//    for(int x = -10; x < 10; x++)
-//        map->push_back(pcl::PointXYZ(x,5,0));
-    map->push_back(pcl::PointXYZ(100,100,0));
+    for(int x = -10; x < 10; x++)
+        map->push_back(pcl::PointXYZ(x,5,0));
+//    map->push_back(pcl::PointXYZ(100,100,0));
     setMap(map);
     path = planner.GetPath();
 }
@@ -44,67 +44,48 @@ void PathAdapter::paintEvent(QPaintEvent *)
     int centerY = painter.window().height()/2;
     int height = painter.window().height();
 
+    painter.setPen(QColor(127,127,127));
+    painter.drawLine(0, painter.window().height()/2, painter.window().width(), painter.window().height()/2);
+    painter.drawLine(painter.window().width()/2, 0, painter.window().width()/2, painter.window().height());
+
     for(auto point : path)
     {
         auto move = point.first;
         auto location = point.second;
-        std::cout << location.x << ", " << location.y << std::endl;
 
-        //painter.drawEllipse(location.x * scale + centerX, height - (location.y * scale + centerY), 3, 3);
-
-        painter.setPen(QColor(20,20,20));
-        painter.drawLine(0, painter.window().height()/2, painter.window().width(), painter.window().height()/2);
-        painter.drawLine(painter.window().width()/2, 0, painter.window().width()/2, painter.window().height());
-
-        painter.setPen(Qt::blue);
-        for(int i = -1; i <= 1; i++)
-            for(int j = -1; j <= 1; j++)
-                painter.drawPoint(location.x * scale + centerX + i, height - (location.y * scale + centerY) + j);
         painter.setPen(Qt::black);
         if(fabs(move.W) < 1e-10)
         {
             // Draw straight line
-            double x2 = location.x + (move.V * move.DeltaT * cos(M_PI_2 - location.theta));
-            double y2 = location.y + (move.V * move.DeltaT * sin(M_PI_2 - location.theta));
-            std::cout << location.x << ", " << location.y << std::endl;
-            painter.setPen(Qt::black);
+            double x2 = location.x - (move.V * move.DeltaT * cos(M_PI_2 - location.theta));
+            double y2 = location.y - (move.V * move.DeltaT * sin(M_PI_2 - location.theta));
             painter.drawLine(location.x * scale + centerX, height - (location.y * scale + centerY), x2 * scale + centerX, height - (y2 * scale + centerY));
         }
         else
         {
             // Draw arc
-            float R = (move.V / fabs(move.W));
-
-            double x, y, a, alen;
-
-            if(move.W > 0)
-            {
-                x = ( location.x ) - R + ( R * cos(location.theta) );
-                y = ( location.y ) - R - ( R * sin(location.theta) );
-                a = AngleUtils::radsToDeg(M_PI - location.theta) * 16;
-                alen = AngleUtils::radsToDeg(move.W * move.DeltaT) * 16;
-                painter.setPen(Qt::red);
-                std::cout << "arc\tw>0\n\tx = " << x + centerX << " y = " << y + centerY << std::endl;
-            }
-            else
-            {
-                x = ( location.x ) - R - ( R * cos(location.theta) );
-                y = ( location.y ) - R + ( R * sin(location.theta) );
-                a = AngleUtils::radsToDeg(-location.theta) * 16;
-                alen = AngleUtils::radsToDeg(-move.W * move.DeltaT) * 16;
-                painter.setPen(Qt::green);
-                std::cout << "arc\tw<0\n\tx = " << (x + centerX) << " y = " << (y + centerY) << std::endl;
-            }
-            auto pen = painter.pen();
-            painter.setPen(Qt::cyan);
-            painter.drawEllipse((x * scale) + centerX, (y  * scale) + centerY, scale * 2 * R, scale * 2 * R);
-            painter.setPen(pen);
-            painter.drawArc((x * scale) + centerX, (y  * scale) + centerY, scale * 2 * R, scale * 2 * R, a, alen);
+            drawArc(&painter, location, move, scale);
         }
     }
     std::cout << std::endl;
 
     painter.end();
+}
+
+void PathAdapter::drawArc(QPainter *painter, SearchLocation dest, SearchMove moveTaken, double scale)
+{
+    double R = (moveTaken.V / moveTaken.W ) * scale;
+    double cx = ( dest.x * scale ) - R * cos(M_PI - dest.theta);
+    double cy = ( dest.y * scale ) - R * sin(M_PI - dest.theta);
+    int scx = painter->window().width() / 2;
+    int scy = painter->window().height() / 2;
+    int height = painter->window().height();
+    if(moveTaken.W > 0)
+        for(double t = M_PI - dest.theta; t <= M_PI - dest.theta + ( moveTaken.W * moveTaken.DeltaT ); t += 0.001)
+            painter->drawPoint(cx + R  * cos(t) + scx, height - (cy + R * sin(t) + scy));
+    else
+        for(double t = M_PI - dest.theta; t >= M_PI - dest.theta + ( moveTaken.W * moveTaken.DeltaT ); t -= 0.001)
+            painter->drawPoint(cx + R  * cos(t) + scx, height - (cy + R * sin(t) + scy));
 }
 
 void PathAdapter::wheelEvent(QWheelEvent *event)
