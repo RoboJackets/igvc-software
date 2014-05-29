0d0a2d7d17ffa756de4ffdfb3a80eb3a8dd177d9
Matthew Barulic <matthew.barulic@gmail.com>
2014-05-29 01:13:52 -0400

    Adds graph to MotorBoardAdapter.

--- src/gui/adapters/motorboardadapter.cpp
+++ src/gui/adapters/motorboardadapter.cpp
@@ -1,5 +1,7 @@
 #include "motorboardadapter.h"
 #include "ui_motorboardadapter.h"
+#include <QPainter>
+#include <iostream>
 
 MotorBoardAdapter::MotorBoardAdapter(MotorDriver *driver, QWidget *parent) :
     QWidget(parent),
@@ -12,15 +14,56 @@ MotorBoardAdapter::MotorBoardAdapter(MotorDriver *driver, QWidget *parent) :
 
 MotorBoardAdapter::~MotorBoardAdapter()
 {
+    disconnect(_driver, SIGNAL(newCurrentVelocities(double,double)), this, SLOT(onNewCurrentVelocities(double,double)));
     delete ui;
 }
 
 void MotorBoardAdapter::onNewCurrentVelocities(double left, double right)
 {
-    ui->label_leftVel->setText(tr("%1").arg(left));
-    ui->label_rightVel->setText(tr("%1").arg(right));
-    ui->label_leftCmd->setText(tr("%1").arg(_driver->getLeftSetVelocity()));
-    ui->label_rightCmd->setText(tr("%1").arg(_driver->getRightSetVelocity()));
+    _data.push_back(VelocityPairs(VelocityPair(_driver->getLeftSetVelocity(), _driver->getRightSetVelocity()), VelocityPair(left, right)));
+    if(_data.size() > 10)
+        _data.erase(_data.begin());
+    update();
+}
+
+void MotorBoardAdapter::paintEvent(QPaintEvent *)
+{
+    if(_data.size() > 0)
+    {
+        QPainter painter(this);
+
+        double MAX = 6.0;
+        double dX = this->width() / _data.size();
+        double X = 0;
+
+        double Y_MID = ui->label->y() / 2;
+
+        double H = ui->label->y() / 2;
+
+        double R = H / MAX;
+
+        painter.setPen(Qt::black);
+        painter.drawLine(0, Y_MID, this->width(), Y_MID);
+
+        VelocityPairs prevItem = _data.at(0);
+        for(VelocityPairs item : _data)
+        {
+            painter.setPen(Qt::blue);
+            painter.drawLine(X, Y_MID - ( prevItem.first.first * R), X + dX, Y_MID - ( item.first.first * R));
+
+            painter.setPen(Qt::red);
+            painter.drawLine(X, Y_MID - ( prevItem.first.second * R), X + dX, Y_MID - (item.first.second * R));
+
+            painter.setPen(Qt::green);
+            painter.drawLine(X, Y_MID - ( prevItem.second.first * R), X + dX, Y_MID - ( item.second.first * R));
+
+            painter.setPen(Qt::magenta);
+            painter.drawLine(X, Y_MID - ( prevItem.second.second * R), X + dX, Y_MID - (item.second.second * R));
+
+            X += dX;
+            prevItem = item;
+        }
+    }
 }
 
 void MotorBoardAdapter::on_lineEdit_leftVel_returnPressed()
