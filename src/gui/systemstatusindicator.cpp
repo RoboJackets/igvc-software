8674ff439c1123dc7c3ae63e960caa882c461bb2
Matthew Barulic <matthew.barulic@gmail.com>
2014-06-03 03:36:39 -0400

    Tonight's worth of work.

--- src/gui/systemstatusindicator.cpp
+++ src/gui/systemstatusindicator.cpp
@@ -7,10 +7,12 @@ SystemStatusIndicator::SystemStatusIndicator(QWidget *parent) :
     QWidget(parent)
 {
     _isEnabled = false;
+    connect(this, SIGNAL(updateBecauseData()), this, SLOT(update()));
 }
 
 void SystemStatusIndicator::paintEvent(QPaintEvent *)
 {
+    _paintMutex.lock();
     QPainter qp(this);
 
     qp.setPen(_isEnabled ? Qt::green : Qt::red);
@@ -19,10 +21,11 @@ void SystemStatusIndicator::paintEvent(QPaintEvent *)
     int size = std::min(this->width(), this->height())-1;
 
     qp.drawEllipse(0,0,size, size);
+    _paintMutex.unlock();
 }
 
 void SystemStatusIndicator::onEStopStatusChanged(bool isEnabled)
 {
     _isEnabled = isEnabled;
-    this->update();
+    updateBecauseData();
 }
