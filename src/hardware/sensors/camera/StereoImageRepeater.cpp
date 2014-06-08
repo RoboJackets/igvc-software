2a1262d777cf862ecae4cbc4e4921139b6b63b8e
IGVC Laptop <gtrobojackets@gmail.com>
2014-06-08 19:01:17 -0400

    comp work.

--- src/hardware/sensors/camera/StereoImageRepeater.cpp
+++ src/hardware/sensors/camera/StereoImageRepeater.cpp
@@ -1,11 +1,20 @@
 #include "StereoImageRepeater.h"
 #include <common/config/configmanager.h>
+#include <common/logger/logger.h>
+#include <QDir>
 
 StereoImageRepeater::StereoImageRepeater(std::string pathLeft, std::string pathRight)
 {
-    _left = cv::imread(pathLeft);
-    _right = cv::imread(pathRight);
-    _thread =  boost::thread(boost::bind(&StereoImageRepeater::thread_run, this));
+    if(QFile(pathLeft.c_str()).exists() && QFile(pathRight.c_str()).exists())
+    {
+        _left = cv::imread(pathLeft);
+        _right = cv::imread(pathRight);
+        _thread =  boost::thread(boost::bind(&StereoImageRepeater::thread_run, this));
+    }
+    else
+    {
+        Logger::Log(LogLevel::Error, "Image repeater could not load one or both of the images.");
+    }
 }
 
 void StereoImageRepeater::thread_run()
