6ba70a1440569e7b38fe0e6a0c550fbc7af13884
Matthew Barulic <matthew.barulic@gmail.com>
2014-09-23 19:39:11 -0400

    Adds apt-get update to setup script.

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ case $yn in
         exit 1;;
 esac
 
+sudo apt-get update
+
 echo "Installing basic dependencies..."
 sudo apt-get install build-essential libflann-dev libeigen3-dev doxygen libboost-all-dev libusb-1.0 libvtk5-dev libopenni-dev
 
