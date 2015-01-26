845d60f8dd98f59d840bb5501882953e21365c73
Matthew Barulic <matthew.barulic@gmail.com>
2015-01-25 20:31:12 -0500

    Ports JoystickDriver to joystick_driver node. Closes #28

---
+++ igvc/src/joystick_driver/joystick_driver.cpp
@@ -0,0 +1,58 @@
+#include <ros/ros.h>
+#include <ros/subscriber.h>
+#include <ros/publisher.h>
+#include <igvc_msgs/velocity_pair.h>
+#include <sensor_msgs/Joy.h>
+
+using namespace std;
+
+ros::Publisher cmd_pub;
+
+ros::NodeHandle *nhp;
+
+void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
+{
+    double absoluteMaxVel, maxVel, maxVelIncr;
+    nhp->param(string("absoluteMaxVel"), absoluteMaxVel, 1.0);
+    nhp->param(string("maxVel"), maxVel, 1.0);
+    nhp->param(string("maxVelIncr"), maxVelIncr, 0.1);
+    
+    if(msg->buttons[1])
+        maxVel -= maxVelIncr;
+    else if(msg->buttons[3])
+        maxVel += maxVelIncr;
+    maxVel = min(maxVel, absoluteMaxVel);
+    maxVel = max(maxVel, 0.0);
+    
+    nhp->setParam("maxVel", maxVel);
+    
+    int leftJoyAxis, rightJoyAxis;
+    bool leftInverted, rightInverted;
+    nhp->param(string("leftAxis"), leftJoyAxis, 1);
+    nhp->param(string("rightAxis"), rightJoyAxis, 3);
+    nhp->param(string("leftInverted"), leftInverted, true);
+    nhp->param(string("rightInverted"), rightInverted, true);
+    
+    igvc_msgs::velocity_pair cmd;
+    cmd.left_velocity = (msg->axes[leftJoyAxis]/32767.0)*maxVel * (leftInverted ? -1.0 : 1.0);
+    cmd.right_velocity = (msg->axes[rightJoyAxis]/32767.0)*maxVel * (rightInverted ? -1.0 : 1.0);
+    
+    cmd_pub.publish(cmd);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "joystick_driver");
+    ros::NodeHandle nh;
+    nhp = new ros::NodeHandle("~");
+    
+    cmd_pub = nh.advertise<igvc_msgs::velocity_pair>("/motors", 1);
+    
+    ros::Subscriber joy_sub = nh.subscribe("/joy", 1, joyCallback);
+    
+    ros::spin();
+    
+    delete nhp;
+    
+    return 0;
+}
