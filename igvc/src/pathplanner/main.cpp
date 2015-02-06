b6cef9b0a3347e6c786b043ffc8a007542faab3b
Matthew Barulic <matthew.barulic@gmail.com>
2015-02-05 20:55:54 -0500

    Some more work to ROS path planning.

--- igvc/src/pathplanner/main.cpp
+++ igvc/src/pathplanner/main.cpp
@@ -6,14 +6,35 @@
 #include <geometry_msgs/PoseStamped.h>
 #include "GraphSearch.hpp"
 #include "igvcsearchproblem.h"
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <tf/transform_datatypes.h>
+#include <mutex>
+#include <pcl_conversions/pcl_conversions.h>
+
+using namespace std;
 
 ros::Publisher disp_path_pub;
 
 IGVCSearchProblem search_problem;
 
+mutex planning_mutex;
+
 void map_callback(const sensor_msgs::PointCloud2ConstPtr &msg)
 {
+    lock_guard<mutex> lock(planning_mutex);
+    pcl::fromROSMsg(*msg, *search_problem.Map);
+}
 
+void position_callback(const geometry_msgs::PoseWithCovarianceStampedConstPtr& msg)
+{
+    lock_guard<mutex> lock(planning_mutex);
+    search_problem.Start.x = msg->pose.pose.position.x;
+    search_problem.Start.y = msg->pose.pose.position.y;
+    tf::Quaternion q{msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w};
+    tf::Matrix3x3 m{q};
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+    search_problem.Start.theta = yaw;
 }
 
 int main(int argc, char** argv)
@@ -24,9 +45,21 @@ int main(int argc, char** argv)
 
     nh.subscribe("/map", 1, map_callback);
 
+    nh.subscribe("robot_pose_ekf/odom_combined", 1, position_callback);
+
     disp_path_pub = nh.advertise<nav_msgs::Path>("/path_display", 1);
 
-    ros::spin();
+    ros::Rate rate(3);
+    while(ros::ok())
+    {
+        ros::spinOnce();
+
+        planning_mutex.lock();
+        // TODO replan if needed.
+        planning_mutex.unlock();
+
+        rate.sleep();
+    }
 
     return 0;
 }
