f65e2559b946cd757e3d744d6f2a551a32f15567
Matthew Barulic <matthew.barulic@gmail.com>
2015-05-03 00:14:59 -0400

    Adds basic logic to path_follower, still need to add logic for publishing velocities.

--- igvc/src/path_follower/main.cpp
+++ igvc/src/path_follower/main.cpp
@@ -1,7 +1,60 @@
+#include <ros/ros.h>
+#include <igvc_msgs/action_path.h>
+#include <igvc_msgs/velocity_pair.h>
+#include <mutex>
+#include <chrono>
 
+using namespace igvc_msgs;
+using namespace std;
+using namespace chrono;
+
+action_pathConstPtr path;
+mutex path_mutex;
+
+int path_index = 0;
+bool path_reset = false;
+
+void newPath(const action_pathConstPtr &msg)
+{
+  lock_guard<mutex> lock(mutex);
+  path = msg;
+  path_index = 0;
+  path_reset = true;
+}
 
 int main(int argc, char** argv)
 {
+
+  ros::init(argc, argv, "path_follower");
+
+  time_point<high_resolution_clock> cmd_start_time;
+
+  ros::Rate rate(60);
+  while(ros::ok())
+  {
+    ros::spinOnce();
+
+    { //scope for mutex lock
+      lock_guard<mutex> lock(mutex);
+      if(path.get() != nullptr)
+      {
+        if(path_reset)
+        {
+          // TODO send motor commands
+          cmd_start_time = high_resolution_clock::now();
+        }
+        auto elapsed_time = high_resolution_clock::now() -  cmd_start_time;
+        if(duration_cast<seconds>(elapsed_time).count() > path->actions[path_index].duration)
+        {
+          path_index++;
+          // TODO send motor commands
+          cmd_start_time = high_resolution_clock::now();
+        }
+      }
+    }
+
+    rate.sleep();
+  }
 
   return 0;
 }
