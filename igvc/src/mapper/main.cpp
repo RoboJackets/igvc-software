d28640152502c71212b7a65751c7b93a601d8afe
Matthew Barulic <matthew.barulic@gmail.com>
2015-05-13 20:21:18 -0400

    More node & topic renaming.

--- igvc/src/mapper/main.cpp
+++ igvc/src/mapper/main.cpp
@@ -8,6 +8,13 @@
 #include <sensor_msgs/point_cloud_conversion.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/io/pcd_io.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
 
 sensor_msgs::PointCloud2 cloud;
 ros::Publisher _pointcloud_pub;
@@ -15,35 +22,24 @@ tf::TransformListener *tf_listener;
 
 void nodeCallback(const sensor_msgs::PointCloud2::ConstPtr &msg)
 {
-    try
-    {
-        sensor_msgs::PointCloud transformed;
-        sensor_msgs::convertPointCloud2ToPointCloud(*msg, transformed);
-        transformed.header.stamp = ros::Time(0);
-        tf_listener->transformPointCloud("/map",transformed,transformed);
+    sensor_msgs::PointCloud transformed;
+    sensor_msgs::convertPointCloud2ToPointCloud(*msg, transformed);
+    transformed.header.stamp = ros::Time(0);
+    tf_listener->transformPointCloud("/map",transformed,transformed);
 
-        sensor_msgs::PointCloud2 transformed2;
-        sensor_msgs::convertPointCloudToPointCloud2(transformed, transformed2);
-        sensor_msgs::PointCloud2 output;
-        pcl::concatenatePointCloud(transformed2, cloud, output);
-        cloud = output;
+    sensor_msgs::PointCloud2 transformed2;
+    sensor_msgs::convertPointCloudToPointCloud2(transformed, transformed2);
+    sensor_msgs::PointCloud2 output;
+    pcl::concatenatePointCloud(transformed2, cloud, output);
+    cloud = output;
 
-//        pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-//        sor.setInputCloud(cloud);
-//        sor.setLeafSize (0.01f, 0.01f, 0.01f);
-//        sor.filter(*cloud);
+//    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+//    sor.setInputCloud(cloud);
+//    sor.setLeafSize (0.01f, 0.01f, 0.01f);
+//    sor.filter(*cloud);
 
-
-//        pcl::PCDWriter w;
-//        w.write("pointcloud_map.pcd", cloud, Eigen::Vector4f::Zero(), Eigen::Quaternionf::Identity(), false);
-        _pointcloud_pub.publish(cloud);
-    }
-    catch (int e)
-    {
-        ROS_ERROR_STREAM("caught error " << e);
-    }
+    _pointcloud_pub.publish(cloud);
 }
-
 
 int main(int argc, char** argv)
 {
@@ -60,25 +56,25 @@ int main(int argc, char** argv)
     ros::NodeHandle pNh("~");
 
     if(!pNh.hasParam("topics"))
-        ROS_ERROR_STREAM("no param");
+        ROS_WARN_STREAM("No topics specified for mapper. No map will be generated.");
 
     pNh.getParam("topics", topics);
 
-    int pos;
-    while((pos = topics.find(delimiter)) != std::string::npos)
-    {
-        std::string token = topics.substr(0, pos);
-        ROS_ERROR_STREAM(token);
-        topics.erase(0,pos+delimiter.length());
+    if(topics.empty())
+        ROS_WARN_STREAM("No topics specified for mapper. No map will be generated.");
 
-        subs.push_back(nh.subscribe(token, 1, nodeCallback));
+    istringstream iss(topics);
+    vector<string> tokens { istream_iterator<string>{iss},
+                            istream_iterator<string>{} };
+    for(auto topic : tokens)
+    {
+        ROS_INFO_STREAM("Mapper subscribing to " << topic);
+        subs.push_back(nh.subscribe(topic, 1, nodeCallback));
     }
-    ROS_ERROR_STREAM(topics);
-    subs.push_back(nh.subscribe(topics, 1, nodeCallback));
 
     cloud.header.frame_id = "/map";
 
-    _pointcloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/pointcloud/map", 1);
+    _pointcloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/map", 1);
 
     tf_listener->waitForTransform("/map", "/lidar", ros::Time(0), ros::Duration(5));
 
