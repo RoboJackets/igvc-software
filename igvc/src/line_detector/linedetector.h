306d542bd32bb693ca17e5d305e4d3e8458a1ebb
Matthew Barulic <matthew.barulic@gmail.com>
2015-08-08 22:50:54 -0700

    Starts massive cleanup of line detector code.

--- igvc/src/line_detector/linedetector.h
+++ igvc/src/line_detector/linedetector.h
@@ -26,65 +26,29 @@ public:
     LineDetector(ros::NodeHandle &handle);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
 
-    bool isWorking();
-    bool primaryMethod();
-	bool otherMethod(const sensor_msgs::ImageConstPtr& msg, cv_bridge::CvImagePtr cv_ptr);
-
 private:
-//    void findLines(cv_bridge::Cv_ImagePtr cv_ptr);
+
     void blackoutSection(int rowl, int rowu, int coll, int colu);
-    void blackoutSection(cv::Mat &imgmat, int rowl, int rowu, int coll, int colu);
-    float getAvg(void);
-    float getAvg(cv::Mat &imgmat);
-    void blackAndWhite(float totalAvg);
-    int display_dst(int delay);
+
     void detectObstacle(int i, int j, cv::Mat* dst);
     
     void img_callback(const sensor_msgs::ImageConstPtr& msg);
 
-    void Erosion(cv::Mat* dst);
-    void Dilation(cv::Mat* dst);
-
-    void transformPoints(cv::Mat &src, cv::Mat &dst);
     pcl::PointCloud<pcl::PointXYZ>::Ptr toPointCloud(cv::Mat src);
 
-    void backgroundSubtractor();
-    void houghLines();
-    void horzVertEdgeDet();
-	void blobFinder();
     void helperSearch(void *totvis, void *input, int i, int j, cv::Mat *imgmat, int thresh);
     bool helperContains(std::tuple<int, int> pos, void *vis);
-	bool adjWhite(int i, int j, cv::Mat *img, int thresh);
-    bool InNature(cv::Mat& img, int r, int c, int size);
-    void Dilation();
+    bool adjWhite(int i, int j, cv::Mat *img, int thresh);
 
-    void drawWhite(cv::Mat &image);
     cv::Mat applyFilter(cv::Mat &image, const cv::Mat &kernal);
     std::vector<cv::Mat> getGeometricMean(cv::Mat &img, std::vector<cv::Mat> &kernals);
     void subtractOrthog(std::vector<cv::Mat>& images);
     void RemoveNonMax(std::vector<cv::Mat>& images);
     void EnforceContinuity(std::vector<cv::Mat>& directions, cv::Mat& out);
-    void binary(cv::Mat& src, cv::Mat& dst);
-
-    /** @brief the VideoCapture of the image/video */
-    cv::VideoCapture cap;
-
-    //For the erosion/dilation stuff
-    /**
-     * @brief contains the number corresponding to the element used for erosion
-     * @note 2 is what we are currently using (an ellipse)
-     */
-    int erosion_elem;
-    /**
-     * @brief specifies the size of the area to be eroded.
-     **/
-    int erosion_size;
-    int dilation_elem;
-    int dilation_size;
 
     const int max_elem;
     const int max_kernel_size;
-    cv::Mat p, pcam, transformMat;
+    cv::Mat p;
 
     /**
      * @brief gaussian_size The size of the Gaussian blur. The bigger the greater the blur
@@ -102,18 +66,10 @@ private:
     cv::Mat *dst;
 
     // ROS COMMUNICATION
-	image_transport::ImageTransport _it;
-	image_transport::Publisher _filt_img;
-	ros::Publisher _line_cloud;
-    image_transport::Publisher _filt_img1;
-    image_transport::Publisher _filt_img2;
-    image_transport::Publisher _filt_img3;
-    image_transport::Publisher _filt_img4;
-    image_transport::Publisher _filt_img5;
-    image_transport::Publisher _filt_img6;
-    image_transport::Publisher _filt_img7;
-    image_transport::Publisher _filt_img8;
-	image_transport::Subscriber _src_img;
+    image_transport::ImageTransport _it;
+    image_transport::Publisher _filt_img;
+    image_transport::Subscriber _src_img;
+    ros::Publisher _line_cloud;
     tf::TransformListener tf_listener;
 };
 #endif // LINEDETECTOR_H
