d2a9aa24fe1bcdbde135659502dd0a7431df719f
Jay Kamat <github@jgkamat.33mail.com>
2015-10-07 21:48:08 -0400

    Explicit root settings in config.docif

--- util/indigo-setup.sh
+++ util/indigo-setup.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-
 # A small script to automate installation of dependencies
 
-# cd to project root
-cd ../../
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$DIR/../.."
 
 catkin_make || true
 apt-get update
-source devel/setup.sh
+source src/devel/setup.sh
 rosdep -y install igvc
-apt-get -y install qt5-default wget
+apt-get -y install qt5-default wget expect
 wget https://files.gitter.im/RoboJackets/igvc-software/BGRW/flycapture2-2.8.3.1-amd64-pkg.tgz
 tar -xvf flycapture2-2.8.3.1-amd64-pkg.tgz
-cd flycapture2-2.8.3.1
-yes |./install_flycapture.sh || true
+cp ./src/util/flycapture-setup.exp flycapture2-2.8.3.1-amd64/
+cd flycapture2-2.8.3.1-amd64
+
+./flycapture-setup.exp || true		# Cross our fingers...
 apt-get install -yf
-yes | ./install_flycapture.sh || true
+./flycapture-setup.exp || true		# Cross our fingers...
 apt-get install -yf
-yes | ./install_flycapture.sh || true
+./flycapture-setup2.exp				# Cross our fingers...
