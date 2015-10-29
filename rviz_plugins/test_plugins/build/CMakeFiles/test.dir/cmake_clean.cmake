f33ec0811756a0d47236c27c9987405b368d3c48
rmkeezer <rmkeezer@yahoo.com>
2015-10-28 20:12:33 -0400

    Added test plugin

---
+++ rviz_plugins/test_plugins/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test"
+  "src/moc_drive_widget.cxx"
+  "src/moc_teleop_panel.cxx"
+  "CMakeFiles/test.dir/src/drive_widget.cpp.o"
+  "CMakeFiles/test.dir/src/teleop_panel.cpp.o"
+  "CMakeFiles/test.dir/src/moc_drive_widget.cxx.o"
+  "CMakeFiles/test.dir/src/moc_teleop_panel.cxx.o"
+  "../lib/libtest.pdb"
+  "../lib/libtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ rviz_plugins/test_plugins/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test/build/devel/lib:/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test/build/devel/lib/x86_64-linux-gnu:/home/robojackets/Desktop/igvc/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/robojackets/Desktop/igvc/devel/lib:/opt/ros/indigo/lib"
+export PATH="/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test/build/devel/lib/pkgconfig:/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test/build/devel/lib/x86_64-linux-gnu/pkgconfig:/home/robojackets/Desktop/igvc/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/robojackets/Desktop/igvc/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PYTHONPATH="/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test:$ROS_PACKAGE_PATH"
