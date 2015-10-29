b2282e780328da5f226b558501921802bbf80d55
rmkeezer <rmkeezer@yahoo.com>
2015-10-28 21:51:29 -0400

    Deleted unecessary files

---
+++ rviz_plugins/test_plugin/build/CMakeFiles/test_plugin.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test_plugin/src/drive_widget.cpp" "/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test_plugin/build/CMakeFiles/test_plugin.dir/src/drive_widget.cpp.o"
+  "/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test_plugin/build/src/moc_drive_widget.cxx" "/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test_plugin/build/CMakeFiles/test_plugin.dir/src/moc_drive_widget.cxx.o"
+  "/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test_plugin/build/src/moc_teleop_panel.cxx" "/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test_plugin/build/CMakeFiles/test_plugin.dir/src/moc_teleop_panel.cxx.o"
+  "/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test_plugin/src/teleop_panel.cpp" "/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test_plugin/build/CMakeFiles/test_plugin.dir/src/teleop_panel.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_NO_KEYWORDS"
+  "ROS_PACKAGE_NAME=\"test_plugin\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/opt/ros/indigo/include"
+  "/usr/include/eigen3"
+  "/usr/include/OGRE"
+  "/usr/include/opencv"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtCore"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ rviz_plugins/test_plugin/build/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test_plugin/lib/libgtest.pdb"
+  "/home/robojackets/Desktop/igvc/src/igvc-software/rviz_plugins/test_plugin/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
