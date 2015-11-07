f194b769f3a24b3d87e513546cecc160dc454f78
Tejasvi Nareddy <tejunareddy@gmail.com>
2015-11-07 15:36:19 -0500

    Reformated code and added different pictures.

--- igvc/src/webcam/main.cpp
+++ igvc/src/webcam/main.cpp
@@ -15,9 +15,6 @@
 #include "opencv2/nonfree/nonfree.hpp"
 #include "opencv2/nonfree/features2d.hpp"
 
-// ros::Publisher info_pub;
-// camera_info_manager::CameraInfoManager* cameraManager;
-
 using namespace cv;
 cv_bridge::CvImagePtr cv_ptr;
 image_transport::Publisher _new_img;
@@ -31,97 +28,100 @@ void callback(const sensor_msgs::ImageConstPtr& msg) {
     Mat img_normal = imread("/home/nareddyt/Desktop/IGVC/src/igvc-software/igvc/src/webcam/normal.jpg", CV_LOAD_IMAGE_COLOR);
 
     //-- Step 1: Detect the keypoints using SURF Detector
-      int minHessian = 400;
+    int minHessian = 400;
 
-      SurfFeatureDetector detector(minHessian);
+    SurfFeatureDetector detector(minHessian);
 
-      std::vector<KeyPoint> keypoints_overhead, keypoints_normal;
+    std::vector<KeyPoint> keypoints_overhead, keypoints_normal;
 
-      detector.detect( img_overhead, keypoints_overhead );
-      detector.detect( img_normal, keypoints_normal );
+    detector.detect(img_overhead, keypoints_overhead);
+    detector.detect(img_normal, keypoints_normal);
 
-      //-- Step 2: Calculate descriptors (feature vectors)
-      SurfDescriptorExtractor extractor;
+    //-- Step 2: Calculate descriptors (feature vectors)
+    SurfDescriptorExtractor extractor;
 
-      Mat descriptors_overhead, descriptors_normal;
+    Mat descriptors_overhead, descriptors_normal;
 
-      extractor.compute( img_overhead, keypoints_overhead, descriptors_overhead );
-      extractor.compute( img_normal, keypoints_normal, descriptors_normal );
+    extractor.compute(img_overhead, keypoints_overhead, descriptors_overhead);
+    extractor.compute(img_normal, keypoints_normal, descriptors_normal);
 
-      //-- Step 3: Matching descriptor vectors using FLANN matcher
-      FlannBasedMatcher matcher;
-      std::vector< DMatch > matches;
-      matcher.match( descriptors_overhead, descriptors_normal, matches );
+    //-- Step 3: Matching descriptor vectors using FLANN matcher
+    FlannBasedMatcher matcher;
+    std::vector< DMatch > matches;
+    matcher.match(descriptors_overhead, descriptors_normal, matches);
 
-      double max_dist = 0; double min_dist = 100;
+    double max_dist = 0;
+    double min_dist = 100;
 
-      //-- Quick calculation of max and min distances between keypoints
-      for( int i = 0; i < descriptors_overhead.rows; i++ )
-      { double dist = matches[i].distance;
-        if( dist < min_dist ) min_dist = dist;
-        if( dist > max_dist ) max_dist = dist;
-      }
+    //-- Quick calculation of max and min distances between keypoints
+    for (int i = 0; i < descriptors_overhead.rows; i++) {
+        double dist = matches[i].distance;
+        if (dist < min_dist) {
+            min_dist = dist;
+        }
+        if (dist > max_dist) {
+            max_dist = dist;
+        }
+    }
 
-      //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
-      std::vector< DMatch > good_matches;
+    //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist)
+    std::vector< DMatch > good_matches;
 
-      for( int i = 0; i < descriptors_overhead.rows; i++ )
-      {
-        if( matches[i].distance < 3*min_dist ) {
-            good_matches.push_back( matches[i]); }
-      }
+    for (int i = 0; i < descriptors_overhead.rows; i++) {
+        if (matches[i].distance < 3*min_dist) {
+            good_matches.push_back(matches[i]);
+        }
+    }
 
-      Mat img_matches;
-    drawMatches( img_overhead, keypoints_overhead, img_normal, keypoints_normal,
-               good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-               vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+    Mat img_matches;
+    drawMatches(img_overhead, keypoints_overhead, img_normal, keypoints_normal,
+         good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+         vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
-      //-- Localize the object
-      std::vector<Point2f> obj;
-      std::vector<Point2f> scene;
+    //-- Localize the object
+    std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
 
-      for( int i = 0; i < good_matches.size(); i++ )
-      {
+    for (int i = 0; i < good_matches.size(); i++) {
         //-- Get the keypoints from the good matches
-        obj.push_back( keypoints_overhead[ good_matches[i].queryIdx ].pt );
-        scene.push_back( keypoints_normal[ good_matches[i].trainIdx ].pt );
-      }
+        obj.push_back(keypoints_overhead[ good_matches[i].queryIdx ].pt);
+        scene.push_back(keypoints_normal[ good_matches[i].trainIdx ].pt);
+    }
 
-      Mat H = findHomography( obj, scene, CV_RANSAC, 3);
+    Mat H = findHomography(obj, scene, CV_RANSAC, 3);
 
-      //-- Get the corners from the image_1 ( the object to be "detected" )
-  std::vector<Point2f> obj_corners(4);
-  obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_overhead.cols, 0 );
-  obj_corners[2] = cvPoint( img_overhead.cols, img_overhead.rows ); obj_corners[3] = cvPoint( 0, img_overhead.rows );
-  std::vector<Point2f> scene_corners(4);
+    //-- Get the corners from the image_1 (the object to be "detected")
+    std::vector<Point2f> obj_corners(4);
+    obj_corners[0] = cvPoint(0,0);
+    obj_corners[1] = cvPoint(img_overhead.cols, 0);
+    obj_corners[2] = cvPoint(img_overhead.cols, img_overhead.rows);
+    obj_corners[3] = cvPoint(0, img_overhead.rows);
+    std::vector<Point2f> scene_corners(4);
 
-  perspectiveTransform( obj_corners, scene_corners, H);
+    perspectiveTransform(obj_corners, scene_corners, H);
 
-  //-- Draw lines between the corners (the mapped object in the scene - image_2 )
-  line( img_matches, scene_corners[0] + Point2f( img_overhead.cols, 0), scene_corners[1] + Point2f( img_overhead.cols, 0), Scalar(0, 255, 0), 4 );
-  line( img_matches, scene_corners[1] + Point2f( img_overhead.cols, 0), scene_corners[2] + Point2f( img_overhead.cols, 0), Scalar( 0, 255, 0), 4 );
-  line( img_matches, scene_corners[2] + Point2f( img_overhead.cols, 0), scene_corners[3] + Point2f( img_overhead.cols, 0), Scalar( 0, 255, 0), 4 );
-  line( img_matches, scene_corners[3] + Point2f( img_overhead.cols, 0), scene_corners[0] + Point2f( img_overhead.cols, 0), Scalar( 0, 255, 0), 4 );
+    //-- Draw lines between the corners (the mapped object in the scene - image_2)
+    line(img_matches, scene_corners[0] + Point2f(img_overhead.cols, 0), scene_corners[1] + Point2f(img_overhead.cols, 0), Scalar(0, 255, 0), 4);
+    line(img_matches, scene_corners[1] + Point2f(img_overhead.cols, 0), scene_corners[2] + Point2f(img_overhead.cols, 0), Scalar(0, 255, 0), 4);
+    line(img_matches, scene_corners[2] + Point2f(img_overhead.cols, 0), scene_corners[3] + Point2f(img_overhead.cols, 0), Scalar(0, 255, 0), 4);
+    line(img_matches, scene_corners[3] + Point2f(img_overhead.cols, 0), scene_corners[0] + Point2f(img_overhead.cols, 0), Scalar(0, 255, 0), 4);
 
-      std::cerr << "H = " << std::endl << H << std::endl << std::endl;
+    std::cerr << "H = " << std::endl << H << std::endl << std::endl;
 
-      cv::imwrite("/home/nareddyt/Desktop/IGVC/src/igvc-software/igvc/src/webcam/matches.jpg", img_matches);
+    cv::imwrite("/home/nareddyt/Desktop/IGVC/src/igvc-software/igvc/src/webcam/matches.jpg", img_matches);
 
-      // Mat img_transformed;
-      // warpPerspective(img_normal, img_transformed, H, img_transformed.size());
+    Mat img_transformed;
+    warpPerspective(img_normal, img_transformed, H, img_transformed.size());
 
-      // cv::imwrite("/home/nareddyt/Desktop/IGVC/src/igvc-software/igvc/src/webcam/transformed.jpg", img_transformed);
+    cv::imwrite("/home/nareddyt/Desktop/IGVC/src/igvc-software/igvc/src/webcam/transformed.jpg", img_transformed);
 
-     //cv_ptr->image = img_transformed;
+    cv_ptr->image = img_transformed;
     _new_img.publish(cv_ptr->toImageMsg());
 }
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "webcam");
     ros::NodeHandle nh;
-
-    // cameraManager = new camera_info_manager::CameraInfoManager(ros::NodeHandle("/usb_cam"), "/usb_cam", "file:///home/nareddyt/Desktop/cal.yml");
-    // info_pub = nh.advertise<sensor_msgs::CameraInfo>("/usb_cam/camera_info", 1);
 
     ros::Subscriber sub = nh.subscribe("/usb_cam/image_raw", 1, callback);
 
