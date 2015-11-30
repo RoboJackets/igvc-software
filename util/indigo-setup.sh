dc2f93c9ba07e9375c236ba3c4f530b0cd1b4bb2
Jay Kamat <github@jgkamat.33mail.com>
2015-11-30 10:38:56 -0500

    Stop using sudo because ros:indigo might be broken

--- util/indigo-setup.sh
+++ util/indigo-setup.sh
@@ -9,17 +9,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/../.."
 
 catkin_make || true
-apt-get update
-source src/devel/setup.sh
+sudo apt-get update
+source ./devel/setup.sh
+rosdep update
 rosdep -y install igvc
-apt-get -y install qt5-default wget expect
+sudo apt-get -y install qt5-default wget expect
 wget https://files.gitter.im/RoboJackets/igvc-software/BGRW/flycapture2-2.8.3.1-amd64-pkg.tgz
 tar -xvf flycapture2-2.8.3.1-amd64-pkg.tgz
 cp ./src/util/flycapture-setup.exp flycapture2-2.8.3.1-amd64/
+cp ./src/util/flycapture-setup2.exp flycapture2-2.8.3.1-amd64/
 cd flycapture2-2.8.3.1-amd64
 
-./flycapture-setup.exp || true		# Cross our fingers...
-apt-get install -yf
-./flycapture-setup.exp || true		# Cross our fingers...
-apt-get install -yf
-./flycapture-setup2.exp				# Cross our fingers...
+sudo ./flycapture-setup.exp || true		# Cross our fingers...
+sudo apt-get install -yf
+sudo ./flycapture-setup.exp || true		# Cross our fingers...
+sudo apt-get install -yf
+sudo ./flycapture-setup2.exp			# Cross our fingers...
