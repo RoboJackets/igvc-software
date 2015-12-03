4c473541766c676d90259d1b0f90e6bcd17819a6
Jay Kamat <github@jgkamat.33mail.com>
2015-12-02 19:23:21 -0500

    Try to preinstall flycapture dependencies

--- util/indigo-setup.sh
+++ util/indigo-setup.sh
@@ -17,11 +17,13 @@ sudo apt-get -y install qt5-default wget expect
 wget https://files.gitter.im/RoboJackets/igvc-software/BGRW/flycapture2-2.8.3.1-amd64-pkg.tgz
 tar -xvf flycapture2-2.8.3.1-amd64-pkg.tgz
 cp ./src/util/flycapture-setup.exp flycapture2-2.8.3.1-amd64/
-cp ./src/util/flycapture-setup2.exp flycapture2-2.8.3.1-amd64/
 cd flycapture2-2.8.3.1-amd64
 
-sudo ./flycapture-setup.exp || true		# Cross our fingers...
-sudo apt-get install -yf
-sudo ./flycapture-setup.exp || true		# Cross our fingers...
-sudo apt-get install -yf
-sudo ./flycapture-setup2.exp			# Cross our fingers...
+
+# Install dependencies for flycapture
+sudo apt-get -y install libatkmm-1.6-1 libcairomm-1.0-1 libglade2-0 libglademm-2.4-1c2a libglibmm-2.4-1c2a \
+	libgtkmm-2.4-1c2a libpangomm-1.4-1 libsigc++-2.0-0c2a libatkmm-1.6-dev libcairomm-1.0-dev libglibmm-2.4-dev \
+	libgtkglext1-dev libgtkglextmm-x11-1.2-0 libgtkglextmm-x11-1.2-dev libgtkmm-2.4-dev libpangomm-1.4-dev \
+	libpangox-1.0-dev libsigc++-2.0-dev libxmu-dev libxmu-headers
+
+sudo ./flycapture-setup.exp			# Cross our fingers...
