e4bf0f414414851ab0ba4de96969ab84552a74b3
Tejasvi Nareddy <tejunareddy@gmail.com>
2016-02-14 18:46:08 -0500

    Pothole Detector now filters out *most* traffic barrel circles and draws contours for the potholes.

--- igvc/src/pothole_detector/potholedetector.cpp
+++ igvc/src/pothole_detector/potholedetector.cpp
@@ -13,14 +13,15 @@ using namespace pcl;
 
 cv_bridge::CvImagePtr cv_ptr;
 typedef pcl::PointCloud<pcl::PointXYZ> PCLCloud;
-//Used to threshold the sum of a 60x60 matrix whose values are between 0-255
-//Want mostly (>50%) white pixels (values around >200) in the matrix
-//60x60x200ish = 720,000/2 = ~400000
-const int sumThreshold = 400000;
 
-constexpr double radians(double degrees) {
-    return degrees / 180.0 * M_PI;
-}
+const int maxRadius = 150;
+const int minRadius = 20;
+
+// Set 1 to disable
+const int minWhiteThreshold = 255 * 0.30;
+const int maxWhiteThreshold = 255 * 1;
+
+const int sizeThreshold = 200;
 
 void PotholeDetector::img_callback(const sensor_msgs::ImageConstPtr& msg) {
     cv_ptr = cv_bridge::toCvCopy(msg, "");
@@ -28,7 +29,8 @@ void PotholeDetector::img_callback(const sensor_msgs::ImageConstPtr& msg) {
     src = cv_ptr->image.clone();
 
     //Crops the image (removes sky)
-    cv::Rect myROI(0, src.rows/2 - 100, src.cols, src.rows/2 - 50);
+    int topCrop = src.rows / 2 - 100;
+    cv::Rect myROI(0, topCrop, src.cols, src.rows - topCrop);
     src = src(myROI);
 
     cvtColor(src, src_gray, CV_BGR2GRAY);
@@ -48,33 +50,58 @@ void PotholeDetector::img_callback(const sensor_msgs::ImageConstPtr& msg) {
     GaussianBlur(src_gray, src_gray, Size(gaussian_size, gaussian_size), 2, 2);
 
     vector<Vec3f> circles;
+
     // TODO tune circle radii for actual potholes
-    HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows/8, 50, 10, 20, 50);
+    HoughCircles(src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows / 8, 50, 10, minRadius, maxRadius);
 
     Mat cloudMat = Mat::zeros(orig.rows, orig.cols, CV_32F);
     /// Put the circles into a matrix
     for( size_t i = 0; i < circles.size(); i++ ) {
         Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+        int radius = cvRound(circles[i][2]);
 
-        //If circle center is too close to bottom and right edge of image
-        if(center.x+30 >= src_gray.cols || center.y+30 >= src_gray.rows) {
+        // If the circle is too close to the top / bottom edges, filter
+        if (center.y <= 100 || center.y >= src_gray.rows - 100) {
             continue;
         }
 
-        //Get 60x60 matrix around circle center
-        cv::Rect myROI(center.x - 30 >= 0 ? center.x-30 : 0, center.y - 30 >= 0 ? center.y-30 : 0, 60, 60);
+        // If the circle is too close to the left / right edges, filter
+        if (center.x <= 200 || center.x >= src_gray.cols - 200) {
+            continue;
+        }
+
+        int cropSize = (100 / (double) src_gray.rows) * center.y;
+        //Get a matrix around circle center
+        cv::Rect myROI(center.x - (cropSize * 2), center.y - cropSize, cropSize * 4, cropSize * 2);
         Mat roi = src_gray(myROI);
 
-        //If the sum of all the pixels in the 60x60 mat is less than sumThreshold
-        //Then this circle is not encompassing mostly white pixels
+        // If the sum of all the pixels in the rectangle is less than sumThreshold
+        // Then this circle is not encompassing mostly white pixels
+        const int sumThreshold = (cropSize * cropSize * 8);
         double sum = cv::sum(roi)[0];
-        if(sum < sumThreshold) {
+        if (sum < sumThreshold * minWhiteThreshold || sum > sumThreshold * maxWhiteThreshold) {
             continue;
         }
 
-        int radius = cvRound(circles[i][2]);
-        circle(cloudMat, center, radius, Scalar(255), 1, 8, 0);
-        circle(src, center, radius, Scalar(255), 1, 8, 0);
+        //circle(cloudMat, center, radius, Scalar(255), 1, 8, 0);
+        rectangle(src, myROI, Scalar(255), 2, 8, 0);
+        cerr << "" + to_string(cropSize) + " " + to_string(radius) << endl;
+        circle(src, center, radius, Scalar(255), 2, 8, 0);
+
+        Point offset(center.x - (cropSize * 2), center.y - cropSize);
+        vector<vector<Point>> contours;
+        findContours(roi, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE, Point((center.x - (cropSize * 2)), center.y - cropSize));
+
+        // Filter false positives - small contours and cone stripes
+        for (vector<vector<Point>>::iterator it = contours.begin(); it != contours.end(); ++it) {
+            vector<Point> curCont = *it;
+            if (curCont.size() <= sizeThreshold) {
+                contours.erase(it);
+                --it;
+            }
+        }
+
+        drawContours(src, contours, -1, Scalar(20, 236, 27), 3, 8);
     }
 
     cvtColor(src_gray, src_gray, CV_GRAY2BGR);
@@ -82,7 +109,7 @@ void PotholeDetector::img_callback(const sensor_msgs::ImageConstPtr& msg) {
     cv_bridge::CvImage out_msg;
     out_msg.header   = msg->header;
     out_msg.encoding = msg->encoding;
-    out_msg.image    = src_gray;
+    out_msg.image    = src;
 
     cv_ptr->image = src;
     _pothole_filt_img.publish(cv_ptr->toImageMsg());
