f1962f5d4d6199338640d594b18540a6e386f35f
rmkeezer <rmkeezer@yahoo.com>
2016-04-06 21:06:50 -0400

    Changed plugin build system to catkin_make

---
+++ igvc/src/rviz_plugins/speed_panel.h
@@ -0,0 +1,39 @@
+#ifndef BARE_PANEL_H
+#define BARE_PANEL_H
+
+#include <ros/ros.h>
+#include <QLabel>
+#include <QApplication>
+
+
+#include <igvc_msgs/velocity_pair.h>
+
+#include <rviz/panel.h>
+#include "speedometer.h"
+class QLineEdit;
+
+class SpeedPanel: public rviz::Panel
+{
+  Q_OBJECT
+  public:
+    SpeedPanel( QWidget* parent = 0 );
+
+  private:
+    void subCallback(const igvc_msgs::velocity_pair &msg);
+    double speed;
+    Speedometer* speedometer;
+
+  public Q_SLOTS:
+
+  protected Q_SLOTS:
+
+  protected:
+    ros::Subscriber sub;
+    ros::NodeHandle nh_;
+    QLabel* velocity_label;
+    QLabel* velocity_right_label;
+    QLabel* velocity_left_label;
+
+};
+
+#endif // BARE_PANEL_H
