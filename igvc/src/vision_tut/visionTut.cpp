7c6397df1bc760b279f521c42d381beda7528436
rmkeezer <rmkeezer@yahoo.com>
2016-10-05 14:00:35 -0400

    finished basic image transport

--- igvc/src/vision_tut/visionTut.cpp
+++ igvc/src/vision_tut/visionTut.cpp
@@ -1,11 +1,18 @@
 #include "visionTut.h"
 
 VisionTut::VisionTut(ros::NodeHandle &handle)
+	: it(handle)
 {
-	
+	_src_img = it.subscribeCamera("/stereo/left/image_raw", 1, &VisionTut::img_callback, this);
+	pub = it.advertise("/stereo/left/filt_img", 1);
 }
 
-void VisionTut::img_callback(const sensor_msgs::ImageConstPtr& msg)
+void VisionTut::img_callback(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
 {
-
+    cam.fromCameraInfo(cam_info);
+	cv_ptr = cv_bridge::toCvCopy(msg, "");
+	// src_img = cv_ptr->image;
+	// cv::GaussianBlur(src_img, fin_img, cv::Size(0,0), 2.0);
+	// cv_ptr->image = fin_img;
+	pub.publish(cv_ptr->toImageMsg());
 }
--- igvc/src/vision_tut/visionTut.h
+++ igvc/src/vision_tut/visionTut.h
@@ -2,6 +2,8 @@
 #include <ros/publisher.h>
 #include <opencv2/opencv.hpp>
 #include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_geometry/pinhole_camera_model.h>
 
 class VisionTut
 {
@@ -11,6 +13,11 @@ public:
 private:
 	cv::Mat src_img, fin_img;
 
-	void img_callback(const sensor_msgs::ImageConstPtr& msg);
+	void img_callback(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info);
 
+	image_transport::ImageTransport it;
+	image_transport::Publisher pub;
+	cv_bridge::CvImagePtr cv_ptr;
+    image_geometry::PinholeCameraModel cam;
+    image_transport::CameraSubscriber _src_img;
 };
