6423ddbe440f875c4d727bf6fdfdd57df4cc6588
Dominic Pattison <dpattison3@gatech.edu>
2016-10-13 13:46:15 -0400

    removed unecessary main file from odometer

--- igvc/src/wheel_odometer/Odometer.cpp
+++ igvc/src/wheel_odometer/Odometer.cpp
@@ -123,3 +123,14 @@ Odometer::Odometer(ros::NodeHandle& nh) {
     y = 0;
     yaw = 0;
 }
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "wheel_odom");
+    ros::NodeHandle nh;
+ 
+    Odometer odom(nh);
+
+    ROS_INFO_STREAM("wheel odometry node has started");
+
+    ros::spin();
+}
