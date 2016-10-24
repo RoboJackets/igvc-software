db66e20cd4a4b83d6c43b79e6064be5303929021
Jay Kamat <jaygkamat@gmail.com>
2016-10-23 21:31:08 -0400

    Reformat graphsearch.hpp and searchproblem
    Fixed windows line endings
    Covnerted tabs to 4 spaces
    Added editorconfig

--- igvc/src/pathplanner/GraphSearch.hpp
+++ igvc/src/pathplanner/GraphSearch.hpp
@@ -38,100 +38,100 @@ public:
 template <class StateType, class ActionType, template <typename ...> class FrontierType>
 struct FrontierTraits
 {
-	using frontier_type = FrontierType <Path <StateType, ActionType>>;
-	using value_type = typename frontier_type::value_type;
+    using frontier_type = FrontierType <Path <StateType, ActionType>>;
+    using value_type = typename frontier_type::value_type;
 
-	static auto PopNextElement(frontier_type &frontier) -> value_type
-	{
-		auto next = frontier.top();
-		frontier.pop();
-		return next;
-	}
+    static auto PopNextElement(frontier_type &frontier) -> value_type
+    {
+        auto next = frontier.top();
+        frontier.pop();
+        return next;
+    }
 };
 
 
 template <class StateType, class ActionType>
 struct FrontierTraits <StateType, ActionType, std::queue>
 {
-	using frontier_type = std::queue <Path<StateType, ActionType>>;
-	using value_type = typename frontier_type::value_type;
-	
-	static auto PopNextElement(frontier_type &frontier) -> value_type
-	{
-		auto next = frontier.front();
-		frontier.pop();
-		return next;
-	}
+    using frontier_type = std::queue <Path<StateType, ActionType>>;
+    using value_type = typename frontier_type::value_type;
+
+    static auto PopNextElement(frontier_type &frontier) -> value_type
+    {
+        auto next = frontier.front();
+        frontier.pop();
+        return next;
+    }
 };
 
 class GraphSearch
 {
 public:
-	// Runs a generic search that DFS and BSF can call.
-	// Getting this to work with the current implementation of A* is possible, but difficult.
-	template <class StateType, class ActionType, template <typename ...> class FrontierType>
-	static auto GenericSearch(SearchProblem<StateType, ActionType> &problem) -> Path<StateType, ActionType>
-	{
-		using frontier_traits = FrontierTraits<StateType, ActionType, FrontierType>;
-		using frontier_type = typename frontier_traits::frontier_type;
-        
-		set<StateType> expanded;
+    // Runs a generic search that DFS and BSF can call.
+    // Getting this to work with the current implementation of A* is possible, but difficult.
+    template <class StateType, class ActionType, template <typename ...> class FrontierType>
+    static auto GenericSearch(SearchProblem<StateType, ActionType> &problem) -> Path<StateType, ActionType>
+    {
+        using frontier_traits = FrontierTraits<StateType, ActionType, FrontierType>;
+        using frontier_type = typename frontier_traits::frontier_type;
+
+        set<StateType> expanded;
         frontier_type frontier;
 
-		{
-		    Path<StateType, ActionType> p;
-		    p.addState(problem.getStartState());
-		    frontier.push(p);
-		}
+        {
+            Path<StateType, ActionType> p;
+            p.addState(problem.getStartState());
+            frontier.push(p);
+        }
 
-		while(!frontier.empty())
-		{
-		    Path<StateType, ActionType> path = frontier_traits::PopNextElement(frontier);
-		    auto const lastState = path.getLastState();
+        while(!frontier.empty())
+        {
+            Path<StateType, ActionType> path = frontier_traits::PopNextElement(frontier);
+            auto const lastState = path.getLastState();
 
-		    // expanded does not contain path's last state
-		    if(expanded.find(lastState) == expanded.cend())
-		    {
-				expanded.insert(lastState);
+            // expanded does not contain path's last state
+            if(expanded.find(lastState) == expanded.cend())
+            {
+                expanded.insert(lastState);
 
-				if(problem.isGoal(lastState))
-				{
-					return path;
-				}
+                if(problem.isGoal(lastState))
+                {
+                    return path;
+                }
 
-				auto legalActions = problem.getActions(lastState);
+                auto legalActions = problem.getActions(lastState);
 
-				for(auto it = legalActions.cbegin(); it != legalActions.cend(); ++it)
-				{
-					ActionType action = (*it);
-					StateType result = problem.getResult(lastState, action);
+                for(auto it = legalActions.cbegin(); it != legalActions.cend(); ++it)
+                {
+                    ActionType action = (*it);
+                    StateType result = problem.getResult(lastState, action);
 
-					Path<StateType, ActionType> newPath(path);
-					newPath.addAction(action);
-					newPath.addState(result);
-					frontier.push(newPath);
-				}
-		    }
-		}
+                    Path<StateType, ActionType> newPath(path);
+                    newPath.addAction(action);
+                    newPath.addState(result);
+                    frontier.push(newPath);
+                }
+            }
+        }
 
-		cerr << __func__ << " Error: Could not find a solution." << endl;
-		Path<StateType, ActionType> empty;
-		return empty;
-	}
+        cerr << __func__ << " Error: Could not find a solution." << endl;
+        Path<StateType, ActionType> empty;
+        return empty;
+    }
 
 
     /** Runs Depth-First graph search on the given search problem */
     template <class StateType, class ActionType>
     static auto DFS (SearchProblem<StateType, ActionType> &problem) -> Path<StateType, ActionType>
     {
-		return GenericSearch<StateType, ActionType, std::stack> (problem);
+        return GenericSearch<StateType, ActionType, std::stack> (problem);
     }
 
     /** Runs Breadth-First graph on the given search problem */
     template <class StateType, class ActionType>
     static auto BFS (SearchProblem<StateType, ActionType> &problem) -> Path<StateType, ActionType>
     {
-		return GenericSearch<StateType, ActionType, std::queue> (problem);
+        return GenericSearch<StateType, ActionType, std::queue> (problem);
     }
 
     /** Runs A* graph search on the given search problem */
