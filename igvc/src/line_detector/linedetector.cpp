418d89008accb2443f7b80b2682e75c2b104628d
Jeremy Schonfeld <jeremyschonfeld@gmail.com>
2016-11-16 20:34:55 -0500

    Consolidate toPointCloud methods in pothole and line detectors

--- igvc/src/line_detector/linedetector.cpp
+++ igvc/src/line_detector/linedetector.cpp
@@ -27,7 +27,7 @@ void LineDetector::img_callback(const sensor_msgs::ImageConstPtr& msg) {
         line(fin_img, cv::Point(lines[i][0], lines[i][1]), cv::Point(lines[i][2], lines[i][3]), cv::Scalar(255, 255, 255), 3, 8);
     }
 
-    cloud = toPointCloud(fin_img);
+    cloud = toPointCloud(tf_listener, MatToContours(fin_img), cam);
     _line_cloud.publish(cloud);
 
     cv_ptr->image = fin_img;
@@ -36,8 +36,8 @@ void LineDetector::img_callback(const sensor_msgs::ImageConstPtr& msg) {
 
 LineDetector::LineDetector(ros::NodeHandle &handle, const std::string& topic)
       : _it(handle)
-      , tf_listener(handle)
       , topic(topic)
+      , tf_listener(handle)
 {
      if (!hasInfo) {
         _src_img = _it.subscribe("stereo/left/image_raw", 1, &LineDetector::img_callback, this);
@@ -48,28 +48,4 @@ LineDetector::LineDetector(ros::NodeHandle &handle, const std::string& topic)
     _line_cloud = handle.advertise<PCLCloud>(topic + "/line_cloud", 100);
 }
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr LineDetector::toPointCloud(cv::Mat img){
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    tf::StampedTransform transform;
-    tf_listener.lookupTransform("/base_footprint", "/camera_left", ros::Time(0), transform);
-    for(int r = img.rows/2; r < img.rows; r++) {
-        for(int c = 0; c < img.cols; c++) {
-            if(img.at<uchar>(r, c) > 0) {
-                if (!hasInfo) {
-                    double roll, pitch, yaw;
-                    tf::Matrix3x3(transform.getRotation()).getRPY(roll, pitch, yaw);
-                    double origin_z = transform.getOrigin().getZ();
-                    double origin_y = transform.getOrigin().getY();
-                    double HFOV = toRadians(66.0);
-                    double VFOV = toRadians(47.6);
-                    pitch = -roll;
-                    cloud->points.push_back(PointFromPixelNoCam(cv::Point(c, r), img.cols, img.rows, HFOV, VFOV, origin_z, origin_y, pitch));
-                } else {
-                    cloud->points.push_back(PointFromPixel(cv::Point(c, r), transform, cam));
-                }
-            }
-        }
-    }
-    cloud->header.frame_id = "base_footprint";
-    return cloud;
-}
+
