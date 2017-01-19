889eced9a0ad84b8447706ac0cb23e34a2202e02
Justin Zheng <jzheng84@gatech.edu>
2017-01-18 20:25:22 -0500

    Included icp algorithm for global mapping

--- igvc/src/global_mapper/main.cpp
+++ igvc/src/global_mapper/main.cpp
@@ -2,30 +2,56 @@
 #include <stdlib.h>
 #include <string>
 #include <ros/publisher.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/registration/icp.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl_ros/transforms.h>
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr map_cloud;
+pcl::PointCloud<pcl::PointXYZ>::Ptr global_map;
 ros::Publisher _pointcloud_pub;
+tf::TransformListener *tf_listener;
+std::set<std::string> frames_seen;
 
-void publish_map()
-{
+void icp_transform(pcl::PointCloud<pcl::PointXYZ>::Ptr input) {
+    pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+    icp.setInputSource(input);
+    icp.setInputTarget(global_map);
+    pcl::PointCloud<pcl::PointXYZ> Final;
+    icp.align(Final);
+    *global_map = Final;
 }
 
 void frame_callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &msg, const std::string &topic)
 {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr transformed;
+    tf::StampedTransform transform;
+    if(frames_seen.find(msg->header.frame_id) == frames_seen.end())
+    {
+        frames_seen.insert(msg->header.frame_id);
+        tf_listener->waitForTransform("/map", msg->header.frame_id, ros::Time(0), ros::Duration(5));
+    }
+    tf_listener->lookupTransform("/map", msg->header.frame_id, ros::Time(0), transform);
+    pcl_ros::transformPointCloud(*msg, *transformed, transform);
+    for (auto point : *transformed) {
+        point.z = 0;
+    }
+
+    icp_transform(transformed);
+
+    _pointcloud_pub.publish(global_map);
 }
 
 int main(int argc, char** argv)
 {
-    map_cloud = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>());
+    global_map = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>());
 
     ros::init(argc, argv, "global_mapper");
 
     ros::NodeHandle nh;
+    tf_listener = new tf::TransformListener();
 
     std::string topics;
 
@@ -50,7 +76,7 @@ int main(int argc, char** argv)
         subs.push_back(nh.subscribe<pcl::PointCloud<pcl::PointXYZ> >(topic, 1, boost::bind(frame_callback, _1, topic)));
     }
 
-    map_cloud->header.frame_id = "/map";
+    global_map->header.frame_id = "/map";
 
     _pointcloud_pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ> >("/map", 1);
 
