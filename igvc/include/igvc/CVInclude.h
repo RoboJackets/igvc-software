3ca7752db1888edd034cdfce8ff7396b360b0a45
rmkeezer <rmkeezer@yahoo.com>
2017-02-19 16:47:42 -0500

    Standardized header guards and cleaned up includes

--- igvc/include/igvc/CVInclude.h
+++ igvc/include/igvc/CVInclude.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef CVINCLUDE_H
+#define CVINCLUDE_H
 
 #include <pcl/point_types.h>
 #include <pcl_ros/point_cloud.h>
@@ -6,3 +7,5 @@
 #include <image_geometry/pinhole_camera_model.h>
 #include <cv_bridge/cv_bridge.h>
 #include <tf/transform_listener.h>
+
+#endif
