5c2bfddd2021d270bbae3534ea6d7d2829d6ab09
IGVC Robot <gtrobojackets@gmail.com>
2017-08-23 20:10:58 -0400

    comp code

--- igvc/src/motor_controller/main.cpp
+++ igvc/src/motor_controller/main.cpp
@@ -4,11 +4,23 @@
 #include <ros/ros.h>
 #include <ros/subscriber.h>
 #include <std_msgs/Bool.h>
+#include <sensor_msgs/Imu.h>
 #include <string>
 
 igvc_msgs::velocity_pair current_motor_command;
 
 bool enabled = false;
+
+double w = 0;
+
+double p = 3;
+double d = 0;
+
+double last_speed = 0;
+double last_error = 0;
+double PWM = 0;
+double right_velocity = 0;
+int power = 0;
 
 void cmdCallback(const igvc_msgs::velocity_pair::ConstPtr& msg)
 {
@@ -20,6 +32,33 @@ void enabledCallback(const std_msgs::BoolConstPtr& msg)
   enabled = msg->data;
 }
 
+void imu_callback(const sensor_msgs::Imu::Ptr& msg) {
+  w = msg->angular_velocity.z;
+  ROS_ERROR_STREAM("callback = " << w);
+  double current = 0;
+  if(w > 0.1) {
+    current = -w/0.472 + right_velocity;
+  } else if(w < -0.1) {
+    current = -w / 0.472 + right_velocity;
+  }
+  double error = current - current_motor_command.left_velocity;
+  double dError = error - last_error;
+  int dPWM = p * error + dError * d;
+  PWM -= dPWM;
+  if(PWM < -255) {
+    PWM = -255;
+  }
+  if(PWM > 255) {
+    PWM = 255;
+  }
+  if(PWM < 2 && PWM > -2) {
+    PWM = 0;
+  }
+  power = 255 - PWM;
+  last_error = error;
+  last_speed = current;
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "motor_controller");
@@ -29,6 +68,8 @@ int main(int argc, char** argv)
   ros::Subscriber cmd_sub = nh.subscribe("/motors", 1, cmdCallback);
 
   ros::Subscriber enabled_sub = nh.subscribe("/robot_enabled", 1, enabledCallback);
+
+  ros::Subscriber IMU_SUB = nh.subscribe("/imu", 100, imu_callback);
 
   ros::Publisher enc_pub = nh.advertise<igvc_msgs::velocity_pair>("/encoders", 1000);
 
@@ -53,11 +94,11 @@ int main(int argc, char** argv)
   {
     ros::spinOnce();
 
-    std::string msg = "$" + std::to_string(enabled ? current_motor_command.left_velocity : 0.0) + "," +
+    std::string msg = "$" + std::to_string(enabled ? power : 0.0) + "," +
                       std::to_string(enabled ? current_motor_command.right_velocity : 0.0) + "\n";
 
-    if (fabs(current_motor_command.left_velocity) > 0 || fabs(current_motor_command.right_velocity) > 0)
-      ROS_INFO_STREAM(msg);
+    //if (fabs(current_motor_command.left_velocity) > 0 || fabs(current_motor_command.right_velocity) > 0)
+      ROS_ERROR_STREAM("sending " << msg);
 
     port.write(msg);
 
@@ -74,8 +115,20 @@ int main(int argc, char** argv)
         std::string rightStr = ret.substr(comma + 1, comma2 - comma - 1);
         std::string deltaT = ret.substr(comma2 + 1, end - comma2 - 1);
         igvc_msgs::velocity_pair enc_msg;
-        enc_msg.left_velocity = atof(leftStr.c_str());
         enc_msg.right_velocity = atof(rightStr.c_str());
+        right_velocity = 0.5;
+        //if( w == 0) {
+        //  ROS_ERROR_STREAM("w = 0");
+        //} else {
+        ROS_ERROR_STREAM("w = " << w); 
+        if(w > 0.1) {
+          enc_msg.left_velocity = -w/0.472 + right_velocity;
+        } else if(w < -0.1) {
+          enc_msg.left_velocity = -w / 0.472 + right_velocity;
+        } else {
+          enc_msg.left_velocity = enc_msg.right_velocity;
+        }
+        //}
         enc_msg.duration = atof(deltaT.c_str());
         enc_pub.publish(enc_msg);
       }
