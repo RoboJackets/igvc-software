5da2782eb30f1ac530bc9d36300d7284531171a4
sokim <sokim@gatech.edu>
2017-11-29 20:45:00 -0500

    Removed left and right cameras

--- igvc_rviz_plugins/include/igvc_rviz_plugins/sensor_panel.h
+++ igvc_rviz_plugins/include/igvc_rviz_plugins/sensor_panel.h
@@ -64,8 +64,6 @@ protected:
   void lidar_callback(const sensor_msgs::PointCloud2ConstPtr &msg, QLabel *label);
   void gps_callback(const sensor_msgs::ImuConstPtr &msg, QLabel *label);
   void cam_center_callback(const sensor_msgs::ImageConstPtr &msg, QLabel *label);
-  void cam_left_callback(const sensor_msgs::ImageConstPtr &msg, QLabel *label);
-  void cam_right_callback(const sensor_msgs::ImageConstPtr &msg, QLabel *label);
   void gps2_callback(const sensor_msgs::NavSatFixConstPtr &msg, QLabel *label);
 
 private:
