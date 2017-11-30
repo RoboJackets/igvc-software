ce89d860e1c721434f97480ead144b4de29b2354
sokim <sokim@gatech.edu>
2017-11-30 00:35:21 -0500

    Made some attempts to fix some stuff

--- igvc_rviz_plugins/src/sensor_panel.cpp
+++ igvc_rviz_plugins/src/sensor_panel.cpp
@@ -14,12 +14,10 @@ sensor_panel::sensor_panel(QWidget *parent) : rviz::Panel(parent)  // Base class
 
   // Initialize a label for displaying some data
 
-  labels.insert( std::pair<std::string, QObject::QLabel*> ("IMU",  new QLabel("IMU: Placehold\n") ) );
-  labels.insert( std::pair<std::string, QObject::QLabel*> ("Lidar", new QLabel("Lidar Sensors: Placehold\n") ) );
-  labels.insert( std::pair<std::string, QObject::QLabel*> ("Center Camera", new QLabel("CamCenter: Placehold\n") ) );
-  labels.insert( std::pair<std::string, QObject::QLabel*> ("Left Camera", new QLabel("CamLeft: Placehold\n") ) );
-  labels.insert( std::pair<std::string, QObject::QLabel*> ("Right Camera", new QLabel("CamRight: Placehold\n") ) );
-  labels.insert( std::pair<std::string, QObject::QLabel*> ("GPS", new QLabel("GPS: Placehold") ) );
+  labels.insert( std::pair<int, QObject::QLabel> (0, new QLabel("IMU: Placehold\n") ) );
+  labels.insert( std::pair<int, QObject::QLabel> (1, new QLabel("Lidar Sensors: Placehold\n") ) );
+  labels.insert( std::pair<int, QObject::QLabel> (2, new QLabel("CamCenter: Placehold\n") ) );
+  labels.insert( std::pair<int, QObject::QLabel> (3, new QLabel("GPS: Placehold") ) );
 
   /*
   *Makes timer that repeats every interval seconds
@@ -37,12 +35,8 @@ sensor_panel::sensor_panel(QWidget *parent) : rviz::Panel(parent)  // Base class
   imu_sub = nh.subscribe<sensor_msgs::Imu>("/imu", 1, boost::bind(&sensor_panel::imu_callback, this, _1, labels["IMU"]));
   lidar_sub = nh.subscribe<sensor_msgs::PointCloud2>("/scan/pointcloud", 1,
                                                      boost::bind(&sensor_panel::lidar_callback, this, _1, labels["Lidar"]));
-  camCenter_sub = nh.subscribe<sensor_msgs::Image>("/usb_cam_center/image_raw", 1,
-                                                   boost::bind(&sensor_panel::camCenter_callback, this, _1, labels["Center Camera"]));
-  camLeft_sub = nh.subscribe<sensor_msgs::Image>("/usb_cam_left/image_raw", 1,
-                                                 boost::bind(&sensor_panel::camLeft_callback, this, _1, labels["Left Camera"]));
-  camRight_sub = nh.subscribe<sensor_msgs::Image>("/usb_cam_right/image_raw", 1,
-                                                  boost::bind(&sensor_panel::camRight_callback, this, _1, labels["Right Camera"]));
+  cam_center_sub = nh.subscribe<sensor_msgs::Image>("/usb_cam_center/image_raw", 1,
+                                                   boost::bind(&sensor_panel::cam_center_callback, this, _1, labels["Center Camera"]));
   gps_sub =
       nh.subscribe<sensor_msgs::NavSatFix>("/fix", 1, boost::bind(&sensor_panel::gps2_callback, this, _1, labels["GPS"]));
 
@@ -52,9 +46,9 @@ sensor_panel::sensor_panel(QWidget *parent) : rviz::Panel(parent)  // Base class
   QVBoxLayout *layout = new QVBoxLayout;
 
   // adds all the widgets to the layout
-  for (QLabel *label : labels.begin())
+  for (int i=0; i<4; i++)
   {
-    layout->addWidget(label);
+    layout->addWidget(labels[i]);
   }
   setLayout(layout);
   timer();  // inits to disabled
@@ -78,42 +72,32 @@ void sensor_panel::cam_center_callback(const sensor_msgs::ImageConstPtr &msg, QL
   isActive[2] = true;
 }
 
-void sensor_panel::cam_left_callback(const sensor_msgs::ImageConstPtr &msg, QLabel *label)
+void sensor_panel::gps2_callback(const sensor_msgs::NavSatFixConstPtr &msg, QLabel *label)
 {
-  label->setText("Left Camera: Enabled");
+  label->setText("GPS: Enabled");
   isActive[3] = true;
 }
 
-void sensor_panel::cam_right_callback(const sensor_msgs::ImageConstPtr &msg, QLabel *label)
-{
-  label->setText("Right Camera: Enabled");
-  isActive[4] = true;
-}
-void sensor_panel::gps_callback(const sensor_msgs::NavSatFixConstPtr &msg, QLabel *label)
-{
-  label->setText("GPS: Enabled");
-  isActive[5] = true;
-}
-
 /*
-*Sets all of the activity statuses to disabled, and sets labels to diabled if already disabled
+*Sets all of the activity statuses to disabled, and sets labels to disabled if already disabled
 */
 void sensor_panel::reset_labels()
 {
-  for (bool b : isActive)
+  for (int i=0; i<4; i++)
   {
-    if (!b)
+    if (!isActive[i])
     {
-      set_label(i, b);  // set to false on second time
+      set_label(labels[i], isActive[i]);  // set to false on second time
     }
-    b = false;  // clear all statuses
+    isActive[i] = false;  // clear all statuses
+		i++;
   }
 }
 
 /*
 *Sets the label at the i position to active status of b
 */
-void sensor_panel::set_label(std::string s, bool b)
+void sensor_panel::set_label(auto &s, bool b)
 {
   std::string active, msg;
   if (b)
@@ -126,7 +110,7 @@ void sensor_panel::set_label(std::string s, bool b)
   }
   msg = msg +": ";
 
-  labels[i]->setText((msg + active).c_str());
+  s->setText((msg + active).c_str());
 }
 
 /*
