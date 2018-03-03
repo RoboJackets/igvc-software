71ce51a47a0f96f721aaf135aca8cec8b7a6da39
Billy Vaughn <vaughnbilly8@gmail.com>
2018-03-03 11:21:41 -0500

    added node to record waypoint data to a csv

---
+++ igvc/src/waypoint_data_recorder/main.cpp
@@ -0,0 +1,31 @@
+#include <geometry_msgs/PointStamped.h>
+#include <ros/ros.h>
+#include <iostream>
+#include <fstream>
+
+std::string fileName;
+
+void waypoint_callback(const geometry_msgs::PointStampedConstPtr& msg)
+{
+	std::ofstream datacsv;
+    datacsv.open (fileName, std::ios_base::app | std::ios_base::out);
+    datacsv << std::to_string(msg->point.x) + ", " + std::to_string(msg->point.y) + ",\n";
+    datacsv.close();
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "waypoint_data_recorder");
+
+    ros::NodeHandle nh;
+
+    ros::NodeHandle nhParam("~");
+
+    nhParam.getParam("file", fileName);
+
+    ros::Subscriber waypoint_sub = nh.subscribe("/waypoint", 1, waypoint_callback);
+
+    ros::spin();
+
+    return 0;
+}
