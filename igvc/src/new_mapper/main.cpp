4984cc7624b728314f44ddf07aa74aae1e33eef4
aaronmcdaniel <Aaronmcdaniel7@gmail.com>
2018-04-02 23:42:27 -0400

    Have Eigen implimentation, need to iterate through points in pointcloud though

--- igvc/src/new_mapper/main.cpp
+++ igvc/src/new_mapper/main.cpp
@@ -3,44 +3,51 @@
 #include <ros/publisher.h>
 #include <ros/ros.h>
 #include <stdlib.h>
-#include <Eigen>
+#include <Eigen/Core>
 
 ros::Publisher pointcloud_pub;
-tf::TransformListener *tf_listener;
-Eigen::Matrix<float, Dynamic, Dynamic> global_map;
+Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> global_map;
 double resolution;
-int[] position = [0, 0];
+double position [2] = {0, 0};
 int length;
 int width;
 
-
-
-void filter(pcl::PointCloud<pcl::PointXYZRGB>::Ptr input, const std::string &topic)
-{
-
-}
-
 void frame_callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &msg, const std::string &topic)
 {
+  bool offMap = false;
+  for(pcl::PointXYZRGB point : msg) // how to iterate through all points??
+  {
+    //assuming is meters from robot origin
+    double x = point.x / resolution;
+    double y = point.y / resolution;
 
+    x += position[0];
+    y += position[1];
+    if(x > 0 && y > 0 && x < length && y < width)
+    {
+      global_map(x, y) = 1.0;
+    } else if(!offMap){
+      ROS_WARN_STREAM("Some points out of range, won't be put on map.");
+      offMap = true;
+    }
+  }
 }
 
 int main(int argc, char **argv)
 {
-  new_map = pcl::PointCloud<pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB>());
 
   ros::init(argc, argv, "new_mapper");
   ros::NodeHandle nh;
-  tf_listener = new tf::TransformListener();
 
   std::string topics;
   std::list<ros::Subscriber> subs;
 
   ros::NodeHandle pNh("~");
 
-  int start_x;
-  int start_y;
+  double start_x;
+  double start_y;
 
+  pNh.getParam("topics", topics);
   pNh.getParam("occupancy_grid_length", length);
   pNh.getParam("occupancy_grid_width", width);
   pNh.getParam("qoccupancy_grid_resolution", resolution);
@@ -49,9 +56,17 @@ int main(int argc, char **argv)
 
   length = (int) (length / resolution);
   width = (int) (width / resolution);
-  position[0] = (int) (start_x / resolution);
-  position[1] = (int) (start_y / resolution);
-  global_map = Eigen::Matrix<float, Dynamic, Dynamic>(length, width);
+  position[0] = start_x / resolution;
+  position[1] = start_y / resolution;
+  //global_map = Eigen::Matrix<float, Dynamic, Dynamic>(length, width);
+  global_map = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>(length, width);
+  //will need to change when subscribe to multiple topics
+  std::list<std::string> tokens = {topics};
+  for (std:: string t : tokens)
+  {
+    ROS_INFO_STREAM("Mapper subscribing to " << t);
+    subs.push_back(nh.subscribe<pcl::PointCloud<pcl::PointXYZ>>(t, 1,boost::bind(frame_callback, _1, t)));
+  }
 
   ros::spin();
 }
