d9f476e13fde352131b644a39294f6477ca3d610
aaronmcdaniel <Aaronmcdaniel7@gmail.com>
2018-04-08 18:35:57 -0400

    No message to commit, and questionable variable instatiation but otherwise probably fills occupancy grid

--- igvc/src/new_mapper/main.cpp
+++ igvc/src/new_mapper/main.cpp
@@ -5,16 +5,18 @@
 #include <stdlib.h>
 #include <Eigen/Core>
 #include <opencv2/opencv.hpp>
+#include <pcl_ros/transforms.h>
 
 ros::Publisher pointcloud_pub;
 cv::Mat published_map; // get to work
 Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> global_map;
 tf::StampedTransform lidar_trans;
 tf::StampedTransform cam_trans;
-
+tf::TransformListener *tf_listener;
+std::string topics;
 
 double resolution;
-double position [2] = {0, 0};
+double position [2];
 int length;
 int width;
 
@@ -22,18 +24,26 @@ void frame_callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &msg, const s
 {
   //transform pointcloud into the occupancy grid, no filtering right now
   bool offMap = false;
-  //for(pcl::PointXYZRGB point : msg) // how to iterate through all points??
   //cv::Mat frame(msg->points);
-  pcl::PointCloud<pcl::PointXYZ>::const_iterator point;
-  for (point = msg->points.begin(); point < msg->points.end(); point++)
-  {
-    //assuming is meters from robot origin
-    //transform coordinates
-    double x = point->x / resolution;
-    double y = point->y / resolution;
 
-    x += position[0];
-    y += position[1];
+  //make transformed clouds
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>());
+
+  if (msg->header.frame_id == "/scan/pointcloud") {
+    pcl_ros::transformPointCloud(*msg, *transformed, lidar_trans);
+
+  } else {
+    pcl_ros::transformPointCloud(*msg, *transformed, cam_trans);
+  }
+
+
+
+  pcl::PointCloud<pcl::PointXYZ>::const_iterator point;
+  for (point = transformed->begin(); point < transformed->points.end(); point++)
+  {
+    int x = (int) std::round((point->x / resolution) + position[0]);
+    int y = (int) std::round((point->y / resolution) + position[1]);
+
     if(x > 0 && y > 0 && x < length && y < width)
     {
       global_map(x, y) = 1.0;
@@ -43,8 +53,9 @@ void frame_callback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &msg, const s
     }
   }
 
-
+  //won;t work until we make a message to publish
   pointcloud_pub.publish(published_map);
+  //pointcloud_pub.publish(&global_map);
 }
 
 int main(int argc, char **argv)
@@ -52,14 +63,11 @@ int main(int argc, char **argv)
 
   ros::init(argc, argv, "new_mapper");
   ros::NodeHandle nh;
-
-  std::string topics;
-  std::list<ros::Subscriber> subs;
-
   ros::NodeHandle pNh("~");
 
-   tf_listener = new tf::TransformListener();
+  std::list<ros::Subscriber> subs;
 
+  tf_listener = new tf::TransformListener();
 
   double start_x;
   double start_y;
@@ -71,24 +79,42 @@ int main(int argc, char **argv)
   pNh.getParam("start_X", start_x);
   pNh.getParam("start_Y", start_y);
 
-  length = (int) (length / resolution);
-  width = (int) (width / resolution);
-  position[0] = start_x / resolution;
-  position[1] = start_y / resolution;
-  //global_map = Eigen::Matrix<float, Dynamic, Dynamic>(length, width);
+  length = (int) std::round(length / resolution);
+  width = (int) std::round(width / resolution);
+  position[0] = (int) std::round(start_x / resolution);
+  position[1] = (int) std::round(start_y / resolution);
 
-  published_map(length, width, CV_32FC1, const cv::Scalar(0,0,0)); // I cant instatiate this
+  //set up tokens and get list of subscribers
+  std::istringstream iss(topics);
+  std::vector<std::string> tokens{ std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>() };
+
+  for (std::string topic : tokens)
+  {
+    ROS_INFO_STREAM("Mapper subscribing to " << topic);
+    subs.push_back(nh.subscribe<pcl::PointCloud<pcl::PointXYZ>>(topic, 1, boost::bind(frame_callback, _1, topic)));
+  }
+
+  //init transforms
+  ros::Time time = ros::Time::now();
+  if (tf_listener->waitForTransform("/odom", "/scan/pointcloud", time, ros::Duration(5.0)))
+  {
+    tf_listener->lookupTransform("/odom", "/scan/pointcloud", time, lidar_trans);
+
+  }
+  if (tf_listener->waitForTransform("/odom", "/usb_cam_center/line_cloud", time, ros::Duration(5.0)))
+  {
+    tf_listener->lookupTransform("/odom", "/usb_cam_center/line_cloud", time, cam_trans);
+
+  }
+
+
+  //TODO: Initialize the map variables, not working
+  global_map = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic>(length, width);
+  //published_map(length, width, CV_32FC1, const cv::Scalar(0,0,0)); // I cant instatiate this
   //https://docs.opencv.org/2.4/doc/tutorials/core/mat_the_basic_image_container/mat_the_basic_image_container.html
-  global_map(published_map.data()); // I can't instantiate this either
+  //global_map(published_map.data()); // I can't instantiate this either
   //https://stackoverflow.com/questions/14783329/opencv-cvmat-and-eigenmatrix
 
-  //will need to change when subscribe to multiple topics
-  std::list<std::string> tokens = {topics};
-  for (std:: string t : tokens)
-  {
-    ROS_INFO_STREAM("Mapper subscribing to " << t);
-    subs.push_back(nh.subscribe<pcl::PointCloud<pcl::PointXYZ>>(t, 1,boost::bind(frame_callback, _1, t)));
-  }
   pointcloud_pub = nh.advertise<pcl::PointCloud<pcl::PointXYZRGB>>("/map", 1);
 
   ros::spin();
