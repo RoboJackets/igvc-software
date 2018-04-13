6f802fc0bfc0e4b68ab640c8d6183d79325b63a0
Jason Gibson <jgibson37@gatech.edu>
2018-04-13 12:08:03 -0400

    added second test case, now avoids obstacles

--- igvc/src/pathplanner/igvcsearchproblemdiscrete.cpp
+++ igvc/src/pathplanner/igvcsearchproblemdiscrete.cpp
@@ -5,57 +5,67 @@ bool IGVCSearchProblemDiscrete::isActionValid(SearchMove& move, SearchLocation s
 {
   double x = start_state.X + move.X;
   double y = start_state.Y + move.Y;
-  if((x < 0 || y < 0) || (x >= Map.size().width && y >= Map.size().height)) {
+  std::cout << "x = " << x << " y = " << y;
+  std::cout << " width = " << Map->image.size().width << " height = " << Map->image.size().height << " val " << static_cast<int>(Map->image.at<uchar>(x, y));
+  if(x < 0 || y < 0 || x >= Map->image.size().width || y >= Map->image.size().height || Map->image.at<uchar>(x, y) > (255 * Threshold)) {
+    cout << " false" << std::endl;
     return false;
   }
+  cout << " true" << std::endl;
   return true;
 }
 
 std::list<SearchMove> IGVCSearchProblemDiscrete::getActions(SearchLocation state, SearchLocation robot_position)
 {
+  std::cout << state.X << "," << state.Y << "," << state.Theta << std::endl;
   std::list<SearchMove> acts;
-  double x = state.X;
-  double y = state.Y;
   double theta = state.Theta;
+  double thetaThreshold = 0.1;
   if(theta == 0) {
-    acts.push_back(SearchMove(x + 1, y + 1));
-    acts.push_back(SearchMove(x + 1, y));
-    acts.push_back(SearchMove(x + 1, y - 1));
-  } else if(theta == M_PI / 4) {
-    acts.push_back(SearchMove(x + 1, y + 1));
-    acts.push_back(SearchMove(x + 1, y));
-    acts.push_back(SearchMove(x, y + 1));
-  } else if(theta == M_PI / 2) {
-    acts.push_back(SearchMove(x + 1, y + 1));
-    acts.push_back(SearchMove(x, y + 1));
-    acts.push_back(SearchMove(x - 1, y + 1));
-  } else if(theta == 3 * M_PI / 4) {
-    acts.push_back(SearchMove(x, y + 1));
-    acts.push_back(SearchMove(x - 1, y + 1));
-    acts.push_back(SearchMove(x - 1, y));
-  } else if(theta == M_PI) {
-    acts.push_back(SearchMove(x - 1, y));
-    acts.push_back(SearchMove(x - 1, y + 1));
-    acts.push_back(SearchMove(x - 1, y - 1));
-  } else if(theta == 5 * M_PI / 4) {
-    acts.push_back(SearchMove(x - 1, y - 1));
-    acts.push_back(SearchMove(x - 1, y));
-    acts.push_back(SearchMove(x, y - 1));
-  } else if(theta == 3 * M_PI / 2) {
-    acts.push_back(SearchMove(x - 1, y - 1));
-    acts.push_back(SearchMove(x, y - 1));
-    acts.push_back(SearchMove(x + 1, y - 1));
-  } else if(theta == 7 * M_PI / 4) {
-    acts.push_back(SearchMove(x + 1, y));
-    acts.push_back(SearchMove(x + 1, y - 1));
-    acts.push_back(SearchMove(x, y - 1));
+    acts.push_back(SearchMove(1, 1));
+    acts.push_back(SearchMove(1, 0));
+    acts.push_back(SearchMove(1, -1));
+  } else if(theta - M_PI / 4 < thetaThreshold) {
+    acts.push_back(SearchMove(1, 1));
+    acts.push_back(SearchMove(1, 0));
+    acts.push_back(SearchMove(0, 1));
+  } else if(theta - M_PI / 2 < thetaThreshold) {
+    acts.push_back(SearchMove(1, 1));
+    acts.push_back(SearchMove(0, 1));
+    acts.push_back(SearchMove(-1, 1));
+  } else if(theta - 3 * M_PI / 4 < thetaThreshold) {
+    acts.push_back(SearchMove(0, 1));
+    acts.push_back(SearchMove(-1, 1));
+    acts.push_back(SearchMove(-1, 0));
+  } else if(theta - M_PI < thetaThreshold) {
+    acts.push_back(SearchMove(-1, 0));
+    acts.push_back(SearchMove(-1, 1));
+    acts.push_back(SearchMove(-1, -1));
+  } else if(theta - 5 * M_PI / 4 < thetaThreshold) {
+    acts.push_back(SearchMove(-1, -1));
+    acts.push_back(SearchMove(-1, 0));
+    acts.push_back(SearchMove(0, -1));
+  } else if(theta - 3 * M_PI / 2 < thetaThreshold) {
+    acts.push_back(SearchMove(-1, -1));
+    acts.push_back(SearchMove(0, -1));
+    acts.push_back(SearchMove(1, -1));
+  } else if(theta - 7 * M_PI / 4 < thetaThreshold) {
+    acts.push_back(SearchMove(1, 0));
+    acts.push_back(SearchMove(1, -1));
+    acts.push_back(SearchMove(0, -1));
   }
+  acts.remove_if([this, state](SearchMove m){ return !isActionValid(m, state); });
+  cout << acts.size() << endl;
   return acts;
 }
 
 SearchLocation IGVCSearchProblemDiscrete::getResult(SearchLocation state, SearchMove action)
 {
   SearchLocation result;
+  result.X = state.X;
+  result.Y = state.Y;
+  cout << "action X = " << action.X << " y = " << action.Y << std::endl;
+  cout << "result before X = " << result.X << " y = " << result.Y << std::endl;
   result.X += action.X;
   result.Y += action.Y;
   if(result.X != 0)  {
@@ -63,5 +73,6 @@ SearchLocation IGVCSearchProblemDiscrete::getResult(SearchLocation state, Search
   } else {
     result.Theta = result.Y == 1 ? M_PI / 4 : 3 * M_PI / 2;
   }
+  cout << "result after X = " << result.X << " y = " << result.Y << std::endl;
   return result;
 }
--- igvc/src/pathplanner/igvcsearchproblemdiscrete.h
+++ igvc/src/pathplanner/igvcsearchproblemdiscrete.h
@@ -6,6 +6,7 @@
 #include <functional>
 #include <vector>
 #include <opencv2/core/core.hpp>
+#include <cv_bridge/cv_bridge.h>
 #include "GraphSearch.hpp"
 #include "searchlocation.h"
 #include "searchmove.h"
@@ -13,7 +14,7 @@
 class IGVCSearchProblemDiscrete : public SearchProblem<SearchLocation, SearchMove>
 {
 public:
-  cv::Mat Map;
+  cv_bridge::CvImageConstPtr Map;
   SearchLocation Start;
   SearchLocation Goal;
   double Threshold;
@@ -32,11 +33,14 @@ public:
 
   bool isGoal(SearchLocation state)
   {
+    // scale by grid resolution
+    std::cout << "distance = " << sqrt(pow(state.X - Goal.X, 2) + pow(state.Y - Goal.Y, 2))  << std::endl;
     return sqrt(pow(state.X - Goal.X, 2) + pow(state.Y - Goal.Y, 2)) < GoalThreshold;
   }
 
   double getStepCost(SearchLocation location, SearchMove action)
   {
+    // scale by grid resolution
     if(abs(action.X) == 1 && abs(action.Y) == 1) {
       return sqrt(2);
     } else if(action.X != 0 || action.Y != 0) {
