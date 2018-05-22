5f1a48688b86f64d80554f9e374a30b09c777984
Jason Gibson <jgibson37@gatech.edu>
2018-05-22 18:26:21 -0400

    started path follower test cases

--- igvc/src/path_follower/main.cpp
+++ igvc/src/path_follower/main.cpp
@@ -19,34 +19,24 @@ double path_resolution;
 
 double max_vel, axle_length, lookahead_dist, threshold;
 
+double k1, k2;
+
 void path_callback(const nav_msgs::PathConstPtr& msg)
 {
   ROS_INFO("Follower got path");
   path = msg;
 }
 
-void get_wheel_speeds(igvc_msgs::velocity_pair& vel, double d, double theta)
+void get_wheel_speeds(igvc_msgs::velocity_pair& vel, double d, double theta, double delta)
 {
-  if(std::abs(theta) < threshold) {
-    vel.right_velocity = max_vel;
-    vel.left_velocity = max_vel;
-    return;
-  }
+  ROS_INFO_STREAM("d = " << d << " theta = " << theta << " delta = " << delta);
+  double K = k2 * (delta = atan(-k1*theta));
+  K += (1 + (k1/(1 + pow(k1 * theta, 2))))*sin(delta);
+  K /= -d;
+  double w = K * max_vel;
 
-  double turn_radius = std::abs(d / (2 * std::sin(theta)));
-  double outer_wheel = max_vel;
-  double inner_wheel = outer_wheel * (turn_radius - (axle_length / 2)) / (turn_radius + (axle_length / 2));
-
-  if (theta > 0)
-  {
-    vel.left_velocity = inner_wheel;
-    vel.right_velocity = outer_wheel;
-  }
-  else
-  {
-    vel.left_velocity = outer_wheel;
-    vel.right_velocity = inner_wheel;
-  }
+  vel.left_velocity = max_vel - w * axle_length / 2;
+  vel.right_velocity = max_vel + w * axle_length / 2;
 }
 
 double get_distance(double x1, double y1, double x2, double y2) {
@@ -74,24 +64,39 @@ void position_callback(const nav_msgs::OdometryConstPtr& msg)
   float cur_y = msg->pose.pose.position.y;
   tf::Quaternion q;
   tf::quaternionMsgToTF(msg->pose.pose.orientation, q);
-  float cur_theta = -tf::getYaw(q) + M_PI / 2;
-  cur_theta = cur_theta > 2 * M_PI ? cur_theta - 2 * M_PI : cur_theta;
+  float cur_theta = tf::getYaw(q);
 
   float tar_x, tar_y;
   geometry_msgs::Point end = path->poses[path->poses.size() - 1].pose.position;
-  if(get_distance(cur_x, cur_y, end.x, end.y) > lookahead_dist) {
+  double theta;
+  double delta;
+  if(get_distance(cur_x, cur_y, end.x, end.y) > lookahead_dist && path->poses.size() >= 2) {
     double path_index = 0;
     double distance = 0;
     bool cont = true;
     while(cont && path_index < path->poses.size() - 1) {
-      geometry_msgs::Point point1 = path->poses[path_index].pose.position;
-      geometry_msgs::Point point2 = path->poses[path_index + 1].pose.position;
+      geometry_msgs::Point point1, point2;
+      if(path->poses.size() == 1) {
+        point1 = msg->pose.pose.position;
+        point2 = path->poses[0].pose.position;
+      } else {
+        point1 = path->poses[path_index].pose.position;
+        point2 = path->poses[path_index + 1].pose.position;
+      }
       double increment = get_distance(point1.x, point1.y, point2.x, point2.y);
-      if(distance + increment >lookahead_dist) {
+      if(distance + increment > lookahead_dist) {
         cont = false;
         Eigen::Vector3d first(point1.x,point1.y, 0);
         Eigen::Vector3d second(point2.x,point2.y, 0);
         Eigen::Vector3d slope = second - first;
+        if(path_index + 2 > path->poses.size()) {
+          geometry_msgs::Point point3 = path->poses[path_index + 2].pose.position;
+          theta = -atan((point3.y - point2.y)/(point3.x - point2.x));
+          delta = -atan(slope[1]/slope[0]);
+        } else {
+          theta = -atan(slope[1]/slope[0]);
+          delta = theta;
+        }
         slope *= lookahead_dist;
         slope += first;
         tar_x = slope[0];
@@ -104,25 +109,28 @@ void position_callback(const nav_msgs::OdometryConstPtr& msg)
   } else {
     tar_x = end.x;
     tar_y = end.y;
+    theta = cur_theta;
+    delta = cur_theta;
   }
+
+  ros::Time time = ros::Time::now();
 
   geometry_msgs::PointStamped target_point;
   target_point.point.x = tar_x;
   target_point.point.y = tar_y;
   target_point.header.frame_id = "/odom";
-  target_point.header.stamp = ros::Time::now();
+  target_point.header.stamp = time;
   target_pub.publish(target_point);
+  ROS_INFO_STREAM("target = " << tar_x << ", " << tar_y);
 
-
-  double d = std::sqrt(std::pow(tar_x - cur_x, 2) + std::pow(tar_y - cur_y, 2));
-  double ang = std::atan2(tar_x - cur_x, tar_y - cur_y);
-  ang = ang < 0 ? ang + 2 * M_PI : ang;
-  double theta = cur_theta - ang;
+  double d = get_distance(cur_x, cur_y, tar_x, tar_y);
 
   igvc_msgs::velocity_pair vel;
-  get_wheel_speeds(vel, d, theta);
+  get_wheel_speeds(vel, d, theta, delta);
+  vel.header.stamp = time;
 
   cmd_pub.publish(vel);
+  ROS_INFO_STREAM("PUB " << vel.right_velocity << " " << vel.left_velocity);
 }
 
 int main(int argc, char** argv)
@@ -135,14 +143,16 @@ int main(int argc, char** argv)
   pNh.param(std::string("max_vel"), max_vel, 1.6);
   pNh.param(std::string("axle_length"), axle_length, 0.52);
   pNh.param(std::string("lookahead_dist"), lookahead_dist, 0.8);
-
-  cmd_pub = nh.advertise<igvc_msgs::velocity_pair>("/motors", 1);
-  target_pub = nh.advertise<geometry_msgs::PointStamped>("/target_point", 1);
+  pNh.param(std::string("k1"), k1, 1.0);
+  pNh.param(std::string("k2"), k2, 3.0);
 
   ros::Subscriber path_sub = nh.subscribe("/path", 1, path_callback);
 
   ros::Subscriber pose_sub = nh.subscribe("/odometry/filtered", 1, position_callback);
 
+  cmd_pub = nh.advertise<igvc_msgs::velocity_pair>("/motors", 1);
+
+  target_pub = nh.advertise<geometry_msgs::PointStamped>("/target_point", 1);
 
   ros::spin();
 
---
+++ igvc/src/tests/test_path_follower.cpp
@@ -0,0 +1,100 @@
+#include <gtest/gtest.h>
+#include <ros/ros.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PointStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <igvc_msgs/velocity_pair.h>
+#include <tf/transform_datatypes.h>
+
+class TestPathFollower : public testing::Test
+{
+public:
+  TestPathFollower()
+    : handle()
+    , mock_path_pub(handle.advertise<nav_msgs::Path>("/path", 1))
+    , mock_pose_pub(handle.advertise<nav_msgs::Odometry>("/odometry/filtered", 1))
+    , target_sub(handle.subscribe("/target_point", 1, &TestPathFollower::targetCallback, this))
+    , motors_sub(handle.subscribe("/motors", 1, &TestPathFollower::motorsCallback, this))
+      //, trajectory_sub(handle.subscribe("/trajectory", 1, &TestPathFollower::trajectoryCallback, this))
+  {
+  }
+
+  /*void trajectoryCallback(const nav_msgs::PathConstPtr& msg)
+  {
+  }*/
+
+  void targetCallback(const geometry_msgs::PointStamped::ConstPtr& msg)
+  {
+  }
+
+  void motorsCallback(const igvc_msgs::velocity_pair::ConstPtr& msg)
+  {
+  }
+
+protected:
+  virtual void SetUp()
+  {
+    while (!IsNodeReady())
+    {
+      ros::spinOnce();
+    }
+  }
+
+  virtual void TearDown()
+  {
+  }
+
+  bool IsNodeReady()
+  {
+    return (mock_path_pub.getNumSubscribers() > 0) && (mock_pose_pub.getNumSubscribers() > 0)
+      && (target_sub.getNumPublishers() > 0) && (motors_sub.getNumPublishers() > 0);
+      /*&& (trajectory_sub.getNumPublishers() > 0);*/
+  }
+
+  ros::NodeHandle handle;
+  ros::Publisher mock_path_pub;
+  ros::Publisher mock_pose_pub;
+  ros::Subscriber target_sub;
+  ros::Subscriber motors_sub;
+  //ros::Subscriber trajectory_sub;
+};
+
+TEST_F(TestPathFollower, Test)
+{
+  nav_msgs::Path path_msg;
+  path_msg.header.frame_id = "odom";
+  geometry_msgs::PoseStamped pose;
+  pose.header.stamp = path_msg.header.stamp;
+  pose.header.frame_id = path_msg.header.frame_id;
+  pose.pose.position.x = 5;
+  pose.pose.position.y = 0;
+  path_msg.poses.push_back(pose);
+
+  mock_path_pub.publish(path_msg);
+
+  nav_msgs::Odometry odom_msg;
+  odom_msg.pose.pose.position.x = 0;
+  odom_msg.pose.pose.position.y = 0;
+  odom_msg.pose.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(0, 0, 0);
+  mock_pose_pub.publish(odom_msg);
+
+  const geometry_msgs::PointStamped::ConstPtr& target_resp = ros::topic::waitForMessage<geometry_msgs::PointStamped>(target_sub.getTopic(), ros::Duration(1));
+  EXPECT_TRUE(target_resp.get() != nullptr);
+
+  const igvc_msgs::velocity_pair::ConstPtr& motors_resp = ros::topic::waitForMessage<igvc_msgs::velocity_pair>(motors_sub.getTopic(), ros::Duration(1));
+  EXPECT_TRUE(motors_resp.get() != nullptr);
+
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "test_path_follower");
+  testing::InitGoogleTest(&argc, argv);
+
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+  int ret = RUN_ALL_TESTS();
+  spinner.stop();
+  ros::shutdown();
+  return ret;
+}
