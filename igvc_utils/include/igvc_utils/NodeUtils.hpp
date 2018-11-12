978e8356f6144d51eeba61a6376ed177a1108847
Oswin So <oswinso@gmail.com>
2018-11-12 11:05:10 -0500

    Renamed paramUtils to NodeUtils, changed param to not exit by default, fixed CMakeLists formatting

--- igvc_utils/include/igvc_utils/NodeUtils.hpp
+++ igvc_utils/include/igvc_utils/NodeUtils.hpp
@@ -1,24 +1,16 @@
-//
-// Created by oswinso on 11/11/18.
-//
-#ifndef PARAMUTILS_CPP
-#define PARAMUTILS_CPP
+#ifndef NODEUTILS_HPP
+#define NODEUTILS_HPP
 
 #include <ros/ros.h>
 #include <stdlib.h>
 
 namespace igvc
 {
-    void resolveError(const std::string &param_name) {
-      ROS_ERROR_STREAM("Missing parameter " << param_name << "... exiting");
-      exit(EXIT_FAILURE);
-    }
-
     template<class T>
     void param(const ros::NodeHandle pNh, const std::string &param_name, T &param_val, const T &default_val)
     {
       if (!pNh.param(param_name, param_val, default_val)) {
-        resolveError(param_name);
+        ROS_ERROR_STREAM("Missing parameter " << param_name << ". Continuing with default values " << default_val);
       }
     }
 
@@ -26,7 +18,8 @@ namespace igvc
     void getParam(const ros::NodeHandle &pNh, const std::string &param_name, T &param_val)
     {
       if (!pNh.getParam(param_name, param_val)) {
-        resolveError(param_name);
+        ROS_ERROR_STREAM("Missing parameter " << param_name << "... exiting");
+        exit(EXIT_FAILURE);
       }
     }
 
