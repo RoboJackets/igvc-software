90ba47fa7919b0f3e93e448653a50fbce14d75d3
Oswin So <oswinso@gmail.com>
2018-12-02 18:32:25 -0500

    Changed pNH to reference, Clang Formatted.

--- igvc_utils/include/igvc_utils/NodeUtils.hpp
+++ igvc_utils/include/igvc_utils/NodeUtils.hpp
@@ -2,27 +2,28 @@
 #define NODEUTILS_HPP
 
 #include <ros/ros.h>
-#include <stdlib.h>
 
 namespace igvc
 {
-    template<class T>
-    void param(const ros::NodeHandle pNh, const std::string &param_name, T &param_val, const T &default_val)
-    {
-      if (!pNh.param(param_name, param_val, default_val)) {
-        ROS_ERROR_STREAM("Missing parameter " << param_name << " from " << pNh.getNamespace() << ". Continuing with default values " << default_val);
-      }
-    }
-
-    template<class T>
-    void getParam(const ros::NodeHandle &pNh, const std::string &param_name, T &param_val)
-    {
-      if (!pNh.getParam(param_name, param_val)) {
-        ROS_ERROR_STREAM("Missing parameter " << param_name << " from " << pNh.getNamespace() << "... exiting");
-        ros::shutdown();
-      }
-    }
-
+template <class T>
+void param(const ros::NodeHandle &pNh, const std::string &param_name, T &param_val, const T &default_val)
+{
+  if (!pNh.param(param_name, param_val, default_val))
+  {
+    ROS_ERROR_STREAM("Missing parameter " << param_name << " from " << pNh.getNamespace()
+                                          << ". Continuing with default values " << default_val);
+  }
 }
-#endif
 
+template <class T>
+void getParam(const ros::NodeHandle &pNh, const std::string &param_name, T &param_val)
+{
+  if (!pNh.getParam(param_name, param_val))
+  {
+    ROS_ERROR_STREAM("Missing parameter " << param_name << " from " << pNh.getNamespace() << "... exiting");
+    ros::shutdown();
+  }
+}
+
+}  // namespace igvc
+#endif
