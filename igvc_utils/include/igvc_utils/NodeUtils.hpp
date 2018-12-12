a80063930e77f5b25142150a604c6c430f5f956e
alescontrela <alejandroescontrela@gmail.com>
2018-12-11 22:44:55 -0500

    implement ethernet wrapper class

--- igvc_utils/include/igvc_utils/NodeUtils.hpp
+++ igvc_utils/include/igvc_utils/NodeUtils.hpp
@@ -27,6 +27,14 @@ void getParam(const ros::NodeHandle &pNh, const std::string &param_name, T &para
 }
 
 /**
+make_unique method for when using versions of c++ < 14 
+*/
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+/**
 Calculates euclidian distance between two points
 */
 inline double get_distance(double x1, double y1, double x2, double y2)
