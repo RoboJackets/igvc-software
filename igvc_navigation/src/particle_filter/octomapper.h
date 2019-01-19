8102e34ee44754e79268ec5e5b565af2a270d8bd
oswinso <oswinso@gmail.com>
2019-01-19 15:23:25 -0500

    Increased noise in urdf for testing

--- igvc_navigation/src/particle_filter/octomapper.h
+++ igvc_navigation/src/particle_filter/octomapper.h
@@ -6,12 +6,12 @@
 #include <octomap/octomap.h>
 #include <octomap_ros/conversions.h>
 
-#include <pcl_ros/point_cloud.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl_ros/point_cloud.h>
 
 #include <tf/message_filter.h>
 #include <tf/transform_listener.h>
@@ -24,26 +24,27 @@
 #include <opencv2/core/eigen.hpp>
 #include <opencv2/opencv.hpp>
 
+struct pc_map_pair
+{
+  boost::shared_ptr<octomap::OcTree> octree;
+  boost::shared_ptr<cv::Mat> map;
+};
+
 class Octomapper
 {
-  struct pc_map_pair {
-    boost::shared_ptr<octomap::OcTree> octree;
-    boost::shared_ptr<cv::Mat> map;
-  };
-
 public:
   using PCL_point_cloud = pcl::PointCloud<pcl::PointXYZ>;
 
-  explicit Octomapper(ros::NodeHandle pNh = ros::NodeHandle("~"));
-  void create_octree(boost::shared_ptr<octomap::OcTree> tree);
-  void Octomapper::insert_scan(const tf::Point& sensor_pos_tf, struct pc_map_pair& pc_map_pair,
-                             const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& raw_pc) const;
-  void Octomapper::filter_range(const pcl::PointCloud<pcl::PointXYZ>& raw_pc, pcl::PointCloud<pcl::PointXYZ>& within_range) const;
+  explicit Octomapper(ros::NodeHandle pNh);
+  void create_octree(pc_map_pair& pair) const;
+  void insert_scan(const tf::Point& sensor_pos_tf, struct pc_map_pair& pc_map_pair,
+                   const PCL_point_cloud& raw_pc) const;
+  void get_updated_map(struct pc_map_pair& pc_map_pair) const;
+  void filter_ground_plane(const PCL_point_cloud& pc, PCL_point_cloud& ground, PCL_point_cloud& nonground) const;
 
 private:
-  void filter_ground_plane(const PCL_point_cloud& pc, PCL_point_cloud& ground, PCL_point_cloud& nonground) const;
-  void get_updated_map(struct pc_map_pair& pc_map_pair) const;
-  void create_map(boost::shared_ptr<cv::Mat>& map_ptr) const;
+  void filter_range(const pcl::PointCloud<pcl::PointXYZ>& raw_pc, pcl::PointCloud<pcl::PointXYZ>& within_range) const;
+  void create_map(pc_map_pair& pair) const;
 
   ros::NodeHandle pNh;
   double m_octree_resolution;
@@ -59,6 +60,7 @@ private:
   int m_map_length;
   int m_map_width;
   int m_map_encoding;
+  float m_odds_sum_default;
 
   // std::unique_ptr<tf::MessageFilter<PCL_point_cloud>> msg_filter;
 };
