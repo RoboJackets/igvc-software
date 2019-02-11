edbe4a7212943f07a3d62b824187748d37093d97
Kyle <kkeirstead3@gatech.edu>
2019-02-10 19:02:27 -0500

    Added purple line detection.

--- igvc_gazebo/nodes/sim_color_detector/main.cpp
+++ igvc_gazebo/nodes/sim_color_detector/main.cpp
@@ -3,16 +3,17 @@
 #include <sensor_msgs/image_encodings.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/mat.hpp>
+#include <fstream>
+#include <iostream>
 
 ros::Publisher img_pub;
 
 
 void handle_image(const sensor_msgs::ImageConstPtr& msg) {
-  // convert to cv matrix
 
-  /*
     cv_bridge::CvImagePtr cv_ptr;
-    Mat frame;
+    cv::Mat frame;
 
     try {
         cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
@@ -21,21 +22,27 @@ void handle_image(const sensor_msgs::ImageConstPtr& msg) {
         return;
     }
     frame = cv_ptr->image;
-    Mat output(frame.rows, frame.cols, CV_8UC1, Scalar::all(0));
+    cv::Mat output(frame.rows, frame.cols, CV_8UC1, cv::Scalar::all(0));
 
+    for(int rowCount=0; rowCount<frame.rows; ++rowCount) {
+        for(int columnCount=0; columnCount<frame.cols; ++columnCount)  {
 
-  */
+	    	cv::Vec3b color = frame.at<cv::Vec3b>(cv::Point(columnCount,rowCount));
 
-  // iterate through frame and in ouput
-  // color detect for purple purple = white, else black
-  // convert back to image message
+	    	if (int(color.val[0])>120 && int(color.val[0]) < 140 && int(color.val[1]) < 20 && int(color.val[2]) > 70 && int(color.val[2]) < 85) {
+				output.at<uchar>(cv::Point(columnCount, rowCount)) = 255;
+			} else {
+				output.at<uchar>(cv::Point(columnCount, rowCount)) = 0;
+			}
+		
+   		}
+	}
 
-  /*  sensor_msgs::Image outmsg;
-    cv_ptr->image = output;
-    cv_ptr->encoding = "mono8";
-    cv_ptr->toImageMsg(outmsg);
-    img_pub.publish(outmsg);
-  */
+	sensor_msgs::Image outmsg;
+	cv_ptr->image = output;
+	cv_ptr->encoding = "mono8";
+	cv_ptr->toImageMsg(outmsg);
+	img_pub.publish(outmsg);
 
 }
 
@@ -46,11 +53,11 @@ int main(int argc, char** argv) {
   ros::NodeHandle pNh("~");
 
   std::string topic_name;
-  pNh.param("image_topic", topic_name, std::string("/usb_cam_center/image_raw"));
-
-  ros::Subscriber img_sub = nh.subscribe(topic_name, 1, handle_image);
+  pNh.param("image_topic", topic_name, std::string("/center_cam/image_raw"));
 
   img_pub = nh.advertise<sensor_msgs::Image>("/usb_cam_center/detected", 1);
+
+  ros::Subscriber img_sub = nh.subscribe(topic_name, 1, handle_image);
 
   ros::spin();
 
