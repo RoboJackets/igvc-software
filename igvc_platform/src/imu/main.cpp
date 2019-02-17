d6d9abd7e2fe01db242ca1ff214a89d79c56e888
oswinso <oswinso@gmail.com>
2019-02-17 11:28:50 -0500

    Refactored IMU class for greater clarity

--- igvc_platform/src/imu/main.cpp
+++ igvc_platform/src/imu/main.cpp
@@ -2,25 +2,20 @@
 
 #include <freespace/freespace.h>
 #include <freespace/freespace_util.h>
-#include <geometry_msgs/Vector3.h>
+
 #include <ros/publisher.h>
 #include <ros/ros.h>
+
+#include <sensor_msgs/MagneticField.h>
 #include <sensor_msgs/Imu.h>
+#include <geometry_msgs/Vector3.h>
 #include <std_msgs/Float64.h>
+
 #include <tf/transform_datatypes.h>
 #include <Eigen/Dense>
 #include <igvc_utils/NodeUtils.hpp>
-#include <vector>
 
-struct IMUError
-{
-  int accelReturn;
-  int accelRawReturn;
-  int angularPosReturn;
-  int angularVelReturn;
-  int magnetometerReturn;
-  int temperatureReturn;
-};
+#include <vector>
 
 struct IMUMessage
 {
@@ -37,232 +32,297 @@ public:
   IMU();
 
 private:
+  using degrees = double;
+  using radians = double;
+
   unsigned int seq;
-  double m_headingDeviationThresholdDegree{};
-  double m_yawOffsetRadians{};
-  Eigen::Matrix3d m_correctionMatrix;
+  degrees m_heading_deviation_threshold{};
+  radians m_yaw_offset{};
+  Eigen::Matrix3d m_correction_matrix;
+  int m_imu_id;
 
-  std::vector<double> m_imuOrientationCorrection;
+  std::vector<double> m_orientation_transform;
+  std::vector<double> m_linear_accel_cov;
+  std::vector<double> m_angular_vel_cov;
+  std::vector<double> m_orientation_cov;
 
-  ros::Publisher m_imu_pub;
-  ros::Publisher m_imu_raw_pub;
+  ros::Publisher m_imu_no_gravity_pub;
+  ros::Publisher m_imu_gravity_pub;
+  ros::Publisher m_magnetic_field_pub;
 
-  int initializeIMU(FreespaceDeviceId *ids);
-  void readImu(IMUError &error, IMUMessage &message) const;
-  void handleError(const IMUError &error) const;
-  void publishImuData(const IMUMessage &message) const;
-
+  int initialize_imu(FreespaceDeviceId *ids);
+  int read_imu(IMUMessage &message) const;
+  void publish_imu_data(const IMUMessage &message) const;
 };
 
-IMU::IMU() : seq{0} {
+IMU::IMU() : seq{ 0 }
+{
   ros::NodeHandle nh;
   ros::NodeHandle pNh("~");
 
   // Get matrix to correct IMU orientation.
-  igvc::getParam(nh, "imu_orientation_correction", m_imuOrientationCorrection);
+  igvc::getParam(nh, "imu_orientation_correction", m_orientation_transform);
 
   // Get threshold value for triggering a warning when the deviation between the filtered
   // heading and magnetometer heading becomes too large.
-  igvc::getParam(nh, "heading_dev_thresh_deg", m_headingDeviationThresholdDegree);
+  igvc::getParam(nh, "heading_dev_thresh_deg", m_heading_deviation_threshold);
 
   // Get yaw offset for making yaw relative to East.
-  igvc::getParam(nh, "yaw_offset", m_yawOffsetRadians);
+  igvc::getParam(nh, "yaw_offset", m_yaw_offset);
 
   // There should be 9 elements (3x3 matrix).
-  if (m_imuOrientationCorrection.size()!=9) {
-    ROS_ERROR_STREAM("IMU orientation correction matrix does not have 9 elements (3x3).");
+  if (m_orientation_transform.size() != 9)
+  {
+    ROS_ERROR_STREAM("IMU orientation transform matrix does not have 9 elements (3x3).");
+    ros::shutdown();
+  }
+  if (m_linear_accel_cov.size() != 3)
+  {
+    ROS_ERROR_STREAM("Linear acceleration covariance must have 3 elements.");
+    ros::shutdown();
+  }
+  if (m_angular_vel_cov.size() != 3)
+  {
+    ROS_ERROR_STREAM("Angular velocity covariance must have 3 elements.");
+    ros::shutdown();
+  }
+  if (m_orientation_cov.size() != 3)
+  {
+    ROS_ERROR_STREAM("Orientation covariance must have 3 elements.");
     ros::shutdown();
   }
 
-  // Populate an Eigen matrix.
-  m_correctionMatrix = Eigen::Matrix3d(m_imuOrientationCorrection.data());
+  m_correction_matrix = Eigen::Matrix3d(m_orientation_transform.data());
 
-  // Publish imu data without gravity for EKF localization.
-  m_imu_pub = nh.advertise<sensor_msgs::Imu>("/imu", 1000);
-
-  // Publish imu data with gravity for factor graph implementation.
-  m_imu_raw_pub = nh.advertise<sensor_msgs::Imu>("/imu_raw", 1000);
+  m_imu_no_gravity_pub = nh.advertise<sensor_msgs::Imu>("/imu", 1000);
+  m_imu_gravity_pub = nh.advertise<sensor_msgs::Imu>("/imu_raw", 1000);
+  m_magnetic_field_pub = nh.advertise<sensor_msgs::MagneticField>("/imu/magnetic_field", 1000);
 
   // Initialize IMU.
   FreespaceDeviceId ids[1];
-  int ret = initializeIMU(ids);
-  if (ret!=FREESPACE_SUCCESS) {
-    ROS_ERROR_STREAM("Failed to initialize IMU (Return Code " << ret << ")");
+  int return_code = initialize_imu(ids);
+  if (return_code != FREESPACE_SUCCESS)
+  {
+    ROS_ERROR_STREAM("Failed to initialize IMU (Return Code " << return_code << ")");
     ros::shutdown();
   }
+  else
+  {
+    m_imu_id = ids[0];
+  }
 
-  // Variable to store data that is read from the IMU.
-  freespace_message response;
-
-  while (ros::ok()) {
+  while (ros::ok())
+  {
     ros::spinOnce();
-    IMUError error;
-    IMUMessage message;
-    readImu(error, message);
-    handleError(error);
-    publishImuData(message);
+    IMUMessage message{};
+    if (read_imu(message) != EXIT_FAILURE) {
+      publish_imu_data(message);
+    }
   }
 }
 
-IMU::readIMU(IMUError &error, IMUMessage &message) {
-    // Read the IMU message every loop. Timeout after 1000 milliseconds.
-    ret = freespace_readMessage(ids[0], &response, 1000);
-    if (ret != FREESPACE_SUCCESS)
+/**
+ * Reads message from IMU
+ * @param[out] imu_message Variable for imu messages to be stored
+ * @return EXIT_SUCCESS if read successfully, EXIT_FAILURE if a failure occured.
+ */
+int IMU::read_imu(IMUMessage &imu_message) const
+{
+  // Read the IMU message every loop. Timeout after 1000 milliseconds.
+  freespace_message response{};
+  int return_code = freespace_readMessage(m_imu_id, &response, 1000);
+  if (return_code != FREESPACE_SUCCESS)
+  {
+    if (return_code == FREESPACE_ERROR_TIMEOUT)
     {
-      if (ret == FREESPACE_ERROR_TIMEOUT)
-      {
-        ROS_ERROR_STREAM("failed to read IMU TIMEOUT");
-      }
-      else
-      {
-        ROS_ERROR_STREAM("failed to read IMU" << ret);
-      }
+      ROS_ERROR_STREAM("failed to read IMU TIMEOUT");
     }
-
-    if (response.messageType != FREESPACE_MESSAGE_MOTIONENGINEOUTPUT)
+    else
     {
-      ROS_ERROR_STREAM("IMU error: got unknown message type " << response.messageType);
+      ROS_ERROR_STREAM("failed to read IMU" << return_code);
     }
-
-    ret = freespace_util_getAccNoGravity(&response.motionEngineOutput, &accel_msg);
-    if (ret != FREESPACE_SUCCESS)
-    {
-      ROS_ERROR_STREAM("failed to read acceleration no gravity." << ret);
-    }
-
-    ret = freespace_util_getAcceleration(&response.motionEngineOutput, &accel_raw_msg);
-    if (ret != FREESPACE_SUCCESS)
-    {
-      ROS_ERROR_STREAM("failed to read acceleration." << ret);
-    }
-    ret = freespace_util_getAngPos(&response.motionEngineOutput, &orientation_msg);
-    if (ret != FREESPACE_SUCCESS)
-    {
-      ROS_ERROR_STREAM("failed to read angular position " << ret);
-    }
-    ret = freespace_util_getAngularVelocity(&response.motionEngineOutput, &angular_vel_msg);
-    if (ret != FREESPACE_SUCCESS)
-    {
-      ROS_ERROR_STREAM("failed to read angular velocity " << ret);
-    }
-
-    Eigen::Vector3d accel_raw_vec(accel_raw_msg.x, accel_raw_msg.y, accel_raw_msg.z);
-    accel_raw_vec = correction_mat * accel_raw_vec;
-
-    Eigen::Vector3d accel_vec(accel_msg.x, accel_msg.y, accel_msg.z);
-    accel_vec = correction_mat * accel_vec;
-
-    tf::Quaternion quaternion_raw(orientation_msg.x, orientation_msg.y, orientation_msg.z, orientation_msg.w);
-    double roll, pitch, yaw;
-    tf::Matrix3x3(quaternion_raw).getRPY(roll, pitch, yaw);
-    yaw += yaw_offset;
-
-    // Get gyroscope measurements.
-
-    Eigen::Vector3d angular_vel_vec(angular_vel_msg.x, angular_vel_msg.y, angular_vel_msg.z);
-    angular_vel_vec = correction_mat * angular_vel_vec;
-
-    // Get magnetometer measurement and convert to heading.
-    ret = freespace_util_getMagnetometer(&response.motionEngineOutput, &magnetometer_msg);
-    if (ret != FREESPACE_SUCCESS)
-    {
-      ROS_ERROR_STREAM("failed to read raw magnetometer values" << ret);
-    }
-
-    // Calculate heading with magnetometer reading.
-    double yaw_mag = atan2(magnetometer_msg.y, magnetometer_msg.x) + yaw_offset;
-
-    if (fabs(yaw - yaw_mag) > heading_dev_thresh_deg * M_PI / 180.0)
-    {
-      ROS_WARN("Magnetometer heading and filtered yaw measurement disagree by > %f degrees.", heading_dev_thresh_deg);
-    }
-
-    // Publish sensor messages with corrected transformation.
-    sensor_msgs::Imu msg;
-    msg.header.frame_id = "imu";
-    msg.header.stamp = ros::Time::now();
-    msg.header.seq = seq++;
-
-    // TODO: Tune values, parameterize.
-    msg.linear_acceleration.x = accel_vec[0];
-    msg.linear_acceleration.y = accel_vec[1];
-    msg.linear_acceleration.z = accel_vec[2];
-    msg.linear_acceleration_covariance = { 0.01, 1e-6, 1e-6, 1e-6, 0.01, 1e-6, 1e-6, 1e-6, 0.01 };
-
-    msg.angular_velocity.x = angular_vel_vec[0];
-    msg.angular_velocity.y = angular_vel_vec[1];
-    msg.angular_velocity.z = angular_vel_vec[2];
-    msg.angular_velocity_covariance = { 0.02, 1e-6, 1e-6, 1e-6, 0.02, 1e-6, 1e-6, 1e-6, 0.02 };
-
-    // This assumes flat ground and uses magnetometer for absolute heading.
-    // TODO: This assumption may not hold when we switch to 3D lidar, and stop using flat ground assumption
-    tf::Quaternion quaternion_mag;
-    quaternion_mag.setRPY(0, 0, yaw_mag);
-
-    geometry_msgs::Quaternion orientation;
-    orientation.x = quaternion_mag.x();
-    orientation.y = quaternion_mag.y();
-    orientation.z = quaternion_mag.z();
-    orientation.w = quaternion_mag.w();
-
-    // TODO: Tune and parameterize.
-    msg.orientation = orientation;
-    msg.orientation_covariance = { 0.0025, 1e-6, 1e-6, 1e-6, 0.0025, 1e-6, 1e-6, 1e-6, 0.0025 };
-
-    m_imu_pub.publish(msg);
-
-    // Publish raw sensor messages.
-    sensor_msgs::Imu msg_raw;
-    msg_raw.header.frame_id = "imu";
-    msg_raw.header.stamp = msg.header.stamp;
-    msg_raw.header.seq = seq;
-
-    msg_raw.linear_acceleration.x = accel_raw_vec[0];
-    msg_raw.linear_acceleration.y = accel_raw_vec[1];
-    msg_raw.linear_acceleration.z = accel_raw_vec[2];
-    msg_raw.linear_acceleration_covariance = { 0.005, 1e-6, 1e-6, 1e-6, 0.005, 1e-6, 1e-6, 1e-6, 0.005 };
-
-    msg_raw.angular_velocity.x = angular_vel_vec[0];
-    msg_raw.angular_velocity.y = angular_vel_vec[1];
-    msg_raw.angular_velocity.z = angular_vel_vec[2];
-    msg_raw.angular_velocity_covariance = { 0.02, 1e-6, 1e-6, 1e-6, 0.02, 1e-6, 1e-6, 1e-6, 0.02 };
-
-    msg_raw.orientation = orientation;
-    msg_raw.orientation_covariance = { 0.0025, 1e-6, 1e-6, 1e-6, 0.0025, 1e-6, 1e-6, 1e-6, 0.0025 };
-
-    m_imu_raw_pub.publish(msg_raw);
+    return EXIT_FAILURE;
   }
+
+  if (response.messageType != FREESPACE_MESSAGE_MOTIONENGINEOUTPUT)
+  {
+    ROS_ERROR_STREAM("IMU error: got unknown message type " << response.messageType);
+    return EXIT_FAILURE;
+  }
+
+  return_code = freespace_util_getAccNoGravity(&response.motionEngineOutput, &imu_message.accel_msg);
+  if (return_code != FREESPACE_SUCCESS)
+  {
+    ROS_ERROR_STREAM("failed to read acceleration no gravity." << return_code);
+    return EXIT_FAILURE;
+  }
+
+  return_code = freespace_util_getAcceleration(&response.motionEngineOutput, &imu_message.accel_raw_msg);
+  if (return_code == FREESPACE_SUCCESS)
+  {
+    ROS_ERROR_STREAM("failed to read acceleration." << return_code);
+    return EXIT_FAILURE;
+  }
+  return_code = freespace_util_getAngPos(&response.motionEngineOutput, &imu_message.orientation_msg);
+  if (return_code != FREESPACE_SUCCESS)
+  {
+    ROS_ERROR_STREAM("failed to read angular position " << return_code);
+    return EXIT_FAILURE;
+  }
+  return_code = freespace_util_getAngularVelocity(&response.motionEngineOutput, &imu_message.angular_vel_msg);
+  if (return_code != FREESPACE_SUCCESS)
+  {
+    ROS_ERROR_STREAM("failed to read angular velocity " << return_code);
+    return EXIT_FAILURE;
+  }
+
+  return_code = freespace_util_getMagnetometer(&response.motionEngineOutput, &imu_message.magnetometer_msg);
+  if (return_code != FREESPACE_SUCCESS)
+  {
+    ROS_ERROR_STREAM("failed to read raw magnetometer values" << return_code);
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
 
+/**
+ * Publishes data from imu
+ * @param[in] imu_message variable containing data to be used for publishing
+ */
+void IMU::publish_imu_data(const IMUMessage &imu_message) const
+{
+  // ============================= Transform message frames using m_correction_matrix ==================================
+  Eigen::Vector3d accel_raw_vec(imu_message.accel_raw_msg.x, imu_message.accel_raw_msg.y, imu_message.accel_raw_msg.z);
+  accel_raw_vec = m_correction_matrix * accel_raw_vec;
+
+  Eigen::Vector3d accel_vec(imu_message.accel_msg.x, imu_message.accel_msg.y, imu_message.accel_msg.z);
+  accel_vec = m_correction_matrix * accel_vec;
+
+  tf::Quaternion quaternion_raw(imu_message.orientation_msg.x, imu_message.orientation_msg.y,
+                                imu_message.orientation_msg.z, imu_message.orientation_msg.w);
+  double roll, pitch, yaw;
+  tf::Matrix3x3(quaternion_raw).getRPY(roll, pitch, yaw);
+  yaw += m_yaw_offset;
+
+  // Get gyroscope measurements.
+
+  Eigen::Vector3d angular_vel_vec(imu_message.angular_vel_msg.x, imu_message.angular_vel_msg.y, imu_message.angular_vel_msg.z);
+  angular_vel_vec = m_correction_matrix * angular_vel_vec;
+
+  // Calculate heading with magnetometer reading.
+  double yaw_mag = atan2(imu_message.magnetometer_msg.y, imu_message.magnetometer_msg.x) + m_yaw_offset;
+
+  if (fabs(yaw - yaw_mag) > m_heading_deviation_threshold * M_PI / 180.0)
+  {
+    ROS_WARN("Magnetometer heading and filtered yaw measurement disagree by > %f degrees.", m_heading_deviation_threshold);
+  }
+
+  // =================================== Publish transformed measurements ==============================================
+  static unsigned int sequence = 0;
+  sensor_msgs::Imu msg{};
+  msg.header.frame_id = "imu";
+  msg.header.stamp = ros::Time::now();
+  msg.header.seq = sequence++;
+
+  msg.linear_acceleration.x = accel_vec[0];
+  msg.linear_acceleration.y = accel_vec[1];
+  msg.linear_acceleration.z = accel_vec[2];
+  msg.linear_acceleration_covariance = { m_linear_accel_cov[0], 1e-6, 1e-6,
+                                         1e-6, m_linear_accel_cov[1], 1e-6,
+                                         1e-6, 1e-6, m_linear_accel_cov[2]};
+
+  msg.angular_velocity.x = angular_vel_vec[0];
+  msg.angular_velocity.y = angular_vel_vec[1];
+  msg.angular_velocity.z = angular_vel_vec[2];
+  msg.angular_velocity_covariance = { m_angular_vel_cov[0], 1e-6, 1e-6,
+                                      1e-6, m_angular_vel_cov[1], 1e-6,
+                                      1e-6, 1e-6, m_angular_vel_cov[2]};
+
+  tf::Quaternion quaternion_mag;
+  quaternion_mag.setRPY(0, 0, yaw_mag);
+
+  geometry_msgs::Quaternion orientation{};
+  orientation.x = quaternion_mag.x();
+  orientation.y = quaternion_mag.y();
+  orientation.z = quaternion_mag.z();
+  orientation.w = quaternion_mag.w();
+
+  // TODO: Tune and parameterize.
+  msg.orientation = orientation;
+  msg.orientation_covariance = { m_orientation_cov[0], 1e-6, 1e-6,
+                                 1e-6, m_orientation_cov[1], 1e-6,
+                                 1e-6, 1e-6, m_orientation_cov[2]};
+
+  m_imu_no_gravity_pub.publish(msg);
+
+  // Publish raw sensor messages.
+  sensor_msgs::Imu msg_imu_gravity{};
+  msg_imu_gravity.header.frame_id = "imu";
+  msg_imu_gravity.header.stamp = msg.header.stamp;
+  msg_imu_gravity.header.seq = seq;
+
+  msg_imu_gravity.linear_acceleration.x = accel_raw_vec[0];
+  msg_imu_gravity.linear_acceleration.y = accel_raw_vec[1];
+  msg_imu_gravity.linear_acceleration.z = accel_raw_vec[2];
+  msg.linear_acceleration_covariance = { m_linear_accel_cov[0], 1e-6, 1e-6,
+                                         1e-6, m_linear_accel_cov[1], 1e-6,
+                                         1e-6, 1e-6, m_linear_accel_cov[2]};
+
+  msg_imu_gravity.angular_velocity.x = angular_vel_vec[0];
+  msg_imu_gravity.angular_velocity.y = angular_vel_vec[1];
+  msg_imu_gravity.angular_velocity.z = angular_vel_vec[2];
+  msg.angular_velocity_covariance = { m_angular_vel_cov[0], 1e-6, 1e-6,
+                                      1e-6, m_angular_vel_cov[1], 1e-6,
+                                      1e-6, 1e-6, m_angular_vel_cov[2]};
+
+  msg_imu_gravity.orientation = orientation;
+  msg.orientation_covariance = { m_orientation_cov[0], 1e-6, 1e-6,
+                                 1e-6, m_orientation_cov[1], 1e-6,
+                                 1e-6, 1e-6, m_orientation_cov[2]};
+
+  m_imu_gravity_pub.publish(msg_imu_gravity);
+
+  // Publish Raw Magnetic Field values. No information about covariances, so left as 0
+  sensor_msgs::MagneticField msg_magnetic_field{};
+  msg_magnetic_field.header.frame_id = "imu";
+  msg_magnetic_field.header.stamp = msg.header.stamp;
+  msg_magnetic_field.header.seq = seq;
+
+  msg_magnetic_field.magnetic_field.x = imu_message.magnetometer_msg.x;
+  msg_magnetic_field.magnetic_field.y = imu_message.magnetometer_msg.y;
+  msg_magnetic_field.magnetic_field.z = imu_message.magnetometer_msg.z;
+
+  m_magnetic_field_pub.publish(msg_magnetic_field);
+}
 
 /** A function for initializing a single FSM-9 IMU. This function sets up the IMU
  * such that it returns expected measurements (ex. linear acceleration, angular velocity, etc.).
  * \param ids an array for storing the identified IMU device ID.
  */
-int IMU::initializeIMU(FreespaceDeviceId *ids)
+int IMU::initialize_imu(FreespaceDeviceId ids[])
 {
-  int ret = freespace_init();
-  if (ret != FREESPACE_SUCCESS)
+  int return_code = freespace_init();
+  if (return_code != FREESPACE_SUCCESS)
   {
     ROS_ERROR_STREAM("Failed to init IMU");
-    return ret;
+    return return_code;
   }
 
   // Initialize freespace device (we expect only 1).
   int num_found = 0;
-  ret = freespace_getDeviceList(ids, 1, &num_found);
+  return_code = freespace_getDeviceList(ids, 1, &num_found);
   if (num_found <= 0)
   {
     ROS_ERROR_STREAM("Failed to connect to IMU, found no devices");
-    return 0;
+    return return_code;
   }
   ROS_INFO_STREAM("Found " << num_found << " devices.");
 
-  ret = freespace_openDevice(ids[0]);
-  if (ret != FREESPACE_SUCCESS)
+  return_code = freespace_openDevice(ids[0]);
+  if (return_code != FREESPACE_SUCCESS)
   {
-    ROS_ERROR_STREAM("failure to connect to device " << ret);
-    return ret;
+    ROS_ERROR_STREAM("failure to connect to device " << return_code);
+    return return_code;
   }
 
   freespace_message request{};
@@ -280,17 +340,13 @@ int IMU::initializeIMU(FreespaceDeviceId *ids)
   request.dataModeControlV2Request.ff5 = 1;              // Enable Temperature
   request.dataModeControlV2Request.ff6 = 1;              // Enable Angular Position (WXYZ)
   request.dataModeControlV2Request.ff7 = 0;              // Nothing
-  ret = freespace_sendMessage(ids[0], &request);
-  if (ret != FREESPACE_SUCCESS)
+  return_code = freespace_sendMessage(ids[0], &request);
+  if (return_code != FREESPACE_SUCCESS)
   {
     ROS_ERROR_STREAM("failed to send message");
   }
-  return ret;
+  return return_code;
 }
-
-void IMU::readImu(IMUError &error, IMUMessage &message) const {};
-void IMU::handleError(const IMUError &error) const {};
-void IMU::publishImuData(const IMUMessage &message) const {};
 
 int main(int argc, char **argv)
 {
