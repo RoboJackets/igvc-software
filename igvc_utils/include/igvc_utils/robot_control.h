b1356acf72bee56e9da6039d6ffe8de1c8e77e56
Oswin So <oswinso@gmail.com>
2019-04-09 20:06:55 -0400

    Removed changes to urdf and joint control pid yaml

--- igvc_utils/include/igvc_utils/robot_control.h
+++ igvc_utils/include/igvc_utils/robot_control.h
@@ -3,19 +3,20 @@
 
 #include <igvc_msgs/velocity_pair.h>
 
-struct CurvatureVelocity {
+struct CurvatureVelocity
+{
   double curvature;
   double velocity;
 };
 
 class RobotControl
 {
- public:
+public:
   double left_{};
   double right_{};
   double axle_length_{};
 
-  friend std::ostream & operator << (std::ostream &out, const RobotControl &robot_control);
+  friend std::ostream &operator<<(std::ostream &out, const RobotControl &robot_control);
 
   igvc_msgs::velocity_pair toMessage(ros::Time stamp) const;
   CurvatureVelocity toKV(double axle_length = -1) const;
