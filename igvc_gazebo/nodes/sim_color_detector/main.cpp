4c079437f09241ddf650b087148970516ad227fd
Kyle <kkeirstead3@gatech.edu>
2019-04-20 11:30:10 -0400

    Ran make format after failing CircleCI tests.

--- igvc_gazebo/nodes/sim_color_detector/main.cpp
+++ igvc_gazebo/nodes/sim_color_detector/main.cpp
@@ -6,15 +6,15 @@
  * a specified rate.
  */
 
+#include <cv_bridge/cv_bridge.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/mat.hpp>
 #include <fstream>
-#include <iostream>
 #include <igvc_utils/NodeUtils.hpp>
+#include <iostream>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/opencv.hpp>
 
 ros::Publisher img_pub_lines;
 ros::Publisher img_pub_barrels;
@@ -25,9 +25,10 @@ sensor_msgs::Image outmsg_barrels;
  * Holds all the ranges for a color being detected in the simulator;
  * done for improved readability
  */
-struct Color {
-
-  Color(int min_b, int max_b, int min_g, int max_g, int min_r, int max_r) {
+struct Color
+{
+  Color(int min_b, int max_b, int min_g, int max_g, int min_r, int max_r)
+  {
     _min_b = min_b;
     _max_b = max_b;
     _min_g = min_g;
@@ -59,9 +60,12 @@ bool color_check(cv::Vec3b pixel_color, Color desired_color)
   int g_value = int(pixel_color.val[1]);
   int r_value = int(pixel_color.val[2]);
 
-  if (b_value >= desired_color._min_b && b_value <= desired_color._max_b) {
-    if (g_value >= desired_color._min_g && g_value <= desired_color._max_g) {
-      if (r_value >= desired_color._min_r && r_value <= desired_color._max_r) {
+  if (b_value >= desired_color._min_b && b_value <= desired_color._max_b)
+  {
+    if (g_value >= desired_color._min_g && g_value <= desired_color._max_g)
+    {
+      if (r_value >= desired_color._min_r && r_value <= desired_color._max_r)
+      {
         return true;
       }
     }
@@ -74,34 +78,41 @@ bool color_check(cv::Vec3b pixel_color, Color desired_color)
  * Responsible for detecting the lines (currently set to a purple color) and
  * illuminating them as white (and setting all other pixels to black).
  */
-void handle_image_lines(const sensor_msgs::ImageConstPtr& msg) {
-
+void handle_image_lines(const sensor_msgs::ImageConstPtr& msg)
+{
   cv_bridge::CvImagePtr cv_ptr;
   cv::Mat frame;
 
-  try {
+  try
+  {
     cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
-  } catch (cv_bridge::Exception& e) {
+  }
+  catch (cv_bridge::Exception& e)
+  {
     ROS_ERROR("CV-Bridge error: %s", e.what());
     return;
   }
 
-  frame = cv_ptr->image; // Input image
-  cv::Mat output(frame.rows, frame.cols, CV_8UC1, cv::Scalar::all(0)); // Ouput image (B&W)
+  frame = cv_ptr->image;                                                // Input image
+  cv::Mat output(frame.rows, frame.cols, CV_8UC1, cv::Scalar::all(0));  // Ouput image (B&W)
 
   const int white_color = 255;
   const int black_color = 0;
 
-  for(int rowCount=0; rowCount<frame.rows; ++rowCount) {
-    for(int columnCount=0; columnCount<frame.cols; ++columnCount)  {
+  for (int rowCount = 0; rowCount < frame.rows; ++rowCount)
+  {
+    for (int columnCount = 0; columnCount < frame.cols; ++columnCount)
+    {
+      cv::Vec3b color = frame.at<cv::Vec3b>(cv::Point(columnCount, rowCount));
 
-      cv::Vec3b color = frame.at<cv::Vec3b>(cv::Point(columnCount,rowCount));
-
-      if (color_check(color, PURPLE)) {
+      if (color_check(color, PURPLE))
+      {
         output.at<uchar>(cv::Point(columnCount, rowCount)) = white_color;
-      } else {
+      }
+      else
+      {
         output.at<uchar>(cv::Point(columnCount, rowCount)) = black_color;
-      }   
+      }
     }
   }
 
@@ -117,38 +128,49 @@ void handle_image_lines(const sensor_msgs::ImageConstPtr& msg) {
  * Responsible for detecting the barrels (white and orange components) and
  * illuminating them as white (and setting all other pixels to black).
  */
-void handle_image_barrels(const sensor_msgs::ImageConstPtr& msg) {
-
+void handle_image_barrels(const sensor_msgs::ImageConstPtr& msg)
+{
   cv_bridge::CvImagePtr cv_ptr;
   cv::Mat frame;
 
-  try {
+  try
+  {
     cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
-  } catch (cv_bridge::Exception& e) {
+  }
+  catch (cv_bridge::Exception& e)
+  {
     ROS_ERROR("CV-Bridge error: %s", e.what());
     return;
   }
 
-  frame = cv_ptr->image; // Input image
-  cv::Mat output(frame.rows, frame.cols, CV_8UC1, cv::Scalar::all(0)); // Ouput image (B&W)
+  frame = cv_ptr->image;                                                // Input image
+  cv::Mat output(frame.rows, frame.cols, CV_8UC1, cv::Scalar::all(0));  // Ouput image (B&W)
 
   const int white_color = 255;
   const int black_color = 0;
 
-  for(int rowCount=0; rowCount<frame.rows; ++rowCount) {
-    for(int columnCount=0; columnCount<frame.cols; ++columnCount)  {
+  for (int rowCount = 0; rowCount < frame.rows; ++rowCount)
+  {
+    for (int columnCount = 0; columnCount < frame.cols; ++columnCount)
+    {
+      cv::Vec3b color = frame.at<cv::Vec3b>(cv::Point(columnCount, rowCount));
 
-      cv::Vec3b color = frame.at<cv::Vec3b>(cv::Point(columnCount,rowCount));
-
-      if (color_check(color, BLACK)) {
+      if (color_check(color, BLACK))
+      {
         output.at<uchar>(cv::Point(columnCount, rowCount)) = white_color;
-      } else if (color_check(color, ORANGE)) {
+      }
+      else if (color_check(color, ORANGE))
+      {
         output.at<uchar>(cv::Point(columnCount, rowCount)) = white_color;
-      } else if (color_check(color, WHITE)) {
+      }
+      else if (color_check(color, WHITE))
+      {
         output.at<uchar>(cv::Point(columnCount, rowCount)) = white_color;
-      } else {
+      }
+      else
+      {
         output.at<uchar>(cv::Point(columnCount, rowCount)) = black_color;
-      }   
+      }
     }
   }
 
@@ -175,17 +197,17 @@ void publish(const ros::TimerEvent&)
  * Responsible for setting up the publishers and triggering the functions to
  * update the detected black-and-white images
  */
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "sim_color_detector");
-  
+
   // LINES
 
   ros::NodeHandle nh_lines;
   ros::NodeHandle pNh_lines("~");
 
   std::string topic_name_lines;
-  
+
   igvc::param(pNh_lines, "image_topic", topic_name_lines, std::string("/center_cam/image_raw"));
 
   std::string name_for_topic_lines = topic_name_lines + "/detected_lines";
@@ -200,7 +222,7 @@ int main(int argc, char** argv) {
   ros::NodeHandle pNh_barrels("~");
 
   std::string topic_name_barrels;
-  
+
   igvc::param(pNh_barrels, "image_topic", topic_name_barrels, std::string("/center_cam/image_raw"));
 
   std::string name_for_topic_barrels = topic_name_barrels + "/detected_barrels";
@@ -212,10 +234,10 @@ int main(int argc, char** argv) {
   ros::NodeHandle nh;
 
   // Will call publish to update the published topics -> Set the publishing rate here
-  
-  const int rate = atoi(argv[1]); // This is the rate for how often it should publish as an argument from command line
 
-  ros::Timer timer = nh.createTimer(ros::Duration(1.0/rate), publish);
+  const int rate = atoi(argv[1]);  // This is the rate for how often it should publish as an argument from command line
+
+  ros::Timer timer = nh.createTimer(ros::Duration(1.0 / rate), publish);
 
   ros::spin();
 
