ec6b04127a8542f1096f6b2b5e84ab59937d519b
Oswin So <oswinso@gmail.com>
2019-04-21 01:48:13 -0400

    Removed async spinner as maybe cause of non-determinism

--- igvc_platform/src/tests/test_joystick_driver.cpp
+++ igvc_platform/src/tests/test_joystick_driver.cpp
@@ -132,10 +132,7 @@ int main(int argc, char** argv)
 
   testing::InitGoogleTest(&argc, argv);
 
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
   int ret = RUN_ALL_TESTS();
-  spinner.stop();
   ros::shutdown();
   return ret;
 }
