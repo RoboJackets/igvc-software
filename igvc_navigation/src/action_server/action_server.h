2da7c9e9cc0f1b495860037394cbd2b707ab1cd6
Tan Gemicioglu <tangem1@hotmail.com>
2019-10-09 19:32:18 -0400

    Adds TEB Local Planner and launching navigation from move base flex (#518)
    * Add teb configs
    
    * Add action server node
    
    * Add differential drive node (incomplete)
    
    * Minor fixes to make teb work
    
    * Rename teb to make things easier if we add any other local planners
    
    * Add speed limit to differential drive
    
    * Fix planner failing upon approaching waypoint
    
    * Make format
    
    * Fix format and convention errors

---
+++ igvc_navigation/src/action_server/action_server.h
@@ -0,0 +1,30 @@
+/**
+ * Class for publishing move base actions for move_base_flex.
+ * Currently, it takes values from Rviz's /move_base_simple/goal topic and the /waypoint topic.
+ *
+ * Author: Tan Gemicioglu <tangem1@hotmail.com>
+ */
+
+#ifndef ACTIONSERVER_H
+#define ACTIONSERVER_H
+
+#include <actionlib/client/simple_action_client.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mbf_msgs/MoveBaseAction.h>
+
+using MoveBaseClient = actionlib::SimpleActionClient<mbf_msgs::MoveBaseAction>;
+
+class ActionServer
+{
+public:
+  ActionServer();
+
+private:
+  MoveBaseClient client = MoveBaseClient("move_base_flex/move_base", true);
+
+  void actionCallbackPose(geometry_msgs::PoseStamped pose);
+  void actionCallbackPoint(geometry_msgs::PointStamped point);
+};
+
+#endif  // ACTIONSERVER_H
