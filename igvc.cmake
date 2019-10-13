7696d631ce70f02f9a2bbb02d7dd1620d8424d7a
oswinso <oswinso@gmail.com>
2019-10-12 23:08:24 -0400

    Force color if ninja generator is used (#528)

--- igvc.cmake
+++ igvc.cmake
@@ -5,3 +5,11 @@ add_compile_options(-W -Wall -Wextra)
 set(GIT_REPO_DIR ${CMAKE_CURRENT_LIST_DIR})
 include(${CMAKE_CURRENT_LIST_DIR}/cmake/ensure_submodules.cmake)
 
+# Add color for ninja generator
+if (CMAKE_GENERATOR STREQUAL "Ninja" AND
+((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.9) OR
+(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.5)))
+    # Force colored warnings in Ninja's output, if the compiler has -fdiagnostics-color support.
+    # Rationale in https://github.com/ninja-build/ninja/issues/814
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=always")
+endif()
