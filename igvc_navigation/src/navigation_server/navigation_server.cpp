e30d1124935d80ce23e39a13fb02ba19e1246f5c
matthewhannay <matthew.scott.hannay@gmail.com>
2019-11-24 18:47:58 -0500

    Added navigation_server class

---
+++ igvc_navigation/src/navigation_server/navigation_server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by matthewhannay on 11/24/19.
+//
+
+#include "navigation_server.h"
---
+++ igvc_navigation/src/navigation_server/navigation_server.h
@@ -0,0 +1,14 @@
+//
+// Created by matthewhannay on 11/24/19.
+//
+
+#ifndef SRC_NAVIGATION_SERVER_H
+#define SRC_NAVIGATION_SERVER_H
+
+
+class navigation_server {
+
+};
+
+
+#endif //SRC_NAVIGATION_SERVER_H
