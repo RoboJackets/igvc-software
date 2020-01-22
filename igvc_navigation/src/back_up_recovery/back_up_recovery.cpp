aee17b0daf2f65df1e163991bb5df00c2cf43e46
matthewhannay <matthew.scott.hannay@gmail.com>
2020-01-22 14:11:15 -0500

    Added back_up_recovery recovery behavior.

---
+++ igvc_navigation/src/back_up_recovery/back_up_recovery.cpp
@@ -0,0 +1,106 @@
+#include "back_up_recovery.h"
+#include <cmath>
+#include <pluginlib/class_list_macros.h>
+#include <geometry_msgs/Twist.h>
+#include <algorithm>
+#include <igvc_utils/NodeUtils.hpp>
+#include <tf2/utils.h>
+
+// register this planner as a RecoveryBehavior plugin
+PLUGINLIB_EXPORT_CLASS(back_up_recovery::BackUpRecovery, nav_core::RecoveryBehavior);
+
+namespace back_up_recovery
+{
+BackUpRecovery::BackUpRecovery() : local_costmap_(nullptr), initialized_(false), world_model_(nullptr)
+{
+}
+
+void BackUpRecovery::initialize(std::string name, tf2_ros::Buffer *, costmap_2d::Costmap2DROS *,
+                                costmap_2d::Costmap2DROS *local_costmap)
+{
+  if (!initialized_)
+  {
+    local_costmap_ = local_costmap;
+
+    // get some parameters from the parameter server
+    ros::NodeHandle private_nh("~/" + name);
+
+    private_nh.param("velocity", velocity_, 0.7);
+    private_nh.param("frequency", frequency_, 20.0);
+    private_nh.param("threshold_distance", threshold_distance_,
+                     0.6);  // distance robot backs up before exiting back up behavior
+    private_nh.param("obstacle_distance", obstacle_distance_,
+                     0.2);  // closest the robot will get to an obstacle from behind
+
+    world_model_ = new base_local_planner::CostmapModel(*local_costmap_->getCostmap());
+
+    initialized_ = true;
+  }
+  else
+  {
+    ROS_ERROR("You should not call initialize twice on this object, doing nothing");
+  }
+}
+
+BackUpRecovery::~BackUpRecovery()
+{
+  delete world_model_;
+}
+
+void BackUpRecovery::runBehavior()
+{
+  if (!initialized_)
+  {
+    ROS_ERROR("This object must be initialized before runBehavior is called");
+    return;
+  }
+
+  if (local_costmap_ == nullptr)
+  {
+    ROS_ERROR("The costmap passed to the BackupRecovery object cannot be NULL. Doing nothing.");
+    return;
+  }
+  ROS_WARN("Backup recovery behavior started.");
+
+  ros::Rate r(frequency_);
+  ros::NodeHandle n;
+  ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 5);
+
+  geometry_msgs::PoseStamped global_pose;
+  local_costmap_->getRobotPose(global_pose);
+
+  double current_yaw = tf2::getYaw(global_pose.pose.orientation);
+  geometry_msgs::Point current_position = global_pose.pose.position;
+  geometry_msgs::Point start_position = current_position;
+  double x = current_position.x - std::cos(current_yaw) * obstacle_distance_;
+  double y = current_position.y - std::sin(current_yaw) * obstacle_distance_;
+  double footprint_cost = world_model_->footprintCost(x, y, current_yaw, local_costmap_->getRobotFootprint(), 0.0, 0.0);
+  bool next_step_valid = footprint_cost >= 0.0;
+
+  geometry_msgs::Twist vel_msg;
+  vel_msg.linear.x = -velocity_;
+  vel_pub.publish(vel_msg);
+
+  ROS_INFO_STREAM("Threshold: " << threshold_distance_);
+  ROS_INFO_STREAM("Distance: " << igvc::get_distance(current_position, start_position));
+
+  while (n.ok() && igvc::get_distance(current_position, start_position) < threshold_distance_ && next_step_valid)
+  {
+    local_costmap_->getRobotPose(global_pose);
+    current_position = global_pose.pose.position;
+    x = current_position.x - std::cos(current_yaw) * obstacle_distance_;
+    y = current_position.y - std::sin(current_yaw) * obstacle_distance_;
+    footprint_cost = world_model_->footprintCost(x, y, current_yaw, local_costmap_->getRobotFootprint(), 0.0, 0.0);
+    next_step_valid = footprint_cost >= 0.0;
+
+    ROS_INFO_STREAM("Distance:" << igvc::get_distance(current_position, start_position));
+    ROS_INFO_STREAM("Next step valid: " << next_step_valid);
+
+    vel_pub.publish(vel_msg);
+    r.sleep();
+  }
+
+  vel_msg.linear.x = 0;
+  vel_pub.publish(vel_msg);
+}
+}  // namespace back_up_recovery
---
+++ igvc_navigation/src/back_up_recovery/back_up_recovery.h
@@ -0,0 +1,52 @@
+#ifndef SRC_BACK_UP_RECOVERY_H
+#define SRC_BACK_UP_RECOVERY_H
+
+#include <nav_core/recovery_behavior.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <tf2_ros/buffer.h>
+#include <base_local_planner/costmap_model.h>
+#include <string>
+
+namespace back_up_recovery
+{
+/**
+ * @class BackUpRecovery
+ * @brief A recovery behavior that makes the robot move backwards a bit
+ */
+class BackUpRecovery : public nav_core::RecoveryBehavior
+{
+public:
+  /**
+   * @brief  Constructor, make sure to call initialize in addition to actually initialize the object
+   */
+  BackUpRecovery();
+
+  /**
+   * @brief  Initialization function for the BackupRecovery recovery behavior
+   * @param name Namespace used in initialization
+   * @param tf (unused)
+   * @param global_costmap (unused)
+   * @param local_costmap A pointer to the local_costmap used by the navigation stack
+   */
+  void initialize(std::string name, tf2_ros::Buffer*, costmap_2d::Costmap2DROS*,
+                  costmap_2d::Costmap2DROS* local_costmap) override;
+
+  /**
+   * @brief  Run the BackupRecovery recovery behavior.
+   */
+  void runBehavior() override;
+
+  /**
+   * @brief  Destructor for the back up recovery behavior
+   */
+  ~BackUpRecovery() override;
+
+private:
+  costmap_2d::Costmap2DROS* local_costmap_;
+  bool initialized_;
+  double velocity_, frequency_, threshold_distance_, obstacle_distance_;
+  base_local_planner::CostmapModel* world_model_;
+};
+
+}  // namespace back_up_recovery
+#endif  // SRC_BACK_UP_RECOVERY_H
