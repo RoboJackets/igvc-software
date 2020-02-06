2e101c9558917f19b432ea24e9e5d7caca4df53b
Oswin So <oswinso@gmail.com>
2020-02-06 12:25:15 -0500

    Added torchscript trace model loading to nn_viz

--- igvc_perception/src/nn_viz/nn_viz.py
+++ igvc_perception/src/nn_viz/nn_viz.py
@@ -7,12 +7,12 @@ if __name__ == '__main__':
     rospy.init_node('nn_viz')
 
     # Read ros params.
-    input_topic = rospy.get_param('input_topic')
-    output_topic = rospy.get_param('output_topic')
-    image_resize_width = rospy.get_param('image_resize_width')
-    image_resize_height = rospy.get_param('image_resize_height')
-    model_path = rospy.get_param('model_path')
-    force_cpu = rospy.get_param('force_cpu')
+    input_topic = rospy.get_param('~input_topic')
+    output_topic = rospy.get_param('~output_topic')
+    image_resize_width = rospy.get_param('~image_resize_width')
+    image_resize_height = rospy.get_param('~image_resize_height')
+    model_path = rospy.get_param('~model_path')
+    force_cpu = rospy.get_param('~force_cpu')
 
     rospy.loginfo("[nn_viz] Testing the model %s", model_path)
 
