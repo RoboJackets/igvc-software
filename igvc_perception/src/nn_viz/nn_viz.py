74abb2083a13f62a1b1b609c77653b23226ee2d2
oswinso <oswinso@gmail.com>
2020-02-18 14:11:21 -0500

    Created CNN model visualizer node (#641)
    * Created CNN model visualizer node
    * Increase line limit to 120
    * Changed to flake8

---
+++ igvc_perception/src/nn_viz/nn_viz.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+from visualizer import Visualizer
+
+if __name__ == '__main__':
+    rospy.init_node('nn_viz')
+
+    # Read ros params.
+    input_topic = rospy.get_param('input_topic')
+    output_topic = rospy.get_param('output_topic')
+    image_resize_width = rospy.get_param('image_resize_width')
+    image_resize_height = rospy.get_param('image_resize_height')
+    model_path = rospy.get_param('model_path')
+    force_cpu = rospy.get_param('force_cpu')
+
+    rospy.loginfo("[nn_viz] Testing the model %s", model_path)
+
+    Visualizer(input_topic, output_topic, image_resize_width, image_resize_height, model_path, force_cpu)
+    rospy.loginfo("[nn_viz] NN visualizer up!")
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        rospy.logerr("Shutting down")
