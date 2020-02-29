b058054817ab5bac9de0a8507994aeeaf80a041d
alescontrela <alejandroescontrela@gmail.com>
2020-02-29 17:30:30 -0500

    Add basic functionality for integrating IMU measurements and optimizing at GPS message.

--- igvc_navigation/src/pose_slam_estimator/PoseSlamEstimator.cpp
+++ igvc_navigation/src/pose_slam_estimator/PoseSlamEstimator.cpp
@@ -13,6 +13,16 @@ PoseSlamEstimator::PoseSlamEstimator(ros::NodeHandle* nodehandle) : nh_(*nodehan
   // Publish estimated trajectory.
   trajectory_publisher_ = nh_.advertise<nav_msgs::Path>("/pose_path", 1);
 
+  p_->setAccelerometerCovariance(measured_acc_cov_);
+  p_->setIntegrationCovariance(integration_error_cov_);
+  p_->setGyroscopeCovariance(measured_omega_cov_);
+  p_->setBiasAccCovariance(bias_acc_cov_);
+  p_->setBiasOmegaCovariance(bias_omega_cov_);
+  p_->setBiasAccOmegaInt(bias_acc_omega_int_);
+
+  imu_preintegrated_ = std::make_shared<gtsam::PreintegratedCombinedMeasurements>(
+    p_, prior_imu_bias_);
+
   // assertions::getParam(pNh, "c_space", configuration_space_);
 
   // ros::Rate rate(rate_);  // path update rate
@@ -27,24 +37,130 @@ PoseSlamEstimator::PoseSlamEstimator(ros::NodeHandle* nodehandle) : nh_(*nodehan
 }
 
 void PoseSlamEstimator::imu_callback(const sensor_msgs::ImuConstPtr& msg) {
-    return;
+    // Update initial gps and orientation readings until both are received.
+    if (!fg_initialized_) {
+      if (!initial_imu_reading_received_ || !initial_gps_coords_received) {
+        initial_imu_reading_received_ = true;
+        initial_state_(3) = msg->orientation.x;
+        initial_state_(4) = msg->orientation.y;
+        initial_state_(5) = msg->orientation.z;
+        initial_state_(6) = msg->orientation.w;
+        initial_state_(7) = 0.0; // Assume zero initial velocity.
+        initial_state_(8) = 0.0;
+        initial_state_(9) = 0.0;
+      } else {
+        initialize_pose_graph();
+      }
+      return;
+    }
+
+    if (last_imu_msg_t_ == -1)
+      last_imu_msg_t_ = msg->header.stamp.sec;
+    
+    double dt = msg->header.stamp.sec - last_imu_msg_t_;
+
+    gtsam::Vector3 measured_acc = (
+      gtsam::Vector(3) << msg->linear_acceleration.x
+                       , msg->linear_acceleration.y
+                       , msg->linear_acceleration.z).finished();
+    gtsam::Vector3 measured_omega = (
+      gtsam::Vector(3) << msg->angular_velocity.x
+                       , msg->angular_velocity.y
+                       , msg->angular_velocity.z).finished();
+    
+    imu_preintegrated_->integrateMeasurement(measured_acc, measured_omega, dt);
+
+    last_imu_msg_t_ = msg->header.stamp.sec;
+
 }
 
 void PoseSlamEstimator::gps_callback(const sensor_msgs::NavSatFixConstPtr& msg) {
 
-    if (!initial_gps_coords_received)
-        initial_gps_coords_received = true;
-
     // Convert gps readings to ned.
     double north, east, down;
-    geodetic2Ned(
+    geodetic_converter_.geodetic2Ned(
         static_cast<double>(msg->latitude),
         static_cast<double>(msg->longitude),
-        static_cast<double>(msg->altitide),
-        north, east, down
+        static_cast<double>(msg->altitude),
+        &north, &east, &down
     );
 
-    return;
+    // Update initial gps and orientation readings until both are received.
+    if (!fg_initialized_) {
+      if (!initial_imu_reading_received_ || !initial_gps_coords_received) {
+        initial_gps_coords_received = true;
+        initial_state_(0) = north;
+        initial_state_(1) = east;
+        initial_state_(2) = down;
+      } else {
+        initialize_pose_graph();
+      }
+      return;
+    }
+  
+    correction_count_++;
+
+    // Add IMU factor to graph.
+    const gtsam::PreintegratedCombinedMeasurements& preint_imu_combined =
+      dynamic_cast<const gtsam::PreintegratedCombinedMeasurements&>(*imu_preintegrated_);
+    gtsam::CombinedImuFactor imu_factor(
+      X(correction_count_ - 1), V(correction_count_ - 1), X(correction_count_),
+      V(correction_count_), B(correction_count_ - 1), B (correction_count_),
+      preint_imu_combined);
+    graph_->add(imu_factor);
+
+    // Add GPS factor.
+    gtsam::noiseModel::Diagonal::shared_ptr correction_noise =
+      gtsam::noiseModel::Isotropic::Sigma(3, 1.0);
+    gtsam::GPSFactor gps_factor (X(correction_count_),
+      gtsam::Point3(north, east, down), correction_noise);
+    graph_->add(gps_factor);
+
+    // Optimize!
+    prop_state_ = imu_preintegrated_->predict(prev_state_, prev_bias_);
+    initial_values_.insert(X(correction_count_), prop_state_.pose());
+    initial_values_.insert(V(correction_count_), prop_state_.v());
+    initial_values_.insert(B(correction_count_), prev_bias_);
+
+    gtsam::LevenbergMarquardtOptimizer optimizer(*graph_, initial_values_);
+    gtsam::Values results = optimizer.optimize();
+
+    // Overwrite the beginning of the preintegration for the next step.
+    prev_state_ = gtsam::NavState(
+      results.at<gtsam::Pose3>(X(correction_count_)),
+      results.at<gtsam::Vector3>(V(correction_count_)));
+    prev_bias_ = results.at<gtsam::imuBias::ConstantBias>(B(correction_count_));
+
+    // Reset the preintegration object.
+    imu_preintegrated_->resetIntegrationAndSetBias(prev_bias_);
+
+}
+
+void PoseSlamEstimator::initialize_pose_graph() {
+  // Assemble initial quaternion through gtsam constructor ::quaternion(w,x,y,z);
+  gtsam::Rot3 prior_rotation = gtsam::Rot3::Quaternion(initial_state_(6), initial_state_(3), 
+                                         initial_state_(4), initial_state_(5));
+  gtsam::Point3 prior_point(initial_state_.head<3>());
+  gtsam::Pose3 prior_pose(prior_rotation, prior_point);
+  gtsam::Vector3 prior_velocity(initial_state_.tail<3>());
+
+  gtsam::Values initial_values;
+  int correction_count = 0;
+  initial_values.insert(X(correction_count), prior_pose);
+  initial_values.insert(V(correction_count), prior_velocity);
+  initial_values.insert(B(correction_count), prior_imu_bias_);
+
+  // Anchor the factor graph by adding all prior factors.
+  graph_->add(gtsam::PriorFactor<gtsam::Pose3>(
+    X(correction_count_), prior_pose, pose_noise_model_));
+  graph_->add(gtsam::PriorFactor<gtsam::Vector3>(
+    V(correction_count_), prior_velocity, velocity_noise_model_));
+  graph_->add(gtsam::PriorFactor<gtsam::imuBias::ConstantBias>(
+    B(correction_count_), prior_imu_bias_, bias_noise_model_));
+  
+  prev_state_ = gtsam::NavState(prior_pose, prior_velocity);
+  prop_state_ = prev_state_;
+  prev_bias_ = prior_imu_bias_;
 }
 
 
--- igvc_navigation/src/pose_slam_estimator/PoseSlamEstimator.h
+++ igvc_navigation/src/pose_slam_estimator/PoseSlamEstimator.h
@@ -28,6 +28,11 @@ Date Created: January 22nd, 2020
 #include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <gtsam/inference/Symbol.h>
+#include <gtsam/base/Vector.h>
+
+#include <cmath>
+
+#include <boost/shared_ptr.hpp>
 
 #include "geodetic_conv.hpp"
 
@@ -63,6 +68,11 @@ public:
   void gps_callback(const sensor_msgs::NavSatFixConstPtr& msg);
 
   /**
+   *  Use initial state to set FG priors and initialize values.
+   */
+  void initialize_pose_graph(void);
+
+  /**
       Publish the estimated trajectory.
   */
   void publish_path();
@@ -70,17 +80,72 @@ public:
   // ---------- Front End ----------- //
 
 private:
-  gtsam::PreintegrationType *imu_preintegrated_;
+  std::shared_ptr<gtsam::PreintegrationType> imu_preintegrated_ = nullptr;
 
-  // Need to obtain initial values to specify prior.
-  bool initial_vel_received_;
-  bool initial_orientation_received_;
-  bool initial_gps_coords_received;
+  // Initial values to anchor FG.
+  bool initial_imu_reading_received_ = false;  // Need initial rotation (R4) and vel (R3).
+  bool initial_gps_coords_received = false;  // Need initial GPS coords (R3).
+  // Format: (N,E,D,qX,qY,qZ,qW,velN,velE,velD)
+  Eigen::Matrix<double,10,1> initial_state_ = Eigen::Matrix<double,10,1>::Zero();
+  bool fg_initialized_ = false;  // Priors set and optimization has begun.
 
+  // Solution initialization values.
+  gtsam::Values initial_values_;
+
+  int correction_count;  // Number of poses thus far.
+
+  // The factor graph.
+  gtsam::NonlinearFactorGraph *graph_ = new gtsam::NonlinearFactorGraph();
+
+  // Assume zero initial bias.
   gtsam::imuBias::ConstantBias prior_imu_bias_; // assume zero initial bias
 
-  // Use whatever this is to convert llh to ned.
-  // http://docs.ros.org/fuerte/api/enu/html/group__coord__system.html
+  // Used to convert from llh to ned.
+  geodetic_converter::GeodeticConverter geodetic_converter_{};
+
+  // Sensor specs.
+  // TODO(alescontrela): Change these to match our actual IMU & GPS specs.
+  double accel_noise_sigma_ = 0.000394;
+  double gyro_noise_sigma_ = 0.000205;
+  double accel_bias_rw_sigma_ = 0.004905;
+  double gyro_bias_rw_sigma_ = 0.00000145;
+  gtsam::Matrix33 measured_acc_cov_ =
+    gtsam::Matrix33::Identity(3,3) * std::pow(accel_noise_sigma_, 2);
+  gtsam::Matrix33 measured_omega_cov_ =
+    gtsam::Matrix33::Identity(3,3) * std::pow(gyro_noise_sigma_, 2);
+  gtsam::Matrix33 integration_error_cov_ =
+    gtsam::Matrix33::Identity(3,3) * 1e-8;
+  gtsam::Matrix33 bias_acc_cov_ =
+    gtsam::Matrix33::Identity(3,3) * std::pow(accel_bias_rw_sigma_, 2);
+  gtsam::Matrix33 bias_omega_cov_ =
+    gtsam::Matrix33::Identity(3,3) * std::pow(gyro_bias_rw_sigma_, 2);
+  gtsam::Matrix66 bias_acc_omega_int_ =
+    gtsam::Matrix::Identity(6, 6) * 1e-5;
+  boost::shared_ptr<gtsam::PreintegratedCombinedMeasurements::Params> p_ =
+    gtsam::PreintegratedCombinedMeasurements::Params::MakeSharedD(0.0);
+
+  // Assemble prior noise models.
+  // Pose: rad, rad, rad, m, m, m.
+  gtsam::noiseModel::Diagonal::shared_ptr pose_noise_model_ =
+    gtsam::noiseModel::Diagonal::Sigmas(
+      (gtsam::Vector(6) << 0.01, 0.01, 0.01, 0.5, 0.5, 0.5).finished());
+  // Velocity: m/s, m/s, m/s.
+  gtsam::noiseModel::Diagonal::shared_ptr velocity_noise_model_ =
+    gtsam::noiseModel::Isotropic::Sigma(3, 0.1);
+  // Bias.
+  gtsam::noiseModel::Diagonal::shared_ptr bias_noise_model_ =
+    gtsam::noiseModel::Isotropic::Sigma(6, 1e-3);
+
+  // Number of measurements added to FG.
+  int correction_count_ = 0;
+
+  // Store previous state for the imu integration and the latest
+  // predicted outcome.
+  gtsam::NavState prev_state_;
+  gtsam::NavState prop_state_;
+  gtsam::imuBias::ConstantBias prev_bias_;
+
+  double last_imu_msg_t_ = -1;
 
 };
 
