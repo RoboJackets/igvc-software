81f3a34b9f06758a36a779dbf20b47e0593b0c78
Matthew Hannay <matthew.scott.hannay@gmail.com>
2020-03-12 10:32:12 -0400

    Traversability Costmap Implementation (#672)
    * removed deprecated files relating to lidar
    
    * Added elevation_mapping launch files and pose converter
    
    * slope gridmap filter added
    
    * removed <depend>grid_map and replaced it with grid_map_core, _ros, and _msgs
    
    * Added kindr_ros and elevation_mapping submodules
    
    * Changed logodd parameter to probability parameter
    
    * Ignore kindr in circleci rosdep install (#685)
    
    Co-authored-by: oswinso <oswinso@gmail.com>

---
+++ igvc_perception/src/slope_filter/slope_filter.h
@@ -0,0 +1,23 @@
+#ifndef SRC_SLOPE_FILTER_H
+#define SRC_SLOPE_FILTER_H
+
+#include <ros/ros.h>
+#include <filters/filter_chain.h>
+#include <grid_map_core/GridMap.hpp>
+#include <grid_map_msgs/GridMap.h>
+
+class TraversabilityFilter
+{
+public:
+  TraversabilityFilter();
+
+private:
+  ros::NodeHandle private_nh_;
+  ros::Subscriber elevation_map_sub_;
+  ros::Publisher traversability_map_pub_;
+  filters::FilterChain<grid_map::GridMap> filter_chain_;
+
+  void elevationMapCallback(const grid_map_msgs::GridMap& message);
+};
+
+#endif  // SRC_SLOPE_FILTER_H
