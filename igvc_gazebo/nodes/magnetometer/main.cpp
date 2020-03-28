161bb8743d890430c8b43bad3bec776fab5f4dbc
Andrew Yarovoi <andrewytiger@gmail.com>
2020-03-28 19:28:57 -0400

    made mag_covariance a param

--- igvc_gazebo/nodes/magnetometer/main.cpp
+++ igvc_gazebo/nodes/magnetometer/main.cpp
@@ -3,6 +3,7 @@
 #include <sensor_msgs/MagneticField.h>
 
 ros::Publisher mag_field_pub_;
+static double mag_field_covar;
 
 void magCallback(const geometry_msgs::Vector3Stamped& msg)
 {
@@ -13,7 +14,7 @@ void magCallback(const geometry_msgs::Vector3Stamped& msg)
   magnet_msg.magnetic_field.x = msg.vector.x;
   magnet_msg.magnetic_field.y = msg.vector.y;
   magnet_msg.magnetic_field.z = msg.vector.z;
-  magnet_msg.magnetic_field_covariance = { 1e-6, 0, 0, 0, 1e-6, 0, 0, 0, 1e-6 };
+  magnet_msg.magnetic_field_covariance = { mag_field_covar, 0, 0, 0, mag_field_covar, 0, 0, 0, mag_field_covar };
   mag_field_pub_.publish(magnet_msg);
 }
 
@@ -24,6 +25,7 @@ int main(int argc, char** argv)
   ros::NodeHandle pnh("~");
   std::string sub_topic = pnh.param("mag_sub_topic", std::string("/magnetometer/vector"));
   std::string pub_topic = pnh.param("mag_pub_topic", std::string("/magnetometer_mag"));
+  mag_field_covar = pnh.param("mag_field_variance", 1e-6);
   mag_field_pub_ = nh.advertise<sensor_msgs::MagneticField>(pub_topic, 10);
   ros::Subscriber scan_sub = nh.subscribe(sub_topic, 1, magCallback);
   ros::spin();
