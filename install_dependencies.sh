89f6cb69b259a8e038dc2944a59a32f18a8fe0bf
Tan Gemicioglu <tangem1@hotmail.com>
2020-03-29 10:23:57 -0400

    Fix kindr and external dependencies (#705)
    * Fix submodule list
    
    * Add installation script
    
    * Fix readme

---
+++ install_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ ! -d "/usr/local/include/kindr/" ]; then
+    git clone https://github.com/ANYbotics/kindr.git
+    cd kindr
+    mkdir build
+    cd build
+    cmake ..
+    sudo make install
+    cd ../..
+    rm -rf kindr
+fi
+rosdep install -iy --from-paths ../../src --skip-keys='kindr'
