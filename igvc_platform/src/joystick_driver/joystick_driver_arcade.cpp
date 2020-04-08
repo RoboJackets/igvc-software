7989193c8d04a90b234ebec6274cf102b46b14c9
Paul <poc.kcchiefs@gmail.com>
2020-04-08 00:10:04 -0400

    Updating to move to a different branch

---
+++ igvc_platform/src/joystick_driver/joystick_driver_arcade.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pcapple on 4/8/20.
+//
+
+#include "joystick_driver_arcade.h"
---
+++ igvc_platform/src/joystick_driver/joystick_driver_arcade.h
@@ -0,0 +1,16 @@
+//
+// Created by pcapple on 4/8/20.
+//
+
+#ifndef SRC_JOYSTICK_DRIVER_ARCADE_H
+#define SRC_JOYSTICK_DRIVER_ARCADE_H
+
+
+
+class joystick_driver_arcade {
+
+};
+
+
+
+#endif //SRC_JOYSTICK_DRIVER_ARCADE_H
