4c186b993482c7fcc6cf2303b9cd2c96f4666330
suhacker1 <suhashussain1@gmail.com>
2020-04-12 14:41:07 -0400

    create data loaders

--- igvc_perception/src/segmentation_cnn/data_loaders.py
+++ igvc_perception/src/segmentation_cnn/data_loaders.py
@@ -8,3 +8,59 @@ import torch
 import collections
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
+
+# Creates Torch dataloaders
+def get_loaders(
+    images: List[Path],
+    masks: List[Path],
+    image_arr_path: str,
+    mask_arr_path: str,
+    random_state: int,
+    valid_size: float = 0.1,
+    batch_size: int = 12,
+    num_workers: int = 4,
+    # train_transforms_fn = None,
+    # valid_transforms_fn = None,
+) -> dict:
+
+    indices = np.arange(len(images))
+
+    train_indices, valid_indices = train_test_split(
+        indices, test_size=valid_size, random_state=random_state, shuffle=True
+    )
+
+    np_images = np.array(images)
+    np_masks = np.array(masks)
+    # print(np_images.shape, np_masks.shape)
+
+    train_dataset = SegmentationDataset(image_arr_path, mask_arr_path)
+    train_dataset.images = np_images[train_indices]
+    train_dataset.masks = np_masks[train_indices]
+    # print(len(train_dataset))
+
+    valid_dataset = SegmentationDataset(image_arr_path, mask_arr_path)
+    valid_dataset.images = np_images[valid_indices]
+    valid_dataset.masks = np_masks[valid_indices]
+    # print(len(valid_dataset))
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        drop_last=False,
+    )
+
+    valid_loader = DataLoader(
+        valid_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        drop_last=False,
+    )
+
+    loaders = collections.OrderedDict()
+    loaders["train"] = train_loader
+    loaders["valid"] = valid_loader
+
+    return loaders
