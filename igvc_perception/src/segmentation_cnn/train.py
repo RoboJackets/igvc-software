2875d364b1f69a9428e07f5a7604a3d0087e1a3e
suhacker1 <suhashussain1@gmail.com>
2020-04-12 14:56:51 -0400

    complete training functions

--- igvc_perception/src/segmentation_cnn/train.py
+++ igvc_perception/src/segmentation_cnn/train.py
@@ -20,6 +20,7 @@ from torch import nn
 from catalyst.contrib.nn import DiceLoss, IoULoss
 from torch import optim
 from catalyst import utils
+from torch.optim import AdamW
 
 from catalyst.contrib.nn import RAdam, Lookahead
 from catalyst.dl import SupervisedRunner
@@ -32,6 +33,132 @@ from catalyst.dl.callbacks import (
     MulticlassDiceMetricCallback,
 )
 
+
 # Dataset and data loaders
 from segmentation_dataset import SegmentationDataset
 from data_loaders import get_loaders
+
+
+# Sets a seed for better reproducibility
+SEED = 42
+utils.set_global_seed(SEED)
+utils.prepare_cudnn(deterministic=True)
+
+# Sets up transfer learning system
+
+ENCODER = 'efficientnet-b3'
+ENCODER_WEIGHTS = 'imagenet'
+DEVICE = 'cuda'
+
+# ACTIVATION = 'softmax'
+ACTIVATION = None
+
+model = smp.Unet(
+    encoder_name=ENCODER,
+    encoder_weights=ENCODER_WEIGHTS,
+    classes=3,
+    activation=ACTIVATION,
+)
+
+# Runs data loaders  
+loaders = get_loaders(
+    images=np.load("/content/drive/My Drive/RoboJackets/Split_Data/train_images.npy"),
+    masks=np.load("/content/drive/My Drive/RoboJackets/Split_Data/train_masks.npy"),
+    image_arr_path="/content/drive/My Drive/RoboJackets/Split_Data/train_images.npy",
+    mask_arr_path="/content/drive/My Drive/RoboJackets/Split_Data/train_masks.npy",
+    random_state=420,
+    valid_size=0.1,
+    batch_size=3,
+    num_workers=2,
+)
+
+# Define loss criterion
+criterion = {
+    "CE": CrossentropyND(),
+}
+
+
+# Set up optimization
+
+learning_rate = 0.001 #0.001
+encoder_learning_rate = 0.0005
+encoder_weight_decay = 0.00003 #0.00003
+optimizer_weight_decay = 0.0003 #0.0003
+optim_factor = 0.25 #0.25
+optim_patience = 2 #2
+
+optimizer = AdamW(model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01, amsgrad=False)
+
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=optim_factor, patience=optim_patience)
+
+num_epochs = 10
+device = utils.get_device()
+
+runner = SupervisedRunner(device=device, input_key="image", input_target_key="mask")
+
+# Uses Catalyst callbacks for metric calculations
+callbacks = [
+        CriterionCallback(
+            input_key="mask",
+            prefix="loss",
+            criterion_key="CE"
+        ),
+        MulticlassDiceMetricCallback(input_key="mask")
+        ]
+
+# Trains and prints training logs for model 
+runner.train(
+    model=model,
+    criterion=criterion,
+    optimizer=optimizer,
+    scheduler=scheduler,
+    loaders=loaders,
+    callbacks=callbacks,
+    logdir='content/full_model2', #this logdir must be changed with every new run
+    num_epochs=num_epochs,
+
+    main_metric="loss",
+    minimize_metric=True,
+    fp16=None,
+
+    monitoring_params=None,
+
+    verbose=True,
+)
+
+# Test model on test dataset
+test_data = SegmentationDataset("/content/drive/My Drive/RoboJackets/Split_Data/test_images.npy", "/content/drive/My Drive/RoboJackets/Split_Data/test_masks.npy")
+
+infer_loader = DataLoader(
+    test_data,
+    batch_size=12,
+    shuffle=False,
+    num_workers=4
+)
+
+# get predictions on test data
+predictions = runner.predict_loader(
+    model=model,
+    loader=infer_loader,
+    resume=f"content/full_model2/checkpoints/best.pth",
+    verbose=False,
+)
+
+print(type(predictions))
+print(predictions.shape)
+
+# Show clean image
+show_image = np.asarray(test_data[30]['image'])
+show_image = np.swapaxes(show_image, 2, 0)
+show_image = np.swapaxes(show_image, 1, 0)
+show_image = show_image.astype(np.uint8)
+np.shape(show_image)
+plt.imshow(show_image)
+
+# Show model prediction for clean image
+show_image = np.asarray(predictions[30])
+show_image = np.swapaxes(show_image, 2, 0)
+show_image = np.swapaxes(show_image, 1, 0)
+show_image = show_image.astype(np.uint8)
+np.shape(show_image)
+plt.imshow(show_image)
