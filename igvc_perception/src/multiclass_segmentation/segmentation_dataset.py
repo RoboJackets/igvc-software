100d058b110e35cf55f24d2756ff1be26afbcf3a
Suha Sabi Hussain <suhashussain1@gmail.com>
2020-05-07 20:31:12 -0400

    Multiclass image segmentation for lines and barrels (#710)
    * add separate folder for multiclass cnn
    
    * create file to make the dataset
    
    * separate image and mask creation in main
    
    * conform to PEP 8 style
    
    * add joseph's helper functions
    
    * upload pytorch dataset class code
    
    * upload original notebook
    
    * remove nbs due to size
    
    * Created using Colaboratory
    
    * Change path
    
    * upload data loaders file
    
    * specify dependencies for data loaders
    
    * explain test for seg dataset
    
    * use black to conform to pep8
    
    * create data loaders
    
    * dependencies for model and training
    
    * complete training functions
    
    * use black on train.py
    
    * fix train.py formatting
    
    * fix imports for train.py
    
    * Delete .train.py.swo
    
    * black reformatting
    
    * fix E302 - 2 blank lines error
    
    * fix E501 error
    
    * fix line length error
    
    * standardize docstring
    
    * PEP8 Fixes
    
    * PEP 8 fixes using black
    
    * Change folder name
    
    * Delete README.md
    
    * fix folder name attempt 2
    
    * standardize comments
    
    * incorporating arg parsing into training
    
    * incorporate arg parsing to data process
    
    * make argparse help messages consistent
    
    * describe requirements
    
    * remove bloated comments
    
    * Update README.md
    
    * remove python from requirements.txt
    
    * fixing reqs
    
    * fix import issues
    
    * clean up comments
    
    * fix string issue with dataset
    
    * Define build instructions
    
    * Include description of structure in README
    
    * Clarify the notebook alternative
    
    * Add example prediction image
    
    * Include prediction image in README
    
    * Fix grammar
    
    * Get rid of space
    
    * add PDF of notebook
    
    * Add pdf info into README
    
    * Fix typo
    
    * change image name
    
    * Change link to image in README
    
    * turn num of images into variable
    
    * add preliminary logic for comparing images and masks
    
    * make sys exit after error
    
    * include glob example
    
    * incorporate tqdm for dataset
    
    * change data type of pred img
    
    * use better image
    
    * improve images in notebooks
    
    * fix typo in README
    
    * fix globbing example
    
    * improve matching assurance
    
    * improve images; allow downloading of sample predictions
    
    * align train.py and notebook
    
    * standardize comments
    
    * update README with changes
    
    * comply with PyTorch Style Guide
    
    * Delete .train.py.swo
    
    * fix cv2 error and add pred num comment
    
    * fix catalyst requirement
    
    * create splitting file
    
    * make train.py's implementation of prediction saving not Colab-specific
    
    * update readme with splitting script
    
    * remove monitoring line
    
    * increase sample # to 30
    
    * improve prediction image saving
    
    * even spacing for comments

---
+++ igvc_perception/src/multiclass_segmentation/segmentation_dataset.py
@@ -0,0 +1,32 @@
+# Import dependencies
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
+class SegmentationDataset(Dataset):
+    # Define a dataset class
+    def __init__(self, image_arr_path, mask_arr_path,) -> None:
+        self.images = np.load(image_arr_path)
+        self.masks = np.load(mask_arr_path)
+
+    def __len__(self) -> int:
+        return len(self.images)
+
+    def __getitem__(self, idx: int) -> dict:
+        image = self.images[idx]
+        image = np.swapaxes(image, 2, 0)
+        image = np.swapaxes(image, 2, 1)
+        image = torch.from_numpy(image).float()
+        result = {"image": image}
+
+        if self.masks is not None:
+            mask = self.masks[idx]
+            mask = np.swapaxes(mask, 2, 0)
+            mask = np.swapaxes(mask, 2, 1)
+            mask = torch.from_numpy(mask).float()
+            result["mask"] = mask
+        return result
+
+
+# dset = SegmentationDataset(image_arr_path="", mask_arr_path="")
