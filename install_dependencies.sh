5dd8c4f052648d0e961738afa0589488a81512f3
matthewhannay567 <matthew.scott.hannay@gmail.com>
2020-10-25 22:56:31 -0400

    Fixed ignored_tags

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,8 +9,6 @@ if [ ! -d "/usr/local/include/kindr/" ]; then
     cd ../..
     rm -rf kindr
 fi
-# manually install nmea-navsat-driver because it won't work otherwise. FIX EVENTUALLY.
-sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install ros-noetic-nmea-navsat-driver
-rosdep install -iy --from-paths ../../src --skip-keys='kindr nmea_navsat_driver'
+
+rosdep install -iy --from-paths ../../src --skip-keys='kindr'
 pip3 install --no-cache-dir torch torchvision
