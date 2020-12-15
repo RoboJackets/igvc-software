da3f8781f457738e88181271b37a1b540e9706fd
yyajima21 <yosuke.yajima21@gmail.com>
2020-12-15 12:06:40 -0500

    Refactor functions

--- igvc_perception/src/pointcloud_segmentation/clustering.cpp
+++ igvc_perception/src/pointcloud_segmentation/clustering.cpp
@@ -30,8 +30,7 @@ ClusteringNode::ClusteringNode()
     marker_pub_ = private_nh_.advertise<visualization_msgs::MarkerArray>("/markers",1);
 };
 
-visualization_msgs::Marker mark_cluster(PCRGB::Ptr cloud, int id)
-{
+bool check_threshold(PCRGB::Ptr cloud) {
   Eigen::Vector4f centroid;
   Eigen::Vector4f min;
   Eigen::Vector4f max;
@@ -41,18 +40,25 @@ visualization_msgs::Marker mark_cluster(PCRGB::Ptr cloud, int id)
   float diff_x = max[0] - min[0];
   float diff_y = max[1] - min[1];
   float diff_z = max[2] - min[2];
-  bool within_thresholds = diff_z > 0.1 && diff_x < 0.9 && diff_y < 0.9 && cloud->size()>20;
-  
-  /*std::cout << "start-----------------" << std::endl;
-  std::cout << "z: " << max[2] - min[2] << std::endl;
-  std::cout << "x: " << max[0] - min[0] << std::endl;
-  std::cout << "y: " << max[1] - min[1] << std::endl;
-  std::cout << "end-------------------" << std::endl;*/
+
+  bool within_thresholds = diff_z > 0.1 && diff_x < 0.9 && diff_y < 0.9 && cloud->size() > 20;
+  if (within_thresholds) return true;
+  else return false;
+};
+
+visualization_msgs::Marker mark_cluster(PCRGB::Ptr cloud, int id)
+{
+  Eigen::Vector4f centroid;
+  Eigen::Vector4f min;
+  Eigen::Vector4f max;
+
+  pcl::compute3DCentroid (*cloud, centroid);
+  pcl::getMinMax3D (*cloud, min, max);
   
   visualization_msgs::Marker msg;
   msg.header.frame_id = "base_link";
   msg.type = visualization_msgs::Marker::LINE_LIST;
-  msg.lifetime = ros::Duration();
+  msg.lifetime = ros::Duration(0.1);
   msg.id = id;
   msg.action = visualization_msgs::Marker::ADD;
   msg.color.r = 1.0f;
@@ -63,29 +69,43 @@ visualization_msgs::Marker mark_cluster(PCRGB::Ptr cloud, int id)
   msg.pose.orientation.w = 1.0;
   msg.points.clear();
   
-  if (within_thresholds) {
-    geometry_msgs::Point p1,p2,p3,p4,p5,p6,p7,p8;
-    p1.x = p2.x = p5.x = p6.x = min[0];
-    p3.x = p4.x = p7.x = p8.x = max[0];
-    p1.y = p3.y = p5.y = p7.y = min[1];
-    p2.y = p4.y = p6.y = p8.y = max[1];
-    p1.z = p2.z = p3.z = p4.z = min[2];
-    p5.z = p6.z = p7.z = p8.z = max[2];
-    msg.points.push_back(p1);msg.points.push_back(p2);
-    msg.points.push_back(p1);msg.points.push_back(p3);
-    msg.points.push_back(p2);msg.points.push_back(p4);
-    msg.points.push_back(p3);msg.points.push_back(p4);
-    msg.points.push_back(p1);msg.points.push_back(p5);
-    msg.points.push_back(p2);msg.points.push_back(p6);
-    msg.points.push_back(p3);msg.points.push_back(p7);
-    msg.points.push_back(p4);msg.points.push_back(p8);
-    msg.points.push_back(p5);msg.points.push_back(p6);
-    msg.points.push_back(p5);msg.points.push_back(p7);
-    msg.points.push_back(p6);msg.points.push_back(p8);
-    msg.points.push_back(p7);msg.points.push_back(p8);
-  }
+  geometry_msgs::Point p1, p2, p3, p4, p5, p6, p7, p8;
+  p1.x = p2.x = p5.x = p6.x = min[0];
+  p3.x = p4.x = p7.x = p8.x = max[0];
+  p1.y = p3.y = p5.y = p7.y = min[1];
+  p2.y = p4.y = p6.y = p8.y = max[1];
+  p1.z = p2.z = p3.z = p4.z = min[2];
+  p5.z = p6.z = p7.z = p8.z = max[2];
+  msg.points.push_back(p1); msg.points.push_back(p2);
+  msg.points.push_back(p1); msg.points.push_back(p3);
+  msg.points.push_back(p2); msg.points.push_back(p4);
+  msg.points.push_back(p3); msg.points.push_back(p4);
+  msg.points.push_back(p1); msg.points.push_back(p5);
+  msg.points.push_back(p2); msg.points.push_back(p6);
+  msg.points.push_back(p3); msg.points.push_back(p7);
+  msg.points.push_back(p4); msg.points.push_back(p8);
+  msg.points.push_back(p5); msg.points.push_back(p6);
+  msg.points.push_back(p5); msg.points.push_back(p7);
+  msg.points.push_back(p6); msg.points.push_back(p8);
+  msg.points.push_back(p7); msg.points.push_back(p8);
 
   return msg;
+};
+
+std::vector<pcl::PointIndices> euclidean_clustering(PC::Ptr input_cloud) {
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);  
+  tree->setInputCloud (input_cloud);
+  std::vector<pcl::PointIndices> cluster_indices;
+  pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+  
+  // Euclidean clustering
+  ec.setClusterTolerance (0.25);
+  ec.setMinClusterSize (20);
+  ec.setMaxClusterSize (500);
+  ec.setSearchMethod (tree);
+  ec.setInputCloud (input_cloud);
+  ec.extract (cluster_indices);
+  return cluster_indices;
 };
 
 void ClusteringNode::clusteringCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
@@ -94,27 +114,30 @@ void ClusteringNode::clusteringCallback(const sensor_msgs::PointCloud2ConstPtr& 
   PCRGB::Ptr cloud_filtered (new PCRGB);
   PCRGB::Ptr curr_cloud (new PCRGB);
   pcl::fromROSMsg(*cloud_msg, *cloud);
-  
+  /*
   pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);  
   tree->setInputCloud (cloud);
   std::vector<pcl::PointIndices> cluster_indices;
   pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
   
+  // Euclidean clustering
   ec.setClusterTolerance (0.25);
   ec.setMinClusterSize (20);
   ec.setMaxClusterSize (500);
   ec.setSearchMethod (tree);
   ec.setInputCloud (cloud);
   ec.extract (cluster_indices);
+  */
 
+  std::vector<pcl::PointIndices> cluster_indices = euclidean_clustering(cloud);
   // declare the output variable instances
   sensor_msgs::PointCloud2 output_msg;
   visualization_msgs::MarkerArray clusters_vis;
   int counter = 0;
+
   for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it) 
   {
     // create a pcl object to hold the extracted cluster
-    //PCRGB::Ptr cluster (new PCRGB);
     unsigned int r = (unsigned int)(rand() % 255);
     unsigned int g = (unsigned int)(rand() % 255);
     unsigned int b = (unsigned int)(rand() % 255);
@@ -126,11 +149,11 @@ void ClusteringNode::clusteringCallback(const sensor_msgs::PointCloud2ConstPtr& 
       curr_cloud->points.push_back(p);
       cloud_filtered->points.push_back(p);
     }
-
-    //std::cout << curr_cloud->points.size() << " cluster points: " << counter << std::endl;
-    visualization_msgs::Marker marker  = mark_cluster(curr_cloud, counter);
-
-    clusters_vis.markers.push_back(marker);
+    //ool threshold = check_threshold(curr_cloud);
+    if (check_threshold(curr_cloud)) {
+      visualization_msgs::Marker marker  = mark_cluster(curr_cloud, counter);
+      clusters_vis.markers.push_back(marker);
+    }
     
     counter++;
     curr_cloud->clear();
