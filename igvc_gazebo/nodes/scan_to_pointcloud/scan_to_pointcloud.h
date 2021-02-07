9ecccab07d28c9b85a2538a5b1a921bbe1d3aea7
vagrant <agreer350@gmail.com>
2021-02-07 18:20:02 +0000

    Refactors the scan_to_pointcloud node into a header and cpp file

---
+++ igvc_gazebo/nodes/scan_to_pointcloud/scan_to_pointcloud.h
@@ -0,0 +1,28 @@
+// scan_to_pointcloud.h
+#ifndef SCAN_TO_POINTCLOUD_H
+#define SCAN_TO_POINTCLOUD_H
+
+#include <laser_geometry/laser_geometry.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+#include <ros/publisher.h>
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+
+
+class ScanToPointcloud
+{
+	public:
+		ScanToPointcloud();
+		
+	private:
+		void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+		ros::NodeHandle nh;
+		ros::Publisher _pointcloud_pub;
+		laser_geometry::LaserProjection projection;
+
+		double min_dist, neighbor_dist;
+		double offset;
+};
+#endif
