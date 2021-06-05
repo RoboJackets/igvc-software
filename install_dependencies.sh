568cc464e4bb147a0ef3423f0fe05d908231fdeb
VAM7686 <vmhatre3@gatech.edu>
2021-06-05 15:35:08 -0400

    Added gtsam and geographiclib installation commands

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -12,3 +12,13 @@ fi
 
 rosdep install -iy --from-paths ../../src --skip-keys='kindr serial'
 pip3 install --no-cache-dir torch torchvision
+
+## GTSAM
+# Add PPA
+sudo add-apt-repository ppa:borglab/gtsam-release-4.0
+sudo apt update  # not necessary since Bionic
+# Install:
+sudo apt install libgtsam-dev libgtsam-unstable-dev
+
+## GeographicLib
+sudo apt-get install -y libgeographic-dev
