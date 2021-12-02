f3d0bc9547ee5e1358b0fc2f2925580ba09a77bb
Nathan Zhong <nzhongathan@gmail.com>
2021-12-02 17:02:08 -0500

    separating main.cpp fixes #739

--- igvc_gazebo/nodes/ground_truth/ground_truth_republisher.cpp
+++ igvc_gazebo/nodes/ground_truth/ground_truth_republisher.cpp
@@ -1,24 +1,45 @@
-#include <geometry_msgs/Vector3.h>
-#include <nav_msgs/Odometry.h>
-#include <parameter_assertions/assertions.h>
-#include <robot_localization/navsat_conversions.h>
-#include <ros/ros.h>
-#include <tf/transform_broadcaster.h>
-#include <tf/transform_datatypes.h>
-#include <tf/transform_listener.h>
 #include <mutex>
+#include <parameter_assertions/assertions.h>
+#include <ros/ros.h>
+#include "ground_truth_republisher.h"
 
-ros::Publisher g_ground_truth_pub;
-// TODO make this a minimal object
-nav_msgs::Odometry g_og_pose;
-ros::Time g_last_estimate;
+GroundTruthRepublisher::GroundTruthRepublisher()
+{
+	ros::NodeHandle nh;
+	ros::NodeHandle pNh("~");
 
-void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+	std::string ground_truth_topic, estimate_topic, pub_topic, diff_topic;
+
+	assertions::param(pNh, "ground_truth_sub_topic", ground_truth_topic, std::string("/ground_truth/state_raw"));
+	assertions::param(pNh, "ground_truth_pub_topic", pub_topic, std::string("/ground_truth"));
+
+	double longitude, latitude;
+	assertions::param(pNh, "longitude", longitude, -84.405001);
+	assertions::param(pNh, "latitude", latitude, 33.774497);
+
+	ros::Subscriber ground_truth = nh.subscribe<nav_msgs::Odometry>(ground_truth_topic, 10, groundTruthCallback);
+
+	ros::Subscriber estimate_sub = nh.subscribe<nav_msgs::Odometry>("/odometry/filtered", 1, odomCallback);
+	g_ground_truth_pub = nh.advertise<nav_msgs::Odometry>(pub_topic, 1);
+
+	double utm_x, utm_y;
+	RobotLocalization::NavsatConversions::UTM(latitude, longitude, &utm_x, &utm_y);
+
+	tf::Transform utm_to_odom;
+	utm_to_odom.setOrigin(
+		tf::Vector3(utm_x - g_og_pose.pose.pose.position.x, utm_y - g_og_pose.pose.pose.position.y, 0.0));
+	utm_to_odom.setRotation(tf::createQuaternionFromYaw(M_PI));
+
+	ros::Timer utm_timer = nh.createTimer(ros::Duration(1.0), boost::bind(utm_callback, _1, utm_to_odom.inverse()));
+	ros::spin();
+}
+
+void GroundTruthRepublisher::odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
   g_last_estimate = msg->header.stamp;
 }
 
-void groundTruthCallback(const nav_msgs::Odometry::ConstPtr& msg)
+void GroundTruthRepublisher::groundTruthCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
   // get the starting location as the origin
   if (g_og_pose.header.stamp.toSec() == 0)
@@ -67,7 +88,7 @@ void groundTruthCallback(const nav_msgs::Odometry::ConstPtr& msg)
   }
 }
 
-void utm_callback(const ros::TimerEvent& event, const tf::Transform& odom_to_utm)
+void GroundTruthRepublisher::utm_callback(const ros::TimerEvent& event, const tf::Transform& odom_to_utm)
 {
   static tf::TransformBroadcaster br;
   static tf::TransformListener tf_listener;
@@ -97,35 +118,3 @@ void utm_callback(const ros::TimerEvent& event, const tf::Transform& odom_to_utm
   }
 }
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "ground_truth_republisher");
-
-  ros::NodeHandle nh;
-  ros::NodeHandle pNh("~");
-
-  std::string ground_truth_topic, estimate_topic, pub_topic, diff_topic;
-
-  assertions::param(pNh, "ground_truth_sub_topic", ground_truth_topic, std::string("/ground_truth/state_raw"));
-  assertions::param(pNh, "ground_truth_pub_topic", pub_topic, std::string("/ground_truth"));
-
-  double longitude, latitude;
-  assertions::param(pNh, "longitude", longitude, -84.405001);
-  assertions::param(pNh, "latitude", latitude, 33.774497);
-
-  ros::Subscriber ground_truth = nh.subscribe<nav_msgs::Odometry>(ground_truth_topic, 10, groundTruthCallback);
-
-  ros::Subscriber estimate_sub = nh.subscribe<nav_msgs::Odometry>("/odometry/filtered", 1, odomCallback);
-  g_ground_truth_pub = nh.advertise<nav_msgs::Odometry>(pub_topic, 1);
-
-  double utm_x, utm_y;
-  RobotLocalization::NavsatConversions::UTM(latitude, longitude, &utm_x, &utm_y);
-
-  tf::Transform utm_to_odom;
-  utm_to_odom.setOrigin(
-      tf::Vector3(utm_x - g_og_pose.pose.pose.position.x, utm_y - g_og_pose.pose.pose.position.y, 0.0));
-  utm_to_odom.setRotation(tf::createQuaternionFromYaw(M_PI));
-
-  ros::Timer utm_timer = nh.createTimer(ros::Duration(1.0), boost::bind(utm_callback, _1, utm_to_odom.inverse()));
-  ros::spin();
-}
---
+++ igvc_gazebo/nodes/ground_truth/ground_truth_republisher.h
@@ -0,0 +1,27 @@
+#ifndef GROUND_TRUTH_REPUBLISHER_HPP_
+#define GROUND_TRUTH_REPUBLISHER_HPP_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/Vector3.h>
+#include <nav_msgs/Odometry.h>
+#include <robot_localization/navsat_conversions.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+
+class GroundTruthRepublisher
+{
+public:
+	GroundTruthRepublisher();
+private:
+	ros::Publisher g_ground_truth_pub;
+	nav_msgs::Odometry g_og_pose;
+	ros::Time g_last_estimate;
+	
+	void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
+	void groundTruthCallback(const nav_msgs::Odometry::ConstPtr& msg);
+	void utm_callback(const ros::TimerEvent& event, const tf::Transform& odom_to_utm);
+	
+};
+
+#endif
