e8a54d41249a535a6d1bb269797965e8ca0a6fa1
Vivek Mhatre <vmhatre3@gatech.edu>
2022-06-02 00:55:31 -0400

    Added resizing functionality and main functions

--- igvc_perception/src/multiclass_segmentation/data_utils/segmentation_dataset.py
+++ igvc_perception/src/multiclass_segmentation/data_utils/segmentation_dataset.py
@@ -1,4 +1,5 @@
 # Import dependencies
+import cv2
 import numpy as np
 import torch
 from torch.utils.data import Dataset
@@ -10,15 +11,18 @@ class SegmentationDataset(Dataset):
         self,
         image_arr_path,
         mask_arr_path,
+        im_size
     ) -> None:
         self.images = np.load(image_arr_path)
         self.masks = np.load(mask_arr_path)
+        self.im_size = im_size
 
     def __len__(self) -> int:
         return len(self.images)
 
     def __getitem__(self, idx: int) -> dict:
         image = self.images[idx]
+        image = cv2.resize(image, (self.im_size[1], self.im_size[2]))
         image = np.swapaxes(image, 2, 0)
         image = np.swapaxes(image, 2, 1)
         image = torch.from_numpy(image).float()
@@ -26,6 +30,7 @@ class SegmentationDataset(Dataset):
 
         if self.masks is not None:
             mask = self.masks[idx]
+            mask = cv2.resize(mask, (self.im_size[1], self.im_size[2]))
             mask = np.swapaxes(mask, 2, 0)
             mask = np.swapaxes(mask, 2, 1)
             mask = torch.from_numpy(mask).float()
